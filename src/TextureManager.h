940b247b6fd7405abd6c357319b3c8472894d381
Martin <emailregisterlogin@gmail.com>
2020-05-04 04:01:54 +0100

    Move renderer and window to Game class

--- src/TextureManager.h
+++ src/TextureManager.h
@@ -1,17 +1,13 @@
 #pragma once
-#include "SDL.h"
-#include "SDL_image.h"
-#include "defines.h"
 #include "common.h"
+#include "SDL_image.h"
 
 #define TEST_PATH "../resources/test_tile_1.png"
 
 class TextureManager {
 private:
 	static TextureManager* instance;
-	static SDL_Window* window;
-	static SDL_Renderer* renderer;
-	static SDL_Texture* texture;
+	SDL_Texture* texture;
 public:
 	TextureManager();
 	void draw(std::string name, SDL_Rect* srcRect, SDL_Rect* destRect);
@@ -21,12 +17,5 @@ public:
 		}
 		return instance;
 	}
-	static SDL_Window* getWindow() {
-		return window;
-	}
-	static SDL_Renderer* getRenderer() {
-		return renderer;
-	}
-	void clean();
 };
 
