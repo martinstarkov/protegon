c6ddc8456a2b63c9d5fe0942ef71f66b15b5823e
Martin <emailregisterlogin@gmail.com>
2020-05-05 02:45:29 +0100

    Accelerate player on key presses

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -1,28 +1,38 @@
 #include "InputHandler.h"
 #include "Game.h"
+#include "Player.h"
 
 InputHandler* InputHandler::instance = nullptr;
 
 InputHandler::InputHandler() {}
 
+#define ACCELERATION_SCALE 2.0f
+#define JUMP 9.0f
+#define RESET_POS Vec2D(128, 128)
+
 void InputHandler::keyStateChange() {
+	Player* player = Player::getInstance();
 	const Uint8* states = SDL_GetKeyboardState(NULL);
-	int velocity = 3;
 	if (states[SDL_SCANCODE_A]) {
-		Game::x -= velocity;
-		std::cout << "Pressed A" << std::endl;
+		player->setAcceleration(Vec2D(-1 * ACCELERATION_SCALE, player->getAcceleration().y));
 	}
 	if (states[SDL_SCANCODE_D]) {
-		Game::x += velocity;
-		std::cout << "Pressed D" << std::endl;
+		player->setAcceleration(Vec2D(1 * ACCELERATION_SCALE, player->getAcceleration().y));
 	}
 	if (states[SDL_SCANCODE_W]) {
-		Game::y -= velocity;
-		std::cout << "Pressed W" << std::endl;
+		player->setAcceleration(Vec2D(player->getAcceleration().x, -1 * JUMP));
 	} 
 	if (states[SDL_SCANCODE_S]) {
-		Game::y += velocity;
-		std::cout << "Pressed S" << std::endl;
+		player->setAcceleration(Vec2D(player->getAcceleration().x, 1 * ACCELERATION_SCALE));
+	}
+	if (!states[SDL_SCANCODE_A] && !states[SDL_SCANCODE_D]) {
+		player->setAcceleration(Vec2D(0, player->getAcceleration().y));
+	}
+	if (!states[SDL_SCANCODE_W] && !states[SDL_SCANCODE_S]) {
+		player->setAcceleration(Vec2D(player->getAcceleration().x, 0));
+	}
+	if (states[SDL_SCANCODE_R]) {
+		player->setPosition(RESET_POS);
 	}
 }
 
