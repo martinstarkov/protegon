e687992a6d577bec62cc1023cf36920a547120dd
Martin <emailregisterlogin@gmail.com>
2020-05-07 19:18:23 +0100

    Move gravity addition back to player class

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -6,39 +6,36 @@ InputHandler* InputHandler::instance = nullptr;
 
 InputHandler::InputHandler() {}
 
-#define ACCELERATION_SCALE 1.0f
-#define JUMP 25.0f
-#define GRAVITY 0.5f
-#define RESET_POS Vec2D(128, 128)
+#define MOVEMENT_ACCELERATION 1.0f
+#define JUMPING_ACCELERATION 1.7f
+#define RESET_POSITION Vec2D(232, 128)
 
 void InputHandler::keyStateChange() {
 	Player* player = Player::getInstance();
 	const Uint8* states = SDL_GetKeyboardState(NULL);
-	player->setAcceleration(Vec2D(0, GRAVITY));
 	if (states[SDL_SCANCODE_A]) {
-		player->setAcceleration(Vec2D(-1 * ACCELERATION_SCALE, player->getAcceleration().y + GRAVITY));
+		player->setAcceleration(Vec2D(-MOVEMENT_ACCELERATION, player->getAcceleration().y));
 	}
 	if (states[SDL_SCANCODE_D]) {
-		player->setAcceleration(Vec2D(1 * ACCELERATION_SCALE, player->getAcceleration().y + GRAVITY));
+		player->setAcceleration(Vec2D(MOVEMENT_ACCELERATION, player->getAcceleration().y));
 	}
 	if (states[SDL_SCANCODE_W]) {
 		if (!player->jumping) {
 			player->jumping = true;
-			std::cout << "JUMP" << std::endl;
-			player->setVelocity(Vec2D(player->getVelocity().x, -1 * JUMP));
+			player->setAcceleration(Vec2D(player->getAcceleration().x, -JUMPING_ACCELERATION));
 		}
 	} 
 	if (states[SDL_SCANCODE_S]) {
-		player->setAcceleration(Vec2D(player->getAcceleration().x, GRAVITY));
+		player->setAcceleration(Vec2D(player->getAcceleration().x, MOVEMENT_ACCELERATION / 5));
 	}
 	if (!states[SDL_SCANCODE_A] && !states[SDL_SCANCODE_D]) {
-		player->setAcceleration(Vec2D(0, player->getAcceleration().y + GRAVITY));
+		player->setAcceleration(Vec2D(0.0f, player->getAcceleration().y));
 	}
 	if (!states[SDL_SCANCODE_W] && !states[SDL_SCANCODE_S]) {
-		player->setAcceleration(Vec2D(player->getAcceleration().x, GRAVITY));
+		// do nothing
 	}
 	if (states[SDL_SCANCODE_R]) {
-		player->setPosition(RESET_POS);
+		player->setPosition(RESET_POSITION);
 	}
 }
 
