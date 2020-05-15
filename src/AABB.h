a0916f44d99da9fef90898b84fbb26743f41aa90
Martin <emailregisterlogin@gmail.com>
2020-05-15 20:51:40 +0100

    Add penetrationNormal vector calculation for equal edges

--- src/AABB.h
+++ src/AABB.h
@@ -14,10 +14,95 @@ struct AABB {
     Vec2D max() {
         return pos + size;
     }
+    AABB surroundingBox(AABB b) {
+        Vec2D bPos;
+        bPos.x = std::min(min().x, b.min().x);
+        bPos.y = std::min(min().y, b.min().y);
+        Vec2D bSize;
+        bSize.x = abs(std::max(max().x, b.max().x) - bPos.x);
+        bSize.y = abs(std::max(max().y, b.max().y) - bPos.y);
+        return AABB(bPos, bSize);
+    }
     AABB minkowskiDifference(AABB other) {
         Vec2D mPos = pos - other.max();
         Vec2D mSize = size + other.size;
         return AABB(mPos, mSize);
+    }
+    Vec2D closestPointOnBoundsToPoint(Vec2D point = Vec2D()) {
+        float minDist = abs(point.x - min().x);
+        Vec2D boundsPoint = Vec2D(min().x, point.y);
+        if (abs(max().x - point.x) < minDist) {
+            minDist = abs(max().x - point.x);
+            boundsPoint = Vec2D(max().x, point.y);
+        }
+        if (abs(max().y - point.y) < minDist) {
+            minDist = abs(max().y - point.y);
+            boundsPoint = Vec2D(point.x, max().y);
+        }
+        if (abs(min().y - point.y) < minDist) {
+            minDist = abs(min().y - point.y);
+            boundsPoint = Vec2D(point.x, min().y);
+        }
+        return boundsPoint;
+    }
+    Vec2D penetrationNormal() {
+        // check if the minkowski origin has hit any of the rectangle's sides
+        Vec2D normal = Vec2D();
+        if (linePoint(min(), Vec2D(min().x, max().y), Vec2D())) { // left
+            normal.x = 1;
+        }
+        if (linePoint(Vec2D(max().x, min().y), max(), Vec2D())) { // right
+            normal.x = -1;
+        }
+        if (linePoint(min(), Vec2D(max().x, min().y), Vec2D())) { // top
+            normal.y = 1;
+        }
+        if (linePoint(Vec2D(min().x, max().y), max(), Vec2D())) { // bottom
+            normal.y = -1;
+        }
+        return normal;
+    }
+
+    // LINE/POINT
+    float linePoint(Vec2D v1, Vec2D v2, Vec2D p) {
+
+        // get distance from the point to the two ends of the line
+        float d1 = (p - v1).magnitude();
+        float d2 = (p - v2).magnitude();
+
+        // get the length of the line
+        float lineLen = (v1 - v2).magnitude();
+
+        // since floats are so minutely accurate, add
+        // a little buffer zone that will give collision
+        float buffer = 0.1f;    // higher # = less accurate
+
+        // if the two distances are equal to the line's 
+        // length, the point is on the line!
+        // note we use the buffer here to give a range, 
+        // rather than one #
+        if (d1 + d2 >= lineLen - buffer && d1 + d2 <= lineLen + buffer) {
+            return 1.0f;
+        }
+        return 0.0f;
+    }
+    // LINE/LINE
+    bool lineLine(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
+
+        // calculate the direction of the lines
+        float uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+        float uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+
+        // if uA and uB are between 0-1, lines are colliding
+        if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) {
+
+            // optionally, draw a circle where the lines meet
+            //float intersectionX = x1 + (uA * (x2 - x1));
+            //float intersectionY = y1 + (uA * (y2 - y1));
+
+            return true;
+        }
+        return false;
     }
     void penetrationVector(Vec2D relativePoint, Vec2D& pv, Vec2D& edge, Vec2D vel) { // find shortest distance from origin to edge of minkowski difference rectangle
         float minTime = std::numeric_limits<float>::infinity();
