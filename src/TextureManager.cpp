e8ccc752755c7ddde4b3e35e40df61b740564b70
Martin <emailregisterlogin@gmail.com>
2020-05-21 21:51:43 +0100

    Add texture loading into map

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -2,15 +2,33 @@
 #include "Game.h"
 
 TextureManager* TextureManager::instance = nullptr;
+std::map<std::string, SDL_Texture*> TextureManager::textureMap;
 
-TextureManager::TextureManager() {
-	SDL_Surface* surface = IMG_Load(TEST_PATH);
-	texture = SDL_CreateTextureFromSurface(Game::getRenderer(), surface);
-	SDL_FreeSurface(surface);
+void TextureManager::load(std::string id, std::string path) {
+	SDL_Surface* tempSurface = IMG_Load(path.c_str());
+	if (tempSurface == 0) {
+		std::cout << "Failed to IMG_Load path: '" << path << "'" << std::endl;
+		return;
+	}
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(Game::getRenderer(), tempSurface);
+	SDL_FreeSurface(tempSurface);
+	if (texture) {
+		if (textureMap.find(id) == textureMap.end()) { // add texture to map
+			textureMap.insert({ id, texture }); 
+		} else { // already contains texture, replace
+			textureMap[id] = texture;
+		}
+	} else {
+		std::cout << "Failed to SDL_CreateTextureFromSurface with path: '" << path << "'" << std::endl;
+	}
 }
 
-void TextureManager::draw(std::string name, SDL_Rect* srcRect, SDL_Rect* destRect) {
-	if (name == "tile") {
-		SDL_RenderCopyEx(Game::getRenderer(), texture, srcRect, destRect, NULL, NULL, SDL_FLIP_NONE);
+void TextureManager::draw(std::string id, SDL_Rect* source, SDL_Rect* destination, float angle, SDL_RendererFlip flip) {
+	if (textureMap.find(id) != textureMap.end()) {
+		SDL_RenderCopyEx(Game::getRenderer(), textureMap[id], source, destination, (double)angle, NULL, flip);
 	}
+}
+
+void TextureManager::removeTexture(std::string id) {
+	textureMap.erase(id);
 }
--- src/TextureManager.h
+++ src/TextureManager.h
@@ -1,22 +1,22 @@
 #pragma once
-#include <iostream>
 #include "common.h"
 #include "SDL_image.h"
-
-#define TEST_PATH "../resources/test_tile_1.png"
+#include <string>
+#include <map>
 
 class TextureManager {
 private:
 	static TextureManager* instance;
-	SDL_Texture* texture;
+	static std::map<std::string, SDL_Texture*> textureMap;
 public:
-	TextureManager();
-	void draw(std::string name, SDL_Rect* srcRect, SDL_Rect* destRect);
 	static TextureManager* getInstance() {
 		if (!instance) {
 			instance = new TextureManager();
 		}
 		return instance;
 	}
+	static void load(std::string id, std::string path);
+	static void draw(std::string id, SDL_Rect* source, SDL_Rect* destination, float angle = 0.0f, SDL_RendererFlip flip = SDL_FLIP_NONE);
+	static void removeTexture(std::string id);
 };
 
