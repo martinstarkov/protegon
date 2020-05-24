b3efcb8c3bc4c746b1ad13578f16af099306549a
Martin <emailregisterlogin@gmail.com>
2020-05-24 21:15:15 +0100

    Create Components.h

---
+++ src/ECS/Components.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "ECS.h"
+#include "TransformComponent.h"
+#include "SizeComponent.h"
+#include "MotionComponent.h"
+#include "PhysicsComponent.h"
+#include "ColorComponent.h"
+#include "AABBComponent.h"
+#include "HitboxComponent.h"
+#include "SpriteComponent.h"
+#include "DrawComponent.h"
