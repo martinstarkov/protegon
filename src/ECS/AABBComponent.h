5f5a9bc6fb2e44ea20e6010296a2708949359a54
Martin <emailregisterlogin@gmail.com>
2020-05-25 19:02:02 +0100

    Inherit Transform and Size components if they are not children of a hitbox

--- src/ECS/AABBComponent.h
+++ src/ECS/AABBComponent.h
@@ -8,32 +8,66 @@ public:
 	AABBComponent() {
 		_transformComponent = nullptr;
 		_sizeComponent = nullptr;
+		_type = nullptr;
 		_rectangle = {};
 	}
-	AABBComponent(AABB rectangle) : AABBComponent() {
+	AABBComponent(AABB rectangle, AABBComponent* type = nullptr) : AABBComponent() {
 		_rectangle = rectangle;
+		_type = type;
 	}
 	void init() override {
-		if (entity->count<TransformComponent>() < 2) {
-			_transformComponent = entity->add<TransformComponent>(_rectangle._position);
+		if (!_type) { // for AABB inherit previous components
+			if (entity->has<TransformComponent>()) {
+				for (auto c : entity->getComponents<TransformComponent>()) {
+					if (!c->isChild()) {
+						_transformComponent = c;
+						break;
+					} else {
+						_transformComponent = entity->get<TransformComponent>();
+					}
+				}
+				if (!_transformComponent) {
+					_transformComponent = entity->add<TransformComponent>();
+				}
+			} else {
+				_transformComponent = entity->add<TransformComponent>();
+			}
+			if (entity->has<SizeComponent>()) {
+				for (auto c : entity->getComponents<SizeComponent>()) {
+					if (!c->isChild()) {
+						_sizeComponent = c;
+						break;
+					} else {
+						_sizeComponent = entity->get<SizeComponent>();
+					}
+				}
+				if (!_sizeComponent) {
+					_sizeComponent = entity->add<SizeComponent>();
+				}
+			} else {
+				_sizeComponent = entity->add<SizeComponent>();
+			}
+		} else { // for hitbox always add new components
+			entity->addGroup(Groups::hitboxes);
+			_transformComponent = entity->add<TransformComponent>();
+			_sizeComponent = entity->add<SizeComponent>();
 		}
-		if (entity->count<SizeComponent>() < 2) {
-			_sizeComponent = entity->add<SizeComponent>(_rectangle._size);
+		entity->addGroup(Groups::drawables);
+		if (_rectangle) { // if rectangle given, set positions appropriately
+			_transformComponent->setPosition(_rectangle._position);
+			_sizeComponent->setSize(_rectangle._size);
 		}
-		if (entity->count<TransformComponent>() == 2) {
-			_transformComponent = entity->get<TransformComponent>();
-		}
-		if (entity->count<SizeComponent>() == 2) {
-			_sizeComponent = entity->get<SizeComponent>();
-		}
+		add(_transformComponent);
+		add(_sizeComponent);
 	}
 	AABB getAABB() {
-		return { Vec2D(_transformComponent->getPosition().x, _transformComponent->getPosition().y), Vec2D(_sizeComponent->getSize().x, _sizeComponent->getSize().y) };
+		return { get<TransformComponent>()->getPosition(), get<SizeComponent>()->getSize() };
 	}
 	SDL_Rect getRectangle() {
 		return getAABB().AABBtoRect();
 	}
 private:
+	AABBComponent* _type;
 	TransformComponent* _transformComponent;
 	SizeComponent* _sizeComponent;
 	AABB _rectangle;
