cc9de5b2bd3f9bad4d96cb0fe4f3d628302e0522
Martin <emailregisterlogin@gmail.com>
2020-05-25 19:01:37 +0100

    Don't create TransformComponent is it doesn't exist

--- src/ECS/PhysicsComponent.h
+++ src/ECS/PhysicsComponent.h
@@ -7,18 +7,17 @@ public:
 		_motionComponent = nullptr;
 	}
 	void init() override {
-		if (!entity->has<MotionComponent>()) {
-			entity->add<MotionComponent>();
+		if (entity->has<MotionComponent>()) {
+			_motionComponent = entity->get<MotionComponent>();
+		} else {
+			_motionComponent = entity->add<MotionComponent>();
 		}
-		if (!entity->has<TransformComponent>()) { // optional addition of default transform component
-			entity->add<TransformComponent>();
-		}
-		_motionComponent = entity->get<MotionComponent>();
+		add(_motionComponent);
 	}
 	void update() override {
 		_motionComponent->addVelocity(_motionComponent->getAcceleration());
-		for (int i = 0; i < entity->count<TransformComponent>(); i++) {
-			entity->get<TransformComponent>()->addPosition(_motionComponent->getVelocity());
+		for (auto c : entity->getComponents<TransformComponent>()) {
+			c->addPosition(_motionComponent->getVelocity());
 		}
 	}
 private:
