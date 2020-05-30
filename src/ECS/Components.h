9458eeb5eda41f6e60afb6bc2649788e75b3694f
Martin <emailregisterlogin@gmail.com>
2020-05-30 16:17:47 +0100

    Revampt components to include only data

--- src/ECS/Components.h
+++ src/ECS/Components.h
@@ -1,6 +1,39 @@
 #pragma once
-#include "ECS.h"
-#include "TransformComponent.h"
+
+#include "SDL.h"
+
+class Component {
+public:
+
+private:
+
+};
+
+enum Components {
+	COMPONENT_NONE = 0,
+	COMPONENT_DISPLACEMENT = 1 << 0,
+	COMPONENT_VELOCITY = 1 << 1,
+	COMPONENT_APPEARANCE = 1 << 2
+};
+
+struct Displacement : public Component {
+	float x;
+	float y;
+};
+
+struct Velocity : public Component {
+	float x;
+	float y;
+};
+
+struct Appearance : public Component {
+	const char* name;
+	SDL_Texture* texture;
+};
+
+
+//#include "ECS.h"
+//#include "TransformComponent.h"
 //#include "SizeComponent.h"
 //#include "ColorComponent.h"
 //#include "MotionComponent.h"
