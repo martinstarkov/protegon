839615ec5acb52427a73333bd3285c1b94547f11
Martin <emailregisterlogin@gmail.com>
2020-06-11 05:36:46 +0100

    Contain texture in sprite component

--- src/ECS/Components/SpriteComponent.h
+++ src/ECS/Components/SpriteComponent.h
@@ -9,9 +9,10 @@
 struct SpriteComponent : public Component<SpriteComponent> {
 	const char* _path;
 	SDL_Rect _source;
+	SDL_Texture* _texture;
 	unsigned int _sprites;
 	SpriteComponent(const char* path = nullptr, Vec2D spriteSize = Vec2D(), unsigned int sprites = 1) : _path(path), _source(AABB(Vec2D(), spriteSize).AABBtoRect()), _sprites(sprites) {
-		TextureManager::load(_path);
+		_texture = TextureManager::load(_path);
 	}
 	virtual ~SpriteComponent() override {
 		//TextureManager::removeTexture(_path); // don't necessarily remove texture if other textures are using it
