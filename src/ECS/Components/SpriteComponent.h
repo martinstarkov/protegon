2d66e92c160f6136efb51075b1ad68e32c6c8872
Martin <emailregisterlogin@gmail.com>
2020-06-12 01:04:56 +0100

    Update SpriteComponent.h

--- src/ECS/Components/SpriteComponent.h
+++ src/ECS/Components/SpriteComponent.h
@@ -11,6 +11,7 @@ struct SpriteComponent : public Component<SpriteComponent> {
 	SDL_Rect _source;
 	SDL_Texture* _texture;
 	unsigned int _sprites;
+	// TODO: Make a spritesheet component which includes multiple animations
 	SpriteComponent(const char* path = nullptr, Vec2D spriteSize = Vec2D(), unsigned int sprites = 1) : _path(path), _source(AABB(Vec2D(), spriteSize).AABBtoRect()), _sprites(sprites) {
 		_texture = TextureManager::load(_path);
 	}
