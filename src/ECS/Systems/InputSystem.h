dad4ad05f9d3b3cc0698b0be396ef5b327269303
Martin Starkov <emailregisterlogin@gmail.com>
2020-07-05 16:59:20 +0100

    Enter into walk state machine

--- src/ECS/Systems/InputSystem.h
+++ src/ECS/Systems/InputSystem.h
@@ -20,17 +20,30 @@ public:
 		if (player) {
 			MotionComponent* motion = e.getComponent<MotionComponent>();
 			if (motion) {
-				if (s[SDL_SCANCODE_W] && !s[SDL_SCANCODE_S]) {
-					motion->_velocity.y += -player->_speed.y;
-				}
-				if (s[SDL_SCANCODE_S] && !s[SDL_SCANCODE_W]) {
-					motion->_velocity.y += player->_speed.y;
-				}
-				if (s[SDL_SCANCODE_A] && !s[SDL_SCANCODE_D]) {
-					motion->_velocity.x += -player->_speed.x;
-				}
-				if (s[SDL_SCANCODE_D] && !s[SDL_SCANCODE_A]) {
-					motion->_velocity.x += player->_speed.x;
+				StateMachineComponent* sm = e.getComponent<StateMachineComponent>();
+				// movement occurs
+				if (s[SDL_SCANCODE_W] || s[SDL_SCANCODE_A] || s[SDL_SCANCODE_S] || s[SDL_SCANCODE_D]) {
+					if (sm) {
+						if (sm->_stateMachines[0]->getCurrentState()->getStateID() != typeid(RunState).hash_code()) {
+							sm->_stateMachines[0]->setCurrentState(new WalkState());
+						}
+					}
+					if (s[SDL_SCANCODE_W] && !s[SDL_SCANCODE_S]) {
+						motion->_velocity.y += -player->_speed.y;
+					}
+					if (s[SDL_SCANCODE_S] && !s[SDL_SCANCODE_W]) {
+						motion->_velocity.y += player->_speed.y;
+					}
+					if (s[SDL_SCANCODE_A] && !s[SDL_SCANCODE_D]) {
+						motion->_velocity.x += -player->_speed.x;
+					}
+					if (s[SDL_SCANCODE_D] && !s[SDL_SCANCODE_A]) {
+						motion->_velocity.x += player->_speed.x;
+					}
+				} else { // player stops
+					if (sm) {
+						sm->_stateMachines[0]->setCurrentState(new IdleState());
+					}
 				}
 			}
 		}
