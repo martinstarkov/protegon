981dea9f85537425f05abfc98847e185825eea34
Martin Starkov <emailregisterlogin@gmail.com>
2020-07-07 22:19:39 +0100

    State switching

--- src/ECS/Systems/InputSystem.h
+++ src/ECS/Systems/InputSystem.h
@@ -18,34 +18,51 @@ public:
 	void keyStateCheck(Entity& e) {
 		PlayerController* player = e.getComponent<PlayerController>();
 		if (player) {
+			StateMachineComponent* sm = e.getComponent<StateMachineComponent>();
+			if (sm) {
+				BaseStateMachine* walkStateMachine = sm->_stateMachines[0];
+				StateID walkStateID = walkStateMachine->getCurrentState()->getStateID();
+				if (walkStateID == typeid(IdleState).hash_code() || walkStateID == typeid(WalkState).hash_code()) {
+					player->_speed.x = player->_originalSpeed.x;
+				} else if (walkStateID == typeid(RunState).hash_code()) {
+					player->_speed.x *= 1.8f;
+				}
+			}
 			MotionComponent* motion = e.getComponent<MotionComponent>();
 			if (motion) {
-				StateMachineComponent* sm = e.getComponent<StateMachineComponent>();
 				// movement occurs
 				if (s[SDL_SCANCODE_W] || s[SDL_SCANCODE_A] || s[SDL_SCANCODE_S] || s[SDL_SCANCODE_D]) {
-					if (sm) {
-						if (sm->_stateMachines[0]->getCurrentState()->getStateID() != typeid(RunState).hash_code()) {
-							sm->_stateMachines[0]->setCurrentState(new WalkState());
-						}
-					}
 					if (s[SDL_SCANCODE_W] && !s[SDL_SCANCODE_S]) { // jump
 						if (sm) {
-							sm->_stateMachines[1]->setCurrentState(new JumpState());
+							sm->_stateMachines[1]->setCurrentState(std::make_unique<JumpState>());
 						}
 						motion->_velocity.y += -player->_speed.y;
 					}
-					if (s[SDL_SCANCODE_S] && !s[SDL_SCANCODE_W]) {
+					if (s[SDL_SCANCODE_S] && !s[SDL_SCANCODE_W]) { // press down
 						motion->_velocity.y += player->_speed.y;
 					}
-					if (s[SDL_SCANCODE_A] && !s[SDL_SCANCODE_D]) {
+					if (s[SDL_SCANCODE_A] && !s[SDL_SCANCODE_D]) { // walk left
 						motion->_velocity.x += -player->_speed.x;
+						if (sm) {
+							if (!sm->_stateMachines[0]->isState(typeid(RunState).hash_code())) {
+								sm->_stateMachines[0]->setCurrentState(std::make_unique<WalkState>());
+							}
+						}
 					}
-					if (s[SDL_SCANCODE_D] && !s[SDL_SCANCODE_A]) {
+					if (s[SDL_SCANCODE_D] && !s[SDL_SCANCODE_A]) { // walk right
 						motion->_velocity.x += player->_speed.x;
+						if (sm) {
+							if (!sm->_stateMachines[0]->isState(typeid(RunState).hash_code())) {
+								sm->_stateMachines[0]->setCurrentState(std::make_unique<WalkState>());
+							}
+						}
+					}
+					if (s[SDL_SCANCODE_A] && s[SDL_SCANCODE_D]) {
+						sm->_stateMachines[0]->setCurrentState(std::make_unique<IdleState>());
 					}
 				} else { // player stops
 					if (sm) {
-						sm->_stateMachines[0]->setCurrentState(new IdleState());
+						sm->_stateMachines[0]->setCurrentState(std::make_unique<IdleState>());
 					}
 				}
 			}
