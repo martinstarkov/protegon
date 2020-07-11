d74c64fb9b8c3fff11d8d28c061f6faeb98d93a6
Martin Starkov <emailregisterlogin@gmail.com>
2020-07-12 02:06:23 +0300

    Move into function

--- src/ECS/Systems/InputSystem.h
+++ src/ECS/Systems/InputSystem.h
@@ -16,38 +16,41 @@ public:
 		}
 	}
 	void keyStateCheck(Entity& e) {
+		if (s[SDL_SCANCODE_Q]) {
+			e.addComponents(Serialization::deserialize<SpriteComponent>("resources/sprite1.json"));
+		}
+		if (s[SDL_SCANCODE_E]) {
+			e.addComponents(Serialization::deserialize<SpriteComponent>("resources/sprite2.json"));
+		}
+		if (s[SDL_SCANCODE_R]) {
+			Serialization::reserialize("resources/animation.json", *e.getComponent<AnimationComponent>());
+		}
+		if (s[SDL_SCANCODE_T]) {
+			Serialization::reserialize("resources/sprite1.json", *e.getComponent<SpriteComponent>());
+		}
+		if (s[SDL_SCANCODE_Y]) {
+			Serialization::reserialize("resources/sprite2.json", *e.getComponent<SpriteComponent>());
+		}
+		if (s[SDL_SCANCODE_C]) {
+			e.removeComponents<SpriteComponent>();
+		}
+		motion(e);
+	}
+	void motion(Entity& e) {
 		PlayerController* player = e.getComponent<PlayerController>();
 		if (player) {
-			if (s[SDL_SCANCODE_Q]) {
-				e.addComponents(Serialization::deserialize<SpriteComponent>("resources/sprite1.json"));
-			}
-			if (s[SDL_SCANCODE_E]) {
-				e.addComponents(Serialization::deserialize<SpriteComponent>("resources/sprite2.json"));
-			}
-			if (s[SDL_SCANCODE_R]) {
-				Serialization::reserialize("resources/animation.json", *e.getComponent<AnimationComponent>());
-			}
-			if (s[SDL_SCANCODE_T]) {
-				Serialization::reserialize("resources/sprite1.json", *e.getComponent<SpriteComponent>());
-			}
-			if (s[SDL_SCANCODE_Y]) {
-				Serialization::reserialize("resources/sprite2.json", *e.getComponent<SpriteComponent>());
-			}
-			if (s[SDL_SCANCODE_C]) {
-				e.removeComponents<SpriteComponent>();
-			}
-			StateMachineComponent* sm = e.getComponent<StateMachineComponent>();
-			if (sm) {
-				BaseStateMachine* walkStateMachine = sm->_stateMachines[0];
-				StateID walkStateID = walkStateMachine->getCurrentState()->getStateID();
-				if (walkStateID == typeid(IdleState).hash_code() || walkStateID == typeid(WalkState).hash_code()) {
-					player->_speed.x = player->_originalSpeed.x;
-				} else if (walkStateID == typeid(RunState).hash_code()) {
-					player->_speed.x = player->_originalSpeed.x * 2.0f;
-				}
-			}
 			MotionComponent* motion = e.getComponent<MotionComponent>();
 			if (motion) {
+				StateMachineComponent* sm = e.getComponent<StateMachineComponent>();
+				if (sm) {
+					BaseStateMachine* walkStateMachine = sm->_stateMachines[0];
+					StateID walkStateID = walkStateMachine->getCurrentState()->getStateID();
+					if (walkStateID == typeid(IdleState).hash_code() || walkStateID == typeid(WalkState).hash_code()) {
+						player->_speed.x = player->_originalSpeed.x;
+					} else if (walkStateID == typeid(RunState).hash_code()) {
+						player->_speed.x = player->_originalSpeed.x * 2.0f;
+					}
+				}
 				// movement occurs
 				if (s[SDL_SCANCODE_W] || s[SDL_SCANCODE_A] || s[SDL_SCANCODE_S] || s[SDL_SCANCODE_D]) {
 					if (s[SDL_SCANCODE_W] && !s[SDL_SCANCODE_S]) { // jump
@@ -87,8 +90,6 @@ public:
 				}
 			}
 		}
-	}
-	void motion(Entity& e) {
 	}
 private:
 	const Uint8* s = nullptr;
