8d19a3abe31bd60182bbb8e0a8c6ed8608cc6909
Martin Starkov <emailregisterlogin@gmail.com>
2020-07-16 07:35:48 +0300

    Add dynamic map of stateMachines instead of two hardcoded ones

--- src/ECS/Components/StateMachineComponent.h
+++ src/ECS/Components/StateMachineComponent.h
@@ -5,10 +5,12 @@
 #include "../../StateMachine/StateMachines.h"
 
 struct StateMachineComponent : public Component<StateMachineComponent> {
-	std::vector<BaseStateMachine*> stateMachines;
-	// TODO: Find out how to pass parameter pack of BaseStateMachine pointers to the component (because there can only exist one component, we store them all in a vector)
-	StateMachineComponent(BaseStateMachine* one, BaseStateMachine* two) {
-		stateMachines.emplace_back(one);
-		stateMachines.emplace_back(two);
+	StateMachineMap stateMachines;
+	StateMachineComponent() {
+	}
+	void setNames() {
+		for (const auto& pair : stateMachines) {
+			pair.second->setName(pair.first);
+		}
 	}
 };
--- src/ECS/Systems/StateMachineSystem.h
+++ src/ECS/Systems/StateMachineSystem.h
@@ -10,8 +10,15 @@ public:
 		for (auto& entityID : entities) {
 			Entity& e = getEntity(entityID);
 			StateMachineComponent* sm = e.getComponent<StateMachineComponent>();
-			for (BaseStateMachine* stateMachine : sm->stateMachines) {
-				stateMachine->update();
+			for (const auto& pair : sm->stateMachines) {
+				pair.second->update();
+				if (pair.first == "walkStateMachine") {
+					MotionComponent* motion = e.getComponent<MotionComponent>();
+					//LOG_(pair.second->getName() << ": " << pair.second->getCurrentState()->getName() << ", velocity: " << motion->velocity);
+				} else if (pair.first == "jumpStateMachine") {
+					MotionComponent* motion = e.getComponent<MotionComponent>();
+					//LOG(" | " << pair.second->getName() << ": " << pair.second->getCurrentState()->getName() << ", acceleration: " << motion->acceleration);
+				}
 			}
 		}
 	}
