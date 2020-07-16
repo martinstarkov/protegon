525cda938d3e5f21ec5678c9e1595a9024891291
Martin Starkov <emailregisterlogin@gmail.com>
2020-07-16 07:36:08 +0300

    Give components names

--- src/ECS/Types.h
+++ src/ECS/Types.h
@@ -7,11 +7,13 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <string>
 
 class BaseComponent;
 
 using EntityID = unsigned int;
 using ComponentID = unsigned int;
 using SystemID = unsigned int;
+using ComponentName = std::string;
 using Signature = std::vector<ComponentID>;
 using ComponentMap = std::map<ComponentID, std::unique_ptr<BaseComponent>>;
