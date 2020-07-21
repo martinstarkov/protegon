f958438764e6aadde89d3a526d331d0eefb22d72
Martin Starkov <emailregisterlogin@gmail.com>
2020-07-21 15:24:27 +0300

    Create ComponentDeclarations.h

---
+++ src/ECS/ComponentDeclarations.h
@@ -0,0 +1,16 @@
+#pragma once
+
+struct AnimationComponent;
+struct CollisionComponent;
+struct DragComponent;
+struct GravityComponent;
+struct InputComponent;
+struct LifetimeComponent;
+struct MotionComponent;
+struct PlayerController;
+struct RenderComponent;
+struct SizeComponent;
+struct SpriteComponent;
+struct SpriteSheetComponent;
+struct StateMachineComponent;
+struct TransformComponent;
