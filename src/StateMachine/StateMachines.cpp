1727b3b219371e6511fb5a8811942592d58592d6
Martin Starkov <emailregisterlogin@gmail.com>
2020-07-22 02:46:17 +0300

    Move method implementations to cpp file

---
+++ src/StateMachine/StateMachines.cpp
@@ -0,0 +1,16 @@
+#include "StateMachines.h"
+
+#include "States.h"
+
+WalkStateMachine::WalkStateMachine(StateName initialState, EntityHandle handle) : StateMachine(initialState) {
+	states.emplace("idle", std::make_unique<IdleState>());
+	states.emplace("walk", std::make_unique<WalkState>());
+	states.emplace("run", std::make_unique<RunState>());
+	init(handle);
+}
+
+JumpStateMachine::JumpStateMachine(StateName initialState, EntityHandle handle) : StateMachine(initialState) {
+	states.emplace("grounded", std::make_unique<GroundedState>());
+	states.emplace("jumped", std::make_unique<JumpState>());
+	init(handle);
+}
--- src/StateMachine/StateMachines.h
+++ src/StateMachine/StateMachines.h
@@ -1,20 +1,13 @@
 #pragma once
 
 #include "StateMachine.h"
-#include "States.h"
 
-class WalkStateMachine : public StateMachine<WalkStateMachine> {
+class WalkStateMachine : public StateMachine {
 public:
-	WalkStateMachine() {
-		states.emplace("idle", std::make_unique<IdleState>());
-		states.emplace("walk", std::make_unique<WalkState>());
-		states.emplace("run", std::make_unique<RunState>());
-	}
+	WalkStateMachine(StateName initialState, EntityHandle handle);
 };
-class JumpStateMachine : public StateMachine<JumpStateMachine> {
+
+class JumpStateMachine : public StateMachine {
 public:
-	JumpStateMachine() {
-		states.emplace("grounded", std::make_unique<GroundedState>());
-		states.emplace("jumped", std::make_unique<JumpState>());
-	}
+	JumpStateMachine(StateName initialState, EntityHandle handle);
 };
