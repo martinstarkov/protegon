1c5022b8dd515013a64513b3cec78afe7efbba10
Martin Starkov <emailregisterlogin@gmail.com>
2020-07-21 15:26:59 +0300

    Move add/remove/get methods for entities to manager class

--- src/ECS/Manager.cpp
+++ src/ECS/Manager.cpp
@@ -1,14 +1,17 @@
 #include "Manager.h"
 
-bool Manager::init() {
-	create<SystemFactory>(RenderSystem(), MovementSystem(), GravitySystem(), LifetimeSystem(), AnimationSystem(), CollisionSystem(), MotionSystem(), InputSystem(), DragSystem(), StateMachineSystem());
-	return true;
+#include "EntityHandle.h"
+#include "Systems.h"
+#include "Components.h"
+#include "../StateMachine/StateMachines.h"
+#include "../StateMachine/States.h"
+
+void Manager::init() {
+	createSystems(RenderSystem(), MovementSystem(), GravitySystem(), LifetimeSystem(), AnimationSystem(), CollisionSystem(), MotionSystem(), InputSystem(), DragSystem(), StateMachineSystem());
 }
 
-Entity& Manager::getEntity(EntityID entityID) {
-	auto iterator = _entities.find(entityID);
-	assert(iterator != _entities.end() && "Must return valid reference to entityID");
-	return *iterator->second; // reference to entity unique ptr
+bool Manager::hasEntity(EntityID id) {
+	return _entities.find(id) != _entities.end();
 }
 
 static EntityID getNewEntityID() {
@@ -16,50 +19,53 @@ static EntityID getNewEntityID() {
 	return lastID++;
 }
 
-Entity& Manager::createEntity() {
+EntityID Manager::createEntity() {
 	EntityID id = getNewEntityID();
-	std::unique_ptr<Entity> uPtr = std::make_unique<Entity>(id, this);
-	Entity& ref = *uPtr;
+	std::unique_ptr<Entity> uPtr = std::make_unique<Entity>();
 	_entities.emplace(id, std::move(uPtr));
-	return ref;
+	return id;
 }
 
-void Manager::destroyEntity(EntityID entityID) {
-	auto iterator = _entities.find(entityID);
-	assert(iterator != _entities.end() && "Attempting to destroy non-existent entity");
-	for (auto& pair : _systems) {
-		pair.second->onEntityDestroyed(entityID);
+void Manager::destroyEntity(EntityID id) {
+	auto it = _entities.find(id);
+	if (it != _entities.end()) {
+		it->second->alive = false;
 	}
-	_entities.erase(entityID);
+	// refresh systems later
 }
 
 EntityID Manager::createTree(Vec2D position) {
-	EntityID entityID = create<EntityFactory>(TransformComponent(position), SizeComponent(Vec2D(64)), SpriteComponent("./resources/textures/tree.png", Vec2D(16)), RenderComponent(), CollisionComponent());
-	return entityID;
+	EntityID id = createEntity();
+	EntityHandle handle = EntityHandle(id, this);
+	handle.addComponents(TransformComponent(position), SizeComponent(Vec2D(64)), SpriteComponent("./resources/textures/tree.png", Vec2D(16)), RenderComponent(), CollisionComponent());
+	return id;
 }
 EntityID Manager::createBox(Vec2D position) {
-	EntityID entityID = create<EntityFactory>(TransformComponent(position), SizeComponent(Vec2D(32)), SpriteComponent("./resources/textures/box.png", Vec2D(16)), MotionComponent(Vec2D(0.5), {}, Vec2D(3.0)), DragComponent(UNIVERSAL_DRAG), RenderComponent(), CollisionComponent());
-	return entityID;
+	EntityID id = createEntity();
+	EntityHandle handle = EntityHandle(id, this);
+	handle.addComponents(TransformComponent(position), SizeComponent(Vec2D(32)), SpriteComponent("./resources/textures/box.png", Vec2D(16)), MotionComponent(Vec2D(0.5), {}, Vec2D(3.0)), DragComponent(UNIVERSAL_DRAG), RenderComponent(), CollisionComponent());
+	return id;
 }
 EntityID Manager::createPlayer(Vec2D position) {
-	EntityID entityID = create<EntityFactory>(TransformComponent(position), SizeComponent(Vec2D(50, 50)), SpriteComponent("./resources/textures/player_anim.png", Vec2D(16)), AnimationComponent(8), RenderComponent(), CollisionComponent());
-	Entity& entity = getEntity(entityID);
+	EntityID id = createEntity();
+	EntityHandle handle = EntityHandle(id, this);
+	handle.addComponents(TransformComponent(position), SizeComponent(Vec2D(50, 50)), SpriteComponent("./resources/textures/player_anim.png", Vec2D(16)), AnimationComponent(8), RenderComponent(), CollisionComponent());
 	StateMachineComponent sm;
 	std::unique_ptr<WalkStateMachine> wsm = std::make_unique<WalkStateMachine>();
-	wsm->init("idle", &entity);
+	wsm->init("idle", handle);
 
 	std::unique_ptr<JumpStateMachine> jsm = std::make_unique<JumpStateMachine>();
-	jsm->init("grounded", &entity);
+	jsm->init("grounded", handle);
 
 	sm.stateMachines.emplace("walkStateMachine", std::move(wsm));
 	sm.stateMachines.emplace("jumpStateMachine", std::move(jsm));
 	sm.setNames();
-	entity.addComponents(MotionComponent(), DragComponent(UNIVERSAL_DRAG), InputComponent(), PlayerController(Vec2D(1.0, 1.0)), std::move(sm));
-	entity.addComponents();
-	return entityID;
+	handle.addComponents(MotionComponent(), DragComponent(UNIVERSAL_DRAG), InputComponent(), PlayerController(Vec2D(1.0, 1.0)), std::move(sm));
+	handle.addComponents();
+	return id;
 }
 
-void Manager::updateSystems() {
+void Manager::update() {
 	getSystem<InputSystem>()->update();
 	getSystem<AnimationSystem>()->update();
 	getSystem<GravitySystem>()->update();
@@ -69,30 +75,35 @@ void Manager::updateSystems() {
 	getSystem<CollisionSystem>()->update();
 	getSystem<LifetimeSystem>()->update();
 	getSystem<StateMachineSystem>()->update();
-	refresh();
+	refreshDeleted();
 }
 
-void Manager::refreshSystems(const EntityID entityID) {
+void Manager::entityChanged(EntityID id) {
 	for (auto& pair : _systems) {
-		pair.second->onEntityChanged(entityID);
+		pair.second->onEntityChanged(id);
 	}
 }
 
-void Manager::refreshSystems() {
-	for (auto& pair : _entities) {
-		refreshSystems(pair.first);
+void Manager::entityDestroyed(EntityID id) {
+	for (auto& pair : _systems) {
+		pair.second->onEntityDestroyed(id);
 	}
 }
 
-void Manager::refresh() { // REWORK
-	std::vector<Entity*> deletables;
+void Manager::refresh() {
 	for (auto& pair : _entities) {
-		if (!pair.second->isAlive()) {
-			deletables.emplace_back(pair.second.get());
+		entityChanged(pair.first);
+	}
+}
+
+void Manager::refreshDeleted() { // REWORK
+	int iteratorOffset = 0;
+	for (auto it = _entities.begin(); it != _entities.end(); ) {
+		if (!it->second->alive) {
+			entityDestroyed(it->first);
+			_entities.erase(it++);
+		} else {
+			++it;
 		}
 	}
-	for (Entity* entity : deletables) {
-		destroyEntity(entity->getID());
-	}
-	deletables.clear();
 }
