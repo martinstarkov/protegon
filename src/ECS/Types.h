dd703b6058512241f3407eec7d0b26f4cce0128b
Martin Starkov <emailregisterlogin@gmail.com>
2020-07-21 15:27:16 +0300

    Switch to platform neutral unsigned ints (size_t)

--- src/ECS/Types.h
+++ src/ECS/Types.h
@@ -11,9 +11,10 @@
 
 class BaseComponent;
 
-using EntityID = unsigned int;
-using ComponentID = unsigned int;
-using SystemID = unsigned int;
+using EntityID = std::size_t;
+using ComponentID = std::size_t;
+using SystemID = std::size_t;
 using ComponentName = std::string;
+using EntitySet = std::set<EntityID>;
 using Signature = std::vector<ComponentID>;
 using ComponentMap = std::map<ComponentID, std::unique_ptr<BaseComponent>>;
