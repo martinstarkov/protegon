c91c7ae29f9d5ba8c795832e9d34f0b1c8ed2d96
Martin Starkov <emailregisterlogin@gmail.com>
2020-07-24 23:48:54 +0300

    Add some commands and remove all state code

--- src/ECS/Systems/InputSystem.cpp
+++ src/ECS/Systems/InputSystem.cpp
@@ -13,48 +13,49 @@ void InputSystem::update() {
 		}
 		if (s[SDL_SCANCODE_Q]) {
 			e.addComponents(Serialization::deserialize<SpriteComponent>("resources/sprite2.json"));
-		}
-		if (s[SDL_SCANCODE_R]) {
-			Serialization::reserialize("resources/animation.json", e.getComponent<AnimationComponent>());
-		}
-		if (s[SDL_SCANCODE_C]) {
-			e.removeComponents<SpriteComponent>();
 		}*/
+		if (e.getID() == 2) {
+			if (s[SDL_SCANCODE_P]) {
+				manager->createBox(e.getComponent<TransformComponent>()->position + Vec2D(50.0, 0.0));
+			}
+			//if (s[SDL_SCANCODE_R]) {
+			//	std::remove("resources/player.json");
+			//	std::ofstream out("resources/player.json");
+			//	json j;
+			//	Serializer::serialize(j[typeid(Entity).name()], e);
+			//	out << std::setw(4) << j; // overloaded setw for pretty printing, number is amount of spaces to indent
+			//	out.close();
+			//}
+			//if (s[SDL_SCANCODE_F]) {
+			//	Entity handle = Entity(manager);
+			//	std::ifstream in("resources/player.json");
+			//	json j;
+			//	in >> j;
+			//	Serializer::deserialize(j[typeid(Entity).name()], handle);
+			//	in.close();
+			//}
+
+			if (s[SDL_SCANCODE_C]) {
+				for (auto& rId : manager->getEntities({ 2 })) {
+					Entity handle(rId, manager);
+					handle.destroy();
+				}
+			}
+		}
 		PlayerController* player = e.getComponent<PlayerController>();
 		if (player) {
 			MotionComponent* motion = e.getComponent<MotionComponent>();
 			if (motion) {
-				/*StateMachineComponent* sm = e.getComponent<StateMachineComponent>();
-				if (sm) {
-					BaseStateMachine* walkStateMachine = sm->stateMachines[0];
-					StateID walkStateID = walkStateMachine->getCurrentState()->getStateID();
-					if (walkStateID == typeid(IdleState).hash_code() || walkStateID == typeid(WalkState).hash_code()) {
-						player->speed.x = player->originalSpeed.x;
-					} else if (walkStateID == typeid(RunState).hash_code()) {
-						player->speed.x = player->originalSpeed.x * 2.0f;
-					}
-				}*/
 				// player stops
 				if ((s[SDL_SCANCODE_A] && s[SDL_SCANCODE_D]) || (!s[SDL_SCANCODE_A] && !s[SDL_SCANCODE_D])) {
-
 					motion->acceleration.x = 0.0;
-					/*if (sm) {
-						sm->stateMachines[0]->setCurrentState(std::move(std::make_unique<IdleState>()));
-					}*/
 				}
 				if ((s[SDL_SCANCODE_W] && s[SDL_SCANCODE_S]) || (!s[SDL_SCANCODE_W] && !s[SDL_SCANCODE_S])) {
-
 					motion->acceleration.y = 0.0;
-					/*if (sm) {
-						sm->stateMachines[0]->setCurrentState(std::move(std::make_unique<IdleState>()));
-					}*/
 				}
 				// key press occurs
 				if (s[SDL_SCANCODE_W] || s[SDL_SCANCODE_A] || s[SDL_SCANCODE_S] || s[SDL_SCANCODE_D]) {
 					if (s[SDL_SCANCODE_W] && !s[SDL_SCANCODE_S]) { // jump
-						/*if (sm) {
-							sm->stateMachines[1]->setCurrentState(std::move(std::make_unique<JumpState>()));
-						}*/
 						motion->acceleration.y = -player->speed.y;
 					}
 					if (s[SDL_SCANCODE_S] && !s[SDL_SCANCODE_W]) { // press down
@@ -62,24 +63,11 @@ void InputSystem::update() {
 					}
 					if (s[SDL_SCANCODE_A] && !s[SDL_SCANCODE_D]) { // walk left
 						motion->acceleration.x = -player->speed.x;
-						/*if (sm) {
-							if (!sm->stateMachines[0]->isState(typeid(RunState).hash_code())) {
-								sm->stateMachines[0]->setCurrentState(std::move(std::make_unique<WalkState>()));
-							}
-						}*/
 					}
 					if (s[SDL_SCANCODE_D] && !s[SDL_SCANCODE_A]) { // walk right
 						motion->acceleration.x = player->speed.x;
-						/*if (sm) {
-							if (!sm->stateMachines[0]->isState(typeid(RunState).hash_code())) {
-								sm->stateMachines[0]->setCurrentState(std::move(std::make_unique<WalkState>()));
-							}
-						}*/
 					}
 				} else { // player stops
-					/*if (sm) {
-						sm->stateMachines[0]->setCurrentState(std::move(std::make_unique<IdleState>()));
-					}*/
 				}
 			}
 		}
