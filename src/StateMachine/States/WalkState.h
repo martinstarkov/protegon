beab424d3779028c03811391bb6c59225f6a1576
Martin Starkov <emailregisterlogin@gmail.com>
2020-07-25 17:53:32 +0300

    Move functionality to header file

--- src/StateMachine/States/WalkState.h
+++ src/StateMachine/States/WalkState.h
@@ -4,7 +4,20 @@
 
 class WalkState : public State<WalkState> {
 public:
-	virtual void onEntry() override final;
-	virtual void update() override final;
+	virtual void onEntry() override final {
+		AnimationComponent* animation = entity.getComponent<AnimationComponent>();
+		if (animation) {
+			animation->name = getName();
+			animation->counter = -1;
+		}
+	}
+	virtual void update() override final {
+		MotionComponent* motion = entity.getComponent<MotionComponent>();
+		if (abs(motion->velocity) >= motion->terminalVelocity * RUN_START_FRACTION) {
+			//parentStateMachine->setCurrentState("run");
+		} else if (!(abs(motion->velocity) > IDLE_START_VELOCITY)) {
+			parentStateMachine->setCurrentState("idle");
+		}
+	}
 };
 
