68ecdc8a21777c3a8ef2c3ab04227a8f818c702b
Martin Starkov <emailregisterlogin@gmail.com>
2020-08-02 15:00:39 +0300

    Update Types.h

--- src/ECS/Types.h
+++ src/ECS/Types.h
@@ -11,6 +11,8 @@
 #include <memory>
 
 class BaseComponent;
+enum class Direction;
+struct SpriteInformation;
 
 using EntityID = std::size_t; // possibly upgrade to unsigned long int later
 using ComponentID = std::size_t;
@@ -20,3 +22,5 @@ using ComponentName = std::string;
 using EntitySet = std::set<EntityID>;
 using Signature = std::vector<ComponentID>;
 using ComponentMap = std::map<ComponentID, std::unique_ptr<BaseComponent>>;
+using DirectionMap = std::map<Direction, SpriteInformation>;
+using AnimationMap = std::map<AnimationName, DirectionMap>;
