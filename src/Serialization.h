50d267517dd86b88f1986b3cff8becd8af09cdae
Martin Starkov <emailregisterlogin@gmail.com>
2020-08-02 15:00:52 +0300

    Add some SDL specific type serialization

--- src/Serialization.h
+++ src/Serialization.h
@@ -9,7 +9,65 @@
 
 #include "Utilities.h"
 
+#include "SDL.h"
+
 using json = nlohmann::json;
+
+// partial specialization for SDL types
+namespace nlohmann {
+	template <>
+	struct adl_serializer<SDL_Color> {
+		static void to_json(json& j, const SDL_Color& o) {
+			j["r"] = o.r;
+			j["g"] = o.g;
+			j["b"] = o.b;
+			j["a"] = o.a;
+		}
+		static void from_json(const json& j, SDL_Color& o) {
+			if (j.find("r") != j.end() || j.find("g") != j.end() || j.find("b") != j.end() || j.find("a") != j.end()) {
+				o = { 0, 0, 0, 0 };
+			}
+			if (j.find("r") != j.end()) {
+				o.r = j.at("r").get<Uint8>();
+			}
+			if (j.find("g") != j.end()) {
+				o.g = j.at("g").get<Uint8>();
+			}
+			if (j.find("b") != j.end()) {
+				o.b = j.at("b").get<Uint8>();
+			}
+			if (j.find("a") != j.end()) {
+				o.a = j.at("a").get<Uint8>();
+			}
+		}
+	};
+	template <>
+	struct adl_serializer<SDL_Rect> {
+		static void to_json(json& j, const SDL_Rect& o) {
+			j["x"] = o.x;
+			j["y"] = o.y;
+			j["w"] = o.w;
+			j["h"] = o.h;
+		}
+		static void from_json(const json& j, SDL_Rect& o) {
+			if (j.find("x") != j.end() || j.find("y") != j.end() || j.find("w") != j.end() || j.find("h") != j.end()) {
+				o = { 0, 0, 0, 0 };
+			}
+			if (j.find("x") != j.end()) {
+				o.x = j.at("x").get<int>();
+			}
+			if (j.find("y") != j.end()) {
+				o.y = j.at("y").get<int>();
+			}
+			if (j.find("w") != j.end()) {
+				o.w = j.at("w").get<int>();
+			}
+			if (j.find("h") != j.end()) {
+				o.h = j.at("h").get<int>();
+			}
+		}
+	};
+}
 
 class Serialization {
 public:
