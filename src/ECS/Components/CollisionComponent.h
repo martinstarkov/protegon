7a330498be7257c155d8449b5594fbe33c112a22
Martin Starkov <emailregisterlogin@gmail.com>
2020-08-04 21:53:23 +0300

    Setup() method sets starting position of collider to transform, later updated in CollisionSystem

--- src/ECS/Components/CollisionComponent.h
+++ src/ECS/Components/CollisionComponent.h
@@ -5,10 +5,19 @@
 #include "../../AABB.h"
 #include "../../Circle.h"
 
+// TODO: Consider holding a pointer to the TransformComponent here instead of an AABB somehow?
+// CONSIDERATIONS ^: How can you default construct a CollisionComponent? For serialization..
+
 struct CollisionComponent : public Component<CollisionComponent> {
 	AABB collider;
 	CollisionComponent(AABB collider = AABB()) : collider(collider) {}
 	CollisionComponent(Vec2D size) : collider({}, size) {}
+	virtual void setup() override final {
+		TransformComponent* transform = entity.getComponent<TransformComponent>();
+		if (transform) {
+			collider.position = transform->position;
+		}
+	}
 };
 
 // json serialization
