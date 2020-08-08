3c508ed3af775440cc8a9892ffb60859eb52833d
Martin Starkov <emailregisterlogin@gmail.com>
2020-08-08 20:01:55 +0300

    Temporarily remove scale and rotation from transform as they are unused and impact FPS while collision system is unoptimized

--- src/ECS/Components/TransformComponent.h
+++ src/ECS/Components/TransformComponent.h
@@ -7,9 +7,12 @@
 struct TransformComponent : public Component<TransformComponent> {
 	Vec2D position;
 	Vec2D originalPosition;
-	double scale;
-	double rotation;
-	TransformComponent(Vec2D position = Vec2D(), double scale = 1.0, double rotation = 0.0) : position(position), rotation(rotation), scale(scale) {
+	//double scale;
+	//double rotation;
+	/*TransformComponent(Vec2D position = Vec2D(), double scale = 1.0, double rotation = 0.0) : position(position), rotation(rotation), scale(scale) {
+		init();
+	}*/
+	TransformComponent(Vec2D position = Vec2D()) : position(position) {
 		init();
 	}
 	void init() {
@@ -20,19 +23,19 @@ struct TransformComponent : public Component<TransformComponent> {
 // json serialization
 inline void to_json(nlohmann::json& j, const TransformComponent& o) {
 	j["position"] = o.position;
-	j["scale"] = o.scale;
-	j["rotation"] = o.rotation;
+	/*j["scale"] = o.scale;
+	j["rotation"] = o.rotation;*/
 }
 
 inline void from_json(const nlohmann::json& j, TransformComponent& o) {
 	if (j.find("position") != j.end()) {
 		o.position = j.at("position").get<Vec2D>();
 	}
-	if (j.find("scale") != j.end()) {
+	/*if (j.find("scale") != j.end()) {
 		o.scale = j.at("scale").get<double>();
 	}
 	if (j.find("rotation") != j.end()) {
 		o.rotation = j.at("rotation").get<double>();
-	}
+	}*/
 	o.init();
 }
