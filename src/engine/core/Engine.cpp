1b23e7c7d2569a594ae5d2cc144a7e365fb92449
Martin Starkov <emailregisterlogin@gmail.com>
2020-10-09 18:05:21 +0300

    Improve initialization and abstract SDL dependence

--- src/engine/core/Engine.cpp
+++ src/engine/core/Engine.cpp
@@ -1,27 +1,22 @@
 #include "Engine.h"
 
-#include <cassert>
+#include <SDL.h>
 #include <SDL_image.h>
+
+#include <cassert>
+
 #include <engine/renderer/TextureManager.h>
 #include <engine/event/InputHandler.h>
 
 namespace engine {
 
 Engine* Engine::instance_{ nullptr };
-SDL_Window* Engine::window_{ nullptr };
-SDL_Renderer* Engine::renderer_{ nullptr };
+Window Engine::window_{ nullptr };
+Renderer Engine::renderer_{ nullptr };
 bool Engine::running_{ false };
-// Defined in Init()
-int Engine::window_width_{ -1 };
-int Engine::window_height_{ -1 };
-int Engine::window_x_{ -1 };
-int Engine::window_y_{ -1 };
+V2_int Engine::window_size_{ 0, 0 };
+V2_int Engine::window_position_{ 0, 0 };
 const char* Engine::window_title_{ "" };
-
-SDL_Window& Engine::GetWindow() { assert(window_ != nullptr); return *window_; }
-SDL_Renderer& Engine::GetRenderer() { assert(renderer_ != nullptr); return *renderer_; }
-int Engine::ScreenWidth() { return window_width_; }
-int Engine::ScreenHeight() { return window_height_; }
 
 void Engine::InputUpdate() {
 	engine::InputHandler::Update();
@@ -29,11 +24,12 @@ void Engine::InputUpdate() {
 
 void Engine::InitSDL(std::uint32_t window_flags, std::uint32_t renderer_flags) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
-		window_ = SDL_CreateWindow(window_title_, window_x_, window_y_, window_width_, window_height_, window_flags);
+		window_ = SDL_CreateWindow(window_title_, window_position_.x, window_position_.y, window_size_.x, window_size_.y, window_flags);
 		if (window_) {
 			renderer_ = SDL_CreateRenderer(window_, -1, renderer_flags);
 			if (renderer_) {
 				// SDL fully initialized
+				return;
 			} else {
 				assert(!"SDL failed to create renderer");
 			}
@@ -47,13 +43,11 @@ void Engine::InitSDL(std::uint32_t window_flags, std::uint32_t renderer_flags) {
 
 void Engine::Clean() {
 	engine::TextureManager::Clean();
-	SDL_DestroyWindow(window_);
-	SDL_DestroyRenderer(renderer_);
+	window_.Destroy();
+	renderer_.Destroy();
 	// Quit SDL subsystems
 	IMG_Quit();
 	SDL_Quit();
 }
-
-void Engine::Quit() { running_ = false; }
 
 } // namespace engine
--- src/engine/core/Engine.h
+++ src/engine/core/Engine.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <cstdint>
-#include <SDL.h>
+
 #include <engine/ecs/ECS.h>
 #include <engine/ecs/Components.h>
 #include <engine/ecs/Systems.h>
+
+#include <engine/renderer/Window.h>
+#include <engine/renderer/Renderer.h>
+
 #include <Defines.h>
 
 namespace engine {
@@ -25,8 +29,6 @@ constexpr int WINDOW_HEIGHT = 480;
 
 } // namespace internal
 
-class BaseGame;
-
 class Engine {
 public:
 	virtual void Init() {}
@@ -35,65 +37,67 @@ public:
 	// Default values defined in engine
 	template <typename T>
 	static void Start(const char* title = internal::WINDOW_TITLE, int width = internal::WINDOW_WIDTH, int height = internal::WINDOW_HEIGHT, int x = internal::WINDOW_X, int y = internal::WINDOW_Y, std::uint32_t window_flags = 0, std::uint32_t renderer_flags = 0) {
+		window_size_ = { width, height };
+		window_position_ = { x, y };
 		window_title_ = title;
-		window_width_ = width;
-		window_height_ = height;
-		window_x_ = x;
-		window_y_ = x;
-		InitSDL(window_flags, renderer_flags);
 		running_ = true;
-		Loop<T>();
+		InitSDL(window_flags, renderer_flags);
+		auto& engine = GetInstance<T>();
+		engine.Init();
+		Loop<T>(engine);
 		Clean();
 	}
-	static void Quit();
-	static SDL_Window& GetWindow();
-	static SDL_Renderer& GetRenderer();
-	static int ScreenWidth();
-	static int ScreenHeight();
-protected:
-	ecs::Manager manager;
-private:
-	static void InputUpdate();
-	template <typename T>
-	static Engine* GetInstance() {
-		if (!instance_) {
-			instance_ = new T();
-		}
-		return instance_;
+	static void Quit() { running_ = false; }
+	static Window& GetWindow() {
+		assert(window_ && "Cannot return uninitialized window");
+		return window_;
 	}
+	static Renderer& GetRenderer() {
+		assert(renderer_ && "Cannot return uninitialized renderer");
+		return renderer_;
+	}
+	static V2_int ScreenSize() { return window_size_; }
+	static int ScreenWidth() { return window_size_.x; }
+	static int ScreenHeight() { return window_size_.y; }
+protected:
+	ecs::Manager ecs;
+private:
+	static Engine* instance_;
+	template <typename T>
+	static T& GetInstance() {
+		if (!instance_) {
+			instance_ = new T{};
+		}
+		return *static_cast<T*>(instance_);
+	}
+	static void InitSDL(std::uint32_t window_flags, std::uint32_t renderer_flags);
+	static void InputUpdate();
 	static void Clean();
 	template <typename T>
-	static void Loop() {
+	static void Loop(T& engine) {
 		const std::uint32_t delay = 1000 / FPS;
 		std::uint32_t start;
 		std::uint32_t time;
-		auto& ref = *GetInstance<T>();
-		ref.Init();
 		while (running_) {
 			start = SDL_GetTicks();
-			// Possibly pass delta T to lambda
 			InputUpdate();
-			ref.Update();
-			SDL_RenderClear(renderer_);
+			engine.Update();
+			renderer_.Clear();
 			TextureManager::SetDrawColor(TextureManager::GetDefaultRendererColor());
-			// Render everything here
-			ref.Render();
-			SDL_RenderPresent(renderer_);
+			// Render everything here.
+			engine.Render();
+			renderer_.Present();
 			time = SDL_GetTicks() - start;
 			if (delay > time) { // cap frame time at an FPS
 				SDL_Delay(delay - time);
 			}
 		}
 	}
-	static void InitSDL(std::uint32_t window_flags, std::uint32_t renderer_flags);
-	static SDL_Window* window_;
-	static SDL_Renderer* renderer_;
-	static Engine* instance_;
+	static Window window_;
+	static Renderer renderer_;
 	static bool running_;
-	static int window_width_;
-	static int window_height_;
-	static int window_x_;
-	static int window_y_;
+	static V2_int window_size_;
+	static V2_int window_position_;
 	static const char* window_title_;
 };
 
