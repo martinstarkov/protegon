59ca014d9362878b5ba17870076aab24c4eaa330
Martin Starkov <emailregisterlogin@gmail.com>
2020-10-18 08:42:26 +0300

    Static check that registered event has static Invoke method, this took 10 straight hours to figure out :cry:

--- src/engine/event/EventHandler.h
+++ src/engine/event/EventHandler.h
@@ -4,10 +4,13 @@
 #include <unordered_map>
 #include <cassert>
 #include <memory>
+#include <type_traits>
 
 #include <engine/ecs/ECS.h>
 
 namespace engine {
+
+namespace detail {
 
 using EventId = std::uint32_t;
 
@@ -27,23 +30,51 @@ static void EventCall(TArgs&&... args) {
 
 using EventFunction = void (*)();
 
-template <typename ...TArgs>
-using SpecificFunction = void (*)(TArgs&&...);
+template <typename T>
+constexpr auto has_invoke_helper(const T&, int)
+-> decltype(&T::Invoke, &T::Invoke);
+
+template <typename T>
+constexpr void* has_invoke_helper(const T&, long) {
+	return nullptr;
+}
+
+template <typename T>
+bool constexpr has_invoke = !std::is_same<decltype(has_invoke_helper(std::declval<T>(), 0)), void*>::value;
+
+template <typename T>
+bool constexpr has_static_invoke = has_invoke<T> && !std::is_member_function_pointer_v<decltype(has_invoke_helper(std::declval<T>(), 0))>;
+
+template <typename T, std::enable_if_t<has_static_invoke<T>, int> = 0>
+constexpr EventFunction EventCast() {
+	return static_cast<EventFunction>(static_cast<void*>(&T::Invoke));
+}
+
+template <typename T, std::enable_if_t<!has_invoke<T> || std::is_member_function_pointer_v<decltype(has_invoke_helper(std::declval<T>(), 0))>, int> = 0>
+constexpr EventFunction EventCast() {
+	return nullptr;
+}
+
+} // namespace detail
 
 class EventHandler {
 public:
-	template <typename T, typename ...TArgs>
+	template <typename T>
 	static void Register(ecs::Entity entity) {
-		auto event_id = GetEventId<T>();
+		static_assert(detail::has_static_invoke<T>, "Cannot register event which does not implement a static method with the name 'Invoke'");
+		//static_assert("Cannot register event which does not implement a static method with the name 'Invoke'");
+		auto event_id = detail::GetEventId<T>();
 		auto caller_it = callers.find(entity);
 		if (caller_it == std::end(callers)) {
-			callers.emplace(std::move(entity), std::move(std::vector<EventId>{ event_id }));
+			callers.emplace(std::move(entity), std::move(std::vector<detail::EventId>{ event_id }));
 		} else {
 			caller_it->second.emplace_back(event_id);
 		}
 		auto event_it = events.find(event_id);
 		if (event_it == std::end(events)) {
-			events.emplace(event_id, reinterpret_cast<EventFunction>(&EventCall<T, TArgs...>));
+			auto function_pointer = detail::EventCast<T>();
+			assert(function_pointer != nullptr && "Could not create valid event invoke function pointer");
+			events.emplace(event_id, function_pointer);
 		}
 	}
 	template <typename ...TArgs>
@@ -53,12 +84,14 @@ public:
 		for (auto event_id : caller_it->second) {
 			auto it = events.find(event_id);
 			assert(it != std::end(events) && "Could not find valid event invoke function pointer");
-			reinterpret_cast<SpecificFunction<TArgs...>>(it->second)(std::forward<TArgs>(args)...);
+			auto function_pointer = reinterpret_cast<void (*)(TArgs &&...)>(it->second);
+			assert(function_pointer != nullptr && "Could not create valid event invoke function pointer");
+			function_pointer(std::forward<TArgs>(args)...);
 		}
 	}
 private:
-	static std::unordered_map<ecs::Entity, std::vector<EventId>> callers;
-	static std::unordered_map<EventId, EventFunction> events;
+	static std::unordered_map<ecs::Entity, std::vector<detail::EventId>> callers;
+	static std::unordered_map<detail::EventId, detail::EventFunction> events;
 };
 
 } // namespace engine
