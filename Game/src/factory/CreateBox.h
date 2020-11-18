367f7aca4835d32db41f7dd3f1fa051f4bc3e774
Martin <emailregisterlogin@gmail.com>
2020-11-18 08:48:41 +0200

    Move object factories to separate files

---
+++ Game/src/factory/CreateBox.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <engine/Include.h>
+
+ecs::Entity CreateBox(V2_double position, ecs::Manager& manager) {
+	auto entity = manager.CreateEntity();
+	entity.AddComponent<RenderComponent>();
+	entity.AddComponent<CollisionComponent>(position, V2_double{ 32, 32 });
+	//entity.AddComponent<SpriteComponent>("./resources/textures/box.png", V2_int{ 32, 32 });
+	entity.AddComponent<TransformComponent>(position);
+	return entity;
+}
