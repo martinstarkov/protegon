c97d686dc40baef363652697b42351fb2e723ae1
Martin <emailregisterlogin@gmail.com>
2020-11-18 08:45:01 +0200

    Make center of rotation default to center of object

--- Engine/engine/ecs/components/TransformComponent.h
+++ Engine/engine/ecs/components/TransformComponent.h
@@ -7,11 +7,16 @@
 struct TransformComponent {
 	V2_double position{};
 	V2_double original_position{};
-	V2_double center_of_rotation{};
+	V2_double* center_of_rotation = nullptr;
 	double rotation = 0.0;
 	TransformComponent() = default;
+	~TransformComponent() {
+		delete center_of_rotation;
+		center_of_rotation = nullptr;
+	}
 	TransformComponent(V2_double position) : position{ position } {}
-	TransformComponent(V2_double position, V2_double center_of_rotation, double rotation) : position{ position }, center_of_rotation{ center_of_rotation }, rotation{ rotation } {
+	TransformComponent(V2_double position, V2_double center_of_rotation, double rotation = 0.0) : position{ position }, center_of_rotation{ new V2_double{ center_of_rotation }
+}, rotation{ rotation } {
 		Init();
 	}
 	void ResetPosition() {
