b52a601e319b4ec6defee2974dfeebd3f7ac8700
Martin <emailregisterlogin@gmail.com>
2020-11-18 08:48:31 +0200

    Move events to separate files

---
+++ Game/src/event/RandomizeColorEvent.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <engine/Include.h>
+
+struct RandomizeColorEvent {
+	static void Invoke(ecs::Entity& invoker) {
+		if (invoker.HasComponent<InfluenceComponent>()) {
+			auto& manager = invoker.GetComponent<InfluenceComponent>().manager;
+			/*auto color_entities = manager.GetComponentTuple<RenderComponent>();
+			for (auto [entity2, render_component2] : color_entities) {
+				render_component2.original_color = engine::Color::RandomSolid();
+			}*/
+			auto camera_entities = manager.GetComponentTuple<CameraComponent>();
+			for (auto [entity, camera] : camera_entities) {
+				camera.primary = false;
+			}
+			auto entities = manager.GetComponentTuple<RenderComponent>();
+			auto count = entities.size();
+			auto random_index = engine::math::GetRandomValue<std::size_t>(0, count - 1);
+			for (std::size_t i = 0; i < count; ++i) {
+				if (i == random_index) {
+					auto [entity, render] = entities[i];
+					entity.AddComponent<CameraComponent>(engine::Camera{}, true);
+				}
+			}
+		}
+	}
+};
