edc835044af3a26b1ecf825d6c8ecf7065b4deb6
Martin <emailregisterlogin@gmail.com>
2020-11-23 10:01:05 +0200

    Add form around hitbox function

--- Engine/engine/parsing/ImageProcessor.cpp
+++ Engine/engine/parsing/ImageProcessor.cpp
@@ -1,121 +1,13 @@
 #include "ImageProcessor.h"
 
-#include <SDL_image.h>
-#include <SDL.h>
-
 #include <cstdint>
 #include <algorithm>
-#include <iterator>
 
 namespace engine {
 
-// Convert an SDL surface coordinate to a 4 byte integer value containg the RGBA32 color of the pixel.
-static std::uint32_t GetSurfacePixelColor(SDL_Surface* image, V2_int position) {
-	// Source: http://sdl.beuc.net/sdl.wiki/Pixel_Access
-	auto bpp = image->format->BytesPerPixel;
-	// Here p is the address to the pixel we want to retrieve.
-	std::uint8_t* p = static_cast<std::uint8_t*>(image->pixels) + static_cast<std::size_t>(position.y) * static_cast<std::size_t>(image->pitch) + static_cast<std::size_t>(position.x) * static_cast<std::size_t>(bpp);
-	switch (bpp) {
-		case 1:
-			return *p;
-			break;
-		case 2:
-			return *(Uint16*)p;
-			break;
-		case 3:
-			if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
-				return p[0] << 16 | p[1] << 8 | p[2];
-			else
-				return p[0] | p[1] << 8 | p[2] << 16;
-			break;
-		case 4:
-			return *(Uint32*)p;
-			break;
-		default:
-			return 0; // Shouldn't happen, but avoids warnings.
-	}
-}
-
-Image::Image(const char* path) {
-	// TODO: Add support for formats other than PNG.
-	SDL_Surface* temp_image = IMG_Load(path);
-	if (!temp_image) {
-		printf("IMG_Load: %s\n", IMG_GetError());
-		// handle error
-		assert(false && "Failed to retrieve image data");
-	}
-	SDL_Surface* image = SDL_ConvertSurfaceFormat(temp_image, SDL_PIXELFORMAT_RGBA32, 0);
-	SDL_FreeSurface(temp_image);
-	assert(image != nullptr && "Failed to convert image to RGBA format");
-	size_.x = image->w;
-	size_.y = image->h;
-	pixels_.resize(static_cast<std::size_t>(size_.x) * static_cast<std::size_t>(size_.y), Color{});
-	for (auto j = 0; j < size_.y; ++j) {
-		for (auto i = 0; i < size_.x; ++i) {
-			SetPixel({ i, j }, GetSurfacePixelColor(image, { i, j }));
-		}
-	}
-	SDL_FreeSurface(image);
-}
-
-Image::Image(std::vector<Color> pixels, V2_int size) : pixels_{ std::move(pixels) }, size_{ size } {}
-
-Color Image::GetPixel(V2_int position) const { 
-	auto index = position.y * size_.x + position.x;
-	assert(pixels_.size() > 0 && position.x < size_.x && position.y < size_.y && index < pixels_.size() && "Pixel out of range of image size");
-	return pixels_[index];
-}
-
-V2_int Image::GetSize() const { return size_; }
-
-Image Image::GetSubImage(V2_int top_left, V2_int bottom_right) const {
-	std::vector<Color> sub_pixels;
-	// Add { 1, 1 } since taking difference omits one row and column of pixels.
-	V2_int sub_image_size = bottom_right - top_left + V2_int{ 1, 1 };
-	auto sub_pixels_size = sub_image_size.x * sub_image_size.y;
-	sub_pixels.reserve(sub_pixels_size);
-	// Find indexes of first and last points to cut down on loop time.
-	auto first_index = top_left.y * size_.x + top_left.x;
-	auto last_index = bottom_right.y * size_.x + bottom_right.x;
-	assert(first_index >= 0 && first_index < pixels_.size() && "Top left coordinate out of range of image pixels");
-	assert(last_index >= 0 && last_index < pixels_.size() && "Top right coordinate out of range of image pixels");
-	for (auto i = first_index; i < last_index + 1; ++i) {
-		auto mod_index = i % size_.x;
-		if (mod_index >= top_left.x && mod_index <= bottom_right.x) {
-			sub_pixels.push_back(pixels_[i]);
-		}
-	}
-	return Image{ std::move(sub_pixels), sub_image_size };
-}
-
-void Image::SetPixel(V2_int position, const Color& color) {
-	auto index = position.y * size_.x + position.x;
-	assert(pixels_.size() > 0 && position.x < size_.x && position.y < size_.y && index < pixels_.size() && "Pixel out of range of image size");
-	pixels_[index] = color;
-}
-
-std::ostream& operator<<(std::ostream& os, Image& image) {
-	for (auto i = 0; i < image.pixels_.size(); ++i) {
-		// Add newline every time the vector loops over the size.x length.
-		if (i % image.size_.x == 0 && i) {
-			os << std::endl;
-		}
-		const auto& color = image.pixels_[i];
-		if (!color.IsTransparent()) {
-			os << "#";
-		} else {
-			os << " ";
-		}
-	}
-	return os;
-}
-
-std::vector<std::pair<Image, V2_int>> ImageProcessor::GetDisconnectedImages(const char* image_path) {
-	std::vector<std::pair<Image, V2_int>> images;
+std::vector<Image> ImageProcessor::GetDisconnectedImages(const char* image_path) {
+	std::vector<Image> images;
 	auto full_image = Image{ image_path };
-	LOG("-----------------------------------");
-	LOG(full_image);
-	LOG("-----------------------------------");
 	auto [top_left, bottom_right] = GetCorners(full_image);
 	auto outer_sub_image = full_image.GetSubImage(top_left, bottom_right);
 	V2_int min{ outer_sub_image.size_ };
@@ -135,22 +27,83 @@ std::vector<std::pair<Image, V2_int>> ImageProcessor::GetDisconnectedImages(cons
 				++transparent_pixels;
 			}
 		}
+		// Column is transparent or is the final column
 		if (transparent_pixels == outer_sub_image.size_.y || column == outer_sub_image.size_.x - 1) {
+			// This means that if the previous column was transparent, skip this column.
+			// (since min.x is reset every time a column is transparent).
 			if (min.x != outer_sub_image.size_.x) {
-				// Column was transparent and previous column wasn't.
-				LOG(min << " -> " << max);
 				auto sub_image = outer_sub_image.GetSubImage(min, max);
-				LOG("-----------------------------------");
-				LOG(sub_image);
-				LOG("-----------------------------------");
+				// Offset by how much the outer_sub_image is within the full image.
+				sub_image.position_ += top_left;
+				// Add sub image to vector.
+				images.emplace_back(sub_image);
 			}
-			// Reset local pixel min / max.
+			// Reset local pixel min / max so the next transparent column isn't considered and new image outlines are found.
 			min = outer_sub_image.size_;
 			max = { 0, 0 };
 		}
 	}
-	
-	return {};
+	return std::move(images);
+}
+
+std::vector<Image>& ImageProcessor::CenterOnHitbox(std::vector<Image>& images, V2_int hitbox_size, Side side) {
+	V2_int size = hitbox_size;
+	V2_int largest_size;
+	for (auto& image : images) {
+		largest_size.x = std::max(largest_size.x, image.size_.x);
+		largest_size.y = std::max(largest_size.y, image.size_.y);
+	}
+	if (size.IsZero()) {
+		size = largest_size;
+	}
+	/*
+	* // TODO: In the future make it so the hitbox selection is smart based on the side you select.
+	 else if (side.IsVertical()) {
+		size.y = largest_size.y;
+	} else if (side.IsHorizontal()) {
+		size.x = largest_size.x;
+	}
+	*/
+	//LOG("Hitbox size: " << size);
+	for (auto& image : images) {
+		V2_int difference = size - image.size_;
+		//LOG("---------------------------------");
+		//LOG("original size: " << image.size_);
+		//LOG("size difference: " << difference);
+		if (!side) {
+			// TODO: Add individual point centering here.
+		} else {
+			int additions = 0;
+			auto [perpendicular_1, perpendicular_2] = side.GetPerpendicular();
+			// By default add differences to X-directions.
+			int& diff = difference.x;
+			if (side.IsVertical()) {
+				if (difference.y > 0) {
+					additions = difference.y;
+				}
+				diff = difference.x;
+			} else if (side.IsHorizontal()) {
+				if (difference.x > 0) {
+					additions = difference.x;
+				}
+				diff = difference.y;
+			}
+			for (; diff > 0; --diff) {
+				if (diff % 2 == 0) { // Even difference.
+					image.AddSide(perpendicular_2);
+				} else if (diff % 2 == 1) { // Odd difference.
+					image.AddSide(perpendicular_1);
+				}
+			}
+			for (auto i = 0; i < additions; ++i) {
+				image.AddSide(side.GetOpposite());
+			}
+		}
+		//LOG(image);
+		//LOG("size: " << image.size_);
+		//LOG("---------------------------------");
+	}
+	return images;
 }
 
 std::pair<V2_int, V2_int> ImageProcessor::GetCorners(const Image& image) {
--- Engine/engine/parsing/ImageProcessor.h
+++ Engine/engine/parsing/ImageProcessor.h
@@ -3,43 +3,19 @@
 #include "utils/Vector2.h"
 
 #include "renderer/Color.h"
+#include "parsing/Image.h"
 
 #include <vector>
 #include <utility>
 
 namespace engine {
 
-class ImageProcessor;
-
-class Image {
-public:
-	// Analyze png file to generate image.
-	Image(const char* path);
-	// Return the color at a given x,y coordinate.
-	Color GetPixel(V2_int position) const;
-	// Returns the size of the image in pixels.
-	V2_int GetSize() const;
-	// Returns an image within the image with the given edge coordinates.
-	// Obligatory comment on imageception.
-	Image GetSubImage(V2_int top_left, V2_int bottom_right) const;
-	// Prints image out in the console.
-	// Non-transparent pixels are hashtags while transparent ones as spaces.
-	friend std::ostream& operator<<(std::ostream& os, Image& image);
-	friend ImageProcessor;
-private:
-	// For creating sub images.
-	Image(std::vector<Color> pixels, V2_int size);
-	// Set a given pixel position to a color, used for populating pixels vector initially.
-	void SetPixel(V2_int position, const Color& color);
-	V2_int size_;
-	// 1D array of 2D points in the format: y * size.x + x
-	std::vector<Color> pixels_;
-};
-
 class ImageProcessor {
 public:
 	// Returns a vector of transparent column disconnected images and their positions within the outer image.
-	static std::vector<std::pair<Image, V2_int>> GetDisconnectedImages(const char* image_path);
+	static std::vector<Image> GetDisconnectedImages(const char* image_path);
+	// Trims / expands each image relative to the center of the given axis to fit the hitbox.
+	static std::vector<Image>& CenterOnHitbox(std::vector<Image>& images, V2_int hitbox_size = V2_int{ 0, 0 }, Side side = Direction::BOTTOM);
 private:
 	// Return the coordinates of the top left and bottom right most non-transparent corner pixels in an image.
 	// I.e. Remove all transparent pixels from around an image and this function will return { { 0, 0 }, { size.x, size.y } }.
