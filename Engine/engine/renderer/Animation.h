d5d944067d7fbdffad382b003278496db63b6354
Martin <emailregisterlogin@gmail.com>
2020-11-29 17:50:35 +0200

    Add animation object

---
+++ Engine/engine/renderer/Animation.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "utils/Vector2.h"
+
+namespace engine {
+
+struct Animation {
+	Animation() = delete;
+	Animation(V2_int top_left, V2_int sprite_size, V2_int hitbox_offset, int sprite_count, int spacing_between_sprites = 0) :
+		position{ top_left }, 
+		sprite_size{ sprite_size }, 
+		sprite_count{ sprite_count },
+		hitbox_offset{ hitbox_offset - top_left },
+		spacing{ spacing_between_sprites } {}
+	V2_int position;
+	V2_int sprite_size;
+	V2_int hitbox_offset;
+	int sprite_count;
+	int spacing;
+};
+
+} // namespace engine
