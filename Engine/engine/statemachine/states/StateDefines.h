75ca9e2565e17701957e696d8c3f5c3f60f3bedb
Martin Starkov <emailregisterlogin@gmail.com>
2020-12-24 00:14:18 +0200

    Remove unnecessary files for new state machine

--- Engine/engine/statemachine/states/StateDefines.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define IDLE_START_VELOCITY LOWEST_VELOCITY // idle starts when velocity is less than or equal to this value
-#define RUN_START_FRACTION 0.6 // run starts when velocity is this fraction of terminal velocity
