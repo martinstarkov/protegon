63f2d423c0ef00c5ae315f987a1591ff9ceb345d
Martin Starkov <emailregisterlogin@gmail.com>
2021-01-02 00:23:03 +0200

    Fix render system bugs

--- Engine/engine/ecs/systems/RenderSystem.h
+++ Engine/engine/ecs/systems/RenderSystem.h
@@ -11,56 +11,59 @@ public:
 	RenderSystem() = default;
 	RenderSystem(engine::Scene* scene) : scene{ scene } {}
 	virtual void Update() override final {
-		for (auto& [entity, render, transform, sprite] : entities) {
-			V2_double position = transform.position;
-			V2_double size = sprite.current_sprite.size;
-			V2_double hitbox_offset{};
-			if (entity.HasComponent<AnimationComponent>()) {
-				auto& animation = entity.GetComponent<AnimationComponent>();
-				hitbox_offset = sprite.sprite_map.GetAnimation(animation.current_animation).hitbox_offset;
-				engine::TextureManager::DrawPoint(position, engine::CYAN);
-				engine::TextureManager::DrawPoint(position + size / 2.0, engine::BLACK);
-				engine::TextureManager::DrawPoint(position + size * sprite.scale / 2.0, engine::RED);
-				engine::TextureManager::DrawPoint(position + (size - hitbox_offset) * sprite.scale / 2.0, engine::ORANGE);
-				engine::TextureManager::DrawPoint(position + (size - hitbox_offset * 2) * sprite.scale / 2.0, engine::BLUE);
-			}
-			assert(!size.IsZero() && "Cannot render sprite without (collision or size) component");
-			auto flip = Flip::NONE;
-			V2_double flip_scaling{};
-			if (entity.HasComponent<DirectionComponent>()) {
-				auto& dir = entity.GetComponent<DirectionComponent>();
-				if (dir.x_direction == Direction::LEFT) {
-					flip = Flip::HORIZONTAL;
-					if (static_cast<int>(size.x) % 2 == 1) {
-						flip_scaling.x = 0;
-					} else {
-						flip_scaling.x = 1;
+		int counter = 0;
+		if (scene) {
+			auto camera = scene->GetCamera();
+			for (auto& [entity, render, transform, sprite] : entities) {
+				V2_double position = transform.position;
+				V2_double size = sprite.current_sprite.size;
+				if (camera) {
+					AABB aabb{ position - camera->offset, size };
+					AABB view{ { 0, 0 }, engine::Engine::ScreenSize() };
+					// Cull objects outside of view.
+					if (!engine::collision::AABBvsAABB(aabb, view)) {
+						continue;
 					}
-					if (dir.y_direction == Direction::UP) {
+				}
+				V2_double hitbox_offset{};
+				if (entity.HasComponent<AnimationComponent>()) {
+					auto& animation = entity.GetComponent<AnimationComponent>();
+					hitbox_offset = sprite.sprite_map.GetAnimation(animation.current_animation).hitbox_offset;
+				}
+				assert(!size.IsZero() && "Cannot render sprite without (collision or size) component");
+				auto flip = Flip::NONE;
+				V2_double flip_scaling{};
+				if (entity.HasComponent<DirectionComponent>()) {
+					auto& dir = entity.GetComponent<DirectionComponent>();
+					if (dir.x_direction == Direction::LEFT) {
+						flip = Flip::HORIZONTAL;
+						if (static_cast<int>(size.x) % 2 == 1) {
+							flip_scaling.x = 0;
+						} else {
+							flip_scaling.x = 1;
+						}
+						if (dir.y_direction == Direction::UP) {
+							flip_scaling.y = hitbox_offset.y;
+							flip = Flip::BOTH;
+						} else {
+							flip_scaling.y = 0;
+						}
+					} else if (dir.y_direction == Direction::UP) {
 						flip_scaling.y = hitbox_offset.y;
-						flip = Flip::BOTH;
+						flip = Flip::VERTICAL;
 					} else {
 						flip_scaling.y = 0;
 					}
-				} else if (dir.y_direction == Direction::UP) {
-					flip_scaling.y = hitbox_offset.y;
-					flip = Flip::VERTICAL;
-				} else {
-					flip_scaling.y = 0;
 				}
-			}
-			if (scene) {
 				auto sprite_position = position - (hitbox_offset - flip_scaling) * sprite.scale;
-				auto collider_size = entity.GetComponent<CollisionComponent>().collider.size;
-				auto camera = scene->GetCamera();
 				if (camera) {
-					sprite_position += camera->offset;
+					sprite_position -= camera->offset;
 					sprite_position *= camera->scale;
 					size *= camera->scale;
-					position += camera->offset;
+					position -= camera->offset;
 					position *= camera->scale;
-					collider_size *= camera->scale;
 				}
+				++counter;
 				engine::TextureManager::DrawRectangle(
 					sprite.sprite_map.path,
 					sprite.current_sprite.position,
@@ -72,6 +75,7 @@ public:
 					transform.rotation);
 			}
 		}
+		//LOG("Rendered " << counter << " sprites");
 	}
 private:
 	engine::Scene* scene = nullptr;
