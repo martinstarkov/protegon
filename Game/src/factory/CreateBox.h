07876d8d23dea6ea1657a84c482e457e26a47483
Martin Starkov <emailregisterlogin@gmail.com>
2021-01-02 15:44:10 +0200

    Add more box textures

--- Game/src/factory/CreateBox.h
+++ Game/src/factory/CreateBox.h
@@ -11,12 +11,12 @@ ecs::Entity CreateBox(ecs::Entity& entity, V2_double position, V2_int size) {
 	return entity;
 }
 
-ecs::Entity CreateBox(V2_double position, V2_int size, ecs::Manager& manager) {
+ecs::Entity CreateBox(V2_int position, V2_int size, ecs::Manager& manager, const char* path) {
 	auto entity = manager.CreateEntity();
 	entity.AddComponent<RenderComponent>();
-	//entity.AddComponent<CollisionComponent>(position, size);
-	entity.AddComponent<SizeComponent>(size);
-	entity.AddComponent<SpriteComponent>("./resources/textures/box.png", V2_int{ 1, 1 }, size);
+	entity.AddComponent<CollisionComponent>(position, size);
+	//entity.AddComponent<SizeComponent>(size);
+	entity.AddComponent<SpriteComponent>(path, V2_int{ 1, 1 }, size);
 	entity.AddComponent<TransformComponent>(position);
 	return entity;
 }
