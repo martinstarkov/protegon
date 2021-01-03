a456872525d0e8d6cad3e1561d16f6d3b5f3cca5
Martin Starkov <emailregisterlogin@gmail.com>
2021-01-03 15:37:55 +0200

    Update the chunk performance test to match

--- Game/src/factory/CreateBox.h
+++ Game/src/factory/CreateBox.h
@@ -4,7 +4,7 @@
 
 ecs::Entity CreateBox(ecs::Entity& entity, V2_double position, V2_int size) {
 	entity.AddComponent<RenderComponent>();
-	entity.AddComponent<CollisionComponent>(position, size);
+	//entity.AddComponent<CollisionComponent>(position, size);
 	//entity.AddComponent<SizeComponent>(V2_double{ 32, 32 });
 	entity.AddComponent<SpriteComponent>("./resources/textures/box.png", V2_int{ 1, 1 }, size);
 	entity.AddComponent<TransformComponent>(position);
@@ -14,7 +14,7 @@ ecs::Entity CreateBox(ecs::Entity& entity, V2_double position, V2_int size) {
 ecs::Entity CreateBox(V2_int position, V2_int size, ecs::Manager& manager, const char* path) {
 	auto entity = manager.CreateEntity();
 	entity.AddComponent<RenderComponent>();
-	entity.AddComponent<CollisionComponent>(position, size);
+	//entity.AddComponent<CollisionComponent>(position, size);
 	//entity.AddComponent<SizeComponent>(size);
 	entity.AddComponent<SpriteComponent>(path, V2_int{ 1, 1 }, size);
 	entity.AddComponent<TransformComponent>(position);
--- Game/src/procedural/chunks/BoxChunk.h
+++ Game/src/procedural/chunks/BoxChunk.h
@@ -15,7 +15,7 @@ public:
 				auto& entity = GetEntity(tile);
 				auto tile_seed = (absolute_tile_position.x & 0xFFFF) << 16 | (absolute_tile_position.y & 0xFFFF);
 				rng.SetSeed(tile_seed * seed);
-				bool draw_box = (rng.RandomInt(0, 20) == 1);
+				bool draw_box = (rng.RandomInt(1, 2) == 1);
 				if (draw_box) {
 					auto absolute_position = absolute_tile_position * tile_size;// - static_cast<V2_int>(camera->offset);
 					V2_int size = tile_size;
@@ -23,7 +23,7 @@ public:
 					//size.y = rng.RandomInt(tile_size.y, engine::math::RoundCast<int>(tile_size.y * 1.5));
 					//auto color = colors[rng.RandomInt(0, 8)];
 					//LOG("Tile count: " << tile_count << ", chunk pos: " << info.position << ", abs tile pos: " << absolute_tile_position);
-					if (rng.RandomInt(0, 2) == 1) {
+					if (rng.RandomInt(1, 2) == 1) {
 						entity = CreateBox(absolute_position, size, *manager, "./resources/textures/box.png");
 					} else if (rng.RandomInt(0, 5) == 1) {
 						entity = CreateBox(absolute_position, size, *manager, "./resources/textures/box2.png");
