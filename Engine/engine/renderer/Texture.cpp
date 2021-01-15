fca561298d6ca27259ccbf9cff6a9e46d1774264
Martin Starkov <emailregisterlogin@gmail.com>
2021-01-15 22:12:43 +0200

    Add texture clear code

--- Engine/engine/renderer/Texture.cpp
+++ Engine/engine/renderer/Texture.cpp
@@ -3,6 +3,7 @@
 #include <SDL.h>
 
 #include "renderer/AABB.h"
+#include "renderer/Color.h"
 
 namespace engine {
 
@@ -11,7 +12,7 @@ SDL_Texture* Texture::operator=(SDL_Texture* texture) { this->texture = texture;
 Texture::operator SDL_Texture* () const { return texture; }
 Texture::operator bool() const { return texture != nullptr; }
 SDL_Texture* Texture::operator&() const { return texture; }
-bool Texture::Lock(void* out_pixels, int* out_pitch, AABB* lock_area) {
+bool Texture::Lock(void** out_pixels, int* out_pitch, AABB* lock_area) {
 	SDL_Rect* lock_rect = NULL;
 	SDL_Rect rect;
 	if (lock_area != nullptr) {
@@ -21,7 +22,7 @@ bool Texture::Lock(void* out_pixels, int* out_pitch, AABB* lock_area) {
 		rect.h = lock_area->size.y;
 		lock_rect = &rect;
 	}
-	if (SDL_LockTexture(texture, lock_rect, &out_pixels, out_pitch) < 0) {
+	if (SDL_LockTexture(texture, lock_rect, out_pixels, out_pitch) < 0) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't lock texture: %s\n", SDL_GetError());
 		return false;
 	}
@@ -30,6 +31,21 @@ bool Texture::Lock(void* out_pixels, int* out_pitch, AABB* lock_area) {
 
 void Texture::Unlock() {
 	SDL_UnlockTexture(texture);
+}
+
+void Texture::Clear() {
+	void* pixels;
+	int pitch;
+	Lock(&pixels, &pitch);
+	int w, h;
+	SDL_QueryTexture(texture, NULL, NULL, &w, &h);
+	for (auto y = 0; y < h; ++y) {
+		auto dst = (std::uint32_t*)((std::uint8_t*)pixels + y * pitch);
+		for (auto x = 0; x < w; ++x) {
+			*dst++ = (0xFF000000 | (0 << 16) | (0 << 8) | 0); // Set texture to black.
+		}
+	}
+	Unlock();
 }
 
 } // namespace engine
--- Engine/engine/renderer/Texture.h
+++ Engine/engine/renderer/Texture.h
@@ -12,8 +12,9 @@ struct Texture {
 	SDL_Texture* operator=(SDL_Texture* texture);
 	operator SDL_Texture* () const;
 	operator bool() const;
-	bool Lock(void* out_pixels, int* out_pitch, AABB* lock_area = nullptr);
+	bool Lock(void** out_pixels, int* out_pitch, AABB* lock_area = nullptr);
 	void Unlock();
+	void Clear();
 	SDL_Texture* operator&() const;
 	SDL_Texture* texture = nullptr;
 };
