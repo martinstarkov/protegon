7f83afa58503cbec082f609b6941480e5bc1b947
Martin Starkov <emailregisterlogin@gmail.com>
2021-01-21 22:24:02 +0200

    Put physics into one file

---
+++ Engine/engine/physics/Physics.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Shape.h"
+#include "Body.h"
+#include "Circle.h"
+#include "Polygon.h"
+#include "Manifold.h"
+#include "collision/CollisionFunctions.h"
+#include "collision/CollisionManifold.h" // TEMPORARY.
+#include "RigidBody.h" // TEMPORARY.
