3d9ce7fb235fae709ef37c8e0030b35e9152be59
Martin Starkov <emailregisterlogin@gmail.com>
2021-01-23 03:24:16 +0200

    Add Body (temporary)

---
+++ Engine/engine/physics/Body.cpp
@@ -0,0 +1,31 @@
+#include "Body.h"
+
+#include "Shape.h"
+
+Body::Body(Shape* shape, const V2_double& position) : shape{ shape->Clone() }, position{ position } {
+	this->shape->body = this;
+	this->shape->Initialize();
+}
+
+Body::~Body() {
+    delete shape;
+    shape = nullptr;
+}
+void Body::ApplyForce(const V2_double& applied_force) {
+    force += applied_force;
+}
+void Body::ApplyImpulse(const V2_double& impulse, const V2_double& contact_vector) {
+    velocity += inverse_mass * impulse;
+    angular_velocity += inverse_inertia * contact_vector.CrossProduct(impulse);
+}
+void Body::SetStatic() {
+    inertia = 0.0;
+    inverse_inertia = 0.0;
+    mass = 0.0;
+    inverse_mass = 0.0;
+}
+
+void Body::SetOrientation(double radians) {
+	orientation = radians;
+	shape->SetOrientation(radians);
+}
---
+++ Engine/engine/physics/Body.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "utils/Vector2.h"
+
+class Shape;
+
+struct Body {
+    Body(Shape* shape, const V2_double& position);
+    ~Body();
+    void ApplyForce(const V2_double& applied_force);
+    void ApplyImpulse(const V2_double& impulse, const V2_double& contact_vector);
+    void SetStatic();
+
+    void SetOrientation(double radians);
+
+    V2_double position{ 0, 0 };
+    V2_double velocity{ 0, 0 };
+
+    double angular_velocity{ 0 };
+    double torque{ 0 };
+    double orientation{ 0 }; // radians
+
+    // TEMPORARY:
+    int name = 0;
+
+    V2_double force{ 0, 0 };
+
+    // Set by shape
+    double inertia{ 0 };  // moment of inertia
+    double inverse_inertia{ 0 }; // inverse inertia
+    double mass{ 0 };  // mass
+    double inverse_mass{ 0 }; // inverse masee
+
+    // http://gamedev.tutsplus.com/tutorials/implementation/how-to-create-a-custom-2d-physics-engine-friction-scene-and-jump-table/
+    double static_friction{ 0 };
+    double dynamic_friction{ 0 };
+    double restitution{ 0 };
+
+    // Shape interface
+    Shape* shape = nullptr;
+};
