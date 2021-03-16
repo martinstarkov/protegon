a24d275faf7d93cd38b50e7600e6501a6f5dd5d2
Martin Starkov <emailregisterlogin@gmail.com>
2021-03-16 16:38:30 +0200

    More clean up

--- Engine/engine/math/PerlinNoise.h
+++ Engine/engine/math/PerlinNoise.h
@@ -22,13 +22,14 @@ public:
 		size_mask_{ size_ - 1 } {
 		RNG<float> flt_rng{ seed };
 		RNG<std::uint32_t> uint_rng{ seed };
-		r.resize(size_, 0);
+		random_values.resize(size_, 0);
 		permutation_table_.resize(size_ * 2, 0);
-		// create an array of random values and initialize permutation table
+		// Create an array of random values and initialize permutation table.
 		for (unsigned k = 0; k < size_; ++k) {
-			r[k] = flt_rng();
+			random_values[k] = flt_rng();
 			permutation_table_[k] = k;
 		}
+		// Shuffle permutation table.
 		for (unsigned k = 0; k < size_; ++k) {
 			unsigned i = uint_rng() & size_mask_;
 			std::swap(permutation_table_[k], permutation_table_[i]);
@@ -36,81 +37,76 @@ public:
 		}
 	}
 	float Evaluate(const V2_float& p) {
-		auto i = Floor(p);
+		auto i{ Floor(p) };
 
-		auto rx0 = i.x & size_mask_;
-		auto rx1 = (rx0 + 1) & size_mask_;
-		auto ry0 = i.y & size_mask_;
-		auto ry1 = (ry0 + 1) & size_mask_;
+		auto rx0{ i.x & size_mask_ };
+		auto rx1{ (rx0 + 1) & size_mask_ };
+		auto ry0{ i.y & size_mask_ };
+		auto ry1{ (ry0 + 1) & size_mask_ };
 
-		// random values at the corners of the cell using permutation table
-		const auto& c00 = r[permutation_table_[permutation_table_[rx0] + ry0]];
-		const auto& c10 = r[permutation_table_[permutation_table_[rx1] + ry0]];
-		const auto& c01 = r[permutation_table_[permutation_table_[rx0] + ry1]];
-		const auto& c11 = r[permutation_table_[permutation_table_[rx1] + ry1]];
+		// Random values at the corners of the cells using the permutation table.
+		auto c00{ random_values[permutation_table_[permutation_table_[rx0] + ry0]] };
+		auto c10{ random_values[permutation_table_[permutation_table_[rx1] + ry0]] };
+		auto c01{ random_values[permutation_table_[permutation_table_[rx0] + ry1]] };
+		auto c11{ random_values[permutation_table_[permutation_table_[rx1] + ry1]] };
 
-		// remapping of coordinate fraction using the Smoothstep function 
-		auto s = SmoothStep(p - i);
+		// Remapping of coordinate fraction using the SmoothStep function.
+		auto s{ SmoothStep(p - i) };
 
-		// linearly interpolate values along the x axis
-		auto nx0 = math::Lerp(c00, c10, s.x);
-		auto nx1 = math::Lerp(c01, c11, s.x);
+		// Linearly interpolate values along the x-axis.
+		auto nx0{ math::Lerp(c00, c10, s.x) };
+		auto nx1{ math::Lerp(c01, c11, s.x) };
 
-		// linearly interpolate the nx0/nx1 along they y axis
+		// Linearly interpolate result along the y-axis.
 		return math::Lerp(nx0, nx1, s.y);
 	}
-	std::vector<float> GenerateNoiseMap(const V2_double& position, const V2_int& size, std::uint32_t octave, float bias) {
+	std::vector<float> GenerateNoiseMap(const V2_double& position, const V2_int& size, std::size_t octaves, float frequency_bias, float amplitude_bias = 0.5f) {
 
-		float amplitude = 1;
-		float maxPossibleNoiseVal = 0;
-		float amplitudeMult = 0.5f;//0.35;
-		unsigned numLayers = octave;//5;
+		auto frequency{ 0.05f };
+		auto amplitude{ 1.0f };
 
-		for (unsigned l = 0; l < numLayers; ++l) {
-			maxPossibleNoiseVal += amplitude;
-			amplitude *= amplitudeMult;
+		// Calculate maximum possible cumulative noise value.
+		auto max_noise{ 0.0f };
+		for (std::size_t octave{ 0 }; octave < octaves; ++octave) {
+			max_noise += amplitude;
+			amplitude *= amplitude_bias;
 		}
+		assert(max_noise >= 0 && "Noise cannot be standardized by negative values");
 
-		std::vector<float> noiseMap;
-		noiseMap.resize(size.x * size.y, 0.0f);
+		std::vector<float> noise_map;
+		std::size_t count{ static_cast<std::size_t>(size.x * size.y) };
+		noise_map.resize(count, 0.0f);
 
-		// FRACTAL NOISE
-		float frequency = 0.05f;//0.02f;
-		float frequencyMult = bias;//1.8;
-		float maxNoiseVal = 0;
-		for (int j = 0; j < size.y; ++j) {
-			for (int i = 0; i < size.x; ++i) {
-				V2_float pNoise{ position.x * size.x + i, position.y * size.y + j };
-				pNoise *= frequency;
+		for (auto j{ 0 }; j < size.y; ++j) {
+			for (auto i{ 0 }; i < size.x; ++i) {
+				V2_float pos{ position.x + i, position.y + j };
+				pos *= frequency;
+				// Reset amplitude.
 				amplitude = 1;
-				for (unsigned l = 0; l < numLayers; ++l) {
-					//LOG("pNoise: " << pNoise.x << "," << pNoise.y);
-					auto fractal_noise = Evaluate(pNoise);
-					auto value = fractal_noise * amplitude;
-					if (std::isnan(value) && l > 10) value = 0;
-					bool assertion = value >= 0;
-					if (!assertion) {
-						LOG("fractal_noise: " << value << ", pNoise: (" << pNoise.x << "," << pNoise.y << "), octave: " << l << ", amplitude: " << amplitude);
+				for (std::size_t octave{ 0 }; octave < octaves; ++octave) {
+					auto noise = Evaluate(pos) * amplitude;
+					if (std::isnan(noise) && octave > 10) noise = 0;
+					if (noise < 0) {
+						LOG("noise: " << noise << ", position: " << pos << ", octave: " << octave << ", amplitude: " << amplitude);
+						assert(!"Noise must be above or equal to 0");
 					}
-					assert(assertion && "fractal_noise must be above or equal to 0");
-					noiseMap[j * size.x + i] += value;
-					pNoise *= frequencyMult;
-					amplitude *= amplitudeMult;
+					noise_map[j * size.x + i] += noise;
+					pos *= frequency_bias;
+					amplitude *= amplitude_bias;
 				}
-				if (noiseMap[j * size.y + i] > maxNoiseVal) maxNoiseVal = noiseMap[j * size.x + i];
 			}
 		}
 		//for (unsigned i = 0; i < imageWidth * imageHeight; ++i) noiseMap[i] /= maxNoiseVal;
-		for (int i = 0; i < size.x * size.y; ++i) {
-			assert(noiseMap[i] >= 0 && "Noise must be above or equal to 0");
-			noiseMap[i] = noiseMap[i] / maxPossibleNoiseVal;
-			assert(noiseMap[i] >= 0 && "Noise divided by something which made it negative");
+		// Standarise all noise values between 0 and 1.
+		for (std::size_t i{ 0 }; i < count; ++i) {
+			assert(noise_map[i] >= 0 && "Noise must be above or equal to 0");
+			noise_map[i] /= max_noise;
 		}
-		return noiseMap;
+		return noise_map;
 	}
 	unsigned int size_;// = 256 * 2;
 	unsigned int size_mask_;// = size_ - 1;
-	std::vector<float> r; // size: size_
+	std::vector<float> random_values; // size: size_
 	std::vector<unsigned int> permutation_table_; // size_ * 2
 };
 
