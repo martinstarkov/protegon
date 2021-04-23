beb8b109b6d37ba45fa48b792b9af6a66b929601
Martin Starkov <emailregisterlogin@gmail.com>
2021-04-24 00:18:45 +0300

    Comment out temporarily

--- protegon/src/renderer/particles/ParticleManager.h
+++ protegon/src/renderer/particles/ParticleManager.h
@@ -1,28 +1,28 @@
-#pragma once
-
-#include <cstdlib>
-
-#include "ecs/ECS.h"
-#include "renderer/particles/Particle.h"
-
-namespace engine {
-
-class ParticleManager {
-public:
-	ParticleManager(std::size_t max_particles);
-	~ParticleManager() = default;
-	void Reset() {
-		particle_pool_.Clear();
-	}
-	void Refresh() {
-		particle_pool_.Refresh();
-	}
-	virtual void Emit(const Particle& properties);
-	virtual void Render();
-	virtual void Update();
-private:
-	std::size_t max_particles_{ 10 };
-	ecs::Manager particle_pool_;
-};
-
-} // namespace engine
+//#pragma once
+//
+//#include <cstdlib>
+//
+//#include "ecs/ECS.h"
+//#include "renderer/particles/Particle.h"
+//
+//namespace engine {
+//
+//class ParticleManager {
+//public:
+//	ParticleManager(std::size_t max_particles);
+//	~ParticleManager() = default;
+//	void Reset() {
+//		particle_pool_.Clear();
+//	}
+//	void Refresh() {
+//		particle_pool_.Refresh();
+//	}
+//	virtual void Emit(const Particle& properties);
+//	virtual void Render();
+//	virtual void Update();
+//private:
+//	std::size_t max_particles_{ 10 };
+//	ecs::Manager particle_pool_;
+//};
+//
+//} // namespace engine
