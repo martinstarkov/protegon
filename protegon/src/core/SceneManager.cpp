7d21e9c59fddf0389999c272d70d4b4a37a1764c
Martin Starkov <emailregisterlogin@gmail.com>
2021-05-02 02:53:55 +0300

    Begin adding scene manager

--- protegon/src/core/SceneManager.cpp
+++ protegon/src/core/SceneManager.cpp
@@ -1,1 +1,23 @@
 #include "SceneManager.h"
+
+namespace engine {
+
+void SceneManager::SetActiveScene(const char* scene_key, std::size_t display_index) {
+	auto key{ Hasher::HashCString(scene_key) };
+	auto scene{ GetScene(key) };
+	scene->SetDisplayIndex(display_index);
+	auto it{ display_scenes_.find(key) };
+	if (it != display_scenes_.end()) {
+		delete it->second;
+		it->second = scene;
+	} else {
+		display_scenes_.emplace(key, scene);
+	}
+
+}
+
+void SceneManager::UnloadScene(const char* scene_key) {
+
+}
+
+}
--- protegon/src/core/SceneManager.h
+++ protegon/src/core/SceneManager.h
@@ -1,29 +1,77 @@
-//#pragma once
-//
-//#include <unordered_map> // std::unordered_map
-//#include <cstdlib> // std::size_t
-//
-//#include "core/Scene.h"
-//
-//namespace engine {
-//
-//class SceneManager {
-//private:
-//public:
-//	static std::size_t CreateScene(const char* texture_key,
-//					const char* texture_path,
-//					std::size_t display_index = 0);
-//
-//	static Texture GetTexture(const char* texture_key);
-//
-//	// Return the location of a 4 byte integer value containg the RGBA32 color of the pixel on an SDL_Surface or SDL_Texture.
-//	static std::uint32_t& GetTexturePixel(void* pixels,
-//										  const int pitch,
-//										  const V2_int& position);
-//	static void Clear();
-//	static void Remove(const char* texture_key);
-//private:
-//	static std::unordered_map<std::size_t, Texture> texture_map_;
-//};
-//
-//} // namespace engine
+#pragma once
+
+#include <cstdlib> // std::size_t
+#include <unordered_map> // std::unordered_map
+
+#include "core/Scene.h"
+
+#include "math/Hasher.h"
+
+#include "utils/TypeTraits.h"
+
+namespace engine {
+
+class SceneManager {
+private:
+public:
+	template <typename T,
+		engine::type_traits::is_base_of<Scene, T>
+	>
+	static T& AddScene(const char* scene_key) {
+		static_assert(std::is_default_constructible_v<T>,
+					  "Cannot add scene to scene manager which is not default constructible");
+		auto scene{ new T{} };
+		auto key{ Hasher::HashCString(scene_key) };
+		auto it{ loaded_scenes_.find(key) };
+		if (it != loaded_scenes_.end()) {
+			delete it->second;
+			it->second = scene;
+		} else {
+			loaded_scenes_.emplace(key, scene);
+		}
+		return *scene;
+	}
+
+	template <typename T,
+		engine::type_traits::is_base_of<Scene, T>
+	>
+	static T& GetScene(const char* scene_key) {
+		return static_cast<T>(GetScene(Hasher::HashCString(scene_key)));
+	}
+
+	static void SetActiveScene(const char* scene_key, std::size_t display_index = 0);
+	static void UnloadScene(const char* scene_key);
+private:
+	static Scene* GetScene(std::size_t scene_key) {
+		auto it{ loaded_scenes_.find(scene_key) };
+		assert(it != loaded_scenes_.end() &&
+			   "Cannot retrieve scene which does not exist in scene manager");
+		return it->second;
+	}
+	static void EnterActiveScenes() {
+		for (auto& active_scene : loaded_scenes_) {
+			if (!active_scene.second->entered_) {
+				active_scene.second->Enter();
+				active_scene.second->entered_ = true;
+			}
+		}
+	}
+	static void UpdateActiveScenes() {
+		for (auto& active_scene : loaded_scenes_) {
+			active_scene.second->Update();
+		}
+	}
+	static void RenderActiveScenes() {
+		for (auto& active_scene : loaded_scenes_) {
+			active_scene.second->Render();
+		}
+	}
+	// Key: scene_key
+	// Value: scene pointer
+	static std::unordered_map<std::size_t, Scene*> loaded_scenes_;
+	// Key: display_index 
+	// Value: scene_key
+	static std::unordered_map<std::size_t, std::size_t> display_scenes_;
+};
+
+} // namespace engine
