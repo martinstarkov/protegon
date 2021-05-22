1fa408ee9c65f9794d595cb87e00c9e2d84250cc
Martin Starkov <emailregisterlogin@gmail.com>
2021-05-22 03:24:20 +0300

    Create Singleton.h

---
+++ protegon/src/utils/Singleton.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace engine {
+
+template <typename T>
+class Singleton {
+public:
+    static T& GetInstance() {
+        static T instance;
+        return instance;
+    }
+    Singleton() = default;
+    ~Singleton() = default;
+    Singleton(Singleton const&) = delete;
+    void operator=(Singleton const&) = delete;
+};
+
+} // namespace engine
