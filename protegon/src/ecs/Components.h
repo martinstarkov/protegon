bcd2851b241a5fcaa9c1a82a5c558ce40ea8ef24
Martin Starkov <emailregisterlogin@gmail.com>
2021-05-25 20:48:19 +0300

    Header refresh

--- protegon/src/ecs/Components.h
+++ protegon/src/ecs/Components.h
@@ -1,20 +1,18 @@
 #pragma once
 
-// TODO: Go over all components and make sure destructors are done properly (no memory leaks).
-
 #include "ecs/components/TransformComponent.h"
 #include "ecs/components/RigidBodyComponent.h"
 #include "ecs/components/ColorComponent.h"
 #include "ecs/components/ShapeComponent.h"
-#include "ecs/components/LifetimeComponent.h"
 #include "ecs/components/PlayerComponent.h"
-#include "ecs/components/TagComponent.h"
-#include "ecs/components/HitboxComponent.h"
 
-#include "ecs/components/SpriteComponent.h"
+//#include "ecs/components/HitboxComponent.h"
+//#include "ecs/components/TagComponent.h"
+//#include "ecs/components/LifetimeComponent.h"
+//#include "ecs/components/SpriteComponent.h"
 
-#include "ecs/components/CameraComponent.h"
-#include "ecs/components/InputComponent.h"
-#include "ecs/components/AnimationComponent.h"
-#include "ecs/components/SpriteSheetComponent.h"
-#include "ecs/components/ParticleComponent.h"
+//#include "ecs/components/CameraComponent.h"
+//#include "ecs/components/InputComponent.h"
+//#include "ecs/components/AnimationComponent.h"
+//#include "ecs/components/SpriteSheetComponent.h"
+//#include "ecs/components/ParticleComponent.h"
