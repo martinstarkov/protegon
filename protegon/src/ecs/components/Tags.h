1dba5a70dc27048eee3d9de2e40108e845ba482c
Martin Starkov <emailregisterlogin@gmail.com>
2021-05-29 19:42:49 +0300

    Consolidate some primitive components into one header

---
+++ protegon/src/ecs/components/Tags.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace engine {
+
+// Acts as a tag implying the entity is used for player-based systems.
+struct PlayerComponent {
+	PlayerComponent() = default;
+};
+
+// Acts as a tag implying the entity is used for input-based systems.
+struct InputComponent {
+	InputComponent() = default;
+};
+
+} // namespace engine
