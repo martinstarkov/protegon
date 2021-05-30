bdbb9f5e91d3424ada3b1237773659b7b3cbd1d8
Martin Starkov <emailregisterlogin@gmail.com>
2021-05-30 18:54:15 +0300

    Update SceneManager

--- protegon/src/core/SceneManager.cpp
+++ protegon/src/core/SceneManager.cpp
@@ -4,18 +4,15 @@
 
 namespace engine {
 
-std::unordered_set<std::size_t> SceneManager::queued_unload_scenes_;
-std::unordered_map<std::size_t, Scene*> SceneManager::loaded_scenes_;
-std::unordered_map<std::size_t, std::size_t> SceneManager::display_scenes_;
-
-void SceneManager::EnterScene(const char* scene_key, std::size_t display_index) {
+void SceneManager::EnterScene(const char* scene_key) {
+	auto& instance{ GetInstance() };
 	auto key{ Hasher::HashCString(scene_key) };
-	auto display_it{ display_scenes_.find(display_index) };
-	bool replacing{ display_it != display_scenes_.end() };
+	auto active_it{ instance.active_scenes_.find(key) };
+	bool replacing{ active_it != instance.active_scenes_.end() };
 	// Exit early if active scene is the scene to be replaced.
-	if (replacing && display_it->second == key) return;
-	auto scene_it{ loaded_scenes_.find(key) };
-	assert(scene_it != loaded_scenes_.end() &&
+	if (replacing && *active_it == key) return;
+	auto scene_it{ instance.loaded_scenes_.find(key) };
+	assert(scene_it != instance.loaded_scenes_.end() &&
 		   "Cannot set scene to active if it has not been added to the scene manager or has been unloaded");
 	auto scene{ scene_it->second };
 	assert(scene != nullptr &&
@@ -26,39 +23,44 @@ void SceneManager::EnterScene(const char* scene_key, std::size_t display_index) 
 	}
 	if (replacing) {
 		// Exit old scene.
-		auto old_scene{ GetScene(display_it->second) };
+		auto old_scene{ instance.GetScene(*active_it) };
 		if (old_scene != nullptr) {
 			old_scene->Exit();
 		}
-		display_it->second = key;
-	} else {
-		display_scenes_.emplace(display_index, key);
 	}
-
+	instance.active_scenes_.emplace(key);
 }
 
 void SceneManager::UnloadScene(const char* scene_key) {
-	queued_unload_scenes_.emplace(Hasher::HashCString(scene_key));
+	auto& instance{ GetInstance() };
+	instance.queued_unload_scenes_.emplace(Hasher::HashCString(scene_key));
 }
 
 void SceneManager::UnloadQueuedScenes() {
-	for (auto scene_key : queued_unload_scenes_) {
+	auto& instance{ GetInstance() };
+	for (auto scene_key : instance.queued_unload_scenes_) {
 		// Remove scene key from display map.
-		for (auto it{ display_scenes_.begin() }; it != display_scenes_.end(); ) {
-			if (it->second == scene_key) {
-				it = display_scenes_.erase(it);
+		for (auto it{ instance.active_scenes_.begin() }; it != instance.active_scenes_.end(); ) {
+			if (*it == scene_key) {
+				it = instance.active_scenes_.erase(it);
 			} else {
 				++it;
 			}
 		}
 		// Remove loaded scene.
-		auto it{ loaded_scenes_.find(scene_key) };
-		if (it != loaded_scenes_.end()) {
+		auto it{ instance.loaded_scenes_.find(scene_key) };
+		if (it != instance.loaded_scenes_.end()) {
 			delete it->second;
-			loaded_scenes_.erase(it);
+			instance.loaded_scenes_.erase(it);
 		}
 	}
-	queued_unload_scenes_.clear();
+	instance.queued_unload_scenes_.clear();
+}
+
+SceneManager::~SceneManager() {
+	for (auto [key, scene] : loaded_scenes_) {
+		delete scene;
+	}
 }
 
 }
--- protegon/src/core/SceneManager.h
+++ protegon/src/core/SceneManager.h
@@ -5,14 +5,13 @@
 #include <unordered_set> // std::unordered_set
 
 #include "core/Scene.h"
-
 #include "math/Hasher.h"
-
+#include "utils/Singleton.h"
 #include "utils/TypeTraits.h"
 
 namespace engine {
 
-class SceneManager {
+class SceneManager : public Singleton<SceneManager> {
 private:
 public:
 	// Load a scene into the scene manager. 
@@ -23,14 +22,15 @@ public:
 	static T& LoadScene(const char* scene_key) {
 		static_assert(std::is_default_constructible_v<T>,
 					  "Cannot add scene to scene manager which is not default constructible");
+		auto& instance{ GetInstance() };
 		auto scene{ new T{} };
 		auto key{ Hasher::HashCString(scene_key) };
-		auto it{ loaded_scenes_.find(key) };
-		if (it != loaded_scenes_.end()) {
+		auto it{ instance.loaded_scenes_.find(key) };
+		if (it != instance.loaded_scenes_.end()) {
 			delete it->second;
 			it->second = scene;
 		} else {
-			loaded_scenes_.emplace(key, scene);
+			instance.loaded_scenes_.emplace(key, scene);
 		}
 		return *scene;
 	}
@@ -39,23 +39,29 @@ public:
 	template <typename T,
 		engine::type_traits::is_base_of_e<Scene, T> = true>
 	static T& GetScene(const char* scene_key) {
+		auto& instance{ GetInstance() };
 		auto key{ Hasher::HashCString(scene_key) };
-		auto scene{ GetScene(scene_key) };
+		auto scene{ instance.GetScene(scene_key) };
 		assert(scene != nullptr &&
 			   "Cannot retrieve scene which does not exist in scene manager");
 		return static_cast<T>(it->second);
 	}
+
 	// Sets the scene to active for the given display_index.
-	static void EnterScene(const char* scene_key, std::size_t display_index = 0);
+	static void EnterScene(const char* scene_key);
+	
 	// Flags a scene to be unloaded after the game loop cycle.
 	static void UnloadScene(const char* scene_key);
-
-	// TODO: TEMPORARY, MOVE TO PRIVATE:
+	
+private:
+	friend class Engine;
+	friend class Singleton<SceneManager>;
 
 	static void EnterActiveScenes() {
-		for (auto& pair : display_scenes_) {
-			auto scene{ GetScene(pair.second) };
-			assert(scene != nullptr && 
+		auto& instance{ GetInstance() };
+		for (auto scene_id : instance.active_scenes_) {
+			auto scene{ instance.GetScene(scene_id) };
+			assert(scene != nullptr &&
 				   "Cannot enter active scene which has been deleted");
 			if (!scene->entered_) {
 				scene->Enter();
@@ -64,9 +70,10 @@ public:
 		}
 	}
 	static void UpdateActiveScenes() {
-		for (auto& pair : display_scenes_) {
-			auto scene{ GetScene(pair.second) };
-			assert(scene != nullptr && 
+		auto& instance{ GetInstance() };
+		for (auto scene_id : instance.active_scenes_) {
+			auto scene{ instance.GetScene(scene_id) };
+			assert(scene != nullptr &&
 				   "Cannot update active scene which has been deleted");
 			if (scene->entered_) {
 				scene->Update();
@@ -74,33 +81,37 @@ public:
 		}
 	}
 	static void RenderActiveScenes() {
-		for (auto& pair : display_scenes_) {
-			auto scene{ GetScene(pair.second) };
-			assert(scene != nullptr && 
+		auto& instance{ GetInstance() };
+		for (auto scene_id : instance.active_scenes_) {
+			auto scene{ instance.GetScene(scene_id) };
+			assert(scene != nullptr &&
 				   "Cannot render active scene which has been deleted");
 			if (scene->entered_) {
 				scene->Render();
 			}
 		}
 	}
+
 	static void UnloadQueuedScenes();
 
-private:
-	static Scene* GetScene(std::size_t scene_key) {
+	Scene* GetScene(std::size_t scene_key) {
 		auto it{ loaded_scenes_.find(scene_key) };
 		if (it != loaded_scenes_.end()) {
 			return it->second;
 		}
 		return nullptr;
 	}
+
+	SceneManager() = default;
+	~SceneManager();
+
 	// Element: scene_key
-	static std::unordered_set<std::size_t> queued_unload_scenes_;
+	std::unordered_set<std::size_t> queued_unload_scenes_;
 	// Key: scene_key
 	// Value: scene pointer
-	static std::unordered_map<std::size_t, Scene*> loaded_scenes_;
-	// Key: display_index
-	// Value: scene_key
-	static std::unordered_map<std::size_t, std::size_t> display_scenes_;
+	std::unordered_map<std::size_t, Scene*> loaded_scenes_;
+	// Element: scene_key
+	std::unordered_set<std::size_t> active_scenes_;
 };
 
 } // namespace engine
