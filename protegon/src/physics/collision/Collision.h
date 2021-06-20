63a87aec276cadc4ceb3e9af79f704f59779e3fa
Martin Starkov <emailregisterlogin@gmail.com>
2021-06-20 21:50:38 +0300

    Add collision resolution wrapper

--- protegon/src/physics/collision/Collision.h
+++ protegon/src/physics/collision/Collision.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "core/ECS.h"
+#include "components/TransformComponent.h"
+#include "components/HitboxComponent.h"
+#include "components/ShapeComponent.h"
 #include "physics/Transform.h"
 #include "physics/Manifold.h"
 #include "physics/shapes/Shape.h"
@@ -23,5 +27,28 @@ inline Manifold StaticCollisionCheck(const Transform& A,
 }
 
 } // namespace math
+
+inline void ResolveCollision(ecs::Entity& entity,
+							 ecs::Entity& entity2,
+							 TransformComponent& transform,
+							 TransformComponent& transform2,
+							 HitboxComponent& hitbox,
+							 HitboxComponent& hitbox2,
+							 ShapeComponent& shape,
+							 ShapeComponent& shape2) {
+	if (entity != entity2 && hitbox.CanCollideWith(entity2) && hitbox2.CanCollideWith(entity)) {
+		auto offset_transform{ transform };
+		offset_transform.transform.position += hitbox.offset;
+		auto offset_transform2{ transform2 };
+		offset_transform2.transform.position += hitbox2.offset;
+		auto manifold{ math::StaticCollisionCheck(offset_transform.transform,
+												  offset_transform2.transform,
+												  shape.shape,
+												  shape2.shape) };
+		if (manifold.CollisionOccured()) {
+			hitbox.Resolve(entity, entity2, manifold);
+		}
+	}
+}
 
 } // namespace ptgn
--- protegon/src/systems/StaticCollisionSystem.h
+++ protegon/src/systems/StaticCollisionSystem.h
@@ -18,19 +18,7 @@ struct StaticCollisionSystem {
 				HitboxComponent& hitbox2,
 				TransformComponent& transform2,
 				ShapeComponent& shape2) {
-			if (entity != entity2 && hitbox.CanCollideWith(entity2) && hitbox2.CanCollideWith(entity)) {
-				auto offset_transform{ transform };
-				offset_transform.transform.position += hitbox.offset;
-				auto offset_transform2{ transform2 };
-				offset_transform2.transform.position += hitbox2.offset;
-				auto manifold{ math::StaticCollisionCheck(offset_transform.transform,
-														  offset_transform2.transform,
-														  shape.shape,
-														  shape2.shape) };
-				if (manifold.CollisionOccured()) {
-					hitbox.Resolve(entity2, manifold);
-				}
-			}
+			ResolveCollision(entity, entity2, transform, transform2, hitbox, hitbox2, shape, shape2);
 		});
 	}
 };
