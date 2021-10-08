26dadef105c2b6e63e55baf620e42536b1f235c3
Martin <emailregisterlogin@gmail.com>
2021-10-09 00:40:30 +0100

    Create Defines.h

---
+++ src/utils/Defines.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "math/Vector2.h"
+
+namespace ptgn {
+
+namespace window {
+
+// Definition for a screen position that is centered on the user's monitor.
+inline constexpr V2_int CENTERED{ 805240832, 805240832 };
+
+} // namespace window
+
+} // namespace ptgn
