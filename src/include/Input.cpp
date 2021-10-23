5ae1986ad03275c5d8910ce76d8de2f58251ad63
Martin <emailregisterlogin@gmail.com>
2021-10-23 09:15:40 +0100

    Add user interface for input handling

---
+++ src/include/Input.cpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "event/InputHandler.h"
+
+namespace ptgn {
+
+namespace input {
+
+void Update() {
+    auto& input_handler{ services::GetInputHandler() };
+    input_handler.Update();
+}
+
+V2_int GetMouseScreenPosition() {
+    auto& input_handler{ services::GetInputHandler() };
+    return input_handler.GetMouseScreenPosition();
+}
+
+V2_int GetMouseWorldPosition() {
+    auto& input_handler{ services::GetInputHandler() };
+    return input_handler.GetMouseWorldPosition();
+}
+
+bool MousePressed(Mouse button) {
+    auto& input_handler{ services::GetInputHandler() };
+    return input_handler.MousePressed(button);
+}
+
+bool MouseReleased(Mouse button) {
+    auto& input_handler{ services::GetInputHandler() };
+    return input_handler.MouseReleased(button);
+}
+
+bool MouseDown(Mouse button) {
+    auto& input_handler{ services::GetInputHandler() };
+    return input_handler.MouseDown(button);
+}
+
+bool MouseUp(Mouse button) {
+    auto& input_handler{ services::GetInputHandler() };
+    return input_handler.MouseUp(button);
+}
+
+bool KeyPressed(Key key) {
+    auto& input_handler{ services::GetInputHandler() };
+    return input_handler.KeyPressed(key);
+}
+
+bool KeyReleased(Key key) {
+    auto& input_handler{ services::GetInputHandler() };
+    return input_handler.KeyReleased(key);
+}
+
+bool KeyDown(Key key) {
+    auto& input_handler{ services::GetInputHandler() };
+    return input_handler.KeyDown(key);
+}
+
+bool KeyUp(Key key) {
+    auto& input_handler{ services::GetInputHandler() };
+    return input_handler.KeyUp(key);
+}
+
+} // namespace input
+
+} // namespace ptgn
---
+++ src/include/Input.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "math/Vector2.h"
+#include "event/Inputs.h"
+
+namespace ptgn {
+
+namespace input {
+
+// Updates the input handler key and mouse state information.
+void Update();
+
+/*
+* @return The x and y positions of the mouse relative to the top left of the focused window.
+*/
+V2_int GetMouseScreenPosition();
+
+/*
+* @return The x and y positions of the mouse relative to the active world camera.
+*/
+V2_int GetMouseWorldPosition();
+
+/*
+* @return True if mouse button is pressed, false otherwise.
+*/
+bool MousePressed(Mouse button);
+
+/*
+* @return True if mouse button is released, false otherwise.
+*/
+bool MouseReleased(Mouse button);
+
+/*
+* @return True the first cycle a mouse button is pressed, false otherwise.
+*/
+bool MouseDown(Mouse button);
+
+/*
+* @return True the first cycle a mouse button is released, false otherwise.
+*/
+bool MouseUp(Mouse button);
+
+/*
+* @return True if given key is pressed, false otherwise.
+*/
+bool KeyPressed(Key key);
+
+/*
+* @return True if given key is released, false otherwise.
+*/
+bool KeyReleased(Key key);
+
+/*
+* @return True the first cycle a key is pressed, false otherwise.
+*/
+bool KeyDown(Key key);
+
+/*
+* @return True the first cycle a key is released, false otherwise.
+*/
+bool KeyUp(Key key);
+
+} // namespace input
+
+} // namespace ptgn
