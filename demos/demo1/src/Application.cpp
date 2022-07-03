1fce02ca8a2511acf00c4eda9f266bc3f919b680
Martin Starkov <emailregisterlogin@gmail.com>
2022-07-03 23:25:00 +0300

    Updates to events

--- demos/demo1/src/Application.cpp
+++ demos/demo1/src/Application.cpp
@@ -3,29 +3,38 @@
 
 using namespace ptgn;
 
-
-enum WindowEvent {
-	QUIT,
-	FOCUS
-};
-
-struct QuitEvent : public event::Event<QuitEvent> {
+struct QuitEvent : public event::Event<QuitEvent> {};
+struct CollisionEvent : public event::Event<CollisionEvent> {
+public:
+	CollisionEvent(int i) : i{ i } {}
+	int i;
 };
 
 int main(int c, char** v) {
 
 	event::Dispatcher dispatcher;
-	auto listener1 = dispatcher.Subscribe<QuitEvent>([](const event::Event<QuitEvent>& event) {
+	auto listener1 = dispatcher.Subscribe([](QuitEvent& event) {
 		std::cout << "WINDOW QUIT SOUND!" << std::endl;
 	});
-	auto listener2 = dispatcher.Subscribe<QuitEvent>([](const event::Event<QuitEvent>& event) {
+	auto listener2 = dispatcher.Subscribe([](const QuitEvent& event) {
 		std::cout << "WINDOW QUIT GRAPHIC!" << std::endl;
+	});
+	auto listener3 = dispatcher.Subscribe([](CollisionEvent& event) {
+		std::cout << "COLLISION1 OCCURED! -> " << event.i << std::endl;
+	});
+	auto listener4 = dispatcher.Subscribe([](CollisionEvent event) {
+		std::cout << "COLLISION2 OCCURED! -> " << event.i << std::endl;
+	});
+	auto listener5 = dispatcher.Subscribe([](QuitEvent& event) {
+		std::cout << "COLLISION3 OCCURED!" << std::endl;
 	});
 	QuitEvent window;
 	dispatcher.Post(window);
-	dispatcher.Unsubscribe<QuitEvent>(listener2);
-	//dispatcher.Unsubscribe<QuitEvent>(listener1);
+	//listener4.Unsubscribe<CollisionEvent>();
 	dispatcher.Post(window);
+	dispatcher.Post(CollisionEvent{ 1 });
+	listener4.Post(CollisionEvent{ 2 });
+	listener5.Post(CollisionEvent{ 3 });
 	std::cin.get();
 	return 0;
 }
