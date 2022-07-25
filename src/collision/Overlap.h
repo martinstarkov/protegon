5c2c6e711df953d34b906689102b40ac8453e210
Martin Starkov <emailregisterlogin@gmail.com>
2022-07-25 13:48:15 +0300

    Add collision include files

---
+++ src/collision/Overlap.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "overlap/OverlapAABBAABB.h"
+#include "overlap/OverlapCircleAABB.h"
+#include "overlap/OverlapCircleCircle.h"
+#include "overlap/OverlapLineAABB.h"
+#include "overlap/OverlapLineCircle.h"
+#include "overlap/OverlapLineLine.h"
+#include "overlap/OverlapPointAABB.h"
+#include "overlap/OverlapPointCircle.h"
