c49a48e3bdd75965c7bc33edf6652de15c1b10c5
Martin Starkov <emailregisterlogin@gmail.com>
2022-08-07 03:29:33 +0300

    Add vector demo (WIP)

---
+++ demos/vector/src/Application.cpp
@@ -0,0 +1,15 @@
+#include "utility/Log.h"
+#include "math/Vector2.h"
+
+#include <vector>
+
+using namespace ptgn;
+using namespace math;
+
+struct MyCustom {
+	int i{};
+};
+
+int main(int c, char** v) {
+	Vector2<int> t2;
+}
