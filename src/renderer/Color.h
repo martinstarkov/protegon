110fee85692acc5c7f4e518b8a41c63bd1dbe376
Martin Starkov <emailregisterlogin@gmail.com>
2022-08-07 03:31:26 +0300

    Default initialization

--- src/renderer/Color.h
+++ src/renderer/Color.h
@@ -83,9 +83,9 @@ public:
 
 	// Default color: black.
 
-	std::uint8_t r{ 0 };
-	std::uint8_t g{ 0 };
-	std::uint8_t b{ 0 };
+	std::uint8_t r{};
+	std::uint8_t g{};
+	std::uint8_t b{};
 	std::uint8_t a{ 255 };
 
 private:
