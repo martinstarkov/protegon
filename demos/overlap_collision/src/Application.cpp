0893d7353a77534032edf5e5ea62e49e76774563
Martin Starkov <emailregisterlogin@gmail.com>
2022-08-08 03:43:38 +0300

    Add shape types and update drawing

--- demos/overlap_collision/src/Application.cpp
+++ demos/overlap_collision/src/Application.cpp
@@ -34,194 +34,195 @@ public:
 
 		auto acolor1 = color1;
 		auto acolor2 = color2;
+
+		AABB aabb1{ position1, size1 };
+		AABB aabb2{ position2, size2 };
+		Circle circle1{ position1, radius1 };
+		Circle circle2{ position2, radius2 };
+		Line line1{ position1, position3 };
+		Line line2{ position2, position4 };
+		Capsule capsule1{ position1, position3, radius1 };
+		Capsule capsule2{ position2, position4, radius2 };
+
 		if (option == 0) {
-			position2 = mouse - size2 / 2;
-			if (overlap::AABBAABB(AABB{ position1, size1 }, AABB{ position2, size2 })) {
+			aabb2.position = mouse - aabb2.size / 2;
+			if (overlap::AABBAABB(aabb1, aabb2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Rectangle(position2, size2, acolor2);
-			draw::Rectangle(position1, size1, acolor1);
+			draw::AABB(aabb2, acolor2);
+			draw::AABB(aabb1, acolor1);
 		} else if (option == 1) {
-			position2 = mouse - size2 / 2;
-			if (overlap::CircleAABB(Circle{ position1, radius1 }, AABB{ position2, size2 })) {
+			aabb2.position = mouse - aabb2.size / 2;
+			if (overlap::CircleAABB(circle1, aabb2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Rectangle(position2, size2, acolor2);
-			draw::Circle(position1, radius1, acolor1);
+			draw::AABB(aabb2, acolor2);
+			draw::Circle(circle1, acolor1);
 		} else if (option == 2) {
-			if (overlap::CircleAABB(Circle{ position2, radius2 }, AABB{ position1, size1 })) {
+			if (overlap::CircleAABB(circle2, aabb1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Rectangle(position1, size1, acolor1);
-			draw::Circle(position2, radius2, acolor2);
+			draw::AABB(aabb1, acolor1);
+			draw::Circle(circle2, acolor2);
 		} else if (option == 3) {
-			if (overlap::CircleCircle(Circle{ position2, radius2 }, Circle{ position1, radius1 })) {
+			if (overlap::CircleCircle(circle2, circle1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Circle(position2, radius2, acolor2);
-			draw::Circle(position1, radius1, acolor1);
+			draw::Circle(circle2, acolor2);
+			draw::Circle(circle1, acolor1);
 		} else if (option == 4) {
-			position2 = mouse - size2 / 2;
-			if (overlap::LineAABB(Line{ position1, position3 }, AABB{ position2, size2 })) {
+			aabb2.position = mouse - aabb2.size / 2;
+			if (overlap::LineAABB(line1, aabb2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Line(position1, position3, acolor1);
-			draw::Rectangle(position2, size2, acolor2);
+			draw::Line(line1, acolor1);
+			draw::AABB(aabb2, acolor2);
 		} else if (option == 5) {
-			if (overlap::LineAABB(Line{ position2, position4 }, AABB{ position1, size1 })) {
+			if (overlap::LineAABB(line2, aabb1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Line(position2, position4, acolor2);
-			draw::Rectangle(position1, size1, acolor1);
+			draw::Line(line2, acolor2);
+			draw::AABB(aabb1, acolor1);
 		} else if (option == 6) {
-			if (overlap::LineCircle(Line{ position2, position4 }, Circle{ position1, radius1 })) {
+			if (overlap::LineCircle(line2, circle1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Line(position2, position4, acolor2);
-			draw::Circle(position1, radius1, acolor1);
+			draw::Line(line2, acolor2);
+			draw::Circle(circle1, acolor1);
 		} else if (option == 7) {
-			if (overlap::LineCircle(Line{ position1, position3 }, Circle{ position2, radius2 })) {
+			if (overlap::LineCircle(line1, circle2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Line(position1, position3, acolor1);
-			draw::Circle(position2, radius2, acolor2);
+			draw::Line(line1, acolor1);
+			draw::Circle(circle2, acolor2);
 		} else if (option == 8) {
-			if (overlap::LineLine(Line{ position1, position3 }, Line{ position2, position4 })) {
+			if (overlap::LineLine(line1, line2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Line(position1, position3, acolor1);
-			draw::Line(position2, position4, acolor2);
+			draw::Line(line1, acolor1);
+			draw::Line(line2, acolor2);
 		} else if (option == 9) {
-			if (overlap::PointAABB(Point{ position2 }, AABB{ position1, size1 })) {
+			if (overlap::PointAABB(position2, aabb1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Rectangle(position1, size1, acolor1);
+			draw::AABB(aabb1, acolor1);
 			draw::Point(position2, acolor2);
 		} else if (option == 10) {
-			position2 = mouse - size2 / 2;
-			if (overlap::PointAABB(Point{ position1 }, AABB{ position2, size2 })) {
+			aabb2.position = mouse - aabb2.size / 2;
+			if (overlap::PointAABB(position1, aabb2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Rectangle(position2, size2, acolor2);
+			draw::AABB(aabb2, acolor2);
 			draw::Point(position1, acolor1);
 		} else if (option == 11) {
-			if (overlap::PointCircle(Point{ position2 }, Circle{ position1, radius1 })) {
+			if (overlap::PointCircle(position2, circle1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Circle(position1, radius1, acolor1);
+			draw::Circle(circle1, acolor1);
 			draw::Point(position2, acolor2);
 		} else if (option == 12) {
-			if (overlap::PointCircle(Point{ position1 }, Circle{ position2, radius2 })) {
+			if (overlap::PointCircle(position1, circle2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Circle(position2, radius2, acolor2);
+			draw::Circle(circle2, acolor2);
 			draw::Point(position1, acolor1);
 		} else if (option == 13) {
-			if (overlap::PointLine(Point{ position1 }, Line{ position2, position4 })) {
+			if (overlap::PointLine(position1, line2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Line(position2, position4, acolor2);
+			draw::Line(line2, acolor2);
 			draw::Point(position1, acolor1);
 		} else if (option == 14) {
-			if (overlap::PointLine(Point{ position2 }, Line{ position1, position3 })) {
+			if (overlap::PointLine(position2, line1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Line(position1, position3, acolor1);
+			draw::Line(line1, acolor1);
 			draw::Point(position2, acolor2);
 		} else if (option == 15) {
-			if (overlap::PointPoint(Point{ position2 }, Point{ position1 })) {
+			if (overlap::PointPoint(position2, position1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
 			draw::Point(position1, acolor1);
 			draw::Point(position2, acolor2);
 		} else if (option == 16) {
-			if (overlap::CapsuleCapsule(Capsule{ position1, position3, radius1 },
-										Capsule{ position2, position4, radius2 })) {
+			if (overlap::CapsuleCapsule(capsule1, capsule2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Capsule(position1, position3, radius1, acolor1);
-			draw::Capsule(position2, position4, radius2, acolor2);
+			draw::Capsule(capsule1, acolor1);
+			draw::Capsule(capsule2, acolor2);
 		} else if (option == 17) {
-			if (overlap::CircleCapsule(Circle{ position1, radius1 },
-									   Capsule{ position2, position4, radius2 })) {
+			if (overlap::CircleCapsule(circle1, capsule2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Capsule(position2, position4, radius2, acolor2);
-			draw::Circle(position1, radius1, acolor1);
+			draw::Capsule(capsule2, acolor2);
+			draw::Circle(circle1, acolor1);
 		} else if (option == 18) {
-			if (overlap::CircleCapsule(Circle{ position2, radius2 },
-									   Capsule{ position1, position3, radius1 })) {
+			if (overlap::CircleCapsule(circle2, capsule1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Capsule(position1, position3, radius1, acolor1);
-			draw::Circle(position2, radius2, acolor2);
+			draw::Capsule(capsule1, acolor1);
+			draw::Circle(circle2, acolor2);
 		} else if (option == 19) {
-			position2 = mouse - size2 / 2;
-			if (overlap::CapsuleAABB(Capsule{ position1, position3, radius1 },
-									 AABB{ position2, size2 })) {
+			aabb2.position = mouse - aabb2.size / 2;
+			if (overlap::CapsuleAABB(capsule1, aabb2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Capsule(position1, position3, radius1, acolor1);
-			draw::Rectangle(position2, size2, acolor2);
+			draw::Capsule(capsule1, acolor1);
+			draw::AABB(aabb2, acolor2);
 		} else if (option == 20) {
-			if (overlap::CapsuleAABB(Capsule{ position2, position4, radius2 },
-									 AABB{ position1, size1 })) {
+			if (overlap::CapsuleAABB(capsule2, aabb1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Capsule(position2, position4, radius2, acolor2);
-			draw::Rectangle(position1, size1, acolor1);
+			draw::Capsule(capsule2, acolor2);
+			draw::AABB(aabb1, acolor1);
 		} else if (option == 21) {
-			if (overlap::LineCapsule(Line{ position1, position3 },
-									 Capsule{ position2, position4, radius2 })) {
+			if (overlap::LineCapsule(line1, capsule2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Capsule(position2, position4, radius2, acolor2);
-			draw::Line(position1, position3, acolor1);
+			draw::Capsule(capsule2, acolor2);
+			draw::Line(line1, acolor1);
 		} else if (option == 22) {
-			if (overlap::LineCapsule(Line{ position2, position4 },
-									 Capsule{ position1, position3, radius1 })) {
+			if (overlap::LineCapsule(line2, capsule1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Capsule(position1, position3, radius1, acolor1);
-			draw::Line(position2, position4, acolor2);
+			draw::Capsule(capsule1, acolor1);
+			draw::Line(line2, acolor2);
 		} else if (option == 23) {
-			if (overlap::PointCapsule(Point{ position1 },
-									  Capsule{ position2, position4, radius2 })) {
+			if (overlap::PointCapsule(position1, capsule2)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Capsule(position2, position4, radius2, acolor2);
+			draw::Capsule(capsule2, acolor2);
 			draw::Point(position1, acolor1);
 		} else if (option == 24) {
-			if (overlap::PointCapsule(Point{ position2 },
-									  Capsule{ position1, position3, radius1 })) {
+			if (overlap::PointCapsule(position2, capsule1)) {
 				acolor1 = color::RED;
 				acolor2 = color::RED;
 			}
-			draw::Capsule(position1, position3, radius1, acolor1);
+			draw::Capsule(capsule1, acolor1);
 			draw::Point(position2, acolor2);
 		}
 	}
