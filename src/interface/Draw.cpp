88a7c5dd5dbb6408dce33e6dc64eed4578ed1bb8
Martin Starkov <emailregisterlogin@gmail.com>
2022-08-08 03:42:50 +0300

    Make draw interface use types

--- src/interface/Draw.cpp
+++ src/interface/Draw.cpp
@@ -51,23 +51,21 @@ void SetColor(const Color& color) {
 	SDL_SetRenderDrawColor(Renderer::Get().renderer_, color.r, color.g, color.b, color.a);
 }
 
-void Point(const V2_int& point,
+void Point(const ptgn::Point<int>& p,
 		   const Color& color) {
 	assert(Exists() && "Cannot draw point with nonexistent renderer");
 	SetColor(color);
-	SDL_RenderDrawPoint(Renderer::Get().renderer_, point.x, point.y);
+	SDL_RenderDrawPoint(Renderer::Get().renderer_, p.x, p.y);
 }
 
-void Line(const V2_int& origin,
-		  const V2_int& destination,
+void Line(const ptgn::Line<int>& l,
 		  const Color& color) {
 	assert(Exists() && "Cannot draw line with nonexistent renderer");
 	SetColor(color);
-	SDL_RenderDrawLine(Renderer::Get().renderer_, origin.x, origin.y, destination.x, destination.y);
+	SDL_RenderDrawLine(Renderer::Get().renderer_, l.origin.x, l.origin.y, l.destination.x, l.destination.y);
 }
 
-void Circle(const V2_int& center,
-			const double radius,
+void Circle(const ptgn::Circle<int>& c,
 			const Color& color) {
 	// Source: https://rosettacode.org/wiki/Bitmap/Midpoint_circle_algorithm
 	// Source (used): https://www.geeksforgeeks.org/mid-point-circle-drawing-algorithm/
@@ -75,18 +73,17 @@ void Circle(const V2_int& center,
 
 	SetColor(color);
 
-	int r{ static_cast<int>(math::FastFloor(radius)) };
-	V2_int position{ r, 0 };
+	V2_int position{ c.radius, 0 };
 	auto renderer = Renderer::Get().renderer_;
-	    SDL_RenderDrawPoint(renderer,  position.x + center.x,  position.y + center.y);
+	    SDL_RenderDrawPoint(renderer, position.x + c.center.x,  position.y + c.center.y);
 
-	if (r > 0) {
-		SDL_RenderDrawPoint(renderer,  position.x + center.x, -position.y + center.y);
-		SDL_RenderDrawPoint(renderer,  position.y + center.x,  position.x + center.y);
-		SDL_RenderDrawPoint(renderer, -position.y + center.x,  position.x + center.y);
+	if (c.radius > 0) {
+		SDL_RenderDrawPoint(renderer,  position.x + c.center.x, -position.y + c.center.y);
+		SDL_RenderDrawPoint(renderer,  position.y + c.center.x,  position.x + c.center.y);
+		SDL_RenderDrawPoint(renderer, -position.y + c.center.x,  position.x + c.center.y);
 	}
 
-	int P{ 1 - r };
+	int P{ 1 - c.radius };
 
 	while (position.x > position.y) {
 		position.y++;
@@ -102,56 +99,52 @@ void Circle(const V2_int& center,
 			break;
 		}
 
-		SDL_RenderDrawPoint(renderer,  position.x + center.x,  position.y + center.y);
-		SDL_RenderDrawPoint(renderer, -position.x + center.x,  position.y + center.y);
-		SDL_RenderDrawPoint(renderer,  position.x + center.x, -position.y + center.y);
-		SDL_RenderDrawPoint(renderer, -position.x + center.x, -position.y + center.y);
+		SDL_RenderDrawPoint(renderer,  position.x + c.center.x,  position.y + c.center.y);
+		SDL_RenderDrawPoint(renderer, -position.x + c.center.x,  position.y + c.center.y);
+		SDL_RenderDrawPoint(renderer,  position.x + c.center.x, -position.y + c.center.y);
+		SDL_RenderDrawPoint(renderer, -position.x + c.center.x, -position.y + c.center.y);
 
 		if (position.x != position.y) {
-			SDL_RenderDrawPoint(renderer,  position.y + center.x,  position.x + center.y);
-			SDL_RenderDrawPoint(renderer, -position.y + center.x,  position.x + center.y);
-			SDL_RenderDrawPoint(renderer,  position.y + center.x, -position.x + center.y);
-			SDL_RenderDrawPoint(renderer, -position.y + center.x, -position.x + center.y);
+			SDL_RenderDrawPoint(renderer,  position.y + c.center.x,  position.x + c.center.y);
+			SDL_RenderDrawPoint(renderer, -position.y + c.center.x,  position.x + c.center.y);
+			SDL_RenderDrawPoint(renderer,  position.y + c.center.x, -position.x + c.center.y);
+			SDL_RenderDrawPoint(renderer, -position.y + c.center.x, -position.x + c.center.y);
 		}
 	}
 }
 
 // Draws filled circle to the screen.
-void SolidCircle(const V2_int& center,
-				 const double radius,
+void SolidCircle(const ptgn::Circle<int>& c,
 				 const Color& color) {
 	assert(Exists() && "Cannot draw solid circle with nonexistent renderer");
 	SetColor(color);
-	int r{ static_cast<int>(math::FastFloor(radius)) };
-	int r_squared{ r * r };
+	int radius2{ c.RadiusSquared() };
 	auto renderer = Renderer::Get().renderer_;
-	for (auto y{ -r }; y <= r; ++y) {
+	for (auto y{ -c.radius }; y <= c.radius; ++y) {
 		auto y_squared{ y * y };
-		auto y_position{ center.y + y };
-		for (auto x{ -r }; x <= r; ++x) {
-			if (x * x + y_squared <= r_squared) {
-				SDL_RenderDrawPoint(renderer, center.x + x, y_position);
+		auto y_position{ c.center.y + y };
+		for (auto x{ -c.radius }; x <= c.radius; ++x) {
+			if (x * x + y_squared <= radius2) {
+				SDL_RenderDrawPoint(renderer, c.center.x + x, y_position);
 			}
 		}
 	}
 }
 
-void Arc(const V2_int& center,
-		 const double radius,
+// Source: https://github.com/martinstarkov/SDL2_gfx/blob/master/SDL2_gfxPrimitives.c#L1183
+// With some modifications.
+void Arc(const ptgn::Circle<int>& arc_circle,
 		 double start_angle,
 		 double end_angle,
 		 const Color& color) {
-	// Source: https://github.com/martinstarkov/SDL2_gfx/blob/master/SDL2_gfxPrimitives.c#L1183
-	// With some modifications.
 	assert(Exists() && "Cannot draw line with nonexistent renderer");
 	using S = double;
 	auto renderer{ Renderer::Get().renderer_ };
-	int r{ static_cast<int>(math::FastFloor(radius)) };
 	int cx = 0;
-	int cy = r;
-	int df = 1 - r;
+	int cy = arc_circle.radius;
+	int df = 1 - arc_circle.radius;
 	int d_e = 3;
-	int d_se = -2 * r + 5;
+	int d_se = -2 * arc_circle.radius + 5;
 	int xpcx, xmcx, xpcy, xmcy;
 	int ypcy, ymcy, ypcx, ymcx;
 	std::uint8_t drawoct;
@@ -161,7 +154,7 @@ void Arc(const V2_int& center,
 	/*
 	* Sanity check radius
 	*/
-	if (r < 0) {
+	if (arc_circle.radius < 0) {
 		return;
 	}
 
@@ -170,8 +163,8 @@ void Arc(const V2_int& center,
 	/*
 	* Special case for radius=0 - draw a point
 	*/
-	if (r == 0) {
-		SDL_RenderDrawPoint(renderer, center.x, center.y);
+	if (arc_circle.radius == 0) {
+		SDL_RenderDrawPoint(renderer, arc_circle.center.x, arc_circle.center.y);
 		return;
 	}
 
@@ -230,7 +223,7 @@ void Arc(const V2_int& center,
 					temp = -sin(dstart * math::pi<double> / 180.);
 					break;
 			}
-			temp *= r;
+			temp *= arc_circle.radius;
 			stopval_start = (int)temp;
 
 			/*
@@ -263,7 +256,7 @@ void Arc(const V2_int& center,
 					temp = -sin(dend * math::pi<double> / 180);
 					break;
 			}
-			temp *= r;
+			temp *= arc_circle.radius;
 			stopval_end = (int)temp;
 
 			/* and whether to draw in this octant initially */
@@ -290,11 +283,11 @@ void Arc(const V2_int& center,
 	* Draw arc
 	*/
 	do {
-		ypcy = center.y + cy;
-		ymcy = center.y - cy;
+		ypcy = arc_circle.center.y + cy;
+		ymcy = arc_circle.center.y - cy;
 		if (cx > 0) {
-			xpcx = center.x + cx;
-			xmcx = center.x - cx;
+			xpcx = arc_circle.center.x + cx;
+			xmcx = arc_circle.center.x - cx;
 
 			/* always check if we're drawing a certain octant before adding a pixel to that octant. */
 			if (drawoct & 4)  SDL_RenderDrawPoint(renderer, xmcx, ypcy);
@@ -302,22 +295,22 @@ void Arc(const V2_int& center,
 			if (drawoct & 32) SDL_RenderDrawPoint(renderer, xmcx, ymcy);
 			if (drawoct & 64) SDL_RenderDrawPoint(renderer, xpcx, ymcy);
 		} else {
-			if (drawoct & 96) SDL_RenderDrawPoint(renderer, center.x, ymcy);
-			if (drawoct & 6)  SDL_RenderDrawPoint(renderer, center.x, ypcy);
+			if (drawoct & 96) SDL_RenderDrawPoint(renderer, arc_circle.center.x, ymcy);
+			if (drawoct & 6)  SDL_RenderDrawPoint(renderer, arc_circle.center.x, ypcy);
 		}
 
-		xpcy = center.x + cy;
-		xmcy = center.x - cy;
+		xpcy = arc_circle.center.x + cy;
+		xmcy = arc_circle.center.x - cy;
 		if (cx > 0 && cx != cy) {
-			ypcx = center.y + cx;
-			ymcx = center.y - cx;
+			ypcx = arc_circle.center.y + cx;
+			ymcx = arc_circle.center.y - cx;
 			if (drawoct & 8)   SDL_RenderDrawPoint(renderer, xmcy, ypcx);
 			if (drawoct & 1)   SDL_RenderDrawPoint(renderer, xpcy, ypcx);
 			if (drawoct & 16)  SDL_RenderDrawPoint(renderer, xmcy, ymcx);
 			if (drawoct & 128) SDL_RenderDrawPoint(renderer, xpcy, ymcx);
 		} else if (cx == 0) {
-			if (drawoct & 24)  SDL_RenderDrawPoint(renderer, xmcy, center.y);
-			if (drawoct & 129) SDL_RenderDrawPoint(renderer, xpcy, center.y);
+			if (drawoct & 24)  SDL_RenderDrawPoint(renderer, xmcy, arc_circle.center.y);
+			if (drawoct & 129) SDL_RenderDrawPoint(renderer, xpcy, arc_circle.center.y);
 		}
 
 		/*
@@ -351,81 +344,72 @@ void Arc(const V2_int& center,
 	} while (cx <= cy);
 }
 
-void Capsule(const V2_int& origin,
-			 const V2_int& destination,
-			 const double radius,
+void Capsule(const ptgn::Capsule<int>& c,
 			 const Color& color,
 			 bool draw_centerline) {
 	assert(Exists() && "Cannot draw capsule with nonexistent renderer");
 	SetColor(color);
 	auto renderer = Renderer::Get().renderer_;
-	V2_int direction{ destination - origin };
+	V2_int direction{ c.Direction() };
 	const double angle{ math::ToDeg(math::ClampAngle2Pi(direction.Angle() + math::half_pi<double>)) };
-	int r{ static_cast<int>(math::FastFloor(radius)) };
 	const int dir2{ direction.MagnitudeSquared() };
 	V2_int tangent_r;
 	if (dir2 == 0) {
-		Circle(origin, radius, color);
+		Circle({ c.origin, c.radius }, color);
 		return;
 	} else {
-		tangent_r = static_cast<V2_int>(FastFloor(direction.Tangent() / std::sqrt(dir2) * r));
+		tangent_r = static_cast<V2_int>(FastFloor(direction.Tangent() / std::sqrt(dir2) * c.radius));
 	}
 	// Draw centerline.
 	if (draw_centerline)
-		SDL_RenderDrawLine(renderer, origin.x, origin.y, destination.x, destination.y);
+		SDL_RenderDrawLine(renderer, c.origin.x, c.origin.y, c.destination.x, c.destination.y);
 	// Draw edge lines.
-	SDL_RenderDrawLine(renderer, origin.x + tangent_r.x, origin.y + tangent_r.y, 
-					             destination.x + tangent_r.x, destination.y + tangent_r.y);
-	SDL_RenderDrawLine(renderer, origin.x - tangent_r.x, origin.y - tangent_r.y,
-					   destination.x - tangent_r.x, destination.y - tangent_r.y);
+	SDL_RenderDrawLine(renderer, c.origin.x + tangent_r.x, c.origin.y + tangent_r.y,
+					             c.destination.x + tangent_r.x, c.destination.y + tangent_r.y);
+	SDL_RenderDrawLine(renderer, c.origin.x - tangent_r.x, c.origin.y - tangent_r.y,
+					             c.destination.x - tangent_r.x, c.destination.y - tangent_r.y);
 	// Draw edge arcs.
-	Arc(origin, r, angle, angle + 180.0, color);
-	Arc(destination, r, angle + 180.0, angle, color);
+	Arc({ c.origin, c.radius }, angle, angle + 180.0, color);
+	Arc({ c.destination, c.radius }, angle + 180.0, angle, color);
 
 }
 
-void Rectangle(const V2_int& top_left,
-               const V2_int& size,
-               const Color& color) {
-	assert(Exists() && "Cannot draw rectangle with nonexistent renderer");
+void AABB(const ptgn::AABB<int>& a,
+          const Color& color) {
+	assert(Exists() && "Cannot draw aabb with nonexistent renderer");
 	SetColor(color);
-	SDL_Rect rect{ top_left.x, top_left.y, size.x, size.y };
+	SDL_Rect rect{ a.position.x, a.position.y, a.size.x, a.size.y };
 	SDL_RenderDrawRect(Renderer::Get().renderer_, &rect);
 }
 
-void SolidRectangle(const V2_int& top_left,
-			        const V2_int& size,
+void SolidAABB(const ptgn::AABB<int>& a,
 			        const Color& color) {
-	assert(Exists() && "Cannot draw solid rectangle with nonexistent renderer");
+	assert(Exists() && "Cannot draw solid aabb with nonexistent renderer");
 	SetColor(color);
-	SDL_Rect rect{ top_left.x, top_left.y, size.x, size.y };
+	SDL_Rect rect{ a.position.x, a.position.y, a.size.x, a.size.y };
 	SDL_RenderFillRect(Renderer::Get().renderer_, &rect);
 }
 
 void Texture(const char* texture_key,
-			 const V2_int& texture_position,
-			 const V2_int& texture_size,
-			 const V2_int& source_position,
-			 const V2_int& source_size) {
+			 const ptgn::AABB<int>& texture,
+			 const ptgn::AABB<int>& source) {
 	assert(Exists() && "Cannot draw texture with nonexistent renderer");
 	const auto& texture_manager{ manager::Get<TextureManager>() };
 	const auto key{ math::Hash(texture_key) };
 	assert(texture_manager.Has(key) && "Cannot draw nonexistent texture");
-	SDL_Rect* source{ NULL };
+	SDL_Rect* src{ NULL };
 	SDL_Rect source_rectangle;
-	if (!source_size.IsZero()) {
-		source_rectangle = { source_position.x, source_position.y, source_size.x, source_size.y };
-		source = &source_rectangle;
+	if (!source.size.IsZero()) {
+		source_rectangle = { source.position.x, source.position.y, source.size.x, source.size.y };
+		src = &source_rectangle;
 	}
-	SDL_Rect destination{ texture_position.x, texture_position.y, texture_size.x, texture_size.y };
-	SDL_RenderCopy(Renderer::Get().renderer_, *texture_manager.Get(key), source, &destination);
+	SDL_Rect destination{ texture.position.x, texture.position.y, texture.size.x, texture.size.y };
+	SDL_RenderCopy(Renderer::Get().renderer_, *texture_manager.Get(key), src, &destination);
 }
 
 void Texture(const char* texture_key,
-			 const V2_int& texture_position,
-			 const V2_int& texture_size,
-			 const V2_int& source_position,
-			 const V2_int& source_size,
+			 const ptgn::AABB<int>& texture,
+			 const ptgn::AABB<int>& source,
 			 const V2_int* center_of_rotation,
 			 const double angle,
 			 Flip flip) {
@@ -434,37 +418,35 @@ void Texture(const char* texture_key,
 	const auto key{ math::Hash(texture_key) };
 	assert(texture_manager.Has(key) && "Cannot draw nonexistent texture");
 	auto renderer = Renderer::Get().renderer_;
-	SDL_Rect* source{ NULL };
+	SDL_Rect* src{ NULL };
 	SDL_Rect source_rectangle;
-	if (!source_position.IsZero() && !source_size.IsZero()) {
-		source_rectangle = { source_position.x, source_position.y, source_size.x, source_size.y };
-		source = &source_rectangle;
+	if (!source.position.IsZero() && !source.size.IsZero()) {
+		source_rectangle = { source.position.x, source.position.y, source.size.x, source.size.y };
+		src = &source_rectangle;
 	}
-	SDL_Rect destination{ texture_position.x, texture_position.y, texture_size.x, texture_size.y };
+	SDL_Rect destination{ texture.position.x, texture.position.y, texture.size.x, texture.size.y };
 	if (center_of_rotation != nullptr) {
 		SDL_Point center{ center_of_rotation->x, center_of_rotation->y };
-		SDL_RenderCopyEx(renderer, *texture_manager.Get(key), source, &destination,
+		SDL_RenderCopyEx(renderer, *texture_manager.Get(key), src, &destination,
 						 angle, &center, static_cast<SDL_RendererFlip>(static_cast<int>(flip)));
 	} else {
-		SDL_RenderCopyEx(renderer, *texture_manager.Get(key), source, &destination,
+		SDL_RenderCopyEx(renderer, *texture_manager.Get(key), src, &destination,
 						 angle, NULL, static_cast<SDL_RendererFlip>(static_cast<int>(flip)));
 	}
 }
 
 void Text(const ptgn::Text& text,
-		  const V2_int& text_position,
-		  const V2_int& text_size) {
+		  const ptgn::AABB<int>& box) {
 	assert(Exists() && "Cannot draw text with nonexistent renderer");
 	const auto& texture_manager{ manager::Get<TextureManager>() };
 	const auto texture_key{ text.GetTextureKey() };
 	assert(texture_manager.Has(texture_key) && "Cannot draw nonexistent text");
-	SDL_Rect destination{ text_position.x, text_position.y, text_size.x, text_size.y };
+	SDL_Rect destination{ box.position.x, box.position.y, box.size.x, box.size.y };
 	SDL_RenderCopy(Renderer::Get().renderer_, *texture_manager.Get(texture_key), NULL, &destination);
 }
 
 void Text(const char* text_key,
-		  const V2_int& text_position,
-		  const V2_int& text_size) {
+		  const ptgn::AABB<int>& box) {
 	assert(Exists() && "Cannot draw text with nonexistent renderer");
 	const auto& texture_manager{ manager::Get<TextureManager>() };
 	const auto& text_manager{ manager::Get<TextManager>() };
@@ -472,7 +454,7 @@ void Text(const char* text_key,
 	assert(text_manager.Has(key) && "Cannot draw text which has not been loaded into the text manager");
 	const auto texture_key{ text_manager.Get(key)->GetTextureKey() };
 	assert(texture_manager.Has(texture_key) && "Cannot draw nonexistent text");
-	SDL_Rect destination{ text_position.x, text_position.y, text_size.x, text_size.y };
+	SDL_Rect destination{ box.position.x, box.position.y, box.size.x, box.size.y };
 	SDL_RenderCopy(Renderer::Get().renderer_, *texture_manager.Get(texture_key), NULL, &destination);
 }
 
@@ -480,10 +462,9 @@ void TemporaryText(const char* texture_key,
 				   const char* font_key,
 				   const char* text_content,
 				   const Color& text_color,
-				   const V2_int& text_position,
-				   const V2_int& text_size) {
+				   const ptgn::AABB<int>& box) {
 	ptgn::Text text{ texture_key, font_key, text_content, text_color };
-	Text(text, text_position, text_size);
+	Text(text, box);
 }
 
 } // namespace draw
--- src/interface/Draw.h
+++ src/interface/Draw.h
@@ -5,6 +5,7 @@
 #include "math/Vector2.h"
 #include "renderer/Colors.h"
 #include "renderer/Flip.h"
+#include "physics/Types.h"
 
 namespace ptgn {
 
@@ -12,6 +13,7 @@ class Text;
 
 namespace draw {
 
+// TODO: Fix flags to be an enum with | operators.
 void Init(int index = 0, std::uint32_t flags = 0);
 
 void Release();
@@ -24,76 +26,55 @@ void Clear();
 
 void SetColor(const Color& color = color::DEFAULT);
 
-void Point(const V2_int& point,
+void Point(const ptgn::Point<int>& p,
            const Color& color = color::DEFAULT);
 
-void Line(const V2_int& origin,
-          const V2_int& destination,
+void Line(const ptgn::Line<int>& l,
           const Color& color = color::DEFAULT);
 
-void Circle(const V2_int& center,
-            const double radius,
+void Circle(const ptgn::Circle<int>& c,
             const Color& color = color::DEFAULT);
 
-void SolidCircle(const V2_int& center,
-                 const double radius,
+void SolidCircle(const ptgn::Circle<int>& c,
                  const Color& color = color::DEFAULT);
 
-/*
-* @param center Center point of the arc circle.
-* @param direction Vector from center to point at which arc circle starts.
-* @param radius Radius of the arc circle.
-* @param angle Angle in radians by which arc is rotated (positive is clockwise, negative is anti-clockwise).
-* @param color Color of arc.
-* @param resolution Number of individual points with which to draw the arc, must be at least 2.
-*/
-void Arc(const V2_int& center,
-         const double radius,
+void Arc(const ptgn::Circle<int>& arc_circle,
          double start_angle,
          double end_angle,
          const Color& color = color::DEFAULT);
 
-void Capsule(const V2_int& origin,
-             const V2_int& destination,
-             const double radius,
+void Capsule(const ptgn::Capsule<int>& c,
              const Color& color = color::DEFAULT,
              bool draw_centerline = false);
 
-void Rectangle(const V2_int& top_left,
-               const V2_int& size,
+// AABB = Axis-aligned bounding-box
+void AABB(const ptgn::AABB<int>& a,
                const Color& color = color::DEFAULT);
 
-void SolidRectangle(const V2_int& top_left,
-                    const V2_int& size,
+void SolidAABB(const ptgn::AABB<int>& a,
                     const Color& color = color::DEFAULT);
 
 // Draws the texture to the screen.
 void Texture(const char* texture_key,
-             const V2_int& texture_position,
-             const V2_int& texture_size,
-             const V2_int& source_position = {},
-             const V2_int& source_size = {});
+             const ptgn::AABB<int>& texture,
+             const ptgn::AABB<int>& source = {});
 
 // Draws the texture to the screen. Allows for rotation and texture flipping.
 // Set center_of_rotation to nullptr if center of rotation is desired to be the center of the texture.
 void Texture(const char* texture_key,
-             const V2_int& texture_position,
-             const V2_int& texture_size,
-             const V2_int& source_position,
-             const V2_int& source_size,
+             const ptgn::AABB<int>& texture,
+             const ptgn::AABB<int>& source,
              const V2_int* center_of_rotation,
              const double angle,
              Flip flip);
 
 // Draws text to the screen.
 void Text(const ptgn::Text& text,
-          const V2_int& text_position,
-          const V2_int& text_size);
+          const ptgn::AABB<int>& box);
 
 // Draws text to the screen.
 void Text(const char* text_key,
-          const V2_int& text_position,
-          const V2_int& text_size);
+          const ptgn::AABB<int>& box);
 
 // Draws text to the screen which is reallocated each frame.
 // Good for changing text such as counters.
@@ -101,8 +82,7 @@ void TemporaryText(const char* texture_key,
                    const char* font_key,
                    const char* text_content,
                    const Color& text_color,
-                   const V2_int& text_position,
-                   const V2_int& text_size);
+                   const ptgn::AABB<int>& box);
 
 } // namespace draw
 
