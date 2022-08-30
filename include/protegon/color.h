653253fab425bb675cf16e350e550ef009d5ffa5
Martin Starkov <emailregisterlogin@gmail.com>
2022-08-30 11:06:03 +0300

    Add basic color struct

--- include/protegon/color.h
+++ include/protegon/color.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#include <cstdint>
+
 namespace ptgn {
 
-
+struct Color {
+	std::uint8_t r{ 0 };
+	std::uint8_t g{ 0 };
+	std::uint8_t b{ 0 };
+	std::uint8_t a{ 0 };
+};
 
 } // namespace ptgn
