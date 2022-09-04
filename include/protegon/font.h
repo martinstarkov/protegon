87047e6d789eaeabe1483dad2266415e9a7f8a69
Martin Starkov <emailregisterlogin@gmail.com>
2022-09-04 14:18:03 +0300

    Update font.h

--- include/protegon/font.h
+++ include/protegon/font.h
@@ -1,7 +1,21 @@
 #pragma once
 
+#include <cstdint> // std::int32_t, std::uint32_t
+
+struct _TTF_Font;
+using TTF_Font = _TTF_Font;
+
 namespace ptgn {
 
-struct Font {};
+class Font {
+public:
+	Font() = delete;
+	Font(const char* font_path, std::uint32_t point_size, std::uint32_t index = 0);
+	~Font();
+	std::int32_t GetHeight() const;
+	bool IsValid() const;
+private:
+	TTF_Font* font_{ nullptr };
+};
 
 } // namespace ptgn
