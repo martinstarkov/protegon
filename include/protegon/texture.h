9adc0426394def8117dfb8a4884aa15dd7dc462b
Martin Starkov <emailregisterlogin@gmail.com>
2022-09-04 14:19:52 +0300

    Add textures

--- include/protegon/texture.h
+++ include/protegon/texture.h
@@ -1,7 +1,26 @@
 #pragma once
 
+#include "rectangle.h"
+#include "vector2.h"
+
+struct SDL_Texture;
+struct SDL_Surface;
+
 namespace ptgn {
 
-struct Texture {};
+class Texture {
+public:
+	Texture() = delete;
+	Texture(const char* texture_path);
+	~Texture();
+	bool IsValid() const;
+	void Draw(const Rectangle<int>& texture,
+			  const Rectangle<int>& source = {}) const;
+private:
+	friend class Text;
+	// Takes ownership of surface pointer.
+	Texture(SDL_Surface* surface);
+	SDL_Texture* texture_{ nullptr };
+};
 
 } // namespace ptgn
