5cf60a07636f9cd0959a3dd750455c56a1f1a70e
Martin Starkov <emailregisterlogin@gmail.com>
2022-09-06 10:36:14 +0300

    Add timer

---
+++ src/utility/timer.cpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "protegon/timer.h"
+
+namespace ptgn {
+
+Timer::Timer(bool start) {
+    if (start) {
+        Start();
+    }
+}
+
+void Timer::Start() {
+    start_time_ = std::chrono::steady_clock::now();
+    running_ = true;
+}
+
+void Timer::Stop() {
+    stop_time_ = std::chrono::steady_clock::now();
+    running_ = false;
+}
+
+bool Timer::IsRunning() const {
+    return running_;
+}
+
+void Timer::Reset() {
+    stop_time_ = start_time_;
+    running_ = false;
+}
+
+} // namespace ptgn
