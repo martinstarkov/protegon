195e4c6042e29c8ec1d97692aec2bd9d1a2c68cd
Martin Starkov <emailregisterlogin@gmail.com>
2022-09-07 15:44:52 +0300

    Add remaining overlap functions

--- src/math/overlap.cpp
+++ src/math/overlap.cpp
@@ -1,5 +1,9 @@
 #include "protegon/overlap.h"
 
+#include <limits> // std::numeric_limits
+
+#include "protegon/math.h"
+
 namespace ptgn {
 
 namespace impl {
@@ -10,10 +14,18 @@ float SquareDistancePointRectangle(const Point<float>& a,
     const V2_float max{ b.Max() };
     for (std::size_t i{ 0 }; i < 2; ++i) {
         const float v{ a[i] };
-        if (v < b.position[i]) dist2 += (b.position[i] - v) * (b.position[i] - v);
-        if (v > max[i])        dist2 += (v - max[i]) * (v - max[i]);
+        if (v < b.position[i])
+            dist2 += (b.position[i] - v) * (b.position[i] - v);
+        if (v > max[i])
+            dist2 += (v - max[i]) * (v - max[i]);
     }
     return dist2;
+}
+
+float ParallelogramArea(const Point<float>& a,
+                        const Point<float>& b,
+                        const Point<float>& c) {
+    return (a - c).Cross(b - c);
 }
 
 } // namespace impl
@@ -22,14 +34,11 @@ namespace overlap {
 
 bool RectangleRectangle(const Rectangle<float>& a,
                         const Rectangle<float>& b) {
-    if (a.position.x + a.size.x < b.position.x || a.position.x > b.position.x + b.size.x) return false;
-    if (a.position.y + a.size.y < b.position.y || a.position.y > b.position.y + b.size.y) return false;
+    if (a.position.x + a.size.x < b.position.x || a.position.x > b.position.x + b.size.x)
+        return false;
+    if (a.position.y + a.size.y < b.position.y || a.position.y > b.position.y + b.size.y)
+        return false;
     return true;
-}
-
-bool PointRectangle(const Point<float>& a,
-                    const Rectangle<float>& b) {
-    return RectangleRectangle({ a, {} }, b);
 }
 
 bool CircleCircle(const Circle<float>& a,
@@ -41,16 +50,145 @@ bool CircleCircle(const Circle<float>& a,
     return dist2 < rad_sum2 || NearlyEqual(dist2, rad_sum2);
 }
 
-bool PointCircle(const Point<float>& a,
-                 const Circle<float>& b) {
-    return CircleCircle({ a, 0.0f }, b);
-}
-
 bool CircleRectangle(const Circle<float>& a,
                      const Rectangle<float>& b) {
     const float dist2{ impl::SquareDistancePointRectangle(a.center, b) };
     const float rad2{ a.radius * a.radius };
     return dist2 < rad2 || NearlyEqual(dist2, rad2);
+}
+
+bool PointRectangle(const Point<float>& a,
+                    const Rectangle<float>& b) {
+    return RectangleRectangle({ a, {} }, b);
+}
+
+bool PointCircle(const Point<float>& a,
+                 const Circle<float>& b) {
+    return CircleCircle({ a, 0.0f }, b);
+}
+
+bool PointLine(const Point<float>& a,
+               const Line<float>& b) {
+    const V2_float ab{ b.Direction() };
+    const V2_float ac{ a - b.a };
+    const V2_float bc{ a - b.b };
+
+    const float e{ ac.Dot(ab) };
+    // Handle cases where c projects outside ab
+    if (e < 0 || NearlyEqual(e, 0.0f))
+        return NearlyEqual(ac.x, 0.0f) && NearlyEqual(ac.y, 0.0f);
+
+    const float f{ ab.Dot(ab) };
+    if (e > f || NearlyEqual(e, f))
+        return NearlyEqual(bc.x, 0.0f) && NearlyEqual(bc.y, 0.0f);
+
+    // Handle cases where c projects onto ab
+    return NearlyEqual(ac.Dot(ac) * f, e * e);
+}
+
+bool LineRectangle(const Line<float>& a,
+                   const Rectangle<float>& b) {
+    const Vector2<float> e{ b.size };
+    const Vector2<float> d{ a.Direction() };
+    const Vector2<float> m{ a.a + a.b - 2 * b.position - b.size };
+
+    // Try world coordinate axes as separating axes
+    float adx{ FastAbs(d.x) };
+    if (FastAbs(m.x) > e.x + adx)
+        return false;
+
+    float ady{ FastAbs(d.y) };
+    if (FastAbs(m.y) > e.y + ady)
+        return false;
+
+    // Add in an epsilon term to counteract arithmetic errors when segment is
+    // (near) parallel to a coordinate axis (see text for detail)
+    adx += epsilon<float>;
+    ady += epsilon<float>;
+
+    // Try cross products of segment direction vector with coordinate axes
+    if (FastAbs(m.x * d.y - m.y * d.x) > e.x * ady + e.y * adx) 
+        return false;
+
+    // No separating axis found; segment must be overlapping AABB
+    return true;
+
+    // Alternative method:
+    // Source: https://en.wikipedia.org/wiki/Cohen%E2%80%93Sutherland_algorithm
+}
+
+bool LineCircle(const Line<float>& a,
+                const Circle<float>& b) {
+    // If the line is inside the circle entirely, exit early.
+    if (PointCircle(a.a, b) && 
+        PointCircle(a.b, b))
+        return true;
+
+    float min_dist2{ std::numeric_limits<float>::infinity() };
+    const float rad2{ b.radius * b.radius };
+
+    // O is the circle center, P is the line origin, Q is the line destination.
+    const V2_float OP{ a.a - b.center };
+    const V2_float OQ{ a.b - b.center };
+    const V2_float PQ{ a.Direction() };
+
+    const float OP_dist2{ OP.Dot(OP) };
+    const float OQ_dist2{ OQ.Dot(OQ) };
+    const float max_dist2{ std::max(OP_dist2, OQ_dist2) };
+
+    if (OP.Dot(-PQ) > 0.0f && OQ.Dot(PQ) > 0.0f) {
+        const float triangle_area{ FastAbs(impl::ParallelogramArea(b.center, a.a, a.b)) / 2.0f };
+        min_dist2 = 4.0f * triangle_area * triangle_area / PQ.Dot(PQ);
+    } else {
+        min_dist2 = std::min(OP_dist2, OQ_dist2);
+    }
+    return (min_dist2 < rad2 || NearlyEqual(min_dist2, rad2)) &&
+           (max_dist2 > rad2 || NearlyEqual(max_dist2, rad2));
+}
+
+bool LineLine(const Line<float>& a,
+              const Line<float>& b) {
+    // Sign of areas correspond to which side of ab points c and d are
+    const float a1{ impl::ParallelogramArea(a.a, a.b, b.b) }; // Compute winding of abd (+ or -)
+    const float a2{ impl::ParallelogramArea(a.a, a.b, b.a) }; // To intersect, must have sign opposite of a1
+    // If c and d are on different sides of ab, areas have different signs
+    bool polarity_diff{ false };
+    bool collinear{ false };
+    // Same as above but for floating points.
+    polarity_diff = a1 * a2 < 0.0f;
+    collinear = NearlyEqual(a1, 0.0f) || NearlyEqual(a2, 0.0f);
+    // For integral implementation use this instead of the above two lines:
+    //if constexpr (std::is_signed_v<T> && std::is_integral_v<T>) {
+    //	// Second part for difference in polarity.
+    //	polarity_diff = (a1 ^ a2) < 0;
+    //	collinear = a1 == 0 || a2 == 0;
+    //}
+    if (!collinear && polarity_diff) {
+        // Compute signs for a and b with respect to segment cd
+        const float a3{ impl::ParallelogramArea(b.a, b.b, a.a) }; // Compute winding of cda (+ or -)
+        // Since area is constant a1 - a2 = a3 - a4, or a4 = a3 + a2 - a1
+        // const T a4 = math::ParallelogramArea(c, d, b); // Must have opposite sign of a3
+        const float a4{ a3 + a2 - a1 };
+        // Points a and b on different sides of cd if areas have different signs
+        // Segments intersect if true.
+        bool intersect{ false };
+        // If either is 0, the line is intersecting with the straight edge of the other line.
+        // (i.e. corners with angles).
+        // Check if a3 and a4 signs are different.
+        intersect = a3 * a4 < 0.0f;
+        collinear = NearlyEqual(a3, 0.0f) || NearlyEqual(a4, 0.0f);
+        // For integral implementation use this instead of the above two lines:
+        //if constexpr (std::is_signed_v<T> && std::is_integral_v<T>) {
+        //	intersect = (a3 ^ a4) < 0;
+        //	collinear = a3 == 0 || a4 == 0;
+        //}
+        if (intersect) return true;
+    }
+    return collinear &&
+        (PointLine(b.b, a) ||
+         PointLine(b.a, a) ||
+         PointLine(a.a, b) ||
+         PointLine(a.b, b));
 }
 
 } // namespace overlap
