b53602cbcd62feb7d22d48ffc485b0f2b48e415b
Martin <emailregisterlogin@gmail.com>
2023-06-30 02:37:42 +0300

    Fixed debug operators

--- include/protegon/debug.h
+++ include/protegon/debug.h
@@ -10,20 +10,19 @@ namespace debug {
 namespace impl {
 
 struct Allocations {
-	std::uint64_t total_allocated_{ 0 };
-	std::uint64_t total_freed_{ 0 };
+	static std::uint64_t total_allocated_;
+	static std::uint64_t total_freed_;
 };
 
-static Allocations allocations;
 
 // Notifies AllocationMetrics that an allocation has been made.
 inline void Allocation(const std::size_t& size) {
-	allocations.total_allocated_ += static_cast<std::uint64_t>(size);
+	Allocations::total_allocated_ += static_cast<std::uint64_t>(size);
 }
 
 // Notifies AllocationMetrics that a deallocation has been made.
 inline void Deallocation(const std::size_t& size) {
-	allocations.total_freed_ += static_cast<std::uint64_t>(size);
+	Allocations::total_freed_ += static_cast<std::uint64_t>(size);
 }
 
 } // namespace impl
@@ -32,39 +31,19 @@ inline void Deallocation(const std::size_t& size) {
 * @return Current heap allocated memory in bytes.
 */
 inline std::uint64_t CurrentUsage() {
-	return impl::allocations.total_allocated_ - impl::allocations.total_freed_;
+	return impl::Allocations::total_allocated_ - impl::Allocations::total_freed_;
 }
 
 /*
 inline std::uint64_t Allocated() {
-	return impl::allocations.total_allocated_;
+	return impl::Allocations::total_allocated_;
 }
 
 inline std::uint64_t Freed() {
-	return impl::allocations.total_freed_;
+	return impl::Allocations::total_freed_;
 }
 */
 
 } // namespace debug
 
 } // namespace ptgn
-
-void* operator new(std::size_t size) {
-	ptgn::debug::impl::Allocation(size);
-	return std::malloc(size);
-}
-
-void* operator new[](std::size_t size) {
-	ptgn::debug::impl::Allocation(size);
-	return std::malloc(size);
-}
-
-void operator delete(void* memory, std::size_t size) {
-	ptgn::debug::impl::Deallocation(size);
-	std::free(memory);
-}
-
-void operator delete[](void* memory, std::size_t size) {
-	ptgn::debug::impl::Deallocation(size);
-	std::free(memory);
-}
--- src/utility/debug.cpp
+++ src/utility/debug.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include "protegon/debug.h"
 
 namespace ptgn {
@@ -8,10 +6,31 @@ namespace debug {
 
 namespace impl {
 
-Allocations allocations{};
+std::uint64_t Allocations::total_allocated_{ 0 };
+std::uint64_t Allocations::total_freed_{ 0 };
 
 } // namespace impl
 
 } // namespace debug
 
 } // namespace ptgn
+
+void* operator new(std::size_t size) {
+	ptgn::debug::impl::Allocation(size);
+	return std::malloc(size);
+}
+
+void* operator new[](std::size_t size) {
+	ptgn::debug::impl::Allocation(size);
+	return std::malloc(size);
+}
+
+void operator delete(void* memory, std::size_t size) {
+	ptgn::debug::impl::Deallocation(size);
+	std::free(memory);
+}
+
+void operator delete[](void* memory, std::size_t size) {
+	ptgn::debug::impl::Deallocation(size);
+	std::free(memory);
+}
