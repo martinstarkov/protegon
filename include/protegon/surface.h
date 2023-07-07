9f13a007f99abfeedd3eb967c4151d56470b7428
Nicole Burg <emailregisterlogin@gmail.com>
2023-07-07 16:38:10 -0400

    Add surfaces for reading image pixel data

---
+++ include/protegon/surface.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <memory> // std::shared_ptr
+
+#include "rectangle.h"
+#include "vector2.h"
+#include "color.h"
+
+struct SDL_Surface;
+
+namespace ptgn {
+
+class Surface {
+public:
+	Surface(const char* image_path);
+	~Surface() = default;
+	Surface(const Surface&) = default;
+	Surface& operator=(const Surface&) = default;
+	Surface(Surface&&) = default;
+	Surface& operator=(Surface&&) = default;
+	bool IsValid() const;
+	V2_int GetSize() const;
+	template <typename T>
+	void ForEachPixel(T function) {
+		Lock();
+
+		const V2_int size{ GetSize() };
+
+		for (int i = 0; i < size.x; i++)
+			for (int j = 0; j < size.y; j++) {
+				V2_int coordinate{ i, j };
+				function(coordinate, GetColor(coordinate));
+			}
+
+		Unlock();
+	}
+	Color GetColor(const V2_int& coordinate);
+private:
+	void Lock();
+	void Unlock();
+	Color GetColorData(std::uint32_t pixel_data);
+	std::uint32_t GetPixelData(const V2_int& coordinate);
+	Surface() = default;
+	std::shared_ptr<SDL_Surface> surface_{ nullptr };
+};
+
+} // namespace ptgn
---
+++ src/renderer/surface.cpp
@@ -0,0 +1,86 @@
+#include "protegon/surface.h"
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+#include <cassert> // assert
+
+#include "protegon/log.h"
+#include "utility/file.h"
+
+namespace ptgn {
+
+Surface::Surface(const char* image_path) {
+	assert(image_path != "" && "Empty image path?");
+	assert(FileExists(image_path) && "Nonexistent image path?");
+	auto surface{ IMG_Load(image_path) };
+	if (surface == nullptr) {
+		PrintLine(IMG_GetError());
+		assert(!"Failed to create surface from image path");
+	}
+	surface_ = std::shared_ptr<SDL_Surface>(surface, SDL_FreeSurface);
+	if (!IsValid()) {
+		PrintLine(SDL_GetError());
+		assert(!"Failed to create surface");
+	}
+}
+
+void Surface::Lock() {
+	int success = SDL_LockSurface(surface_.get());
+	if (success != 0) {
+		PrintLine(SDL_GetError());
+		assert(!"Failed to lock surface");
+	}
+}
+
+V2_int Surface::GetSize() const {
+	return { surface_.get()->w, surface_.get()->h };
+}
+
+void Surface::Unlock() {
+	SDL_UnlockSurface(surface_.get());
+}
+
+Color Surface::GetColor(const V2_int& coordinate) {
+	return GetColorData(GetPixelData(coordinate));
+}
+
+Color Surface::GetColorData(std::uint32_t pixel_data) {
+	Color color;
+	SDL_GetRGB(pixel_data, surface_.get()->format, &color.r, &color.g, &color.b);
+	return color;
+}
+
+std::uint32_t Surface::GetPixelData(const V2_int& coordinate) {
+	int bpp = surface_.get()->format->BytesPerPixel;
+	std::uint8_t* p = (std::uint8_t*)surface_.get()->pixels + coordinate.y * surface_.get()->pitch + coordinate.x * bpp;
+	assert(p != nullptr && "Failed to find coordinate pixel data");
+	switch (bpp) {
+		case 1:
+			return *p;
+			break;
+		case 2:
+			return *(std::uint16_t*)p;
+			break;
+
+		case 3:
+			if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
+				return p[0] << 16 | p[1] << 8 | p[2];
+			else
+				return p[0] | p[1] << 8 | p[2] << 16;
+			break;
+
+		case 4:
+			return *(uint32_t*)p;
+			break;
+
+		default:
+			return 0;       /* shouldn't happen, but avoids warnings */
+	}
+}
+
+bool Surface::IsValid() const {
+	return surface_ != nullptr;
+}
+
+} // namespace ptgn
