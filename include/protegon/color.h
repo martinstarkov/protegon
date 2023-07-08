b1825d8d56ddedc066e8e494f7a8c07f84ac20bb
Nicole Burg <emailregisterlogin@gmail.com>
2023-07-08 19:38:48 -0400

    Add more colors and format

--- include/protegon/color.h
+++ include/protegon/color.h
@@ -35,27 +35,28 @@ inline bool operator!=(const Color& lhs, const Color& rhs) {
 
 namespace color {
 
-inline constexpr Color BLACK{ 0, 0, 0, 255 };
-inline constexpr Color WHITE{ 255, 255, 255, 255 };
-inline constexpr Color TRANSPARENT{ 0, 0, 0, 0 };
-inline constexpr Color RED{ 255, 0, 0, 255 };
-inline constexpr Color DARK_RED{ 128, 0, 0, 255 };
-inline constexpr Color ORANGE{ 255, 165, 0, 255 };
-inline constexpr Color YELLOW{ 255, 255, 0, 255 };
-inline constexpr Color GOLD{ 255, 215, 0, 255 };
-inline constexpr Color GREEN{ 0, 128, 0, 255 };
-inline constexpr Color LIME{ 0, 255, 0, 255 };
-inline constexpr Color DARK_GREEN{ 0, 100, 0, 255 };
-inline constexpr Color BLUE{ 0, 0, 255, 255 };
-inline constexpr Color DARK_BLUE{ 0, 0, 128, 255 };
-inline constexpr Color CYAN{ 0, 255, 255, 255 };
-inline constexpr Color TEAL{ 0, 128, 128, 255 };
-inline constexpr Color MAGENTA{ 255, 0, 255, 255 };
-inline constexpr Color PURPLE{ 128, 0, 128, 255 };
-inline constexpr Color PINK{ 255, 192, 203, 255 };
-inline constexpr Color GREY{ 128, 128, 128, 255 };
-inline constexpr Color LIGHT_GREY{ 83, 83, 83, 255 };
-inline constexpr Color SILVER{ 192, 192, 192, 255 };
+inline constexpr Color BLACK         { 0, 0, 0, 255 };
+inline constexpr Color WHITE         { 255, 255, 255, 255 };
+inline constexpr Color TRANSPARENT   { 0, 0, 0, 0 };
+inline constexpr Color RED           { 255, 0, 0, 255 };
+inline constexpr Color DARK_RED      { 128, 0, 0, 255 };
+inline constexpr Color ORANGE        { 255, 165, 0, 255 };
+inline constexpr Color YELLOW        { 255, 255, 0, 255 };
+inline constexpr Color GOLD          { 255, 215, 0, 255 };
+inline constexpr Color GREEN         { 0, 128, 0, 255 };
+inline constexpr Color LIME          { 0, 255, 0, 255 };
+inline constexpr Color DARK_GREEN    { 0, 100, 0, 255 };
+inline constexpr Color BLUE          { 0, 0, 255, 255 };
+inline constexpr Color DARK_BLUE     { 0, 0, 128, 255 };
+inline constexpr Color CYAN          { 0, 255, 255, 255 };
+inline constexpr Color TEAL          { 0, 128, 128, 255 };
+inline constexpr Color MAGENTA       { 255, 0, 255, 255 };
+inline constexpr Color PURPLE        { 128, 0, 128, 255 };
+inline constexpr Color PINK          { 255, 192, 203, 255 };
+inline constexpr Color LIGHT_PINK    { 255, 128, 255, 255 };
+inline constexpr Color GREY          { 128, 128, 128, 255 };
+inline constexpr Color LIGHT_GREY    { 83, 83, 83, 255 };
+inline constexpr Color SILVER        { 192, 192, 192, 255 };
 
 } // namespace color
 
