e3bb52fdf56f0b004c55852c0fd7f004704d6014
Martin <emailregisterlogin@gmail.com>
2023-07-20 18:45:28 -0400

    Mac OS download

--- scripts/setup.py
+++ scripts/setup.py
@@ -14,15 +14,18 @@ from pathlib import Path
 class PythonConfiguration:
     @classmethod
     def Validate(cls):
+        print("Validating python...")
         if not cls.__ValidatePython():
+            print("Failed to validate python. Exiting early.")
             return # cannot validate further
 
-        for packageName in ["requests"]:
+        packages = ["requests"]
+        for packageName in packages:
             if not cls.__ValidatePackage(packageName):
                 return # cannot validate further
 
     @classmethod
-    def __ValidatePython(cls, versionMajor = 3, versionMinor = 3):
+    def __ValidatePython(cls, versionMajor = 3, versionMinor = 6):
         if sys.version is not None:
             print("Python version {0:d}.{1:d}.{2:d} detected.".format( \
                 sys.version_info.major, sys.version_info.minor, sys.version_info.micro))
@@ -47,8 +50,8 @@ class PythonConfiguration:
                 return False
             permissionGranted = (reply == 'y')
         
-        print(f"Installing {packageName} module...")
-        subprocess.check_call(['python', '-m', 'pip', 'install', packageName])
+        print("Installing {packageName} module...".format(packageName=packageName))
+        subprocess.check_call(['python3', '-m', 'pip', 'install', packageName])
 
         return cls.__ValidatePackage(packageName)
 
@@ -64,18 +67,18 @@ def DownloadFile(url, filepath):
                 DownloadFile(url_option, filepath)
                 return
             except urllib.error.URLError as e:
-                print(f"URL Error encountered: {e.reason}. Proceeding with backup...\n\n")
+                print("URL Error encountered: {reason}. Proceeding with backup...{newline}".format(reason=e.reason, newline="\n\n"))
                 os.remove(filepath)
                 pass
             except urllib.error.HTTPError as e:
-                print(f"HTTP Error  encountered: {e.code}. Proceeding with backup...\n\n")
+                print("HTTP Error  encountered: {code}. Proceeding with backup...{newline}".format(code=e.code, newline="\n\n"))
                 os.remove(filepath)
                 pass
             except:
-                print(f"Something went wrong. Proceeding with backup...\n\n")
+                print("Something went wrong. Proceeding with backup...{newline}".format(newline="\n\n"))
                 os.remove(filepath)
                 pass
-        raise ValueError(f"Failed to download {filepath}")
+        raise ValueError("Failed to download {filepath}".format(filepath=filepath))
     if not(type(url) is str):
         raise TypeError("Argument 'url' must be of type list or string")
 
@@ -99,7 +102,7 @@ def UnzipFile(filepath, deleteZipFile=True):
         for name in zipFileFolder.namelist():
             zipFileContent[name] = zipFileFolder.getinfo(name).file_size
         for zippedFileName, zippedFileSize in zipFileContent.items():
-            UnzippedFilePath = os.path.abspath(f"{zipFileLocation}/{zippedFileName}")
+            UnzippedFilePath = os.path.abspath("{zipFileLocation}/{zippedFileName}".format(zipFileLocation=zipFileLocation, zippedFileName=zippedFileName))
             os.makedirs(os.path.dirname(UnzippedFilePath), exist_ok=True)
             if not os.path.isfile(UnzippedFilePath):
                 zipFileFolder.extract(zippedFileName, path=zipFileLocation, pwd=None)
@@ -107,24 +110,40 @@ def UnzipFile(filepath, deleteZipFile=True):
     if deleteZipFile:
         os.remove(zipFilePath) # delete zip file
 
-def InstallZip(directory, zip_url, version, name):
+def InstallZip(directory, url, version, name):
     permissionGranted = False
     while not permissionGranted:
         reply = str(input("{1:s} not found. Would you like to download {1:s} {0:s}? [Y/N]: ".format(version, name))).lower().strip()[:1]
         if reply == 'n':
             return False
         permissionGranted = (reply == 'y')
-
-    path = f"{directory}/{name}-{version}-windows.zip"
-    print("Downloading {0:s} to {1:s}".format(zip_url, path))
-    DownloadFile(zip_url, path)
-    print("Extracting", path)
-    UnzipFile(path, deleteZipFile=True)
-    print(f"{name} {version} has been downloaded to '{directory}'")
+    
+    if (platform == "win32"):
+        path = "{directory}/{name}-{version}.zip".format(directory=directory, name=name, version=version)
+        print("Downloading {0:s} to {1:s}".format(url, path))
+        DownloadFile(url, path)
+        print("Extracting", path)
+        UnzipFile(path, deleteZipFile=True)
+        print("{name} {version} has been downloaded to '{directory}'".format(name=name, version=version, directory=directory))
+    elif (platform == "darwin"):
+        deleteZipFile = True
+        path = "{directory}/{name}-{version}.dmg".format(directory=directory, name=name, version=version)
+        print("Downloading {0:s} to {1:s}".format(url, path))
+        DownloadFile(url, path)
+        print("Extracting", path)
+        os.system("hdiutil attach {path} -quiet".format(path=path))
+        os.system("cp -r '/Volumes/{name}/{name}.framework' {directory}".format(name=name, directory=directory))
+        os.system("hdiutil detach /Volumes/{name} -quiet".format(name=name))
+        if deleteZipFile:
+            os.remove(os.path.abspath(path)) # delete zip file
     return True
 
-def ValidateDependency(path, directory, zip, version, name):
-    return path.exists() or InstallZip(directory, zip, version, name)
+def ValidateDependency(path, directory, url, version, name):
+    if (platform == "win32"):
+        return path.exists() or InstallZip(directory, url, version, name)
+    elif (platform == "darwin"):
+        path = Path("{directory}/{name}.framework".format(directory=directory, name=name))
+        return path.exists() or InstallZip(directory, url, version, name)
 
 def ValidateDependencies():
     directory = os.path.dirname(__file__)
@@ -135,16 +154,23 @@ def ValidateDependencies():
     SDL2_image_version = "2.6.3"
     SDL2_ttf_version = "2.20.2"
     SDL2_mixer_version = "2.6.3"
-    
-    SDL2_zip = f"https://github.com/libsdl-org/SDL/releases/download/release-2.26.5/SDL2-devel-{SDL2_version}-VC.zip"
-    SDL2_image_zip = f"https://github.com/libsdl-org/SDL_image/releases/download/release-2.6.3/SDL2_image-devel-{SDL2_image_version}-VC.zip"
-    SDL2_ttf_zip = f"https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.20.2/SDL2_ttf-devel-{SDL2_ttf_version}-VC.zip"
-    SDL2_mixer_zip = f"https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.6.3/SDL2_mixer-devel-{SDL2_mixer_version}-VC.zip"
 
-    SDL2_path = Path(f"{vendor_directory}/SDL2-{SDL2_version}/include/SDL.h");
-    SDL2_image_path = Path(f"{vendor_directory}/SDL2_image-{SDL2_image_version}/include/SDL_image.h");
-    SDL2_mixer_path = Path(f"{vendor_directory}/SDL2_mixer-{SDL2_mixer_version}/include/SDL_mixer.h");
-    SDL2_ttf_path = Path(f"{vendor_directory}/SDL2_ttf-{SDL2_ttf_version}/include/SDL_ttf.h");
+    # TODO: Change zip variable name to more generic version.
+    if (platform == "win32"):
+        SDL2_zip = "https://github.com/libsdl-org/SDL/releases/download/release-{SDL2_version}/SDL2-devel-{SDL2_version}-VC.zip".format(SDL2_version=SDL2_version)
+        SDL2_image_zip = "https://github.com/libsdl-org/SDL_image/releases/download/release-{SDL2_image_version}/SDL2_image-devel-{SDL2_image_version}-VC.zip".format(SDL2_image_version=SDL2_image_version)
+        SDL2_ttf_zip = "https://github.com/libsdl-org/SDL_ttf/releases/download/release-{SDL2_ttf_version}/SDL2_ttf-devel-{SDL2_ttf_version}-VC.zip".format(SDL2_ttf_version=SDL2_ttf_version)
+        SDL2_mixer_zip = "https://github.com/libsdl-org/SDL_mixer/releases/download/release-{SDL2_mixer_version}/SDL2_mixer-devel-{SDL2_mixer_version}-VC.zip".format(SDL2_mixer_version=SDL2_mixer_version)
+    elif (platform == "darwin"): # MacOS
+        SDL2_zip = "https://github.com/libsdl-org/SDL/releases/download/release-{SDL2_version}/SDL2-{SDL2_version}.dmg".format(SDL2_version=SDL2_version)
+        SDL2_image_zip = "https://github.com/libsdl-org/SDL_image/releases/download/release-{SDL2_image_version}/SDL2_image-{SDL2_image_version}.dmg".format(SDL2_image_version=SDL2_image_version)
+        SDL2_ttf_zip = "https://github.com/libsdl-org/SDL_ttf/releases/download/release-{SDL2_ttf_version}/SDL2_ttf-{SDL2_ttf_version}.dmg".format(SDL2_ttf_version=SDL2_ttf_version)
+        SDL2_mixer_zip = "https://github.com/libsdl-org/SDL_mixer/releases/download/release-{SDL2_mixer_version}/SDL2_mixer-{SDL2_mixer_version}.dmg".format(SDL2_mixer_version=SDL2_mixer_version)
+
+    SDL2_path = Path("{vendor_directory}/SDL2-{SDL2_version}/include/SDL.h".format(vendor_directory=vendor_directory, SDL2_version=SDL2_version));
+    SDL2_image_path = Path("{vendor_directory}/SDL2_image-{SDL2_image_version}/include/SDL_image.h".format(vendor_directory=vendor_directory, SDL2_image_version=SDL2_image_version));
+    SDL2_mixer_path = Path("{vendor_directory}/SDL2_mixer-{SDL2_mixer_version}/include/SDL_mixer.h".format(vendor_directory=vendor_directory, SDL2_mixer_version=SDL2_mixer_version));
+    SDL2_ttf_path = Path("{vendor_directory}/SDL2_ttf-{SDL2_ttf_version}/include/SDL_ttf.h".format(vendor_directory=vendor_directory, SDL2_ttf_version=SDL2_ttf_version));
 
     SDL2_found = ValidateDependency(SDL2_path, vendor_directory, SDL2_zip, SDL2_version, "SDL2")
     SDL2_image_found = ValidateDependency(SDL2_image_path, vendor_directory, SDL2_image_zip, SDL2_image_version, "SDL2_image")
@@ -160,9 +186,7 @@ if platform == "linux" or platform == "linux2": # Linux
                        libmikmod-dev libfishsound1-dev libsmpeg-dev liboggz2-dev libflac-dev libfluidsynth-dev libsdl2-mixer-dev libsdl2-mixer-2.0-0 \
                        libfreetype6-dev libsdl2-ttf-dev libsdl2-ttf-2.0-0")
     print("Installed SDL modules on Linux")
-elif platform == "darwin": # OS X
-    raise Exception("Cannot automatically install SDL for OS X")
-elif platform == "win32": # Windows
+elif platform == "win32" or platform == "darwin": # Windows or MacOS
     dependencies_installed = ValidateDependencies()
     if not dependencies_installed:
         raise Exception("Failed to install a dependency on Windows")
