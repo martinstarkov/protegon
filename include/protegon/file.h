6a5d0c51b3f5989ecc17eef6aeab262331b4c8b1
Martin <emailregisterlogin@gmail.com>
2023-07-21 00:55:13 -0400

    Add cross-platform file path support

---
+++ include/protegon/file.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <fstream> // std::ifstream
+#include <string>
+
+namespace ptgn {
+
+std::string GetExecutablePath();
+std::string GetExecutableDirectory();
+std::string MergePaths(std::string path_A, std::string path_B);
+bool FileExists (const std::string& file_path);
+std::string GetAbsolutePath(const std::string& relative_file_path);
+
+} // namespace ptgn
---
+++ src/utility/file.cpp
@@ -0,0 +1,122 @@
+#include "protegon/file.h"
+
+// Source: https://stackoverflow.com/a/60250581
+#if defined(_WIN32)
+    #include <windows.h>
+    #include <Shlwapi.h>
+    #include <io.h>
+
+    #define access _access_s
+#endif
+
+#ifdef __APPLE__
+    #include <libgen.h>
+    #include <limits.h>
+    #include <mach-o/dyld.h>
+    #include <unistd.h>
+#endif
+
+#ifdef __linux__
+    #include <limits.h>
+    #include <libgen.h>
+    #include <unistd.h>
+
+    #if defined(__sun)
+        #define PROC_SELF_EXE "/proc/self/path/a.out"
+    #else
+        #define PROC_SELF_EXE "/proc/self/exe"
+    #endif
+
+#endif
+
+namespace ptgn {
+
+#if defined(_WIN32)
+
+std::string GetExecutablePath() {
+   char rawPathName[MAX_PATH];
+   GetModuleFileNameA(NULL, rawPathName, MAX_PATH);
+   return std::string(rawPathName);
+}
+
+std::string GetExecutableDirectory() {
+    std::string executablePath = GetExecutablePath();
+    char* exePath = new char[executablePath.length()];
+    strcpy(exePath, executablePath.c_str());
+    PathRemoveFileSpecA(exePath);
+    std::string directory = std::string(exePath);
+    delete[] exePath;
+    return directory;
+}
+
+std::string MergePaths(std::string pathA, std::string pathB) {
+  char combined[MAX_PATH];
+  PathCombineA(combined, pathA.c_str(), pathB.c_str());
+  std::string mergedPath(combined);
+  return mergedPath;
+}
+
+#endif
+
+#ifdef __linux__
+
+std::string GetExecutablePath() {
+   char rawPathName[PATH_MAX];
+   realpath(PROC_SELF_EXE, rawPathName);
+   return  std::string(rawPathName);
+}
+
+std::string GetExecutableDirectory() {
+    std::string executablePath = GetExecutablePath();
+    char *executablePathStr = new char[executablePath.length() + 1];
+    strcpy(executablePathStr, executablePath.c_str());
+    char* executableDir = dirname(executablePathStr);
+    delete [] executablePathStr;
+    return std::string(executableDir);
+}
+
+std::string MergePaths(std::string pathA, std::string pathB) {
+  return pathA+"/"+pathB;
+}
+
+#endif
+
+#ifdef __APPLE__
+    std::string GetExecutablePath() {
+        char rawPathName[PATH_MAX];
+        char realPathName[PATH_MAX];
+        uint32_t rawPathSize = (uint32_t)sizeof(rawPathName);
+
+        if(!_NSGetExecutablePath(rawPathName, &rawPathSize)) {
+            realpath(rawPathName, realPathName);
+        }
+        return  std::string(realPathName);
+    }
+
+    std::string GetExecutableDirectory() {
+        std::string executablePath = GetExecutablePath();
+        char *executablePathStr = new char[executablePath.length() + 1];
+        strcpy(executablePathStr, executablePath.c_str());
+        char* executableDir = dirname(executablePathStr);
+        delete [] executablePathStr;
+        return std::string(executableDir);
+    }
+
+    std::string MergePaths(std::string pathA, std::string pathB) {
+        return pathA+"/"+pathB;
+    }
+
+#endif
+
+bool FileExists(const std::string& file_path) {
+        return access( file_path.c_str(), 0 ) == 0 ||
+               access( GetAbsolutePath(file_path).c_str(), 0 ) == 0;
+}
+
+std::string GetAbsolutePath(const std::string& relative_file_path) {
+    std::filesystem::path abs_path{ GetExecutableDirectory() };
+    abs_path.append(relative_file_path);
+    return abs_path.string();
+}
+
+} // namespace ptgn
