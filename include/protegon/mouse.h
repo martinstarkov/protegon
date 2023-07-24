bfabb219c9147796355c2e5c6b33f34e20fce680
Nicole Burg <emailregisterlogin@gmail.com>
2023-07-24 19:04:13 -0400

    Update mouse.h

--- include/protegon/mouse.h
+++ include/protegon/mouse.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include <cstdlib> // std::size_t
+#include <cstdint> // std::uint8_t
 
 namespace ptgn {
 
-enum class Mouse : std::size_t {
+enum class Mouse : std::uint8_t {
 	LEFT = 1,				// SDL_BUTTON_LEFT
 	MIDDLE = 2,				// SDL_BUTTON_MIDDLE
 	RIGHT = 3				// SDL_BUTTON_RIGHT
