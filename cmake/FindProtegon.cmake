5ad784303d265d4d6796d1edc8ac8a3bde33e7f8
Martin <emailregisterlogin@gmail.com>
2023-08-01 17:07:53 +0300

    Remove need for python download script

--- cmake/FindProtegon.cmake
+++ cmake/FindProtegon.cmake
@@ -1,62 +1,67 @@
+
 if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     set(MACOSX TRUE)
 endif()
 
 set(CMAKE_WARN_DEPRECATED OFF CACHE BOOL "" FORCE)
 
-set(PROTEGON_DIR         ${CMAKE_CURRENT_SOURCE_DIR})
-set(PROTEGON_SRC_DIR     ${PROTEGON_DIR}/src)
-set(PROTEGON_INCLUDE_DIR ${PROTEGON_DIR}/include)
-set(PROTEGON_VENDOR_DIR  ${PROTEGON_DIR}/external)
+set(EXTERNAL_DIR         ${CMAKE_SOURCE_DIR}/external)
+set(PROTEGON_SRC_DIR     ${CMAKE_SOURCE_DIR}/src)
+set(PROTEGON_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/include)
 
-# Ensure that these versions are consistent with those found in the protegon/scripts/setup.py file.
-set(SDL2_RELEASE_VERSION       2.26.5)
-set(SDL2_IMAGE_RELEASE_VERSION 2.6.3)
-set(SDL2_TTF_RELEASE_VERSION   2.20.2)
-set(SDL2_MIXER_RELEASE_VERSION 2.6.3)
+function(download_and_extract url folder_name)
+	if (NOT EXISTS ${EXTERNAL_DIR}/${folder_name})
+		get_filename_component(zip_name ${url} NAME)
+		message(STATUS "Downloading ${url}...")
+		set(zip_location ${EXTERNAL_DIR}/${zip_name})
+		file(DOWNLOAD ${url} ${zip_location} SHOW_PROGRESS INACTIVITY_TIMEOUT 60)
+		message(STATUS "Successfully extracted ${folder_name}")
+		file(ARCHIVE_EXTRACT INPUT ${zip_location} DESTINATION ${EXTERNAL_DIR})
+		file(REMOVE ${zip_location})
+		#message(STATUS "Successfully downloaded and extracted ${folder_name} library files")
+	endif()
+endfunction()
 
-include(FetchContent)
+download_and_extract(https://github.com/martinstarkov/ecs/archive/refs/heads/main.zip ecs-main)
+download_and_extract(https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent/archive/refs/tags/v3.11.2.zip nlohmann_json_cmake_fetchcontent-3.11.2)
 
-FetchContent_Declare(ecs 
-					 GIT_REPOSITORY https://github.com/martinstarkov/ecs.git
-					 GIT_TAG main)
-FetchContent_MakeAvailable(ecs)
+set(ECS_INCLUDE_DIR  ${EXTERNAL_DIR}/ecs-main/include)
+set(JSON_INCLUDE_DIR ${EXTERNAL_DIR}/nlohmann_json_cmake_fetchcontent-3.11.2/single_include)
 
-FetchContent_Declare(nlohmann_json
-  GIT_REPOSITORY https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent
-  GIT_PROGRESS TRUE
-  GIT_SHALLOW TRUE
-  GIT_TAG v3.11.2)
-FetchContent_MakeAvailable(nlohmann_json)
-
-set(ECS_INCLUDE_DIR  ${ecs_SOURCE_DIR}/include)
-set(JSON_INCLUDE_DIR ${nlohmann_json_SOURCE_DIR}/single_include)
+if(AUTOMATIC_SDL)
+  set(SDL2_VERSION       2.28.1)
+  set(SDL2_IMAGE_VERSION 2.6.3)
+  set(SDL2_TTF_VERSION   2.20.2)
+  set(SDL2_MIXER_VERSION 2.6.3)
+	if (WIN32)
+    download_and_extract(https://github.com/libsdl-org/SDL/releases/download/release-${SDL2_VERSION}/SDL2-devel-${SDL2_VERSION}-VC.zip SDL2-${SDL2_VERSION})
+    download_and_extract(https://github.com/libsdl-org/SDL_image/releases/download/release-${SDL2_IMAGE_VERSION}/SDL2_image-devel-${SDL2_IMAGE_VERSION}-VC.zip SDL2_image-${SDL2_IMAGE_VERSION})
+    download_and_extract(https://github.com/libsdl-org/SDL_ttf/releases/download/release-${SDL2_TTF_VERSION}/SDL2_ttf-devel-${SDL2_TTF_VERSION}-VC.zip SDL2_ttf-${SDL2_TTF_VERSION})
+    download_and_extract(https://github.com/libsdl-org/SDL_mixer/releases/download/release-${SDL2_MIXER_VERSION}/SDL2_mixer-devel-${SDL2_MIXER_VERSION}-VC.zip SDL2_mixer-${SDL2_MIXER_VERSION})
+    set(SDL2_DIR       ${EXTERNAL_DIR}/SDL2-${SDL2_VERSION}/cmake             CACHE BOOL "" FORCE)
+    set(SDL2_image_DIR ${EXTERNAL_DIR}/SDL2_image-${SDL2_IMAGE_VERSION}/cmake CACHE BOOL "" FORCE)
+    set(SDL2_ttf_DIR   ${EXTERNAL_DIR}/SDL2_ttf-${SDL2_TTF_VERSION}/cmake     CACHE BOOL "" FORCE)
+    set(SDL2_mixer_DIR ${EXTERNAL_DIR}/SDL2_mixer-${SDL2_MIXER_VERSION}/cmake CACHE BOOL "" FORCE)
+  elseif(MACOSX)
+    download_and_extract(https://github.com/libsdl-org/SDL/releases/download/release-${SDL2_VERSION}/SDL2-${SDL2_VERSION}.dmg SDL2.framework)
+    download_and_extract(https://github.com/libsdl-org/SDL_image/releases/download/release-${SDL2_IMAGE_VERSION}/SDL2-${SDL2_IMAGE_VERSION}.dmg SDL2_image.framework)
+    download_and_extract(https://github.com/libsdl-org/SDL_ttf/releases/download/release-${SDL2_TTF_VERSION}/SDL2-${SDL2_TTF_VERSION}.dmg SDL2_ttf.framework)
+    download_and_extract(https://github.com/libsdl-org/SDL_mixer/releases/download/release-${SDL2_MIXER_VERSION}/SDL2-${SDL2_MIXER_VERSION}.dmg SDL2_mixer.framework)
+    set(SDL2_DIR       ${EXTERNAL_DIR}/SDL2.framework/Resources/CMake       CACHE BOOL "" FORCE)
+    set(SDL2_image_DIR ${EXTERNAL_DIR}/SDL2_image.framework/Resources/CMake CACHE BOOL "" FORCE)
+    set(SDL2_ttf_DIR   ${EXTERNAL_DIR}/SDL2_ttf.framework/Resources/CMake   CACHE BOOL "" FORCE)
+    set(SDL2_mixer_DIR ${EXTERNAL_DIR}/SDL2_mixer.framework/Resources/CMake CACHE BOOL "" FORCE)
+  endif()
+    list(APPEND CMAKE_MODULE_PATH ${SDL2_DIR})
+    list(APPEND CMAKE_MODULE_PATH ${SDL2_image_DIR})
+    list(APPEND CMAKE_MODULE_PATH ${SDL2_ttf_DIR})
+    list(APPEND CMAKE_MODULE_PATH ${SDL2_mixer_DIR})
+endif()
 
 file(GLOB_RECURSE PROTEGON_FILES CONFIGURE_DEPENDS LIST_DIRECTORIES false 
-     ${PROTEGON_SRC_DIR}/*.h
-	 ${PROTEGON_SRC_DIR}/*.cpp
-	 ${PROTEGON_INCLUDE_DIR}/*.h)
-
-if (AUTOMATIC_SDL)
-set(PROTEGON_SETUP_SCRIPT ${PROTEGON_DIR}/scripts/setup.py)
-	if (WIN32)
-		execute_process(COMMAND py ${PROTEGON_SETUP_SCRIPT})
-		set(SDL2_DIR       ${PROTEGON_VENDOR_DIR}/SDL2-${SDL2_RELEASE_VERSION}/cmake             CACHE BOOL "" FORCE)
-		set(SDL2_image_DIR ${PROTEGON_VENDOR_DIR}/SDL2_image-${SDL2_IMAGE_RELEASE_VERSION}/cmake CACHE BOOL "" FORCE)
-		set(SDL2_ttf_DIR   ${PROTEGON_VENDOR_DIR}/SDL2_ttf-${SDL2_TTF_RELEASE_VERSION}/cmake     CACHE BOOL "" FORCE)
-		set(SDL2_mixer_DIR ${PROTEGON_VENDOR_DIR}/SDL2_mixer-${SDL2_MIXER_RELEASE_VERSION}/cmake CACHE BOOL "" FORCE)
-	elseif(MACOSX)
-		execute_process(COMMAND python3 ${PROTEGON_SETUP_SCRIPT})
-		set(SDL2_DIR       ${PROTEGON_VENDOR_DIR}/SDL2.framework/Resources/CMake       CACHE BOOL "" FORCE)
-		set(SDL2_image_DIR ${PROTEGON_VENDOR_DIR}/SDL2_image.framework/Resources/CMake CACHE BOOL "" FORCE)
-		set(SDL2_ttf_DIR   ${PROTEGON_VENDOR_DIR}/SDL2_ttf.framework/Resources/CMake   CACHE BOOL "" FORCE)
-		set(SDL2_mixer_DIR ${PROTEGON_VENDOR_DIR}/SDL2_mixer.framework/Resources/CMake CACHE BOOL "" FORCE)
-	endif()
-		list(APPEND CMAKE_MODULE_PATH ${SDL2_DIR})
-		list(APPEND CMAKE_MODULE_PATH ${SDL2_image_DIR})
-		list(APPEND CMAKE_MODULE_PATH ${SDL2_ttf_DIR})
-		list(APPEND CMAKE_MODULE_PATH ${SDL2_mixer_DIR})
-endif()
+  ${PROTEGON_SRC_DIR}/*.h
+	${PROTEGON_SRC_DIR}/*.cpp
+	${PROTEGON_INCLUDE_DIR}/*.h)
 
 set(SDL2MAIN_LIBRARY FALSE)
 
@@ -66,14 +71,14 @@ if (WIN32)
 	find_package(SDL2_ttf   REQUIRED)
 	find_package(SDL2_mixer REQUIRED)
 elseif(MACOSX)
-    set(CMAKE_SKIP_BUILD_RPATH FALSE)
+	set(CMAKE_SKIP_BUILD_RPATH FALSE)
 	set(CMAKE_BUILD_WITH_INSTALL_RPATH YES)
-	set(CMAKE_INSTALL_RPATH ${PROTEGON_VENDOR_DIR})
+	set(CMAKE_INSTALL_RPATH ${EXTERNAL_DIR})
 	set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
-	find_package(SDL2       REQUIRED NAMES SDL2 PATHS       ${PROTEGON_VENDOR_DIR}/SDL2.framework/Versions/A/       NO_DEFAULT_PATH)
-	find_package(SDL2_image REQUIRED NAMES SDL2_image PATHS ${PROTEGON_VENDOR_DIR}/SDL2_image.framework/Versions/A/ NO_DEFAULT_PATH)
-	find_package(SDL2_ttf   REQUIRED NAMES SDL2_ttf PATHS   ${PROTEGON_VENDOR_DIR}/SDL2_ttf.framework/Versions/A/   NO_DEFAULT_PATH)
-	find_package(SDL2_mixer REQUIRED NAMES SDL2_mixer PATHS ${PROTEGON_VENDOR_DIR}/SDL2_mixer.framework/Versions/A/ NO_DEFAULT_PATH)
+	find_package(SDL2       REQUIRED NAMES SDL2 PATHS       ${EXTERNAL_DIR}/SDL2.framework/Versions/A/       NO_DEFAULT_PATH)
+	find_package(SDL2_image REQUIRED NAMES SDL2_image PATHS ${EXTERNAL_DIR}/SDL2_image.framework/Versions/A/ NO_DEFAULT_PATH)
+	find_package(SDL2_ttf   REQUIRED NAMES SDL2_ttf PATHS   ${EXTERNAL_DIR}/SDL2_ttf.framework/Versions/A/   NO_DEFAULT_PATH)
+	find_package(SDL2_mixer REQUIRED NAMES SDL2_mixer PATHS ${EXTERNAL_DIR}/SDL2_mixer.framework/Versions/A/ NO_DEFAULT_PATH)
 endif()
 
 MACRO(SUBDIRLIST result curdir)
@@ -97,6 +102,9 @@ if (WIN32)
 endif()
 
 add_library(protegon STATIC ${PROTEGON_FILES})
+
+target_compile_features(protegon PUBLIC cxx_std_17)
+set_target_properties(protegon PROPERTIES CXX_EXTENSIONS OFF)
 
 target_link_libraries(protegon PRIVATE SDL2::SDL2
                                        SDL2_image::SDL2_image
@@ -176,10 +184,11 @@ function(create_protegon_link TARGET_NAME DIR_NAME)
 	endif()
 endfunction()
 
-mark_as_advanced(PROTEGON_DIR
-	             PROTEGON_SRC_DIR
-				 PROTEGON_VENDOR_DIR
-				 PROTEGON_FILES
-				 protegon)
+mark_as_advanced(
+	PROTEGON_SRC_DIR
+	PROTEGON_INCLUDE_DIR
+	EXTERNAL_DIR
+	PROTEGON_FILES
+	protegon)
 
 message(STATUS "Found protegon")
