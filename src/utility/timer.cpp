2c684ff869dd034a9e3c0c2b33a0de1bd0cd60c2
Martin <emailregisterlogin@gmail.com>
2024-05-20 17:51:19 +0200

    Add pause check

--- src/utility/timer.cpp
+++ src/utility/timer.cpp
@@ -19,23 +19,29 @@ void Timer::Stop() {
 }
 
 void Timer::Pause() {
-    if (running_) {
+    if (running_ && !paused_) {
         stop_time_ = std::chrono::steady_clock::now();
         pause_time_ = std::chrono::steady_clock::now();
         running_ = false;
+        paused_ = true;
     }
 }
 
 void Timer::Unpause() {
-    if (!running_) {
+    if (!running_ && paused_) {
         // Calculate elapsed time during pause.
         auto pause_duration = std::chrono::steady_clock::now() - pause_time_;
         // Adjust start time to account for pause.
         start_time_ += pause_duration;
         running_ = true;
+        paused_ = false;
         pause_time_ = std::chrono::steady_clock::time_point(); // Reset paused time on unpause
         stop_time_ = start_time_;
     }
+}
+
+bool Timer::IsPaused() const {
+    return paused_;
 }
 
 bool Timer::IsRunning() const {
