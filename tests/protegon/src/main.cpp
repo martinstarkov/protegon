8061f5033e1d64aecc5c38d6d5779ede234239b7
Martin <emailregisterlogin@gmail.com>
2024-05-25 05:34:36 +0200

    Add more tests

--- tests/protegon/src/main.cpp
+++ tests/protegon/src/main.cpp
@@ -7,8 +7,10 @@
 
 using namespace ptgn;
 
-class Tests : public Engine {
-    void Create() final {
+class Tests : public Scene {
+public:
+    Tests() {
+        window::SetSize({ 800, 800 });
         TestECS();
         TestMath();
         TestVector2();
@@ -28,6 +30,11 @@ class Tests : public Engine {
         Point<float> test11{ 600, 600 };
         Capsule<float> test12{ { V2_float{ 600, 600 }, V2_float{ 700, 700 } }, 10 };
         Capsule<float> test13{ { V2_float{ 650, 600 }, V2_float{ 750, 700 } }, 20 };
+        Capsule<float> test14{ { V2_float{ 640, 700 }, V2_float{ 690, 750 } }, 20 };
+        Arc<float> test15{ V2_float{ 510, 550 }, 15, 0, 90 };
+        Arc<float> test16{ V2_float{ 510, 600 }, 10, 180, 360 };
+        Arc<float> test17{ V2_float{ 510, 500 }, 20, 90, 180 };
+        Arc<float> test18{ V2_float{ 510, 700 }, 80, -270, 0 };
         test1.Draw(color::RED);
         test2.DrawSolid(color::BLUE);
         test3.Draw(color::GREEN, 40);
@@ -39,8 +46,13 @@ class Tests : public Engine {
         test9.Draw(color::BLACK);
         test10.Draw(color::BLUE);
         test11.Draw(color::DARK_GREY);
-        test12.Draw(color::GOLD);
+        test12.Draw(color::DARK_GREY);
         test13.Draw(color::BROWN, true);
+        test14.DrawSolid(color::RED);
+        test15.Draw(color::DARK_GREEN);
+        test16.Draw(color::DARK_RED);
+        test17.DrawSolid(color::BLACK);
+        test18.DrawSolid(color::DARK_BLUE);
     }
     void Update(float dt) final {
         static std::chrono::time_point<std::chrono::steady_clock> oldTime = std::chrono::high_resolution_clock::now();
@@ -57,7 +69,6 @@ class Tests : public Engine {
 };
 
 int main(int c, char** v) {
-    Tests game;
-    game.Construct("Tests", { 800, 800 });
+    ptgn::game::Start<Tests>();
     return 0;
 }
