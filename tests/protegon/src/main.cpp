0cbd70babed0b8783fa36cb1a63cab2df21fe0f2
Martin <emailregisterlogin@gmail.com>
2024-05-26 02:17:05 +0200

    Add drawing tests (visual)

--- tests/protegon/src/main.cpp
+++ tests/protegon/src/main.cpp
@@ -10,49 +10,130 @@ using namespace ptgn;
 class Tests : public Scene {
 public:
     Tests() {
-        window::SetSize({ 800, 800 });
+        window::SetSize({ 800, 400 });
         TestECS();
         TestMath();
         TestVector2();
         TestRNG();
     }
     void Render() {
-        Rectangle<float> test1{ { 0, 0 }, { 400, 400 } };
-        Rectangle<float> test2{ { 400, 0 }, { 400, 400 } };
-        Rectangle<float> test3{ { 0, 400 }, { 400, 400 } };
-        Circle<float> test4{ { 200, 200 }, 100 };
-        Circle<float> test5{ { 600, 200 }, 100 };
-        Circle<float> test6{ { 200, 600 }, 100 };
-        Line<float> test7{ { 200, 200 }, { 300, 300 } };
-        Line<float> test8{ { 200, 600 }, { 300, 700 } };
-        Point<float> test9{ 500, 500 };
-        Point<float> test10{ 550, 550 };
-        Point<float> test11{ 600, 600 };
-        Capsule<float> test12{ { V2_float{ 600, 600 }, V2_float{ 700, 700 } }, 10 };
-        Capsule<float> test13{ { V2_float{ 650, 600 }, V2_float{ 750, 700 } }, 20 };
-        Capsule<float> test14{ { V2_float{ 640, 700 }, V2_float{ 690, 750 } }, 20 };
-        Arc<float> test15{ V2_float{ 510, 550 }, 15, 0, 90 };
-        Arc<float> test16{ V2_float{ 510, 600 }, 10, 180, 360 };
-        Arc<float> test17{ V2_float{ 510, 500 }, 20, 90, 180 };
-        Arc<float> test18{ V2_float{ 510, 700 }, 80, -270, 0 };
-        test1.Draw(color::RED);
-        test2.DrawSolid(color::BLUE);
-        test3.Draw(color::GREEN, 40);
-        test4.Draw(color::BLUE);
-        test5.DrawSolid(color::GREEN);
-        test6.Draw(color::RED, 40);
-        test7.Draw(color::ORANGE);
-        test8.Draw(color::CYAN, 40);
-        test9.Draw(color::BLACK);
-        test10.Draw(color::BLUE);
-        test11.Draw(color::DARK_GREY);
-        test12.Draw(color::DARK_GREY);
-        test13.Draw(color::BROWN, true);
-        test14.DrawSolid(color::RED);
-        test15.Draw(color::DARK_GREEN);
-        test16.Draw(color::DARK_RED);
-        test17.DrawSolid(color::BLACK);
-        test18.DrawSolid(color::DARK_BLUE);
+        static bool drawing_tests_passed = false;
+
+        if (!drawing_tests_passed) {
+            std::cout << "Starting drawing tests..." << std::endl;
+        }
+
+        Point<float> test01{ 30, 10 };
+        Point<float> test02{ 10, 10 };
+
+        test01.Draw(color::BLACK);
+        test02.Draw(color::BLACK, 6);
+
+        Rectangle<float> test11{ { 20, 20 }, { 30, 20 } };
+        Rectangle<float> test12{ { 60, 20 }, { 40, 20 } };
+        Rectangle<float> test13{ { 110, 20 }, { 50, 20 } };
+
+        test11.Draw(color::RED);
+        test12.Draw(color::RED, 4);
+        test13.DrawSolid(color::RED);
+
+        RoundedRectangle<float> test21{ { 20, 50 }, { 30, 20 }, 5 };
+        RoundedRectangle<float> test22{ { 60, 50 }, { 40, 20 }, 8 };
+        RoundedRectangle<float> test23{ { 110, 50 }, { 50, 20 }, 10 };
+        RoundedRectangle<float> test24{ { 30, 180 }, { 160, 50 }, 10 };
+
+        test21.Draw(color::GREEN);
+        test22.Draw(color::GREEN, 5);
+        test23.DrawSolid(color::GREEN);
+        test24.Draw(color::GREEN, 4);
+
+        Triangle test31{ { V2_int{ 180 + 120 * 0, 60 }, V2_int{ 180 + 50 + 120 * 0, 20 }, V2_int{ 180 + 50 + 50 + 120 * 0, 80 } } };
+        Triangle test32{ { V2_int{ 180 + 120 * 1, 60 }, V2_int{ 180 + 50 + 120 * 1, 20 }, V2_int{ 180 + 50 + 50 + 120 * 1, 80 } } };
+        Triangle test33{ { V2_int{ 180 + 120 * 2, 60 }, V2_int{ 180 + 50 + 120 * 2, 20 }, V2_int{ 180 + 50 + 50 + 120 * 2, 80 } } };
+
+        test31.Draw(color::PURPLE);
+        test32.Draw(color::PURPLE, 5);
+        test33.DrawSolid(color::PURPLE);
+
+        std::vector<V2_int> star1{
+            V2_int{ 550, 60 },
+            V2_int{ 650 - 44, 60 },
+            V2_int{ 650, 60 - 44 },
+            V2_int{ 650 + 44, 60 },
+            V2_int{ 750, 60 },
+            V2_int{ 750 - 44, 60 + 44 },
+            V2_int{ 750 - 44, 60 + 44 + 44 },
+            V2_int{ 650, 60 + 44 },
+            V2_int{ 550 + 44, 60 + 44 + 44 },
+            V2_int{ 550 + 44, 60 + 44 },
+        };
+
+        std::vector<V2_int> star2;
+        std::vector<V2_int> star3;
+
+        for (const auto& s : star1) {
+            star2.push_back({ s.x, s.y + 100 });
+        }
+
+        for (const auto& s : star1) {
+            star3.push_back({ s.x, s.y + 200 });
+        }
+
+        Polygon test41{ star1 };
+        Polygon test42{ star2 };
+        Polygon test43{ star3 };
+
+        test41.Draw(color::DARK_BLUE);
+        test42.Draw(color::DARK_BLUE, 5);
+        test43.DrawSolid(color::DARK_BLUE);
+
+        Circle<float> test51{ { 30, 130 }, 15 };
+        Circle<float> test52{ { 100, 130 }, 30 };
+        Circle<float> test53{ { 180, 130 }, 20 };
+
+        test51.Draw(color::DARK_GREY);
+        test52.Draw(color::DARK_GREY, 5);
+        test53.DrawSolid(color::DARK_GREY);
+
+        Capsule<float> test61{ { V2_float{ 240, 130 }, V2_float{ 350, 200 } }, 10 };
+        Capsule<float> test62{ { V2_float{ 230, 170 }, V2_float{ 340, 250 } }, 20 };
+        Capsule<float> test63{ { V2_float{ 400, 230 }, V2_float{ 530, 200 } }, 20 };
+        Capsule<float> test64{ { V2_float{ 350, 130 }, V2_float{ 500, 100 } }, 15 };
+        Capsule<float> test65{ { V2_float{ 300, 320 }, V2_float{ 150, 250 } }, 15 };
+
+        test61.Draw(color::BROWN);
+        test62.Draw(color::BROWN, 8);
+        test63.Draw(color::BROWN, 5);
+        test64.DrawSolid(color::BROWN);
+        test65.Draw(color::BROWN, 3);
+
+        Line<float> test71{ V2_float{ 370, 160 }, V2_float{ 500, 130 } };
+        Line<float> test72{ V2_float{ 370, 180 }, V2_float{ 500, 150 } };
+
+        test71.Draw(color::BLACK);
+        test72.Draw(color::BLACK, 5);
+
+        Arc<float> test81{ V2_float{ 40, 300 }, 15, 0, 90 };
+        Arc<float> test82{ V2_float{ 40 + 50, 300 }, 10, 180, 360 };
+        Arc<float> test83{ V2_float{ 40 + 50 + 50, 300 }, 20, -90, 180 };
+
+        test81.Draw(color::DARK_GREEN);
+        test82.Draw(color::DARK_GREEN, 3);
+        test83.DrawSolid(color::DARK_GREEN);
+
+        Ellipse<float> test91{ { 380, 300 }, { 10, 30 } };
+        Ellipse<float> test92{ { 440, 300 }, { 40, 15 } };
+        Ellipse<float> test93{ { 510, 300 }, { 5, 40 } };
+
+        test91.Draw(color::SILVER);
+        test92.Draw(color::SILVER, 5);
+        test93.DrawSolid(color::SILVER);
+
+        if (!drawing_tests_passed) {
+            std::cout << "All drawing tests passed!" << std::endl;
+            drawing_tests_passed = true;
+        }
+
     }
     void Update(float dt) final {
         static std::chrono::time_point<std::chrono::steady_clock> oldTime = std::chrono::high_resolution_clock::now();
