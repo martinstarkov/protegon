48107384394d2e4d5caaf5d5d849899a3ee6d613
Martin <emailregisterlogin@gmail.com>
2024-06-04 18:10:21 +0200

    Changed fs::path to fs

--- src/utility/file.cpp
+++ src/utility/file.cpp
@@ -6,7 +6,7 @@
 
 namespace ptgn {
 
-std::string FileToString(const fs::path& file) {
+std::string FileToString(const path& file) {
     // Source: https://stackoverflow.com/a/2602258
     std::ifstream f(file, std::ios::in | std::ios::binary);
 	assert(f && "Could not open file to convert it to string");
@@ -15,27 +15,27 @@ std::string FileToString(const fs::path& file) {
     return buffer.str();
 }
 
-fs::path GetExecutablePath() {
+path GetExecutablePath() {
    return fs::current_path();
 }
 
-fs::path GetExecutableDirectory() {
+path GetExecutableDirectory() {
     return GetExecutablePath().parent_path();
 }
 
-fs::path MergePaths(const fs::path& pathA, const fs::path& pathB) {
+path MergePaths(const path& pathA, const path& pathB) {
   return pathA / pathB;
 }
 
-bool FileExists(const fs::path& file_path) {
+bool FileExists(const path& file_path) {
     return fs::exists(file_path);
 }
 
-fs::path GetAbsolutePath(const fs::path& relative_file_path) {
+path GetAbsolutePath(const path& relative_file_path) {
     return fs::current_path() / relative_file_path;
 }
 
-fs::path GetRelativePath(const fs::path& absolute_file_path) {
+path GetRelativePath(const path& absolute_file_path) {
     return absolute_file_path.relative_path();
 }
 
