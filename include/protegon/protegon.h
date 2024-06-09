1df884bf26586a09b9644756855c2e195fa97003
Martin <emailregisterlogin@gmail.com>
2024-06-09 08:44:21 +0200

    Update protegon.h

--- include/protegon/protegon.h
+++ include/protegon/protegon.h
@@ -7,6 +7,7 @@ using json = nlohmann::json;
 #include "ecs/ecs.h"
 
 #include "protegon/a_star.h"
+#include "protegon/buffer.h"
 #include "protegon/button.h"
 #include "protegon/circle.h"
 #include "protegon/collision.h"
@@ -38,4 +39,5 @@ using json = nlohmann::json;
 #include "protegon/timer.h"
 #include "protegon/type_traits.h"
 #include "protegon/vector2.h"
+#include "protegon/vertex_array.h"
 #include "protegon/window.h"
