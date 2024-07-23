7e024f333c6962c24cdbc0bb96884c9dca5904d9
Martin Starkov <emailregisterlogin@gmail.com>
2024-07-24 01:09:47 +0300

    Added more specific window resizing events

--- include/protegon/events.h
+++ include/protegon/events.h
@@ -1,16 +1,34 @@
 #pragma once
 
-#include "protegon/event.h"
+#include "event/key.h"
 #include "event/mouse.h"
+#include "protegon/event.h"
 #include "protegon/vector2.h"
 
 namespace ptgn {
 
-enum class MouseEvent {
-	Move,
+enum class KeyEvent {
 	Down,
 	Up,
-	Scroll
+};
+
+struct KeyDownEvent : public Event {
+	KeyDownEvent(Key key) : key{ key } {}
+
+	Key key;
+};
+
+struct KeyUpEvent : public Event {
+	KeyUpEvent(Key key) : key{ key } {}
+
+	Key key;
+};
+
+enum class MouseEvent {
+	Move, /* fires repeatedly on mouse or trackpad movement */
+	Down,
+	Up,
+	Scroll /* fires repeatedly on mouse or trackpad scroll */
 };
 
 struct MouseMoveEvent : public Event {
@@ -23,8 +41,7 @@ struct MouseMoveEvent : public Event {
 
 class MouseDownEvent : public Event {
 public:
-	MouseDownEvent(Mouse mouse, const V2_int& current) :
-		mouse{ mouse }, current{ current } {}
+	MouseDownEvent(Mouse mouse, const V2_int& current) : mouse{ mouse }, current{ current } {}
 
 	Mouse mouse;
 	V2_int current;
@@ -32,8 +49,7 @@ public:
 
 class MouseUpEvent : public Event {
 public:
-	MouseUpEvent(Mouse mouse, const V2_int& current) :
-		mouse{ mouse }, current{ current } {}
+	MouseUpEvent(Mouse mouse, const V2_int& current) : mouse{ mouse }, current{ current } {}
 
 	Mouse mouse;
 	V2_int current;
@@ -46,8 +62,11 @@ struct MouseScrollEvent : public Event {
 };
 
 enum class WindowEvent {
-	Quit,
-	Resize
+	Quit,	  /* fires once when the window is quit */
+	Resized,  /* fires one or more times (consult SDL_PollEvent rate and game FPS) after size change
+				 occurs or resizing  is finished (window is released) */
+	Resizing, /* fires repeatedly while window is being resized */
+	Drag	  /* fires while window is being dragged (moved around) */
 };
 
 class WindowQuitEvent : public Event {
@@ -55,10 +74,21 @@ public:
 	WindowQuitEvent() = default;
 };
 
-class WindowResizeEvent : public Event {
+class WindowDragEvent : public Event {
 public:
-	WindowResizeEvent(const V2_int& size) : size{ size } {}
+	WindowDragEvent() = default;
+};
+
+class WindowResizedEvent : public Event {
+public:
+	WindowResizedEvent(const V2_int& size) : size{ size } {}
+
 	V2_int size;
+};
+
+class WindowResizingEvent : public WindowResizedEvent {
+public:
+	using WindowResizedEvent::WindowResizedEvent;
 };
 
 } // namespace ptgn
