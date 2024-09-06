cad4b98b6e30c2b9b0f57d791bc95ca46d8f7686
Martin <emailregisterlogin@gmail.com>
2024-09-06 08:11:36 +0200

    Added parallax test

--- tests/protegon/src/test_camera.h
+++ tests/protegon/src/test_camera.h
@@ -12,6 +12,7 @@ const std::vector<Key> camera_test_switch_keys{ Key::Q, Key::E };
 const std::string camera_test_instructions{ "'Q' (cycle back); 'E' (cycle forward)" };
 
 enum class CameraTest {
+	Parallax,
 	Switching,
 	Movement,
 	Count
@@ -96,16 +97,16 @@ void TestCameraMovement(float dt) {
 			// TODO: Move this stuff into camera controller class.
 
 			if (game.input.KeyPressed(Key::W)) {
-				velocity.y = +speed;
-			}
-			if (game.input.KeyPressed(Key::S)) {
 				velocity.y = -speed;
 			}
+			if (game.input.KeyPressed(Key::S)) {
+				velocity.y = +speed;
+			}
 			if (game.input.KeyPressed(Key::A)) {
-				velocity.x = +speed;
+				velocity.x = -speed;
 			}
 			if (game.input.KeyPressed(Key::D)) {
-				velocity.x = -speed;
+				velocity.x = speed;
 			}
 			camera.Translate(velocity);
 
@@ -168,6 +169,76 @@ void TestCameraMovement(float dt) {
 	}*/
 }
 
+void TestParallax(float dt) {
+	Texture background{ "resources/sprites/parallax/background.png" };
+	Texture planet_b{ "resources/sprites/parallax/planet_b.png" };
+	Texture planet_s{ "resources/sprites/parallax/planet_s.png" };
+	Texture stars{ "resources/sprites/parallax/stars.png" };
+
+	static V2_float bg_pos		 = game.window.GetCenter();
+	static V2_float planet_b_pos = game.window.GetCenter() - V2_float{ 200, 200 };
+	static V2_float planet_s_pos = game.window.GetCenter() + V2_float{ 200, 200 };
+	static V2_float stars_pos	 = game.window.GetCenter();
+
+	TestCameraLoop(
+		dt,
+		[&](float dt) {
+			V2_float w = game.window.GetSize();
+
+			V2_float background_size{ background.GetSize() };
+			float bg_aspect_ratio{ background_size.x / background_size.y };
+
+			float scale{ 2.0f };
+
+			V2_float size{ w * scale };
+
+			auto& camera{ game.camera.GetPrimary() };
+
+			camera.SetSize(w);
+
+			float speed = 200.5f * dt;
+
+			V2_float velocity;
+
+			if (game.input.KeyPressed(Key::W)) {
+				velocity.y = -speed;
+			}
+			if (game.input.KeyPressed(Key::S)) {
+				velocity.y = +speed;
+			}
+			if (game.input.KeyPressed(Key::A)) {
+				velocity.x = -speed;
+			}
+			if (game.input.KeyPressed(Key::D)) {
+				velocity.x = +speed;
+			}
+			camera.Translate({ velocity.x, velocity.y, 0.0f });
+
+			if (game.input.KeyDown(Key::R)) {
+				camera.SetPosition({ center.x, center.y, 0.0f });
+			}
+
+			auto pos = camera.GetPosition();
+
+			camera.SetPosition({ 0.0f, 0.0f, 0.0f });
+
+			stars_pos	 += velocity / 6.0f;
+			bg_pos		 += velocity / 4.0f;
+			planet_s_pos += velocity / 3.0f;
+			planet_b_pos += velocity / 2.0f;
+
+			game.renderer.DrawTexture(background, bg_pos, { size.x * bg_aspect_ratio, size.y });
+			game.renderer.DrawTexture(stars, stars_pos, { size.x * bg_aspect_ratio, size.y });
+
+			game.renderer.DrawTexture(planet_b, planet_b_pos, planet_b.GetSize() * scale);
+			game.renderer.DrawTexture(planet_s, planet_s_pos, planet_s.GetSize() * scale);
+
+			camera.SetPosition(pos);
+		},
+		PTGN_FUNCTION_NAME()
+	);
+}
+
 void TestCamera() {
 	game.PushLoopFunction([&](float dt) {
 		ws	   = game.window.GetSize();
@@ -176,6 +247,7 @@ void TestCamera() {
 		game.renderer.SetClearColor(color::DarkGrey);
 		static std::size_t count{ game.LoopFunctionCount() };
 		switch (static_cast<CameraTest>(camera_test)) {
+			case CameraTest::Parallax:	TestParallax(dt); break;
 			case CameraTest::Movement:	TestCameraMovement(dt); break;
 			case CameraTest::Switching: TestCameraSwitching(dt); break;
 			default:					PTGN_ERROR("Failed to find a valid camera test");
