d5faf418da9269c2c74683f1e6926d504b6523cc
Martin Starkov <emailregisterlogin@gmail.com>
2024-10-09 00:56:51 +0300

    Add origin print

--- src/renderer/origin.cpp
+++ src/renderer/origin.cpp
@@ -6,35 +6,18 @@
 namespace ptgn {
 
 V2_float GetOffsetFromCenter(const V2_float& size, Origin draw_origin) {
-	// Not in the switch to skip half calculation.
-	if (draw_origin == Origin::Center) {
-		return {};
-	}
-
-	V2_float half{ size * 0.5f };
-	V2_float offset;
-
 	switch (draw_origin) {
-		case Origin::TopLeft:	   offset = -half; break;
-
-		case Origin::CenterBottom: offset = { 0.0f, half.y }; break;
-
-		case Origin::CenterTop:	   offset = { 0.0f, -half.y }; break;
-
-		case Origin::BottomRight:  offset = half; break;
-
-		case Origin::BottomLeft:   offset = V2_float{ -half.x, half.y }; break;
-
-		case Origin::TopRight:	   offset = V2_float{ half.x, -half.y }; break;
-
-		case Origin::CenterLeft:   offset = { -half.x, 0.0f }; break;
-
-		case Origin::CenterRight:  offset = { half.x, 0.0f }; break;
-
+		case Origin::Center:	   return {};
+		case Origin::TopLeft:	   return -size * 0.5f;
+		case Origin::CenterBottom: return V2_float{ 0.0f, size.y * 0.5f };
+		case Origin::CenterTop:	   return { 0.0f, -size.y * 0.5f };
+		case Origin::BottomRight:  return size * 0.5f;
+		case Origin::BottomLeft:   return V2_float{ -size.x * 0.5f, size.y * 0.5f };
+		case Origin::TopRight:	   return V2_float{ size.x * 0.5f, -size.y * 0.5f };
+		case Origin::CenterLeft:   return { -size.x * 0.5f, 0.0f };
+		case Origin::CenterRight:  return { size.x * 0.5f, 0.0f };
 		default:				   PTGN_ERROR("Failed to identify draw origin");
 	}
-
-	return offset;
 }
 
 } // namespace ptgn
--- src/renderer/origin.h
+++ src/renderer/origin.h
@@ -1,21 +1,42 @@
 #pragma once
 
+#include <iosfwd>
+#include <string>
+
+#include "protegon/log.h"
 #include "protegon/vector2.h"
 
 namespace ptgn {
 
 enum class Origin {
-	Center,
-	CenterTop,
-	CenterBottom,
-	CenterRight,
-	CenterLeft,
 	TopLeft,
+	CenterTop,
 	TopRight,
-	BottomRight,
+	CenterLeft,
+	Center,
+	CenterRight,
 	BottomLeft,
+	CenterBottom,
+	BottomRight,
 };
 
 [[nodiscard]] V2_float GetOffsetFromCenter(const V2_float& size, Origin draw_origin);
+
+inline std::ostream& operator<<(std::ostream& os, Origin origin) {
+	switch (origin) {
+		case Origin::TopLeft:	   os << "Top Left"; break;
+		case Origin::CenterTop:	   os << "Center Top"; break;
+		case Origin::TopRight:	   os << "Top Right"; break;
+		case Origin::CenterLeft:   os << "Center Left"; break;
+		case Origin::Center:	   os << "Center"; break;
+		case Origin::CenterRight:  os << "Center Right"; break;
+		case Origin::BottomLeft:   os << "Bottom Left"; break;
+		case Origin::CenterBottom: os << "Center Bottom"; break;
+		case Origin::BottomRight:  os << "Bottom Right"; break;
+		default:				   PTGN_ERROR("Invalid origin");
+	}
+
+	return os;
+}
 
 } // namespace ptgn
