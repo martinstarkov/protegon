a39e04b8a6e416e34157778cc0c9d9ad71bbf6fb
Martin Starkov <emailregisterlogin@gmail.com>
2024-10-24 14:57:28 +0300

    Add key and value retrieval utility functions

--- src/utility/utility.h
+++ src/utility/utility.h
@@ -1,10 +1,56 @@
 #pragma once
 
 #include <array>
+#include <map>
+#include <type_traits>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
 namespace ptgn {
+
+namespace impl {
+
+template <typename ReturnType, typename Map>
+std::vector<ReturnType> GetElements(const Map& map) {
+	using K = typename Map::key_type;
+	using V = typename Map::mapped_type;
+
+	static_assert(std::is_same_v<ReturnType, K> || std::is_same_v<ReturnType, V>);
+
+	std::vector<ReturnType> elements;
+	elements.reserve(map.size());
+	for (const auto& [key, value] : map) {
+		if constexpr (std::is_same_v<ReturnType, K>) {
+			elements.emplace_back(key);
+		} else if constexpr (std::is_same_v<ReturnType, V>) {
+			elements.emplace_back(value);
+		}
+	}
+	return elements;
+}
+
+} // namespace impl
+
+template <typename Key, typename Value, typename Hash, typename Pred, typename Alloc>
+[[nodiscard]] static auto GetKeys(const std::unordered_map<Key, Value, Hash, Pred, Alloc>& map) {
+	return impl::GetElements<Key>(map);
+}
+
+template <typename Key, typename Value, typename Hash, typename Pred, typename Alloc>
+[[nodiscard]] static auto GetValues(const std::unordered_map<Key, Value, Hash, Pred, Alloc>& map) {
+	return impl::GetElements<Value>(map);
+}
+
+template <typename Key, typename Value, typename Compare, typename Alloc>
+[[nodiscard]] static auto GetKeys(const std::map<Key, Value, Compare, Alloc>& map) {
+	return impl::GetElements<Key>(map);
+}
+
+template <typename Key, typename Value, typename Compare, typename Alloc>
+[[nodiscard]] static auto GetValues(const std::map<Key, Value, Compare, Alloc>& map) {
+	return impl::GetElements<Value>(map);
+}
 
 template <typename T>
 static bool VectorContains(const std::vector<T>& container, const T& value) {
