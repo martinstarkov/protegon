58045ea3bc0ba8abeaccde457ff1be3c70c8278b
Martin Starkov <emailregisterlogin@gmail.com>
2024-10-24 13:48:48 +0300

    Add rigid body util functions

--- src/components/rigid_body.cpp
+++ src/components/rigid_body.cpp
@@ -1,17 +1,27 @@
 #include "components/rigid_body.h"
 
 #include "core/game.h"
+#include "math/vector2.h"
+#include "physics/physics.h"
 
 namespace ptgn {
 
 void RigidBody::Update() {
-	velocity.y += gravity * game.dt();
-	velocity   += acceleration * game.dt();
-	velocity   *= 1.0f / (1.0f + drag * game.dt());
-	// Or alternatively: velocity *= std::clamp(1.0f - drag * game.dt(), 0.0f, 1.0f);
-	if (velocity.MagnitudeSquared() > max_velocity * max_velocity) {
+	float dt{ game.physics.dt() };
+	velocity += gravity * game.physics.GetGravity() * dt;
+	velocity *= 1.0f / (1.0f + drag * dt);
+	// Or alternatively: velocity *= std::clamp(1.0f - drag * dt, 0.0f, 1.0f);
+	if (max_velocity != -1.0f && velocity.MagnitudeSquared() > max_velocity * max_velocity) {
 		velocity = velocity.Normalized() * max_velocity;
 	}
+}
+
+void RigidBody::AddAcceleration(const V2_float& acceleration) {
+	velocity += acceleration * game.physics.dt();
+}
+
+void RigidBody::AddImpulse(const V2_float& impulse) {
+	velocity += impulse;
 }
 
 } // namespace ptgn
--- src/components/rigid_body.h
+++ src/components/rigid_body.h
@@ -6,11 +6,17 @@ namespace ptgn {
 
 struct RigidBody {
 	V2_float velocity;
-	V2_float acceleration;
-	float max_velocity{ 0.0f };
+	// -1.0f means no enforcement of maximum velocity.
+	float max_velocity{ -1.0f };
 	float drag{ 0.0f };
-	// Positive is down.
-	float gravity{ 0.0f };
+	// Gravity relative to game.physics.GetGravity().
+	float gravity{ 1.0f };
+
+	// vel += accel * dt
+	void AddAcceleration(const V2_float& acceleration);
+
+	// vel += impulse
+	void AddImpulse(const V2_float& impulse);
 
 	void Update();
 };
