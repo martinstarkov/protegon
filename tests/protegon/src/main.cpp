b6b2f579dd4ed0495e192f85789675b194f97c65
Martin Starkov <emailregisterlogin@gmail.com>
2024-11-10 01:24:37 +0200

    Add platforming test

--- tests/protegon/src/main.cpp
+++ tests/protegon/src/main.cpp
@@ -9,8 +9,10 @@
 #include "test_math.h"
 #include "test_matrix4.h"
 #include "test_pathfinding.h"
+#include "test_platforming.h"
 #include "test_renderer.h"
 #include "test_rng.h"
+#include "test_scenes.h"
 #include "test_text.h"
 #include "test_tween.h"
 #include "test_vector.h"
@@ -40,6 +42,8 @@ public:
 
 		// Visual tests.
 
+		TestPlatforming();
+		TestScenes();
 		TestCollisions();
 		TestLighting();
 		TestRenderer();
@@ -55,10 +59,7 @@ public:
 		PTGN_INFO("Initialized test scene");
 	}
 
-	void Update() final {
-		PTGN_INFO("Updated test scene");
-		// game.Stop();
-	}
+	void Update() final {}
 
 	void Shutdown() final {
 		PTGN_INFO("Shutdown test scene");
@@ -69,89 +70,7 @@ public:
 	}
 };
 
-static void TransitionScene(
-	std::string_view from, std::string_view to, milliseconds duration = milliseconds{ 250 }
-) {
-	if (game.input.KeyDown(Key::W)) {
-		game.scene.TransitionActive(from, to, { TransitionType::CoverDown, duration });
-	} else if (game.input.KeyDown(Key::S)) {
-		game.scene.TransitionActive(from, to, { TransitionType::CoverUp, duration });
-	} else if (game.input.KeyDown(Key::D)) {
-		game.scene.TransitionActive(from, to, { TransitionType::CoverLeft, duration });
-	} else if (game.input.KeyDown(Key::A)) {
-		game.scene.TransitionActive(from, to, { TransitionType::CoverRight, duration });
-	}
-	if (game.input.KeyDown(Key::T)) {
-		game.scene.TransitionActive(from, to, { TransitionType::UncoverDown, duration });
-	} else if (game.input.KeyDown(Key::G)) {
-		game.scene.TransitionActive(from, to, { TransitionType::UncoverUp, duration });
-	} else if (game.input.KeyDown(Key::F)) {
-		game.scene.TransitionActive(from, to, { TransitionType::UncoverLeft, duration });
-	} else if (game.input.KeyDown(Key::H)) {
-		game.scene.TransitionActive(from, to, { TransitionType::UncoverRight, duration });
-	} else if (game.input.KeyDown(Key::DOWN)) {
-		game.scene.TransitionActive(from, to, { TransitionType::PushDown, duration });
-	} else if (game.input.KeyDown(Key::UP)) {
-		game.scene.TransitionActive(from, to, { TransitionType::PushUp, duration });
-	} else if (game.input.KeyDown(Key::LEFT)) {
-		game.scene.TransitionActive(from, to, { TransitionType::PushLeft, duration });
-	} else if (game.input.KeyDown(Key::RIGHT)) {
-		game.scene.TransitionActive(from, to, { TransitionType::PushRight, duration });
-	} else if (game.input.KeyDown(Key::Q)) {
-		game.scene.TransitionActive(from, to, { TransitionType::Fade, milliseconds{ 1000 } });
-	} else if (game.input.KeyDown(Key::E)) {
-		game.scene.TransitionActive(
-			from, to, { TransitionType::FadeThroughBlack, milliseconds{ 1000 } }
-		);
-	}
-}
-
-class Scene2 : public Scene {
-public:
-	Texture test{ "resources/sprites/bg2.png" };
-
-	Scene2() = default;
-
-	void Init() final {
-		// game.draw.SetClearColor(color::Orange);
-	}
-
-	void Update() final {
-		game.draw.Texture(test, { 400, 400 }, { 800, 800 });
-		TransitionScene("scene2", "scene1");
-	}
-};
-
-class Scene1 : public Scene {
-public:
-	Texture test{ "resources/sprites/bg1.png" };
-
-	Scene1() = default;
-
-	void Init() final {
-		// game.draw.SetClearColor(color::Yellow);
-	}
-
-	void Update() final {
-		game.draw.Texture(test, { 400, 400 }, { 800, 800 });
-		TransitionScene("scene1", "scene2");
-	}
-};
-
-class SceneTransitionTest : public Scene {
-public:
-	SceneTransitionTest() {}
-
-	void Init() {
-		game.scene.Load<Scene1>("scene1");
-		game.scene.Load<Scene2>("scene2");
-		game.window.SetSize({ 800, 800 });
-		game.scene.AddActive("scene1");
-	}
-};
-
 int main() {
-	// game.Start<Tests>();
-	game.Start<SceneTransitionTest>();
+	game.Start<Tests>();
 	return 0;
 }
--- tests/protegon/src/test_platforming.h
+++ tests/protegon/src/test_platforming.h
@@ -1,19 +1,84 @@
 #include <memory>
 #include <vector>
 
+#include "collision/collider.h"
 #include "common.h"
+#include "components/sprite.h"
+#include "components/transform.h"
 #include "core/game.h"
+#include "ecs/ecs.h"
+#include "math/geometry/polygon.h"
+#include "math/vector2.h"
+#include "physics/movement.h"
+#include "physics/rigid_body.h"
+#include "renderer/color.h"
+#include "renderer/origin.h"
 
-class TemplateTest : public Test {
-	void Init() final {}
+constexpr CollisionCategory ground_category{ 1 };
 
-	void Update() final {}
+class PlatformingTest : public Test {
+	ecs::Manager manager;
+
+	ecs::Entity CreatePlatform(const Rect& r) {
+		ecs::Entity entity = manager.CreateEntity();
+		entity.Add<Transform>(r.position, r.rotation);
+		auto& box = entity.Add<BoxCollider>(entity, r.size, r.origin);
+		box.SetCollisionCategory(ground_category);
+		entity.Add<DrawColor>(color::Purple);
+		return entity;
+	}
+
+	ecs::Entity CreatePlayer() {
+		ecs::Entity entity = manager.CreateEntity();
+
+		entity.Add<Transform>(ws / 2.0f + V2_float{ 100, 100 });
+		auto& rb		 = entity.Add<RigidBody>();
+		rb.gravity		 = 1.0f;
+		auto& m			 = entity.Add<PlatformerMovement>();
+		auto& j			 = entity.Add<PlatformerJump>();
+		auto ground_func = [](Collision c) {
+			PlatformerJump::Ground(c, ground_category);
+		};
+		auto& b				 = entity.Add<BoxCollider>(entity, V2_float{ 55, 129 }, Origin::Center);
+		b.on_collision_start = ground_func;
+		b.on_collision		 = ground_func;
+		b.on_collision_stop	 = ground_func;
+		b.continuous		 = true;
+
+		entity.Add<DrawColor>(color::DarkGreen);
+		entity.Add<DrawLineWidth>(-1.0f);
+
+		return entity;
+	}
+
+	void Init() final {
+		manager.Clear();
+		game.window.SetSize({ 960, 540 });
+		ws = game.window.GetSize();
+
+		CreatePlayer();
+		CreatePlatform({ { 0, ws.y - 10 }, { ws.x, 10 }, Origin::TopLeft });
+		CreatePlatform({ { 0, ws.y / 2.0f }, { 200, 10 }, Origin::TopLeft });
+		CreatePlatform({ { ws.x, ws.y / 2.0f }, { 200, 10 }, Origin::TopRight });
+		manager.Refresh();
+	}
+
+	void Update() final {
+		game.physics.Update(manager);
+		Draw();
+	}
+
+	void Draw() {
+		for (auto [e, b] : manager.EntitiesWith<BoxCollider>()) {
+			DrawRect(e, b.GetAbsoluteRect());
+		}
+	}
 };
 
-void TestTemplateNames() {
+void TestPlatforming() {
 	std::vector<std::shared_ptr<Test>> tests;
 
-	tests.emplace_back(new TemplateTest());
+	tests.emplace_back(new PlatformingTest());
 
 	AddTests(tests);
 }
