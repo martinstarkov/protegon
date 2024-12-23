e56d9795316a37bdd7f3b36cb4e8dd6535f90e8a
Martin <emailregisterlogin@gmail.com>
2024-12-23 12:08:57 -0500

    Update gl_loader.cpp

--- src/renderer/gl_loader.cpp
+++ src/renderer/gl_loader.cpp
@@ -2,9 +2,13 @@
 
 namespace ptgn::gl {
 
+#ifndef PTGN_PLATFORM_MACOS
+
 #define GLE(name, caps_name) PFNGL##caps_name##PROC name;
 GL_LIST_1
 #undef GLE
+
+#endif
 
 #ifdef __EMSCRIPTEN__
 
@@ -17,11 +21,16 @@ GL_LIST_3
 
 #else
 
+#ifndef PTGN_PLATFORM_MACOS
+
 #define GLE(name, caps_name) PFNGL##caps_name##PROC name;
 GL_LIST_2
 GL_LIST_3
 #undef GLE
 
 #endif
+
+#endif
+
 
 } // namespace ptgn::gl
