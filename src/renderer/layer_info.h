a42280c57f5fcc203a8e8492e2f871b7a8e96eb9
Martin Starkov <emailregisterlogin@gmail.com>
2024-12-31 09:23:17 -0500

    Add render target to layer info

--- src/renderer/layer_info.h
+++ src/renderer/layer_info.h
@@ -2,20 +2,27 @@
 
 #include <cstddef>
 
+#include "renderer/render_target.h"
+
 namespace ptgn {
 
+// Information relating to the z index and render target of the texture.
+// Defaults to currently active scene.
 struct LayerInfo {
 	LayerInfo() = default;
 
-	explicit LayerInfo(float z_index) : z_index{ z_index } {}
+	explicit LayerInfo(const RenderTarget& render_target) : render_target{ render_target } {}
 
-	explicit LayerInfo(std::size_t render_layer) : render_layer{ render_layer } {}
-
-	LayerInfo(float z_index, std::size_t render_layer) :
-		z_index{ z_index }, render_layer{ render_layer } {}
+	// @param z_index The z coordinate at which an object is drawn.
+	// Higher values are closer to the camera, leading to objects being rendered on top of others.
+	// Negative values are furthest away from camera.
+	// @param render_target The render target which is used for rendering. Default value of {}
+	// refers to the currently active scene.
+	LayerInfo(float z_index, const RenderTarget& render_target = {}) :
+		z_index{ z_index }, render_target{ render_target } {}
 
 	float z_index{ 0.0f };
-	std::size_t render_layer{ 0 };
+	RenderTarget render_target;
 };
 
 } // namespace ptgn
