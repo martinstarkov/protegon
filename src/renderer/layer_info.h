96572aad2a240b919649f4367002afae50298f29
Martin Starkov <emailregisterlogin@gmail.com>
2025-01-02 08:52:06 -0500

    Change z index to render layer

--- src/renderer/layer_info.h
+++ src/renderer/layer_info.h
@@ -13,15 +13,15 @@ struct LayerInfo {
 
 	explicit LayerInfo(const RenderTarget& render_target) : render_target{ render_target } {}
 
-	// @param z_index The z coordinate at which an object is drawn.
+	// @param render_layer The render layer on which the object is drawn.
 	// Higher values are closer to the camera, leading to objects being rendered on top of others.
 	// Negative values are furthest away from camera.
 	// @param render_target The render target which is used for rendering. Default value of {}
 	// refers to the currently active scene.
-	LayerInfo(float z_index, const RenderTarget& render_target = {}) :
+	LayerInfo(std::int32_t render_layer, const RenderTarget& render_target = {}) :
 		z_index{ z_index }, render_target{ render_target } {}
 
-	float z_index{ 0.0f };
+	std::int32_t render_layer{ 0 };
 	RenderTarget render_target;
 };
 
