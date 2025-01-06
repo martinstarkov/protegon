a7c569c2a68a5944a57e138be68f769ed97469ad
Martin Starkov <emailregisterlogin@gmail.com>
2025-01-05 23:12:41 -0500

    ToString with precision

--- src/utility/string.h
+++ src/utility/string.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iomanip>
 #include <iosfwd>
 #include <sstream>
 #include <string>
@@ -13,6 +14,22 @@ std::string ToString(const T& object) {
 	std::ostringstream ss;
 	ss << object;
 	return ss.str();
+}
+
+// @param precision The number of decimal places of precision to have in numbers converted to
+// string.
+template <typename T, tt::stream_writable<std::ostream, T> = true>
+std::string ToString(const T& object, int precision) {
+	std::ostringstream ss;
+	ss << std::fixed << std::setprecision(precision);
+	ss << object;
+	if constexpr (std::is_floating_point_v<T>) {
+		// Catch and remove -0s. As per: https://stackoverflow.com/a/21538723
+		const std::string& t{ ss.str() };
+		return t.find_first_of("123456789") == t.npos && t[0] == '-' ? t.substr(1) : t;
+	} else {
+		return ss.str();
+	}
 }
 
 } // namespace ptgn
