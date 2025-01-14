8218bddb260174ad644489717585fb0987c61683
Martin <emailregisterlogin@gmail.com>
2025-01-14 14:39:12 +0100

    Added demo folders

---
+++ demos/parallax/src/application.cpp
@@ -0,0 +1,18 @@
+#include "protegon/protegon.h"
+
+using namespace ptgn;
+
+constexpr V2_int resolution{ 800, 800 };
+
+class TemplateScene : public Scene {
+public:
+	void Init() override {}
+
+	void Update() override {}
+};
+
+int main([[maybe_unused]] int c, [[maybe_unused]] char** v) {
+	game.Init("TemplateTitle", resolution);
+	game.scene.LoadActive<TemplateScene>("template_scene");
+	return 0;
+}
