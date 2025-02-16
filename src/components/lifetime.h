1ca5884f60afe27c8831cc541961144da496a1f2
Martin <emailregisterlogin@gmail.com>
2025-02-16 20:37:52 +0100

    Lifetime component improvements

--- src/components/lifetime.h
+++ src/components/lifetime.h
@@ -1,13 +1,33 @@
 #pragma once
 
-#include "utility/timer.h"
+#include "ecs/ecs.h"
 #include "utility/time.h"
+#include "utility/timer.h"
 
 namespace ptgn {
 
 struct Lifetime {
+	Lifetime(milliseconds duration, bool start = false) : duration{ duration } {
+		if (start) {
+			timer_.Start();
+		}
+	}
+
+	// Will restart if lifetime is already running.
+	void Start() {
+		timer_.Start();
+	}
+
+	void Update(ecs::Entity e) const {
+		if (timer_.Completed(duration)) {
+			e.Destroy();
+		}
+	}
+
 	milliseconds duration{ 0 };
-	Timer timer;
+
+private:
+	Timer timer_;
 };
 
 } // namespace ptgn
