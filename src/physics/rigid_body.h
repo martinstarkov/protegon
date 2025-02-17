51bde5fabbf54dbb085d0cf52d2e6d0db046ea91
Martin Starkov <emailregisterlogin@gmail.com>
2025-02-17 04:45:59 +0200

    Fixed aggregate init

--- src/physics/rigid_body.h
+++ src/physics/rigid_body.h
@@ -1,17 +1,21 @@
 #pragma once
 
 #include "math/vector2.h"
+#include "serialization/fwd.h"
 
 namespace ptgn {
 
 struct RigidBody {
-	V2_float velocity;
-	// -1 means no enforcement of maximum velocity.
-	float max_velocity{ -1.0f };
+	RigidBody() = default;
+	RigidBody(float max_speed, float drag, float gravity, bool immovable);
+
+	// -1 means no enforcement of maximum speed.
+	float max_speed{ -1.0f };
 	float drag{ 0.0f };
 	// Gravity relative to game.physics.GetGravity().
 	float gravity{ 0.0f };
 	bool immovable{ false };
+	V2_float velocity;
 
 	// vel += accel * dt
 	// @param dt Unit: seconds.
@@ -23,5 +27,9 @@ struct RigidBody {
 	// @param dt Unit: seconds.
 	void Update(const V2_float& physics_gravity, float dt);
 };
+
+void to_json(json& j, const RigidBody& rb);
+
+void from_json(const json& j, RigidBody& rb);
 
 } // namespace ptgn
