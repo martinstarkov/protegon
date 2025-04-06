9da0dcce24a2894fb3d904f1a409318a27e3475c
Martin Starkov <emailregisterlogin@gmail.com>
2025-04-06 13:07:17 +0300

    Add constexpr string_view hash

--- src/math/hash.h
+++ src/math/hash.h
@@ -8,13 +8,21 @@
 
 namespace ptgn {
 
+//[[nodiscard]] inline std::size_t Hash(std::string_view string) {
+//	return std::hash<std::string_view>()(string);
+//}
+
 /*
  * Hash a string into a number.
  * @param string The string to hash.
  * @return Unique positive integer corresponding to the string.
  */
-[[nodiscard]] inline std::size_t Hash(std::string_view string) {
-	return std::hash<std::string_view>()(string);
+[[nodiscard]] constexpr std::size_t Hash(std::string_view string) {
+	std::size_t hash{ 5381 };
+	for (const auto& c : string) {
+		hash = (33 * hash) ^ c;
+	}
+	return hash;
 }
 
 template <typename T>
