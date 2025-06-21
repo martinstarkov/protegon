a4862b5d5e52189a862ae70494990c50e81c5a5c
Martin <emailregisterlogin@gmail.com>
2025-06-21 04:43:47 +0200

    Add circle

---
+++ src/rendering/graphics/circle.cpp
@@ -0,0 +1,92 @@
+#include "rendering/graphics/circle.h"
+
+#include <array>
+#include <vector>
+
+#include "algorithm"
+#include "common/assert.h"
+#include "components/draw.h"
+#include "components/offsets.h"
+#include "components/transform.h"
+#include "core/entity.h"
+#include "core/game.h"
+#include "math/geometry.h"
+#include "math/vector2.h"
+#include "rendering/api/color.h"
+#include "rendering/batching/render_data.h"
+#include "rendering/resources/shader.h"
+#include "scene/camera.h"
+#include "scene/scene.h"
+
+namespace ptgn {
+
+Circle::Circle(float circle_radius) : radius{ circle_radius } {}
+
+void Circle::Draw(impl::RenderData& ctx, const Entity& entity) {
+	PTGN_ASSERT(entity.Has<Circle>());
+
+	auto transform{ entity.GetAbsoluteTransform() };
+	auto depth{ entity.GetDepth() };
+	auto blend_mode{ entity.GetBlendMode() };
+	auto tint{ entity.GetTint() };
+	auto origin{ entity.GetOrigin() };
+
+	const auto& circle{ entity.Get<Circle>() };
+	auto line_width{ entity.GetOrDefault<LineWidth>() };
+
+	auto offset_transform{ GetOffset(entity) };
+	transform = transform.RelativeTo(offset_transform);
+
+	auto camera{ entity.GetOrDefault<Camera>() };
+
+	auto scaled_radius{ circle.radius * Abs(transform.scale) };
+
+	// TODO: Make a zero scaled_size use cached camera vertices instead.
+	PTGN_ASSERT(!scaled_radius.IsZero());
+
+	// TODO: Cache everything from here onward.
+
+	std::array<V2_float, 4> quad_points{ impl::GetVertices(transform, scaled_radius, origin) };
+
+	impl::RenderState render_state;
+
+	render_state.blend_mode	   = blend_mode;
+	render_state.shader_passes = { game.shader.Get<ShapeShader::Circle>() };
+	render_state.camera		   = camera;
+
+	if (line_width == -1.0f) {
+		// Internally line width for a filled ellipse is 1.0f.
+		line_width = 1.0f;
+	} else {
+		PTGN_ASSERT(line_width >= ctx.min_line_width, "Invalid line width for circle");
+
+		// Internally line width for a completely hollow ellipse is 0.0f.
+		// TODO: Check that dividing by std::max(scaled_radius.x, scaled_radius.y) does not cause
+		// any unexpected bugs.
+		line_width = 0.005f + line_width / std::min(scaled_radius.x, scaled_radius.y);
+	}
+
+	auto quad_vertices{ impl::GetQuadVertices(
+		quad_points, tint, depth, line_width, impl::default_texture_coordinates
+	) };
+	ctx.AddQuad(quad_vertices, render_state);
+}
+
+Entity CreateCircle(
+	Scene& scene, const V2_float& position, float radius, const Color& color, float line_width
+) {
+	auto circle{ scene.CreateEntity() };
+
+	circle.SetDraw<Circle>();
+	circle.Show();
+
+	circle.Add<Transform>(position);
+	circle.Add<Circle>(radius);
+
+	circle.SetTint(color);
+	circle.Add<LineWidth>(line_width);
+
+	return circle;
+}
+
+} // namespace ptgn
---
+++ src/rendering/graphics/circle.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "components/drawable.h"
+#include "core/entity.h"
+#include "math/vector2.h"
+#include "rendering/api/color.h"
+#include "serialization/serializable.h"
+
+namespace ptgn {
+
+class Scene;
+
+namespace impl {
+
+class RenderData;
+
+} // namespace impl
+
+struct Circle : public Drawable<Circle> {
+	Circle() = default;
+
+	Circle(float circle_radius);
+
+	static void Draw(impl::RenderData& ctx, const Entity& entity);
+
+	float radius{ 0.0f };
+
+	PTGN_SERIALIZER_REGISTER(Circle, radius)
+};
+
+/**
+ * @brief Creates a circle entity in the scene.
+ *
+ * @param scene       Reference to the scene where the circle will be created.
+ * @param position    The position of the circle relative to its parent camera.
+ * @param radius        The radius of the circle.
+ * @param color       The tint color of the circle.
+ * @param line_width  Optional outline width. If -1.0f, the circle is filled. If positive, an
+ * outlined circle is created.
+ * @return Entity     A handle to the newly created circle entity.
+ */
+Entity CreateCircle(
+	Scene& scene, const V2_float& position, float radius, const Color& color,
+	float line_width = -1.0f
+);
+
+} // namespace ptgn
