1c12164f8ab981ab9d0bf6d0f76d5f07ae5dcc26
Martin <emailregisterlogin@gmail.com>
2025-06-21 04:47:27 +0200

    Moved out rendering code

--- demos/sandbox/src/application.cpp
+++ demos/sandbox/src/application.cpp
@@ -1,96 +1,22 @@
-#include <array>
-#include <vector>
 
-#include "components/drawable.h"
-#include "components/offsets.h"
-#include "core/entity.h"
 #include "core/game.h"
-#include "math/math.h"
 #include "math/rng.h"
 #include "math/vector2.h"
-#include "rendering/api/blend_mode.h"
 #include "rendering/api/color.h"
-#include "rendering/batching/render_data.h"
+#include "rendering/graphics/circle.h"
+#include "rendering/graphics/rect.h"
 #include "rendering/graphics/vfx/light.h"
-#include "rendering/resources/shader.h"
 #include "scene/scene.h"
 #include "scene/scene_manager.h"
 
 using namespace ptgn;
 
-constexpr V2_int window_size{ 1000, 1000 }; //{ 1280, 720 };
-
-/*
-
-ctx.DrawQuad();
-ctx.DrawQuad();
-ctx.DrawQuad();
-ctx.DrawQuad();
-ctx.DrawQuad();
-ctx.DrawCircle();
-ctx.DrawCircle();
-ctx.DrawCircle();
-ctx.DrawLight();
-ctx.DrawLight();
-ctx.DrawLight();
-
-*/
-
-struct Quad : public Drawable<Quad> {
-	Quad() {}
-
-	static void Draw(impl::RenderData& ctx, const Entity& entity) {
-		impl::RenderState render_state;
-		render_state.blend_mode	   = entity.GetBlendMode();
-		render_state.shader_passes = { game.shader.Get<ShapeShader::Quad>() };
-		ctx.AddQuad(entity.Get<QuadVertices>().vertices, render_state);
-	}
-};
-
-struct Circle : public Drawable<Circle> {
-	Circle() {}
-
-	static void Draw(impl::RenderData& ctx, const Entity& entity) {
-		impl::RenderState render_state;
-		render_state.blend_mode	   = entity.GetBlendMode();
-		render_state.shader_passes = { game.shader.Get<ShapeShader::Circle>() };
-		ctx.AddQuad(entity.Get<QuadVertices>().vertices, render_state);
-	}
-};
-
-struct Light : public Drawable<Light> {
-	Light() {}
-
-	static void SetUniform(const Entity& entity, const Shader& shader) {
-		PointLight light{ entity };
-
-		auto offset_transform{ GetOffset(entity) };
-		auto transform{ entity.GetAbsoluteTransform() };
-		transform = transform.RelativeTo(offset_transform);
-		float radius{ light.GetRadius() * Abs(transform.scale.x) };
-
-		shader.SetUniform("u_LightPosition", transform.position);
-		shader.SetUniform("u_LightIntensity", light.GetIntensity());
-		shader.SetUniform("u_LightRadius", radius);
-		shader.SetUniform("u_Falloff", light.GetFalloff());
-		shader.SetUniform("u_Color", light.GetColor().Normalized());
-		auto ambient_color{ PointLight::GetShaderColor(light.GetAmbientColor()) };
-		shader.SetUniform("u_AmbientColor", ambient_color);
-		shader.SetUniform("u_AmbientIntensity", light.GetAmbientIntensity());
-	}
-
-	static void Draw(impl::RenderData& ctx, const Entity& entity) {
-		impl::RenderState render_state;
-		render_state.blend_mode	   = BlendMode::Add;
-		render_state.shader_passes = { impl::ShaderPass{ game.shader.Get<OtherShader::Light>(),
-														 &SetUniform } };
-		ctx.AddShader(entity, render_state, BlendMode::Add, false);
-	}
-};
+constexpr V2_int window_size{ 800, 800 };
 
 struct SandboxScene : public Scene {
 	static constexpr int X = 100;									// Number of random quads
-	static constexpr int Y = 10;									// Number of random lights
+	static constexpr int Y = 100;									// Number of random circles
+	static constexpr int Z = 10;									// Number of random lights
 
 	RNG<float> pos_rngx{ 0.0f, static_cast<float>(window_size.x) }; // Position range
 	RNG<float> pos_rngy{ 0.0f, static_cast<float>(window_size.y) }; // Position
@@ -100,64 +26,29 @@ struct SandboxScene : public Scene {
 
 	void Enter() override {
 		for (int i = 0; i < X; ++i) {
-			V2_float top_left{ pos_rngx(), pos_rngy() };
-
-			V2_float size{ size_rng(), size_rng() };
-
-			std::array<V2_float, 4> points{ top_left,
-											{ top_left.x + size.x, top_left.y },
-											{ top_left.x + size.x, top_left.y + size.y },
-											{ top_left.x, top_left.y + size.y } };
-
-			/*auto e{ CreatePointLight(*this, top_left, 50.0f, color::Blue, 1.0f, 2.0f) };
-			e.SetDraw<Light>();
-			e.Show();*/
-			auto e{ CreateEntity() };
-			float texture_index{ 0.0f };
-			float line_width{ 10.0f };
-			V2_float radius{ size };
-			texture_index = 0.0f; // 1.0f; // 0.005f + line_width / std::min(radius.x, radius.y);
-								  // e.SetDraw<Circle>();
-			e.SetDraw<Quad>();
-			e.Add<QuadVertices>(
-				impl::GetQuadVertices(points, Color::RandomTransparent(), {}, texture_index)
+			CreateRect(
+				*this, { pos_rngx(), pos_rngy() }, { size_rng(), size_rng() },
+				Color::RandomTransparent(), -1.0f
 			);
-			e.Show();
 		}
 
 		for (int i = 0; i < Y; ++i) {
-			V2_float top_left{ pos_rngx(), pos_rngy() };
+			CreateCircle(
+				*this, { pos_rngx(), pos_rngy() }, size_rng(), Color::RandomTransparent(), -1.0f
+			);
+		}
 
-			V2_float size{ size_rng(), size_rng() };
-
-			std::array<V2_float, 4> points{ top_left,
-											{ top_left.x + size.x, top_left.y },
-											{ top_left.x + size.x, top_left.y + size.y },
-											{ top_left.x, top_left.y + size.y } };
-
-			auto e{ CreatePointLight(
-				*this, top_left, light_radius_rng(), color::Blue, intensity_rng(), 2.0f
-			) };
-			e.SetDraw<Light>();
-			e.Show();
-			//   auto e{ CreateEntity() };
-			//   float texture_index{ 0.0f };
-			//   float line_width{ 10.0f };
-			//   V2_float radius{ size };
-			//   texture_index = 0.0f; // 1.0f; // 0.005f + line_width / std::min(radius.x,
-			//   radius.y);
-			//					  // e.SetDraw<Circle>();
-			//   e.SetDraw<Quad>();
-			//   e.Add<QuadVertices>(
-			//	impl::GetQuadVertices(points, Color::RandomTransparent(), {}, texture_index)
-			//);
-			//   e.Show();
+		for (int i = 0; i < Z; ++i) {
+			CreatePointLight(
+				*this, { pos_rngx(), pos_rngy() }, light_radius_rng(), color::Blue, intensity_rng(),
+				2.0f
+			);
 		}
 	}
 };
 
 int main([[maybe_unused]] int c, [[maybe_unused]] char** v) {
-	game.Init("SandboxScene", window_size, color::Transparent);
+	game.Init("SandboxScene", window_size);
 	game.scene.Enter<SandboxScene>("");
 	return 0;
 }
