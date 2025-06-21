b11a238324f1f079ca86a89786272f2a20838c6c
Martin <emailregisterlogin@gmail.com>
2025-06-21 04:43:51 +0200

    Add rect

---
+++ src/rendering/graphics/rect.cpp
@@ -0,0 +1,104 @@
+#include "rendering/graphics/rect.h"
+
+#include <array>
+#include <vector>
+
+#include "common/assert.h"
+#include "components/draw.h"
+#include "components/offsets.h"
+#include "components/transform.h"
+#include "core/entity.h"
+#include "core/game.h"
+#include "math/geometry.h"
+#include "math/vector2.h"
+#include "rendering/api/color.h"
+#include "rendering/api/origin.h"
+#include "rendering/batching/render_data.h"
+#include "rendering/batching/vertex.h"
+#include "rendering/resources/shader.h"
+#include "scene/camera.h"
+#include "scene/scene.h"
+
+namespace ptgn {
+
+Rect::Rect(const V2_float& rect_size) : size{ rect_size } {}
+
+void Rect::Draw(impl::RenderData& ctx, const Entity& entity) {
+	PTGN_ASSERT(entity.Has<Rect>());
+
+	auto transform{ entity.GetAbsoluteTransform() };
+	auto depth{ entity.GetDepth() };
+	auto blend_mode{ entity.GetBlendMode() };
+	auto tint{ entity.GetTint() };
+	auto origin{ entity.GetOrigin() };
+
+	const auto& rect{ entity.Get<Rect>() };
+	auto line_width{ entity.GetOrDefault<LineWidth>() };
+
+	auto offset_transform{ GetOffset(entity) };
+	transform = transform.RelativeTo(offset_transform);
+
+	auto camera{ entity.GetOrDefault<Camera>() };
+
+	auto scaled_size{ rect.size * Abs(transform.scale) };
+
+	// TODO: Make a zero scaled_size use cached camera vertices instead.
+	PTGN_ASSERT(!scaled_size.IsZero());
+
+	// TODO: Cache everything from here onward.
+
+	std::array<V2_float, 4> quad_points{ impl::GetVertices(transform, scaled_size, origin) };
+
+	auto quad_vertices{
+		impl::GetQuadVertices(quad_points, tint, depth, 0.0f, impl::default_texture_coordinates)
+	};
+
+	impl::RenderState render_state;
+
+	render_state.blend_mode	   = blend_mode;
+	render_state.shader_passes = { game.shader.Get<ShapeShader::Quad>() };
+	render_state.camera		   = camera;
+
+	if (line_width == -1.0f) {
+		ctx.AddQuad(quad_vertices, render_state);
+		return;
+	}
+
+	PTGN_ASSERT(line_width >= ctx.min_line_width, "Invalid line width for Rect");
+
+	// Compose the quad out of 4 lines, each made up of line_width wide quads.
+	for (std::size_t i{ 0 }; i < quad_points.size(); i++) {
+		auto start{ camera.ZoomIfNeeded(quad_points[i]) };
+		auto end{ camera.ZoomIfNeeded(quad_points[(i + 1) % quad_points.size()]) };
+
+		auto line_points{ impl::GetLineQuadVertices(start, end, line_width) };
+
+		for (std::size_t j{ 0 }; j < line_points.size(); j++) {
+			quad_vertices[j].position[0] = line_points[j].x;
+			quad_vertices[j].position[1] = line_points[j].y;
+		}
+
+		ctx.AddQuad(quad_vertices, render_state);
+	}
+}
+
+Entity CreateRect(
+	Scene& scene, const V2_float& position, const V2_float& size, const Color& color,
+	float line_width, Origin origin
+) {
+	auto rect{ scene.CreateEntity() };
+
+	rect.SetDraw<Rect>();
+	rect.Show();
+
+	rect.Add<Transform>(position);
+	rect.Add<Rect>(size);
+	rect.SetOrigin(origin);
+
+	rect.SetTint(color);
+	rect.Add<LineWidth>(line_width);
+
+	return rect;
+}
+
+} // namespace ptgn
---
+++ src/rendering/graphics/rect.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "components/drawable.h"
+#include "core/entity.h"
+#include "math/vector2.h"
+#include "rendering/api/color.h"
+#include "rendering/api/origin.h"
+#include "serialization/serializable.h"
+
+namespace ptgn {
+
+class Scene;
+
+namespace impl {
+
+class RenderData;
+
+} // namespace impl
+
+struct Rect : public Drawable<Rect> {
+	Rect() = default;
+
+	Rect(const V2_float& rect_size);
+
+	static void Draw(impl::RenderData& ctx, const Entity& entity);
+
+	V2_float size;
+
+	PTGN_SERIALIZER_REGISTER(Rect, size)
+};
+
+/**
+ * @brief Creates a rectangle entity in the scene.
+ *
+ * @param scene       Reference to the scene where the rectangle will be created.
+ * @param position    The position of the rectangle relative to its parent camera.
+ * @param size        The width and height of the rectangle.
+ * @param color       The tint color of the rectangle.
+ * @param line_width  Optional outline width. If -1.0f, the rectangle is filled. If positive, an
+ * outlined rectangle is created.
+ * @param origin      The origin of the rectangle position (e.g., center, top-left).
+ * @return Entity     A handle to the newly created rectangle entity.
+ */
+Entity CreateRect(
+	Scene& scene, const V2_float& position, const V2_float& size, const Color& color,
+	float line_width = -1.0f, Origin origin = Origin::Center
+);
+
+} // namespace ptgn
