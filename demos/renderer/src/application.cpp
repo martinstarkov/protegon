e4ba2e9dd7433a7cd61d60ce3a8fa3ee7b78b367
Martin <emailregisterlogin@gmail.com>
2025-06-22 04:32:13 +0200

    Add sprite tests

--- demos/renderer/src/application.cpp
+++ demos/renderer/src/application.cpp
@@ -1,4 +1,5 @@
 
+#include "components/draw.h"
 #include "core/game.h"
 #include "events/input_handler.h"
 #include "events/key.h"
@@ -19,31 +20,6 @@ constexpr int start_test_index{ 2 };
 using SceneBuilder = std::function<void(Scene&)>;
 std::vector<SceneBuilder> tests;
 
-struct RendererScene : public Scene {
-	int test_index{ start_test_index };
-
-	void CycleTest(bool condition, int amount) {
-		if (!condition) {
-			return;
-		}
-		test_index = Mod(test_index + amount, static_cast<int>(tests.size()));
-		ReEnter();
-	}
-
-	void Enter() override {
-		PTGN_LOG("-------- Test ", test_index, " --------");
-		PTGN_ASSERT(test_index < tests.size());
-		if (tests[test_index]) {
-			std::invoke(tests[test_index], *this);
-		}
-	}
-
-	void Update() override {
-		CycleTest(game.input.KeyDown(Key::Q), -1);
-		CycleTest(game.input.KeyDown(Key::E), 1);
-	}
-};
-
 RNG<float> pos_rngx{ 0.0f, static_cast<float>(window_size.x) };
 RNG<float> pos_rngy{ 0.0f, static_cast<float>(window_size.y) };
 RNG<float> size_rng{ 10.0f, 70.0f };
@@ -51,8 +27,11 @@ RNG<float> light_radius_rng{ 10.0f, 200.0f };
 RNG<float> intensity_rng{ 0.0f, 10.0f };
 
 void GenerateTestCases() {
+	LoadResource("test", "resources/test1.jpg");
+
 	auto rect = [](Scene& s) {
 		CreateRect(s, { 100, 100 }, { 50, 50 }, color::Red, -1.0f);
+
 		PTGN_LOG("Rect");
 	};
 
@@ -61,8 +40,13 @@ void GenerateTestCases() {
 		PTGN_LOG("Circle");
 	};
 
+	auto sprite = [](Scene& s) {
+		CreateSprite(s, "test").SetPosition({ 500, 500 });
+		PTGN_LOG("Sprite");
+	};
+
 	auto light = [](Scene& s) {
-		CreatePointLight(s, { 300, 300 }, 100.0f, color::Purple, 1.0f, 1.0f);
+		CreatePointLight(s, { 400, 400 }, 100.0f, color::Purple, 1.0f, 1.0f);
 		PTGN_LOG("Point light");
 	};
 
@@ -71,7 +55,7 @@ void GenerateTestCases() {
 		PTGN_LOG("Bloom");
 	};
 
-	std::vector<std::function<void(Scene&)>> primitives = { rect, circle, light, fx };
+	std::vector<std::function<void(Scene&)>> primitives = { rect, circle, sprite, light, fx };
 
 	// Generate all combinations of 1, 2, and 3 object creations in different orders
 	for (size_t i = 0; i < primitives.size(); ++i) {
@@ -101,9 +85,10 @@ void GenerateTestCases() {
 
 	// Add one test with all 4
 	tests.emplace_back([](Scene& s) {
-		CreateRect(s, { 120, 120 }, { 40, 40 }, color::Magenta, -1);
-		CreateCircle(s, { 220, 220 }, 35.0f, color::Cyan, -1);
-		CreatePointLight(s, { 320, 320 }, 90.0f, color::Orange, 6.0f, 2.0f);
+		CreateRect(s, { 100, 100 }, { 40, 40 }, color::Magenta, -1);
+		CreateCircle(s, { 200, 200 }, 35.0f, color::Cyan, -1);
+		CreateSprite(s, "test").SetPosition({ 500, 500 });
+		CreatePointLight(s, { 400, 400 }, 90.0f, color::Orange, 1.0f, 2.0f);
 		// CreateFX(s, "Grain");
 		PTGN_LOG("All 4 test case");
 	});
@@ -112,8 +97,36 @@ void GenerateTestCases() {
 	tests.emplace_back([](Scene& s) { PTGN_LOG("Empty scene"); });
 }
 
+struct RendererScene : public Scene {
+	int test_index{ start_test_index };
+
+	RendererScene() {
+		GenerateTestCases();
+	}
+
+	void CycleTest(bool condition, int amount) {
+		if (!condition) {
+			return;
+		}
+		test_index = Mod(test_index + amount, static_cast<int>(tests.size()));
+		ReEnter();
+	}
+
+	void Enter() override {
+		PTGN_LOG("-------- Test ", test_index, " --------");
+		PTGN_ASSERT(test_index < tests.size());
+		if (tests[test_index]) {
+			std::invoke(tests[test_index], *this);
+		}
+	}
+
+	void Update() override {
+		CycleTest(game.input.KeyDown(Key::Q), -1);
+		CycleTest(game.input.KeyDown(Key::E), 1);
+	}
+};
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
-	GenerateTestCases();
 	game.Init("RendererScene", window_size, color::White);
 	game.scene.Enter<RendererScene>("");
 	return 0;
