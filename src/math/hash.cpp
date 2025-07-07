04f4938336aa79a03a6300fab79677c6128775f0
Martin Starkov <emailregisterlogin@gmail.com>
2025-07-06 20:46:49 -0400

    Update hash.cpp

--- src/math/hash.cpp
+++ src/math/hash.cpp
@@ -10,9 +10,9 @@ std::size_t Hash(const Vector2<T>& vector) {
 	return std::hash<Vector2<T>>()(vector);
 }
 
-// template std::size_t Hash<int>(const Vector2<int>&);
-// template std::size_t Hash<float>(const Vector2<float>&);
-// template std::size_t Hash<double>(const Vector2<double>&);
+template std::size_t Hash<int>(const Vector2<int>&);
+template std::size_t Hash<float>(const Vector2<float>&);
+template std::size_t Hash<double>(const Vector2<double>&);
 
 std::size_t Hash(const Entity& entity) {
 	return std::hash<Entity>()(entity);
