5b6d89fc41ee61d13827727d7c84c2b3d04fecd9
Martin <emailregisterlogin@gmail.com>
2025-07-17 01:52:49 +0200

    Add chunk serialization and caching to ChunkManager

--- src/tile/chunk.cpp
+++ src/tile/chunk.cpp
@@ -1,18 +1,34 @@
 #include "tile/chunk.h"
 
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <list>
 #include <type_traits>
+#include <unordered_map>
 #include <unordered_set>
 #include <utility>
 #include <vector>
 
 #include "common/assert.h"
 #include "core/entity.h"
+#include "core/manager.h"
+#include "math/noise.h"
 #include "math/vector2.h"
+#include "nlohmann/json.hpp"
+#include "rendering/api/color.h"
+#include "rendering/api/origin.h"
+#include "rendering/renderer.h"
 #include "scene/camera.h"
+#include "serialization/fwd.h"
 
 namespace ptgn {
 
 Chunk::Chunk(const std::vector<Entity>& chunk_entities) : entities{ chunk_entities } {}
+
+Chunk::Chunk(const json& j, Manager& manager) {
+	Deserialize(j, manager);
+}
 
 Chunk::Chunk(Chunk&& other) noexcept : entities{ std::exchange(other.entities, {}) } {}
 
@@ -23,6 +39,37 @@ Chunk& Chunk::operator=(Chunk&& other) noexcept {
 	return *this;
 }
 
+bool Chunk::HasChanged() const {
+	return has_changed_;
+}
+
+void Chunk::FlagAsChanged(bool changed) {
+	has_changed_ = changed;
+}
+
+json Chunk::Serialize() const {
+	json j{};
+
+	for (const auto& entity : entities) {
+		j["entities"].emplace_back(entity.Serialize());
+	}
+
+	return j;
+}
+
+void Chunk::Deserialize(const json& j, Manager& manager) {
+	PTGN_ASSERT(entities.empty());
+	const auto& json_entities{ j["entities"] };
+	entities.reserve(json_entities.size());
+	for (const auto& entity : json_entities) {
+		PTGN_ASSERT(entity != json{});
+		auto e{ manager.CreateEntity() };
+		manager.Refresh();
+		e.Deserialize(entity);
+		entities.emplace_back(e);
+	}
+}
+
 Chunk::~Chunk() {
 	for (auto& entity : entities) {
 		entity.Destroy();
@@ -30,7 +77,7 @@ Chunk::~Chunk() {
 }
 
 Entity NoiseLayer::GetEntity(const V2_int& tile_coordinate, const V2_int& tile_size) const {
-	if (callback == nullptr) {
+	if (!callback) {
 		return {};
 	}
 	float noise_value{
@@ -42,16 +89,16 @@ Entity NoiseLayer::GetEntity(const V2_int& tile_coordinate, const V2_int& tile_s
 
 ChunkManager::ChunkManager(ChunkManager&& other) noexcept :
 	chunks{ std::exchange(other.chunks, {}) },
-	noise_layers{ std::exchange(other.noise_layers, {}) },
 	tile_size{ std::exchange(other.tile_size, {}) },
-	chunk_size{ std::exchange(other.chunk_size, {}) } {}
+	chunk_size{ std::exchange(other.chunk_size, {}) },
+	noise_layers_{ std::exchange(other.noise_layers_, {}) } {}
 
 ChunkManager& ChunkManager::operator=(ChunkManager&& other) noexcept {
 	if (this != &other) {
-		chunks		 = std::exchange(other.chunks, {});
-		noise_layers = std::exchange(other.noise_layers, {});
-		tile_size	 = std::exchange(other.tile_size, {});
-		chunk_size	 = std::exchange(other.chunk_size, {});
+		chunks		  = std::exchange(other.chunks, {});
+		tile_size	  = std::exchange(other.tile_size, {});
+		chunk_size	  = std::exchange(other.chunk_size, {});
+		noise_layers_ = std::exchange(other.noise_layers_, {});
 	}
 	return *this;
 }
@@ -66,39 +113,61 @@ ChunkManager::~ChunkManager() {
 	*/
 }
 
-void ChunkManager::Update(const Camera& camera) {
+void ChunkManager::GetBounds(
+	V2_int& out_min, V2_int& out_max, const Camera& camera, const V2_int& chunk_padding
+) const {
 	auto cam_rect{ camera.GetVertices() };
 
 	auto chunk_pixel_size{ tile_size * chunk_size };
-
-	// Number of additional chunks on each side that are loaded past the camera view rectangle.
-	V2_int chunk_padding{ 1, 1 };
 
 	// TODO: Consider accounting for rotation?
 	V2_int min{ cam_rect[0] / chunk_pixel_size - V2_int{ 1 } - chunk_padding };
 	V2_int max{ cam_rect[2] / chunk_pixel_size + V2_int{ 1 } + chunk_padding };
 
-	min.x = std::min(min.x, max.x);
-	min.y = std::min(min.y, max.y);
-	max.x = std::max(min.x, max.x);
-	max.y = std::max(min.y, max.y);
+	out_min.x = std::min(min.x, max.x);
+	out_min.y = std::min(min.y, max.y);
+	out_max.x = std::max(min.x, max.x);
+	out_max.y = std::max(min.y, max.y);
 
-	PTGN_ASSERT(min.x <= max.x, "Invalid camera rectangle chunk extents");
-	PTGN_ASSERT(min.y <= max.y, "Invalid camera rectangle chunk extents");
+	PTGN_ASSERT(out_min.x <= out_max.x, "Invalid camera rectangle chunk extents");
+	PTGN_ASSERT(out_min.y <= out_max.y, "Invalid camera rectangle chunk extents");
+}
+
+void ChunkManager::Update(Manager& manager, const Camera& camera) {
+	V2_int chunk_padding{ 1, 1 };
+
+	V2_int min;
+	V2_int max;
+
+	GetBounds(min, max, camera, chunk_padding);
+
+	if (min == previous_min_ && max == previous_max_) {
+		return;
+	}
+
+	previous_min_ = min;
+	previous_max_ = max;
 
 	std::unordered_set<V2_int> visible_chunks;
 
 	for (int i{ min.x }; i < max.x; i++) {
 		for (int j{ min.y }; j < max.y; j++) {
-			V2_int chunk_coordinate{ i, j };
-			visible_chunks.emplace(chunk_coordinate);
+			V2_int coordinate{ i, j };
+			visible_chunks.emplace(coordinate);
 
-			if (auto it{ chunks.find(chunk_coordinate) }; it == chunks.end()) {
-				// PTGN_LOG("Loading chunk: ", chunk_coordinate);
-				// Newly visible chunk.
-				auto entities{ GenerateEntities(chunk_coordinate) };
-				chunks.emplace(chunk_coordinate, entities);
+			if (auto it{ chunks.find(coordinate) }; it != chunks.end()) {
+				continue;
 			}
+
+			if (auto cache_it{ chunk_cache.find(coordinate) }; cache_it != chunk_cache.end()) {
+				chunks.try_emplace(coordinate, cache_it->second, manager);
+				continue;
+			}
+
+			// PTGN_LOG("Loading chunk for the first time: ", coordinate);
+			// Newly visible chunk.
+			auto entities{ GenerateEntities(coordinate) };
+			[[maybe_unused]] auto [new_it, _] = chunks.try_emplace(coordinate, entities);
 		}
 	}
 
@@ -113,23 +182,38 @@ void ChunkManager::Update(const Camera& camera) {
 
 	for (const auto& coordinate : chunks_to_unload) {
 		// PTGN_LOG("Unloading chunk: ", coordinate);
+		auto it{ chunks.find(coordinate) };
+		PTGN_ASSERT(it != chunks.end());
+		if (const auto& chunk{ it->second }; chunk.HasChanged()) {
+			chunk_cache[coordinate] = chunk.Serialize();
+		}
 		chunks.erase(coordinate);
 	}
 
-	// Debug draw chunk borders.
-	/*for (const auto& [coordinate, chunk] : chunks) {
+	chunks.rehash(0);
+
+	manager.Refresh();
+
+	// DrawDebugChunkBorders();
+}
+
+void ChunkManager::AddNoiseLayer(const NoiseLayer& noise_layer) {
+	noise_layers_.emplace_back(noise_layer);
+}
+
+void ChunkManager::DrawDebugChunkBorders() const {
+	for (const auto& [coordinate, chunk] : chunks) {
 		DrawDebugRect(
 			coordinate * chunk_size * tile_size, chunk_size * tile_size, color::Red,
 			Origin::TopLeft, 2.0f
 		);
-	}*/
+	}
 }
 
-[[nodiscard]] std::vector<Entity> ChunkManager::GenerateEntities(
-	const V2_int& chunk_coordinate
+[[nodiscard]] std::vector<Entity> ChunkManager::GenerateEntities(const V2_int& chunk_coordinate
 ) const {
 	std::vector<Entity> entities;
-	for (const auto& layer : noise_layers) {
+	for (const auto& layer : noise_layers_) {
 		for (int i{ 0 }; i < chunk_size.x; i++) {
 			for (int j{ 0 }; j < chunk_size.y; j++) {
 				auto tile_coordinate{ chunk_coordinate * chunk_size + V2_int{ i, j } };
--- src/tile/chunk.h
+++ src/tile/chunk.h
@@ -7,23 +7,43 @@
 #include "core/entity.h"
 #include "math/noise.h"
 #include "math/vector2.h"
-
-// TODO: Add serialization.
+#include "serialization/json.h"
 
 namespace ptgn {
 
 class Camera;
+class Manager;
+class ChunkManager;
 
 class Chunk {
 public:
+	Chunk() = default;
 	explicit Chunk(const std::vector<Entity>& entities);
+	Chunk(const json& j, Manager& manager);
 	Chunk(const Chunk&)			   = delete;
 	Chunk& operator=(const Chunk&) = delete;
 	Chunk(Chunk&& other) noexcept;
 	Chunk& operator=(Chunk&& other) noexcept;
 	~Chunk();
 
+	// @return True if the chunk has changed from its base generated state and should be serialized,
+	// false otherwise.
+	[[nodiscard]] bool HasChanged() const;
+
+	// Flag the chunk as changed which will lead to it being serialized on the next chunk manager
+	// update.
+	void FlagAsChanged(bool changed = true);
+
+	[[nodiscard]] json Serialize() const;
+
+	void Deserialize(const json& j, Manager& manager);
+
 	std::vector<Entity> entities;
+
+private:
+	friend class ChunkManager;
+
+	bool has_changed_{ false };
 };
 
 struct NoiseLayer {
@@ -51,15 +71,31 @@ public:
 	ChunkManager& operator=(ChunkManager&& other) noexcept;
 	~ChunkManager();
 
-	void Update(const Camera& camera);
+	void Update(Manager& manager, const Camera& camera);
 
 	std::unordered_map<V2_int, Chunk> chunks;
-	std::vector<NoiseLayer> noise_layers;
 	V2_int tile_size{ 64, 64 };
 	V2_int chunk_size{ 16, 16 };
 
+	void AddNoiseLayer(const NoiseLayer& noise_layer);
+
+	std::unordered_map<V2_int, json> chunk_cache;
+
 private:
+	V2_int previous_min_;
+	V2_int previous_max_;
+
+	void DrawDebugChunkBorders() const;
+
+	// @param chunk_padding Number of additional chunks on each side that are loaded past the camera
+	// view rectangle.
+	void GetBounds(
+		V2_int& out_min, V2_int& out_max, const Camera& camera, const V2_int& chunk_padding
+	) const;
+
 	[[nodiscard]] std::vector<Entity> GenerateEntities(const V2_int& chunk_coordinate) const;
+
+	std::vector<NoiseLayer> noise_layers_;
 };
 
 } // namespace ptgn
