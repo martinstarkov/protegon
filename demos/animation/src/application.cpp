741427094c0418ac95ba720bf89588e072c39cc5
Martin <emailregisterlogin@gmail.com>
2025-07-17 02:15:34 +0200

    Update application.cpp

--- demos/animation/src/application.cpp
+++ demos/animation/src/application.cpp
@@ -9,7 +9,7 @@
 
 using namespace ptgn;
 
-class AnimationExample : public Scene {
+class AnimationScene : public Scene {
 public:
 	Animation animation;
 	Animation sprite;
@@ -17,12 +17,12 @@ public:
 	void Enter() override {
 		game.texture.Load("anim", "resources/animation.png");
 
-		/*sprite = CreateSprite(*this, "anim");
-		sprite.SetPosition(camera.primary.GetPosition());
+		sprite = CreateSprite(*this, "anim");
+		sprite.SetPosition(camera.primary.GetPosition() + V2_int{ 64, 0 });
 		sprite.SetScale(3.0f);
 		auto& crop	  = sprite.Add<TextureCrop>();
 		crop.size	  = V2_int{ 16, 32 };
-		crop.position = V2_int{ 0, 0 };*/
+		crop.position = V2_int{ 0, 0 };
 
 		animation =
 			CreateAnimation(*this, "anim", milliseconds{ 500 }, 4, V2_int{ 16, 32 }, -1, {});
@@ -32,14 +32,14 @@ public:
 		animation.Start();
 	}
 
-	void Exit() {
+	void Exit() override {
 		json j = *this;
 		SaveJson(j, "resources/animation_scene.json");
 	}
 };
 
 int main([[maybe_unused]] int c, [[maybe_unused]] char** v) {
-	game.Init("AnimationExample");
-	game.scene.Enter<AnimationExample>("animation_example");
+	game.Init("AnimationScene");
+	game.scene.Enter<AnimationScene>("");
 	return 0;
 }
