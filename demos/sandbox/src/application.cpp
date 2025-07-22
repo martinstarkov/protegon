6fb5e81b20988a834359c74c61337493018e9712
Martin Starkov <emailregisterlogin@gmail.com>
2025-07-22 08:06:48 +0200

    Update application.cpp

--- demos/sandbox/src/application.cpp
+++ demos/sandbox/src/application.cpp
@@ -1,5 +1,6 @@
 
 #include "core/game.h"
+#include "events/input_handler.h"
 #include "math/vector2.h"
 #include "scene/scene.h"
 #include "scene/scene_manager.h"
@@ -10,6 +11,21 @@ constexpr V2_int window_size{ 800, 800 };
 
 struct SandboxScene : public Scene {
 	void Enter() override {}
+
+	void Update() override {
+		if (game.input.KeyDown(Key::E)) {
+			CreateEntity().Enable();
+		}
+
+		if (game.input.KeyDown(Key::R)) {
+			for (auto e : update_list) {
+				e.Destroy();
+			}
+			update_list.clear();
+		}
+
+		PTGN_LOG("Update list: ", update_list.size());
+	}
 };
 
 int main([[maybe_unused]] int c, [[maybe_unused]] char** v) {
