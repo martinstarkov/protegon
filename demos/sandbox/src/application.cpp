fc80adb44cb9f8261d89cd3cc991b9ecebbbf9cf
Martin <emailregisterlogin@gmail.com>
2025-07-27 05:57:29 +0200

    Clear sandbox demo

--- demos/sandbox/src/application.cpp
+++ demos/sandbox/src/application.cpp
@@ -12,37 +12,7 @@ constexpr V2_int window_size{ 800, 800 };
 struct SandboxScene : public Scene {
 	void Enter() override {}
 
-	std::vector<Entity> update_list;
-
-	void AddToUpdateList(Entity entity) {
-		update_list.emplace_back(entity);
-	}
-
-	void RemoveFromUpdateList(Entity entity) {
-		update_list.erase(
-			std::remove(update_list.begin(), update_list.end(), entity), update_list.end()
-		);
-	}
-
-	SandboxScene() {
-		OnConstruct<Enabled>().Connect<SandboxScene, &SandboxScene::AddToUpdateList>(this);
-		OnDestruct<Enabled>().Connect<SandboxScene, &SandboxScene::RemoveFromUpdateList>(this);
-	}
-
-	void Update() override {
-		if (game.input.KeyDown(Key::E)) {
-			CreateEntity().Enable();
-		}
-
-		if (game.input.KeyDown(Key::R)) {
-			for (auto e : update_list) {
-				e.Destroy();
-			}
-			update_list.clear();
-		}
-
-		PTGN_LOG("Update list: ", update_list.size());
-	}
+	void Update() override {}
 };
 
 int main([[maybe_unused]] int c, [[maybe_unused]] char** v) {
