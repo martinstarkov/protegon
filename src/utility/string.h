265a3edeee9b98bc4ce9b8dd59f238632a702271
Martin <emailregisterlogin@gmail.com>
2025-07-28 06:13:37 +0200

    Add BeginsWith and EndsWith utility functions

--- src/utility/string.h
+++ src/utility/string.h
@@ -4,6 +4,7 @@
 #include <iosfwd>
 #include <sstream>
 #include <string>
+#include <string_view>
 
 #include "common/type_traits.h"
 
@@ -35,5 +36,16 @@ std::string ToString(const T& object, int precision) {
 std::string ToLower(const std::string& str);
 
 std::string ToUpper(const std::string& str);
+
+// @return True if str begins with the specified prefix.
+constexpr bool BeginsWith(std::string_view str, std::string_view prefix) {
+	return str.size() >= prefix.size() && str.compare(0, prefix.size(), prefix) == 0;
+}
+
+// @return True if str ends with the specified suffix.
+constexpr bool EndsWith(std::string_view str, std::string_view suffix) {
+	return str.size() >= suffix.size() &&
+		   str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
 
 } // namespace ptgn
