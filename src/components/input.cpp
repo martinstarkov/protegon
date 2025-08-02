fd912485db5410dc74c471b93325ebcabd8be1f8
Martin <emailregisterlogin@gmail.com>
2025-08-02 12:58:36 +0200

    Add interactive clear

--- src/components/input.cpp
+++ src/components/input.cpp
@@ -1,3 +1,16 @@
 #include "components/input.h"
 
-namespace ptgn {} // namespace ptgn
+#include <vector>
+
+#include "core/entity.h"
+
+namespace ptgn {
+
+void Interactive::Clear() {
+	for (Entity shape : shapes) {
+		shape.Destroy();
+	}
+	shapes.clear();
+}
+
+} // namespace ptgn
