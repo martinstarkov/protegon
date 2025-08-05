8969f50c262619f31951a2723c12a72370fe6866
Martin Starkov <emailregisterlogin@gmail.com>
2025-08-06 00:55:53 +0200

    Add broadphase file

---
+++ src/physics/collision/broadphase.cpp
@@ -0,0 +1,130 @@
+#include "physics/collision/collider.h"
+
+#include <functional>
+#include <memory>
+#include <vector>
+
+#include "common/assert.h"
+#include "components/transform.h"
+#include "core/entity.h"
+#include "core/script.h"
+#include "math/geometry.h"
+#include "physics/rigid_body.h"
+#include "utility/span.h"
+
+namespace ptgn {
+
+bool PhysicsBody::IsImmovable() const {
+	if (Has<RigidBody>() && Get<RigidBody>().immovable) {
+		return true;
+	}
+	if (HasParent()) {
+		return PhysicsBody{ GetParent() }.IsImmovable();
+	}
+	return false;
+}
+
+[[nodiscard]] Transform& PhysicsBody::GetRootTransform() {
+	auto root_entity{ Entity::GetRootEntity() };
+	PTGN_ASSERT(root_entity, "Physics body must have a valid root entity (or itself)");
+	PTGN_ASSERT(root_entity.Has<Transform>(), "Root entity must have a transform component");
+	return root_entity.GetTransform();
+}
+
+Collider::Collider(const Shape& shape) : shape{ shape } {}
+
+Collider& Collider::SetOverlapOnly() {
+	overlap_only = true;
+	return *this;
+}
+
+CollisionCategory Collider::GetCollisionCategory() const {
+	return category_;
+}
+
+void Collider::SetCollisionCategory(const CollisionCategory& category) {
+	category_ = category;
+}
+
+void Collider::ResetCollisionCategory() {
+	category_ = 0;
+}
+
+void Collider::ResetCollidesWith() {
+	mask_ = {};
+}
+
+bool Collider::ProcessCallback(Entity e1, Entity e2) const {
+	if (!e1.Has<Scripts>()) {
+		return true;
+	}
+
+	const auto& scripts{ e1.Get<Scripts>() };
+
+	// Check that each entity script allows for the pre-collision condition to pass.
+	for (const auto& [key, script] : scripts.scripts) {
+		PTGN_ASSERT(script != nullptr, "Cannot invoke nullptr script");
+		bool condition_met{ std::invoke(&impl::IScript::PreCollisionCondition, script, e2) };
+		if (!condition_met) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool Collider::CanCollideWith(const CollisionCategory& category) const {
+	return mask_.empty() || VectorContains(mask_, category);
+}
+
+bool Collider::IsCategory(const CollisionCategory& category) const {
+	return category_ == category;
+}
+
+void Collider::AddCollidesWith(const CollisionCategory& category) {
+	PTGN_ASSERT(
+		!VectorContains(mask_, category),
+		"Cannot add the same collision category to a collider more than once"
+	);
+	mask_.emplace_back(category);
+}
+
+void Collider::RemoveCollidesWith(const CollisionCategory& category) {
+	VectorErase(mask_, category);
+}
+
+void Collider::SetCollidesWith(const CollidesWithCategories& categories) {
+	mask_.reserve(mask_.size() + categories.size());
+	for (auto category : categories) {
+		AddCollidesWith(category);
+	}
+}
+
+void Collider::InvokeCollisionCallbacks(Entity& entity) const {
+	for (const auto& prev : prev_collisions_) {
+		if (!VectorContains(collisions_, prev)) {
+			entity.InvokeScript<&impl::IScript::OnCollisionStop>(prev);
+		} else {
+			entity.InvokeScript<&impl::IScript::OnCollision>(prev);
+		}
+	}
+	for (const auto& current : collisions_) {
+		if (!VectorContains(prev_collisions_, current)) {
+			entity.InvokeScript<&impl::IScript::OnCollisionStart>(current);
+		}
+	}
+}
+
+void Collider::ResetCollisions() {
+	prev_collisions_ = collisions_;
+	collisions_.clear();
+}
+
+void Collider::AddCollision(const Collision& collision) {
+	if (VectorContains(collisions_, collision)) {
+		return;
+	}
+	collisions_.emplace_back(collision);
+}
+
+} // namespace ptgn
---
+++ src/physics/collision/broadphase.h
@@ -0,0 +1,137 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+#include "core/entity.h"
+#include "math/geometry.h"
+#include "math/vector2.h"
+#include "serialization/enum.h"
+
+namespace ptgn {
+
+struct Transform;
+
+namespace impl {
+
+class CollisionHandler;
+
+} // namespace impl
+
+struct PhysicsBody : public Entity {
+	using Entity::Entity;
+
+	PhysicsBody(const Entity& entity) : Entity{ entity } {}
+
+	// @return True if the current entity or any of its parent entities is immovable.
+	[[nodiscard]] bool IsImmovable() const;
+
+	[[nodiscard]] Transform& GetRootTransform();
+};
+
+} // namespace ptgn
+
+template <>
+struct std::hash<ptgn::Collision> {
+	std::size_t operator()(const ptgn::Collision& c) const noexcept {
+		// Hashing combination algorithm from:
+		// https://stackoverflow.com/a/17017281
+		std::size_t value{ 17 };
+		value = value * 31 + c.entity.GetHash();
+		value = value * 31 + std::hash<ptgn::V2_float>()(c.normal);
+		return value;
+	}
+};
+
+namespace ptgn {
+
+using CollisionCategory		 = std::int64_t;
+using CollidesWithCategories = std::vector<CollisionCategory>;
+
+enum class CollisionResponse {
+	Slide,	// Velocity set perpendicular to collision normal at same speed.
+	Bounce, // Velocity set at 45 degrees to collision normal.
+	Push,	// Velocity set perpendicular to collision normal at partial speed.
+	Stick	// Velocity set to 0.
+};
+
+struct Collider {
+	// TODO: Implement local physics world bounds.
+	// Rect bounds;
+
+	Collider() = default;
+
+	Collider(const Shape& shape);
+
+	Shape shape;
+
+	// Overwrites continuous/regular collision in favor of overlap checks.
+	bool overlap_only{ false };
+
+	// Continuous collision detection for high velocity colliders.
+	bool continuous{ false };
+
+	Collider& SetOverlapOnly();
+
+	// How the velocity of the sweep should respond to obstacles.
+	// Not applicable if continuous is set to false.
+	CollisionResponse response{ CollisionResponse::Slide };
+
+	[[nodiscard]] CollisionCategory GetCollisionCategory() const;
+
+	void SetCollisionCategory(const CollisionCategory& category);
+
+	void ResetCollisionCategory();
+
+	// Allow collider to collide with anything.
+	void ResetCollidesWith();
+
+	// May invalidate all existing component references.
+	[[nodiscard]] bool ProcessCallback(Entity e1, Entity e2) const;
+
+	[[nodiscard]] bool CanCollideWith(const CollisionCategory& category) const;
+
+	[[nodiscard]] bool IsCategory(const CollisionCategory& category) const;
+
+	void AddCollidesWith(const CollisionCategory& category);
+
+	void RemoveCollidesWith(const CollisionCategory& category);
+
+	void SetCollidesWith(const CollidesWithCategories& categories);
+
+	void InvokeCollisionCallbacks(Entity& entity) const;
+
+	PTGN_SERIALIZER_REGISTER_NAMED(
+		Collider, KeyValue("shape", shape), KeyValue("collisions", collisions_),
+		KeyValue("prev_collisions", prev_collisions_), KeyValue("overlap_only", overlap_only),
+		KeyValue("continuous", continuous), KeyValue("response", response), KeyValue("mask", mask_),
+		KeyValue("category", category_)
+	)
+
+private:
+	friend class impl::CollisionHandler;
+
+	void ResetCollisions();
+
+	void AddCollision(const Collision& collision);
+
+	// Which categories this collider collides with.
+	std::vector<CollisionCategory> mask_;
+	// Which category this collider is a part of.
+	CollisionCategory category_{ 0 };
+
+	// Collisions from the current frame (updated after calling game.collision.Update()).
+	std::vector<Collision> collisions_;
+
+	// Collisions from the previous frame.
+	std::vector<Collision> prev_collisions_;
+};
+
+PTGN_SERIALIZER_REGISTER_ENUM(
+	CollisionResponse, { { CollisionResponse::Slide, "slide" },
+						 { CollisionResponse::Bounce, "bounce" },
+						 { CollisionResponse::Push, "push" },
+						 { CollisionResponse::Stick, "stick" } }
+);
+
+} // namespace ptgn
