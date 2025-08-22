d74439b7b0ece56424e3784ee9406daf937b55de
Martin <emailregisterlogin@gmail.com>
2025-08-22 04:46:29 +0200

    Add IsOrDerivedFrom concept

--- src/common/concepts.h
+++ src/common/concepts.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <concepts>
 #include <type_traits>
 
 namespace ptgn {
@@ -9,5 +10,8 @@ concept Enum = std::is_enum_v<T>;
 
 template <typename T>
 concept ScopedEnum = std::is_enum_v<T> && !std::is_convertible_v<T, int>;
+
+template <typename T, typename BaseType>
+concept IsOrDerivedFrom = std::is_same_v<T, BaseType> || std::derived_from<T, BaseType>;
 
 } // namespace ptgn
