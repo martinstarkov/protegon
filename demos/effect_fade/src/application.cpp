d8b8c4efb757a203bd7da7c80204a51ec250fde6
Martin <emailregisterlogin@gmail.com>
2025-08-23 19:03:22 +0200

    Add scene fade

--- demos/effect_fade/src/application.cpp
+++ demos/effect_fade/src/application.cpp
@@ -2,6 +2,7 @@
 #include "components/sprite.h"
 #include "core/game.h"
 #include "input/input_handler.h"
+#include "renderer/renderer.h"
 #include "scene/scene.h"
 #include "scene/scene_manager.h"
 #include "tweens/tween_effects.h"
@@ -13,6 +14,10 @@ struct FadeEffectScene : public Scene {
 	Sprite sprite2;
 
 	void Enter() override {
+		// SetBackgroundColor(color::LightBlue);
+		// SetBackgroundColor(color::Green);
+		game.renderer.SetBackgroundColor(color::Green);
+
 		LoadResource("tree", "resources/tree.jpg");
 		LoadResource("smile", "resources/smile.png");
 
@@ -34,11 +39,17 @@ struct FadeEffectScene : public Scene {
 		if (input.MouseDown(Mouse::Right)) {
 			FadeOut(sprite1, milliseconds{ 4000 }, SymmetricalEase::Linear, true);
 		}
+		if (input.KeyDown(Key::T)) {
+			FadeOut(GetRenderTarget(), milliseconds{ 3000 });
+		}
+		if (input.KeyDown(Key::R)) {
+			FadeIn(GetRenderTarget(), milliseconds{ 3000 });
+		}
 	}
 };
 
 int main([[maybe_unused]] int c, [[maybe_unused]] char** v) {
-	game.Init("FadeEffectScene: left/right click to fade in/out");
+	game.Init("FadeEffectScene: left/right click to fade in/out", { 800, 800 });
 	game.scene.Enter<FadeEffectScene>("");
 	return 0;
 }
