2c8abd4bc49ed707d8f0d48b5a10794d464dfdda
Martin <emailregisterlogin@gmail.com>
2025-08-24 17:31:03 +0200

    Use a single tinted smile

--- demos/tween_follow/src/application.cpp
+++ demos/tween_follow/src/application.cpp
@@ -3,6 +3,7 @@
 #include "core/game.h"
 #include "input/input_handler.h"
 #include "math/vector2.h"
+#include "renderer/renderer.h"
 #include "scene/scene.h"
 #include "scene/scene_manager.h"
 #include "tweens/tween_effects.h"
@@ -16,57 +17,79 @@ struct FollowEffectScene : public Scene {
 	Sprite entity2;
 	Sprite entity3;
 	Sprite entity4;
+	Sprite entity5;
 
 	TargetFollowConfig config1;
 	TargetFollowConfig config2;
 	TargetFollowConfig config3;
 	PathFollowConfig config4;
+	PathFollowConfig config5;
 
 	std::vector<V2_float> waypoints;
 
+	Entity CreateFollower(const Color& color, const V2_float& start_position) {
+		auto follower{ CreateSprite(*this, "smile", start_position) };
+		SetTint(follower, color);
+		return follower;
+	}
+
 	void Enter() override {
-		LoadResources({ { "smile1", "resources/smile1.png" },
-						{ "smile2", "resources/smile2.png" },
-						{ "smile3", "resources/smile3.png" },
-						{ "smile4", "resources/smile4.png" } });
+		LoadResource("smile", "resources/smile.png");
 
 		mouse = CreateEntity();
 		SetPosition(mouse, {});
 
-		entity1 = CreateSprite(*this, "smile1", { 150, 150 }); // Red
-		entity2 = CreateSprite(*this, "smile2", { 600, 600 }); // Green
-		entity3 = CreateSprite(*this, "smile3", { 150, 600 }); // Blue
-		entity4 = CreateSprite(*this, "smile4", { 150, 600 }); // Yellow
+		entity1 = CreateFollower(color::Red, { 100, 100 });
+		entity2 = CreateFollower(color::Green, { 200, 200 });
+		entity3 = CreateFollower(color::Blue, { 300, 300 });
+		entity4 = CreateFollower(color::Yellow, { 400, 400 });
+		entity5 = CreateFollower(color::Magenta, { 500, 500 });
 
-		// Red: Instant follow.
-		config1.lerp_factor = { 1.0f, 1.0f };
-		// Green: Delayed lerp follow.
-		config2.lerp_factor = { 0.5f, 0.5f };
-		// Blue: Velocity follow.
+		// Instant follow.
+		config1.smooth_lerp_factor = { 1.0f, 1.0f };
+
+		// Delayed lerp follow.
+		config2.smooth_lerp_factor = { 0.5f, 0.5f };
+
+		// Velocity follow.
 		config3.move_mode = MoveMode::Velocity;
-		// Yellow: Path follow.
-		config4.lerp_factor	  = { 0.5f, 0.5f };
-		waypoints			  = { V2_float{ 0, 0 }, V2_float{ 400, 400 } };
-		config4.stop_distance = 10.0f;
 
-		StartFollow(entity1, mouse, config1);
-		StartFollow(entity2, mouse, config2);
-		StartFollow(entity3, mouse, config3);
+		// Path follow (lerp).
+		config4.smooth_lerp_factor = { 0.5f, 0.5f };
+		config4.loop_path		   = true;
+		config4.stop_distance	   = 40.0f;
+
+		// Path follow (velocity).
+		config5.move_mode	  = MoveMode::Velocity;
+		config5.loop_path	  = true;
+		config5.stop_distance = 40.0f;
+
+		auto e{ game.renderer.GetLogicalResolution() };
+		auto c{ e / 2.0f };
+
+		waypoints = { V2_float{}, c, V2_float{ e.x, 0 }, c, e, c, V2_float{ 0, e.y }, c };
+
+		/*	StartFollow(entity1, mouse, config1);
+			StartFollow(entity2, mouse, config2);
+			StartFollow(entity3, mouse, config3);*/
 		StartFollow(entity4, waypoints, config4);
+		StartFollow(entity5, waypoints, config5);
 	}
 
 	void Update() override {
 		SetPosition(mouse, input.GetMousePosition());
 		if (input.MouseDown(Mouse::Left)) {
-			StopFollow(entity1);
+			/*StopFollow(entity1);
 			StopFollow(entity2);
-			StopFollow(entity3);
+			StopFollow(entity3);*/
 			StopFollow(entity4);
+			StopFollow(entity5);
 		} else if (input.MouseDown(Mouse::Right)) {
-			StartFollow(entity1, mouse, config1);
-			StartFollow(entity2, mouse, config2);
-			StartFollow(entity3, mouse, config3);
+			/*	StartFollow(entity1, mouse, config1);
+				StartFollow(entity2, mouse, config2);
+				StartFollow(entity3, mouse, config3);*/
 			StartFollow(entity4, waypoints, config4);
+			StartFollow(entity5, waypoints, config5);
 		}
 	}
 };
