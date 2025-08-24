a674b457c8f181fd8ca9226332298092d8b1cc2a
Martin <emailregisterlogin@gmail.com>
2025-08-24 17:31:13 +0200

    Reduce code repetition between configs

--- src/tweens/follow_config.h
+++ src/tweens/follow_config.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 
+#include "math/easing.h"
 #include "math/vector2.h"
 #include "serialization/enum.h"
 #include "serialization/serializable.h"
@@ -9,55 +10,29 @@
 namespace ptgn {
 
 enum class MoveMode {
-	Lerp,
+	Ease,
+	SmoothLerp,
 	Velocity
 };
 
-class TargetFollowConfig {
-public:
-	MoveMode move_mode{ MoveMode::Lerp };
+namespace impl {
+
+struct FollowConfig {
+	MoveMode move_mode{ MoveMode::Ease };
 
 	bool follow_x{ true };
 	bool follow_y{ true };
 
 	bool teleport_on_start{ false };
-
-	bool loop_path{ false };
-
-	float stop_distance{ -1.0f }; // Never stop following the target.
-
-	V2_float lerp_factor{ 1.0f, 1.0f };
-
-	V2_float deadzone;
-
-	V2_float offset;
-
-	float max_speed{ 4.0f * 60.0f };
-	float max_acceleration{ 20.0f * 60.0f };
-
-	bool operator==(const TargetFollowConfig&) const = default;
-
-	PTGN_SERIALIZER_REGISTER_IGNORE_DEFAULTS(
-		TargetFollowConfig, move_mode, follow_x, follow_y, teleport_on_start, loop_path,
-		stop_distance, lerp_factor, deadzone, offset, max_speed, max_acceleration
-	)
-};
-
-class PathFollowConfig {
-public:
-	MoveMode move_mode{ MoveMode::Lerp };
-
-	bool follow_x{ true };
-	bool follow_y{ true };
-
-	bool teleport_on_start{ false };
-
-	bool loop_path{ false };
 
 	// What is considered close enough to the target.
 	float stop_distance{ 10.0f };
 
-	V2_float lerp_factor{ 1.0f, 1.0f };
+	// Applies when MoveMode::Ease.
+	Ease ease{ SymmetricalEase::Linear };
+
+	// Applies when MoveMode::SmoothLerp.
+	V2_float smooth_lerp_factor{ 1.0f, 1.0f };
 
 	V2_float deadzone;
 
@@ -66,16 +41,40 @@ public:
 	float max_speed{ 4.0f * 60.0f };
 	float max_acceleration{ 20.0f * 60.0f };
 
-	bool operator==(const PathFollowConfig&) const = default;
+	bool operator==(const FollowConfig&) const = default;
 
-	PTGN_SERIALIZER_REGISTER_IGNORE_DEFAULTS(
-		PathFollowConfig, move_mode, follow_x, follow_y, teleport_on_start, loop_path,
-		stop_distance, lerp_factor, deadzone, offset, max_speed, max_acceleration
+	PTGN_SERIALIZER_REGISTER(
+		FollowConfig, move_mode, follow_x, follow_y, teleport_on_start, stop_distance, ease,
+		smooth_lerp_factor, deadzone, offset, max_speed, max_acceleration
 	)
 };
 
+} // namespace impl
+
+struct TargetFollowConfig : public impl::FollowConfig {
+	bool operator==(const TargetFollowConfig&) const = default;
+};
+
+struct PathFollowConfig : public impl::FollowConfig {
+	bool loop_path{ false };
+
+	bool operator==(const PathFollowConfig&) const = default;
+
+	friend void to_json(json& j, const PathFollowConfig& config) {
+		to_json(j, static_cast<const impl::FollowConfig&>(config));
+		j["loop_path"] = config.loop_path;
+	}
+
+	friend void from_json(const json& j, PathFollowConfig& config) {
+		from_json(j, static_cast<impl::FollowConfig&>(config));
+		j.at("loop_path").get_to(config.loop_path);
+	}
+};
+
 PTGN_SERIALIZER_REGISTER_ENUM(
-	MoveMode, { { MoveMode::Lerp, "lerp" }, { MoveMode::Velocity, "velocity" } }
+	MoveMode, { { MoveMode::Ease, "ease" },
+				{ MoveMode::SmoothLerp, "smooth_lerp" },
+				{ MoveMode::Velocity, "velocity" } }
 );
 
 } // namespace ptgn
