bbba762b12c2792f4873b69c3b25925c00535601
Martin <emailregisterlogin@gmail.com>
2025-08-27 15:35:23 +0200

    Remove logs

--- src/scene/scene_transition.cpp
+++ src/scene/scene_transition.cpp
@@ -5,14 +5,11 @@
 #include "common/assert.h"
 #include "components/draw.h"
 #include "core/entity.h"
-#include "core/game.h"
 #include "core/script_sequence.h"
 #include "core/time.h"
-#include "debug/log.h"
 #include "renderer/api/color.h"
 #include "renderer/render_target.h"
 #include "scene/scene.h"
-#include "scene/scene_manager.h"
 #include "tweens/tween.h"
 #include "tweens/tween_effects.h"
 
@@ -37,11 +34,7 @@ void FadeInTransition::Start() {
 	SetTint(render_target, color::Transparent);
 
 	After(*scene, delay_, [*this, render_target](Entity) mutable {
-		PTGN_LOG("Starting fade in");
-		FadeIn(render_target, duration_).OnComplete([*this](Entity) mutable {
-			PTGN_LOG("Stopping fade in");
-			Stop();
-		});
+		FadeIn(render_target, duration_).OnComplete([*this](Entity) mutable { Stop(); });
 	});
 }
 
@@ -52,11 +45,7 @@ void FadeOutTransition::Start() {
 	SceneTransition::Start();
 	RenderTarget render_target{ scene->GetRenderTarget() };
 	After(*scene, delay_, [*this, render_target](Entity) mutable {
-		PTGN_LOG("Starting fade out");
-		FadeOut(render_target, duration_).OnComplete([*this](Entity) mutable {
-			PTGN_LOG("Stopping fade out");
-			Stop();
-		});
+		FadeOut(render_target, duration_).OnComplete([*this](Entity) mutable { Stop(); });
 	});
 }
 
