efcae0848778176af432bcd359b00e45eec08729
Martin <emailregisterlogin@gmail.com>
2025-09-03 23:57:09 +0200

    Remove emscripten shaders

--- src/resources/shader/common/circle.glsl
+++ src/resources/shader/common/circle.glsl
@@ -1,12 +1,12 @@
-R"(
-#version 330 core
-#extension GL_ARB_separate_shader_objects : require
+#option auto_layout
 
-layout (location = 0) out vec4 o_Color;
+#type fragment
 
-layout (location = 0) in vec4 v_Color;
-layout (location = 1) in vec2 v_TexCoord;
-layout (location = 2) in float v_TexIndex;
+out vec4 o_Color;
+
+in vec4 v_Color;
+in vec2 v_TexCoord;
+in float v_TexIndex;
 
 void main() {
     float fade = 0.005f;
@@ -25,4 +25,3 @@ void main() {
     o_Color = v_Color;
 	o_Color.a *= circle;
 }
-)"
