ce8650b70b6e400bf035b5f39ebc00224f3b6185
Martin <emailregisterlogin@gmail.com>
2025-09-06 18:55:30 +0200

    Add radius variable to circle

--- src/resources/shader/common/circle.glsl
+++ src/resources/shader/common/circle.glsl
@@ -8,18 +8,19 @@ in vec4 v_Color;
 in vec2 v_TexCoord;
 in vec4 v_Data; // x = thickness, y = fade
 
-float CircleDistance(vec2 point) {
-    return 1.0f - length(point);
+float CircleDistance(vec2 point, float radius) {
+    return radius - length(point) / radius;
 }
 
 void main() {
     float thickness = v_Data.x; // 0.0f = hollow, 1.0f = filled
     float fade = v_Data.y;
     
+    float radius = 1.0f;
     vec2 uv = v_TexCoord * 2.0f - 1.0f; // Normalize to: [-1, 1]
     // Not technically an exact ellipse, for that see: https://iquilezles.org/articles/distfunctions2d/
 
-    float distance = CircleDistance(uv);
+    float distance = CircleDistance(uv, radius);
 
     float alpha = smoothstep(0.0f, fade, distance);
     alpha *= smoothstep(thickness + fade, thickness, distance);
