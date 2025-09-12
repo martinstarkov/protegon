2cd5cb219c5e746b4a122b3774d09f1bc8dea50a
Martin <emailregisterlogin@gmail.com>
2025-09-13 00:32:04 +0200

    Add draw functions

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -17,7 +17,6 @@
 #include "core/game.h"
 #include "core/window.h"
 #include "debug/log.h"
-#include "math/geometry.h"
 #include "math/geometry/arc.h"
 #include "math/geometry/capsule.h"
 #include "math/geometry/circle.h"
@@ -26,6 +25,7 @@
 #include "math/geometry/polygon.h"
 #include "math/geometry/rect.h"
 #include "math/geometry/rounded_rect.h"
+#include "math/geometry/shape.h"
 #include "math/geometry/triangle.h"
 #include "math/vector2.h"
 #include "renderer/api/blend_mode.h"
@@ -47,193 +47,253 @@ namespace ptgn {
 
 namespace impl {
 
-const Depth max_depth{ std::numeric_limits<std::int32_t>::max() };
-const BlendMode debug_blend_mode{ BlendMode::Blend };
+void Renderer::DrawTexture(
+	const Texture& texture, const Transform& transform, const V2_float& texture_size, Origin origin,
+	const Tint& tint, const Depth& depth, BlendMode blend_mode, const Camera& camera,
+	const PreFX& pre_fx, const PostFX& post_fx, const std::array<V2_float, 4>& texture_coordinates
+) {
+	Rect rect{ !texture_size.IsZero() ? texture_size : V2_float{ texture.GetSize() } };
 
-RenderState GetDebugRenderState(const Camera& camera) {
-	impl::RenderState state;
-	state.blend_mode = debug_blend_mode;
-	state.camera	 = camera;
-	state.post_fx	 = {};
-	return state;
+	DrawTextureCommand cmd;
+
+	cmd.transform				= transform;
+	cmd.texture_id				= texture.GetId();
+	cmd.texture_size			= texture.GetSize();
+	cmd.texture_format			= texture.GetFormat();
+	cmd.rect					= rect;
+	cmd.origin					= origin;
+	cmd.depth					= depth;
+	cmd.pre_fx					= pre_fx;
+	cmd.tint					= tint;
+	cmd.texture_coordinates		= texture_coordinates;
+	cmd.render_state.blend_mode = blend_mode;
+	cmd.render_state.camera		= camera;
+	cmd.render_state.post_fx	= post_fx;
+
+	render_data_.Submit(cmd);
 }
 
-} // namespace impl
-
-void DrawDebugTexture(
-	const TextureHandle& texture_key, const V2_float& position, const V2_float& size, Origin origin,
-	float rotation, const Camera& camera
+void Renderer::DrawTexture(
+	const TextureHandle& texture_key, const Transform& transform, const V2_float& texture_size,
+	Origin origin, const Tint& tint, const Depth& depth, BlendMode blend_mode, const Camera& camera,
+	const PreFX& pre_fx, const PostFX& post_fx, const std::array<V2_float, 4>& texture_coordinates
 ) {
-	Rect rect{ size.IsZero() ? V2_float{ texture_key.GetSize() } : size };
-
-	impl::DrawTextureCommand cmd;
-
-	cmd.transform	 = Transform{ position, rotation };
-	cmd.texture		 = &texture_key.GetTexture();
-	cmd.rect		 = rect;
-	cmd.origin		 = origin;
-	cmd.depth		 = impl::max_depth;
-	cmd.render_state = impl::GetDebugRenderState(camera);
-
-	game.renderer.GetRenderData().Submit(cmd);
-}
-
-void DrawDebugText(
-	const std::string& content, const V2_float& position, const TextColor& color, Origin origin,
-	const FontSize& font_size, bool hd_text, const ResourceHandle& font_key,
-	const TextProperties& properties, const V2_float& size, float rotation, const Camera& camera
-) {
-	auto& render_data{ game.renderer.GetRenderData() };
-	FontSize final_font_size{ font_size };
-	Transform transform{ position, rotation };
-	if (hd_text) {
-		const auto& scene{ game.scene.GetCurrent() };
-		auto render_target_scale{ scene.GetRenderTargetScaleRelativeTo(camera) };
-		PTGN_ASSERT(render_target_scale.BothAboveZero());
-		transform.Scale(1.0f / render_target_scale);
-		final_font_size =
-			static_cast<std::int32_t>(static_cast<float>(font_size) * render_target_scale.y);
-	}
-	auto texture{ Text::CreateTexture(content, color, final_font_size, font_key, properties) };
-
-	Rect rect{ size.IsZero() ? V2_float{ Text::GetSize(content, font_key, final_font_size) }
-							 : size };
-
-	impl::DrawTextureCommand cmd;
-
-	cmd.transform	 = transform;
-	cmd.texture		 = &texture;
-	cmd.rect		 = rect;
-	cmd.origin		 = origin;
-	cmd.depth		 = impl::max_depth;
-	cmd.render_state = impl::GetDebugRenderState(camera);
-
-	render_data.Submit(cmd);
-	render_data.AddTemporaryTexture(std::move(texture));
-}
-
-void DrawDebugLines(
-	const std::vector<V2_float>& points, const Color& color, float line_width,
-	bool connect_last_to_first, const Camera& camera
-) {
-	impl::DrawLinesCommand cmd;
-
-	cmd.points				  = points;
-	cmd.connect_last_to_first = connect_last_to_first;
-	cmd.tint				  = color;
-	cmd.depth				  = impl::max_depth;
-	cmd.line_width			  = line_width;
-	cmd.render_state		  = impl::GetDebugRenderState(camera);
-
-	game.renderer.GetRenderData().Submit(cmd);
-}
-
-void DrawDebugLine(
-	const V2_float& line_start, const V2_float& line_end, const Color& color, float line_width,
-	const Camera& camera
-) {
-	DrawDebugShape({}, Line{ line_start, line_end }, color, line_width, camera);
-}
-
-void DrawDebugTriangle(
-	const std::array<V2_float, 3>& vertices, const Color& color, float line_width,
-	const Camera& camera
-) {
-	DrawDebugShape({}, Triangle{ vertices }, color, line_width, camera);
-}
-
-void DrawDebugRect(
-	const V2_float& position, const V2_float& size, const Color& color, Origin origin,
-	float line_width, float rotation, const Camera& camera
-) {
-	impl::DrawShapeCommand cmd;
-
-	cmd.transform	 = Transform{ position, rotation };
-	cmd.shape		 = Rect{ size };
-	cmd.tint		 = color;
-	cmd.origin		 = origin;
-	cmd.depth		 = impl::max_depth;
-	cmd.line_width	 = line_width;
-	cmd.render_state = impl::GetDebugRenderState(camera);
-
-	game.renderer.GetRenderData().Submit(cmd);
-}
-
-void DrawDebugRoundedRect(
-	const V2_float& position, const V2_float& size, float radius, const Color& color, Origin origin,
-	float line_width, float rotation, const Camera& camera
-) {
-	impl::DrawShapeCommand cmd;
-
-	cmd.transform	 = Transform{ position, rotation };
-	cmd.shape		 = RoundedRect{ size, radius };
-	cmd.tint		 = color;
-	cmd.origin		 = origin;
-	cmd.depth		 = impl::max_depth;
-	cmd.line_width	 = line_width;
-	cmd.render_state = impl::GetDebugRenderState(camera);
-
-	game.renderer.GetRenderData().Submit(cmd);
-}
-
-void DrawDebugEllipse(
-	const V2_float& center, const V2_float& radii, const Color& color, float line_width,
-	float rotation, const Camera& camera
-) {
-	DrawDebugShape(Transform{ center, rotation }, Ellipse{ radii }, color, line_width, camera);
-}
-
-void DrawDebugCircle(
-	const V2_float& center, float radius, const Color& color, float line_width, const Camera& camera
-) {
-	DrawDebugShape(Transform{ center }, Circle{ radius }, color, line_width, camera);
-}
-
-void DrawDebugCapsule(
-	const V2_float& start, const V2_float& end, float radius, const Color& color, float line_width,
-	const Camera& camera
-) {
-	DrawDebugShape({}, Capsule{ start, end, radius }, color, line_width, camera);
-}
-
-void DrawDebugArc(
-	const V2_float& center, float radius, float start_angle, float end_angle, const Color& color,
-	float line_width, bool clockwise, const Camera& camera
-) {
-	DrawDebugShape(
-		Transform{ center }, Arc{ radius, start_angle, end_angle, clockwise }, color, line_width,
-		camera
+	DrawTexture(
+		texture_key.GetTexture(), transform, texture_size, origin, tint, depth, blend_mode, camera,
+		pre_fx, post_fx, texture_coordinates
 	);
 }
 
-void DrawDebugPolygon(
-	const std::vector<V2_float>& vertices, const Color& color, float line_width,
+void Renderer::DrawLines(
+	const Transform& transform, const std::vector<V2_float>& line_points, const Tint& color,
+	const LineWidth& line_width, bool connect_last_to_first, const Depth& depth,
+	BlendMode blend_mode, const Camera& camera, const PostFX& post_fx
+) {
+	DrawLinesCommand cmd;
+
+	cmd.transform				= transform;
+	cmd.points					= line_points;
+	cmd.tint					= color;
+	cmd.line_width				= line_width;
+	cmd.connect_last_to_first	= connect_last_to_first;
+	cmd.depth					= depth;
+	cmd.render_state.blend_mode = blend_mode;
+	cmd.render_state.camera		= camera;
+	cmd.render_state.post_fx	= post_fx;
+
+	render_data_.Submit(cmd);
+}
+
+void Renderer::DrawLines(
+	const std::vector<V2_float>& line_points, const Tint& color, const LineWidth& line_width,
+	bool connect_last_to_first, const Depth& depth, BlendMode blend_mode, const Camera& camera,
+	const PostFX& post_fx
+) {
+	DrawLines(
+		{}, line_points, color, line_width, connect_last_to_first, depth, blend_mode, camera,
+		post_fx
+	);
+}
+
+void Renderer::DrawShape(
+	const Transform& transform, const Shape& shape, const Tint& color, const LineWidth& line_width,
+	Origin origin, const Depth& depth, BlendMode blend_mode, const Camera& camera,
+	const PostFX& post_fx, const ShaderPass& shader_pass
+) {
+	DrawShapeCommand cmd;
+
+	cmd.transform				 = transform;
+	cmd.shape					 = shape;
+	cmd.tint					 = color;
+	cmd.line_width				 = line_width;
+	cmd.origin					 = origin;
+	cmd.depth					 = depth;
+	cmd.render_state.shader_pass = shader_pass;
+	cmd.render_state.blend_mode	 = blend_mode;
+	cmd.render_state.camera		 = camera;
+	cmd.render_state.post_fx	 = post_fx;
+
+	render_data_.Submit(cmd);
+}
+
+void Renderer::DrawShader(
+	const ShaderPass& shader_pass, const Entity& entity, bool clear_between_consecutive_calls,
+	const Color& target_clear_color, const TextureOrSize& texture_or_size,
+	BlendMode intermediate_blend_mode, const Depth& depth, BlendMode blend_mode,
+	const Camera& camera, TextureFormat texture_format, const PostFX& post_fx
+) {
+	DrawShaderCommand cmd;
+
+	cmd.entity							= entity;
+	cmd.clear_between_consecutive_calls = clear_between_consecutive_calls;
+	cmd.target_clear_color				= target_clear_color;
+	cmd.texture_or_size					= texture_or_size;
+	cmd.intermediate_blend_mode			= intermediate_blend_mode;
+	cmd.depth							= depth;
+	cmd.texture_format					= texture_format;
+	cmd.render_state.shader_pass		= shader_pass;
+	cmd.render_state.post_fx			= post_fx;
+	cmd.render_state.blend_mode			= blend_mode;
+	cmd.render_state.camera				= camera;
+
+	render_data_.Submit(cmd);
+}
+
+void Renderer::DrawText(
+	const std::string& content, Transform transform, const TextColor& color, Origin origin,
+	const FontSize& font_size, const ResourceHandle& font_key, const TextProperties& properties,
+	const V2_float& text_size, const Tint& tint, bool hd_text, const Depth& depth,
+	BlendMode blend_mode, const Camera& camera, const PreFX& pre_fx, const PostFX& post_fx,
+	const std::array<V2_float, 4>& texture_coordinates
+) {
+	FontSize final_font_size{ font_size };
+
+	if (hd_text) {
+		const auto& scene{ game.scene.GetCurrent() };
+
+		auto render_target_scale{ scene.GetRenderTargetScaleRelativeTo(camera) };
+
+		PTGN_ASSERT(render_target_scale.BothAboveZero());
+
+		transform.Scale(1.0f / render_target_scale);
+
+		final_font_size =
+			static_cast<std::int32_t>(static_cast<float>(font_size) * render_target_scale.y);
+	}
+
+	auto texture{ Text::CreateTexture(content, color, final_font_size, font_key, properties) };
+
+	auto texture_size{ !text_size.IsZero()
+						   ? text_size
+						   : V2_float{ Text::GetSize(content, font_key, final_font_size) } };
+
+	DrawTexture(
+		texture, transform, texture_size, origin, tint, depth, blend_mode, camera, pre_fx, post_fx,
+		texture_coordinates
+	);
+
+	render_data_.AddTemporaryTexture(std::move(texture));
+}
+
+void Renderer::DrawRect(
+	const Transform& transform, const Rect& rect, const Tint& color, const LineWidth& line_width,
+	Origin origin, const Depth& depth, BlendMode blend_mode, const Camera& camera,
+	const PostFX& post_fx
+) {
+	DrawShape(transform, rect, color, line_width, origin, depth, blend_mode, camera, post_fx);
+}
+
+void Renderer::DrawRoundedRect(
+	const Transform& transform, const RoundedRect& rounded_rect, const Tint& color,
+	const LineWidth& line_width, Origin origin, const Depth& depth, BlendMode blend_mode,
+	const Camera& camera, const PostFX& post_fx
+) {
+	DrawShape(
+		transform, rounded_rect, color, line_width, origin, depth, blend_mode, camera, post_fx
+	);
+}
+
+void Renderer::DrawLine(
+	const V2_float& start, const V2_float& end, const Tint& color, const LineWidth& line_width,
+	const Depth& depth, BlendMode blend_mode, const Camera& camera, const PostFX& post_fx
+) {
+	DrawLine({}, Line{ start, end }, color, line_width, depth, blend_mode, camera, post_fx);
+}
+
+void Renderer::DrawLine(
+	const Transform& transform, const Line& line, const Tint& color, const LineWidth& line_width,
+	const Depth& depth, BlendMode blend_mode, const Camera& camera, const PostFX& post_fx
+) {
+	DrawShape(
+		transform, line, color, line_width, Origin::Center, depth, blend_mode, camera, post_fx
+	);
+}
+
+void Renderer::DrawTriangle(
+	const Transform& transform, const Triangle& triangle, const Tint& color,
+	const LineWidth& line_width, const Depth& depth, BlendMode blend_mode, const Camera& camera,
+	const PostFX& post_fx
+) {
+	DrawShape(
+		transform, triangle, color, line_width, Origin::Center, depth, blend_mode, camera, post_fx
+	);
+}
+
+void Renderer::DrawEllipse(
+	const Transform& transform, const Ellipse& ellipse, const Tint& color,
+	const LineWidth& line_width, const Depth& depth, BlendMode blend_mode, const Camera& camera,
+	const PostFX& post_fx
+) {
+	DrawShape(
+		transform, ellipse, color, line_width, Origin::Center, depth, blend_mode, camera, post_fx
+	);
+}
+
+void Renderer::DrawCircle(
+	const Transform& transform, const Circle& circle, const Tint& color,
+	const LineWidth& line_width, const Depth& depth, BlendMode blend_mode, const Camera& camera,
+	const PostFX& post_fx
+) {
+	DrawShape(
+		transform, circle, color, line_width, Origin::Center, depth, blend_mode, camera, post_fx
+	);
+}
+
+void Renderer::DrawCapsule(
+	const Transform& transform, const Capsule& capsule, const Tint& color,
+	const LineWidth& line_width, const Depth& depth, BlendMode blend_mode, const Camera& camera,
+	const PostFX& post_fx
+) {
+	DrawShape(
+		transform, capsule, color, line_width, Origin::Center, depth, blend_mode, camera, post_fx
+	);
+}
+
+void Renderer::DrawArc(
+	const Transform& transform, const Arc& arc, const Tint& color, const LineWidth& line_width,
+	const Depth& depth, BlendMode blend_mode, const Camera& camera, const PostFX& post_fx
+) {
+	DrawShape(
+		transform, arc, color, line_width, Origin::Center, depth, blend_mode, camera, post_fx
+	);
+}
+
+void Renderer::DrawPolygon(
+	const Transform& transform, const Polygon& polygon, const Tint& color,
+	const LineWidth& line_width, const Depth& depth, BlendMode blend_mode, const Camera& camera,
+	const PostFX& post_fx
+) {
+	DrawShape(
+		transform, polygon, color, line_width, Origin::Center, depth, blend_mode, camera, post_fx
+	);
+}
+
+void Renderer::DrawPoint(
+	const V2_float& point, const Tint& color, const Depth& depth, BlendMode blend_mode,
 	const Camera& camera
 ) {
-	DrawDebugShape({}, Polygon{ vertices }, color, line_width, camera);
+	DrawShape({}, point, color, -1.0f, Origin::Center, depth, blend_mode, camera, {});
 }
-
-void DrawDebugPoint(const V2_float position, const Color& color, const Camera& camera) {
-	DrawDebugShape({}, position, color, -1.0f, camera);
-}
-
-void DrawDebugShape(
-	const Transform& transform, const Shape& shape, const Color& color, float line_width,
-	const Camera& camera
-) {
-	impl::DrawShapeCommand cmd;
-
-	cmd.transform	 = transform;
-	cmd.shape		 = shape;
-	cmd.tint		 = color;
-	cmd.depth		 = impl::max_depth;
-	cmd.line_width	 = line_width;
-	cmd.render_state = impl::GetDebugRenderState(camera);
-
-	game.renderer.GetRenderData().Submit(cmd);
-}
-
-namespace impl {
 
 void Renderer::Init() {
 	render_data_.Init();
@@ -290,10 +350,6 @@ V2_int Renderer::GetGameSize() const {
 
 ScalingMode Renderer::GetScalingMode() const {
 	return render_data_.resolution_mode_;
-}
-
-RenderData& Renderer::GetRenderData() {
-	return render_data_;
 }
 
 void Renderer::PresentScreen() {
