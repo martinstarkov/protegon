25a20e5fdbaf51015aca705a744725e7f55ed1bf
Martin <emailregisterlogin@gmail.com>
2025-10-27 00:09:28 +0100

    Add NoTransition

--- engine/src/world/scene/scene_transition.h
+++ engine/src/world/scene/scene_transition.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <concepts>
 #include <memory>
+#include <type_traits>
 
 #include "core/util/time.h"
 
@@ -47,5 +49,15 @@ private:
 	milliseconds duration_{ 0 };
 	milliseconds delay_{ 0 };
 };
+
+struct NoTransition {};
+
+namespace impl {
+
+template <typename T>
+concept SceneTransitionType = std::derived_from<std::remove_cvref_t<T>, SceneTransition> ||
+							  std::same_as<std::remove_cvref_t<T>, NoTransition>;
+
+} // namespace impl
 
 } // namespace ptgn
