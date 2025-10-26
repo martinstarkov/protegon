2174559c9a47ca73ec0c3113f4a3489fd653c4e0
Martin <emailregisterlogin@gmail.com>
2025-10-27 00:08:12 +0100

    Update engine_context.h

--- engine/src/core/app/engine_context.h
+++ engine/src/core/app/engine_context.h
@@ -1,43 +1,6 @@
 #pragma once
 
-#include <string>
-
-#include "core/app/window.h"
-#include "core/events/event_handler.h"
-#include "core/input/input_handler.h"
-#include "core/resource/asset_manager.h"
-#include "debug/runtime/debug_system.h"
-#include "math/vector2.h"
-#include "renderer/renderer.h"
-#include "world/scene/scene_manager.h"
-
 namespace ptgn {
 
-struct ApplicationConfig {
-	std::string title{ "Default Title" };
-	V2_int window_size{ 800, 800 };
-};
-
-class Application {
-public:
-	Application(const ApplicationConfig& config = {});
-	~Application();
-	Application(const Application&)			   = delete;
-	Application& operator=(const Application&) = delete;
-	Application(Application&&)				   = delete;
-	Application& operator=(Application&&)	   = delete;
-
-private:
-	Window window_;
-	Renderer render_;
-	InputHandler input_;
-	AssetManager assets_;
-	SceneManager scenes_;
-	EventRegistry events_;
-	DebugSystem debug_;
-
-	float dt_{ 0.0f };
-	bool running_{ false };
-};
 
 } // namespace ptgn
