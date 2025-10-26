d120d451cbcc6e24460fdf28dab6c7d18a09988e
Martin <emailregisterlogin@gmail.com>
2025-10-27 00:08:46 +0100

    Add initialization code

--- engine/src/core/app/application.cpp
+++ engine/src/core/app/application.cpp
@@ -1,3 +1,5 @@
+#include "core/app/application.h"
+
 #include <chrono>
 #include <filesystem>
 #include <memory>
@@ -7,8 +9,6 @@
 #include <vector>
 
 #include "audio/audio.h"
-#include "core/app/game.h"
-#include "core/app/sdl_instance.h"
 #include "core/app/window.h"
 #include "core/input/input_handler.h"
 #include "core/util/file.h"
@@ -23,151 +23,251 @@
 #include "math/vector2.h"
 #include "renderer/api/color.h"
 #include "renderer/gl/gl_context.h"
-#include "renderer/materials/shader.h"
-#include "renderer/materials/texture.h"
+#include "renderer/gl/gl_renderer.h"
 #include "renderer/renderer.h"
 #include "renderer/text/font.h"
+#include "SDL.h"
+#include "SDL_error.h"
+#include "SDL_hints.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
 #include "SDL_timer.h"
+#include "SDL_ttf.h"
+#include "SDL_version.h"
+#include "SDL_video.h"
 #include "serialization/json/json.h"
 #include "serialization/json/json_manager.h"
 #include "world/scene/scene_manager.h"
 
+#ifdef __EMSCRIPTEN__
+
+#include <emscripten.h>
+#include <emscripten/html5.h>
+
+EM_JS(int, get_screen_width, (), { return window.screen.width; });
+EM_JS(int, get_screen_height, (), { return window.screen.height; });
+EM_JS(double, get_device_pixel_ratio, (), { return window.devicePixelRatio || 1.0; });
+
+#endif
+
+#ifdef PTGN_PLATFORM_MACOS
+
+#include <mach-o/dyld.h>
+
+#include <filesystem>
+#include <iostream>
+
+#include "CoreFoundation/CoreFoundation.h"
+
+#endif
+
+inline std::ostream& operator<<(std::ostream& os, const SDL_version& v) {
+	os << static_cast<int>(v.major) << "." << static_cast<int>(v.minor) << "."
+	   << static_cast<int>(v.patch);
+	return os;
+}
+
 namespace ptgn {
 
-impl::Game game;
+#ifdef __EMSCRIPTEN__
 
 namespace impl {
 
-Game::Game() :
-	sdl_instance_{ std::make_unique<SDLInstance>() },
-	window_{ std::make_unique<Window>() },
-	window{ *window_ },
-	gl_context_{ std::make_unique<GLContext>() },
-	input_{ std::make_unique<InputHandler>() },
-	input{ *input_ },
-	renderer_{ std::make_unique<Renderer>() },
-	renderer{ *renderer_ },
-	scene_{ std::make_unique<SceneManager>() },
-	scene{ *scene_ },
-	music_{ std::make_unique<MusicManager>() },
-	music{ *music_ },
-	sound_{ std::make_unique<SoundManager>() },
-	sound{ *sound_ },
-	json_{ std::make_unique<JsonManager>() },
-	json{ *json_ },
-	font_{ std::make_unique<FontManager>() },
-	font{ *font_ },
-	texture_{ std::make_unique<TextureManager>() },
-	texture{ *texture_ },
-	shader_{ std::make_unique<ShaderManager>() },
-	shader{ *shader_ },
-	debug_{ std::make_unique<DebugSystem>() },
-	debug{ *debug_ } {
-	// TODO: Move all of this init code into respective constructors.
-#if defined(PTGN_PLATFORM_MACOS) && !defined(__EMSCRIPTEN__)
-	impl::InitApplePath();
-#endif
-	if (!sdl_instance_->IsInitialized()) {
-		sdl_instance_->Init();
+static EM_BOOL EmscriptenResize(
+	int event_type, const EmscriptenUiEvent* ui_event, void* window_ptr
+) {
+	auto& window{ *static_cast<::ptgn::Window*>(window_ptr) };
+	V2_int window_size{ ui_event->windowInnerWidth, ui_event->windowInnerHeight };
+	// TODO: Figure out how to deal with itch.io fullscreen button not changing SDL status to
+	// fullscreen.
+	V2_int screen_size{ get_screen_width(), get_screen_height() };
+	if (window_size == screen_size) {
+		auto device_pixel_ratio{ get_device_pixel_ratio() };
+		window_size = window_size * device_pixel_ratio;
 	}
-	font.Init();
-	window.Init();
-	gl_context_->Init();
-	input.Init();
-
-	shader.Init();
-	renderer.Init();
+	window.SetSize(window_size);
+	return 0;
 }
 
-Game::~Game() {
-	gl_context_->Shutdown();
-	sdl_instance_->Shutdown();
+static void EmscriptenInit(Window& window) {
+	emscripten_set_resize_callback(
+		EMSCRIPTEN_EVENT_TARGET_WINDOW, static_cast<void*>(&window), 0, EmscriptenResize
+	);
 }
 
-float Game::dt() const {
-	return dt_;
+void EmscriptenMainLoop(void* application) {
+	auto& app{ *static_cast<Application*>(application) };
+
+	app.Update();
+
+	if (!app.IsRunning()) {
+		emscripten_cancel_main_loop();
+	}
 }
 
-float Game::time() const {
-	// TODO: Consider casting to chrono duration instead.
-	return static_cast<float>(SDL_GetTicks64());
+} // namespace impl
+
+#endif
+
+Application::SDLInstance::SDLInstance() {
+#if defined(PTGN_PLATFORM_MACOS) && !defined(__EMSCRIPTEN__)
+	// When using AppleClang, the working directory for the executable is set to $HOME instead of
+	// the executable directory. Therefore, the C++ code corrects the working directory using
+	// std::filesystem so that relative paths work properly.
+	//
+	// TODO: Add check that this hasnt happened yet.
+	char path[1024];
+	std::uint32_t size = sizeof(path);
+	std::filesystem::path exe_dir;
+	if (_NSGetExecutablePath(path, &size) == 0) {
+		exe_dir = std::filesystem::path(path).parent_path();
+	} else {
+		std::cout << "Buffer too small to retrieve executable path. Please run "
+					 "the executable from a terminal"
+				  << std::endl;
+		exe_dir = std::getenv("PWD");
+	}
+	std::filesystem::current_path(exe_dir);
+	// TODO: Check if needed:
+	/*CFBundleRef main_bundle = CFBundleGetMainBundle();
+	CFURLRef resources_url = CFBundleCopyResourcesDirectoryURL(main_bundle);
+	char path[PATH_MAX];
+	if (!CFURLGetFileSystemRepresentation(resources_url, TRUE, (UInt8*)path,
+	PATH_MAX)) { std::cout << "Couldn't get file system representation! " <<
+	std::endl;
+	}
+	CFRelease(resources_url);
+	chdir(path);*/
+#endif
+
+#ifdef PTGN_DEBUG
+	PTGN_INFO("Build Type: Debug");
+#else
+	PTGN_INFO("Build Type: Release");
+#endif
+
+	std::uint32_t sdl_flags{ SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS | SDL_INIT_TIMER };
+	PTGN_ASSERT(
+		SDL_WasInit(sdl_flags) != sdl_flags, "Cannot reinitialize SDL instance before shutting down"
+	);
+
+	// Ensures window and elements scale by monitor zoom level for constant
+	// appearance.
+	SDL_SetHint(SDL_HINT_WINDOWS_DPI_AWARENESS, "permonitorv2");
+
+	int sdl_init{ SDL_Init(sdl_flags) };
+	PTGN_ASSERT(sdl_init == 0, SDL_GetError());
+
+	SDL_version sdl_version;
+	SDL_GetVersion(&sdl_version);
+	PTGN_INFO("Initialized SDL version: ", sdl_version);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, PTGN_OPENGL_CONTEXT_PROFILE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, PTGN_OPENGL_MAJOR_VERSION);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, PTGN_OPENGL_MINOR_VERSION);
+
+	int img_flags{ IMG_INIT_PNG | IMG_INIT_JPG };
+
+	PTGN_ASSERT(
+		IMG_Init(0) != img_flags, "Cannot reinitialize SDL_image instance before shutting down"
+	);
+
+	int img_init{ IMG_Init(img_flags) };
+
+	PTGN_ASSERT(img_init == img_flags, IMG_GetError());
+
+	const SDL_version* sdl_image_version = IMG_Linked_Version();
+	PTGN_INFO("Initialized SDL_image version: ", *sdl_image_version);
+
+	PTGN_ASSERT(TTF_WasInit() == 0, "Cannot reinitialize SDL_ttf instance before shutting down");
+
+	int ttf_init{ TTF_Init() };
+	PTGN_ASSERT(ttf_init != -1, TTF_GetError());
+
+	const SDL_version* sdl_ttf_version = TTF_Linked_Version();
+	PTGN_INFO("Initialized SDL_ttf version: ", *sdl_ttf_version);
+
+#ifdef PTGN_PLATFORM_MACOS
+	int mixer_flags{ MIX_INIT_MP3 | MIX_INIT_OGG };
+#else
+	int mixer_flags{ MIX_INIT_MP3 | MIX_INIT_OGG | MIX_INIT_OPUS |
+					 MIX_INIT_WAVPACK /* | MIX_INIT_FLAC | MIX_INIT_MOD | MIX_INIT_MID*/ };
+#endif
+
+#ifdef __EMSCRIPTEN__
+	mixer_flags = { MIX_INIT_OGG };
+#endif
+
+	PTGN_ASSERT(
+		Mix_Init(0) != mixer_flags, "Cannot reinitialize SDL_mixer instance before shutting down"
+	);
+
+	if (int mixer_init{ Mix_Init(mixer_flags) }; mixer_init != mixer_flags) {
+		PTGN_WARN(Mix_GetError());
+	}
+
+	int audio_open{ Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) };
+	PTGN_ASSERT(audio_open != -1, Mix_GetError());
+
+	const SDL_version* sdl_mixer_version = Mix_Linked_Version();
+	PTGN_INFO("Initialized SDL_mixer version: ", *sdl_mixer_version);
 }
 
-void Game::Stop() {
-	running_ = false;
+Application::SDLInstance::~SDLInstance() {
+	Mix_CloseAudio();
+	PTGN_INFO("Closed SDL_mixer audio");
+	Mix_Quit();
+	PTGN_INFO("Deinitialized SDL_mixer");
+	TTF_Quit();
+	PTGN_INFO("Deinitialized SDL_ttf");
+	IMG_Quit();
+	PTGN_INFO("Deinitialized SDL_image");
+	SDL_Quit();
+	PTGN_INFO("Deinitialized SDL");
 }
 
-bool Game::IsInitialized() const {
-	return gl_context_->IsInitialized() && sdl_instance_->IsInitialized();
+Application::Application(const ApplicationConfig& config) :
+	window_{ config.title, config.window_size } /*renderer_{ config.window_size }*/ {
+	window_.SetSetting(WindowSetting::FixedSize);
+
+	PTGN_ASSERT(!instance_, "Only one Application instance allowed at a time");
+	instance_ = this;
 }
 
-bool Game::IsRunning() const {
+Application::~Application() {
+	instance_ = nullptr;
+}
+
+Application& Application::Get() {
+	PTGN_ASSERT(instance_, "Application instance not initialized");
+	return *instance_;
+}
+
+bool Application::IsRunning() const {
 	return running_;
 }
 
-void Game::Init(const std::string& title, const V2_int& game_size) {
-	window.SetTitle(title);
-	// Order matters here.
-	window.SetSize(game_size);
-	renderer.SetGameSize(game_size);
-	window.SetSetting(WindowSetting::FixedSize);
-}
-
-void Game::Shutdown() {
-	scene.Shutdown();
-
-	sound.Stop(-1);
-	music.Stop();
-
-	// TODO: Simply reset all the unique pointers instead of doing this.
-	debug.Shutdown();
-
-	renderer.Shutdown();
-	shader.Shutdown();
-	input.Shutdown();
-	window.Shutdown();
-
-	// Keep SDL2 instance and OpenGL context alive to ensure SDL2 objects such as TTF_Font are
-	// consistent across game instances. For instance: If the user does the following:
-	//
-	// game.scene.Enter<StartScene>();
-	// // Inside StartScene:
-	// static Font test_font;
-	// Text test_text{ test_font };
-	// // After window quit start again:
-	// game.scene.Enter<StartScene>();
-	// static Font test_font; // handle already created in the previous SDL initialization.
-	// Text test_text{ test_font }; // if SDL has been shutdown, this would not work due to
-	// inconsistent SDL versions.
-	//
-	// Instead, these are called in the Game destructor, which is called upon main() termination.
-	// gl_context_.Shutdown();
-	// sdl_instance_.Shutdown();
-}
-
-void Game::MainLoop() {
-	PTGN_ASSERT(window.IsValid(), "Game must be initialized before entering a scene");
+void Application::EnterMainLoop() {
 	// Design decision: Latest possible point to show window is right before
 	// loop starts. Comment this if you wish the window to appear hidden for an
 	// indefinite period of time.
-	window.SetSetting(WindowSetting::Shown);
+	window_.SetSetting(WindowSetting::Shown);
 	running_ = true;
+
 #ifdef __EMSCRIPTEN__
-	EmscriptenInit();
-	emscripten_set_main_loop(EmscriptenLoop, 0, 1);
-	// TODO: emscripten_set_main_loop_arg(em_arg_callback_func func, void *arg, int fps, bool
-	// simulate_infinite_loop)
-	// From: https://emscripten.org/docs/api_reference/emscripten.h.html#c.emscripten_set_main_loop
+	EmscriptenInit(window_);
+	emscripten_set_main_loop_arg(
+		impl::EmscriptenMainLoop, this, /*fps=*/0, /*simulateInfiniteLoop=*/true
+	);
 #else
-	while (running_) {
+	while (IsRunning()) {
 		Update();
 	}
-	Shutdown();
 #endif
 }
 
-void Game::Update() {
-	debug.PreUpdate();
+void Application::Update() {
+	debug_.PreUpdate();
 
 	static auto start{ std::chrono::system_clock::now() };
 	static auto end{ std::chrono::system_clock::now() };
@@ -190,13 +290,24 @@ void Game::Update() {
 
 	start = end;
 
-	scene.Update(*this);
+	scene_.Update();
 
-	debug.PostUpdate();
+	debug_.PostUpdate();
 
 	end = std::chrono::system_clock::now();
 }
 
-} // namespace impl
+void Application::Stop() {
+	running_ = false;
+}
+
+float Application::dt() const {
+	return dt_;
+}
+
+float Application::time() const {
+	// TODO: Consider casting to chrono duration instead.
+	return static_cast<float>(SDL_GetTicks64());
+}
 
 } // namespace ptgn
--- engine/src/core/app/application.h
+++ engine/src/core/app/application.h
@@ -1,20 +1,37 @@
 #pragma once
 
+#include <memory>
 #include <string>
 
 #include "core/app/window.h"
 #include "core/asset/asset_manager.h"
-#include "core/event/event_handler.h"
+// #include "core/event/event_handler.h"
+#include "audio/audio.h"
 #include "core/input/input_handler.h"
 #include "debug/runtime/debug_system.h"
 #include "math/vector2.h"
+#include "renderer/material/shader.h"
 #include "renderer/renderer.h"
+#include "serialization/json/json_manager.h"
+#include "world/scene/scene_key.h"
 #include "world/scene/scene_manager.h"
 
 namespace ptgn {
 
+namespace impl {
+
+class ProfileInstance;
+
+#ifdef __EMSCRIPTEN__
+
+void EmscriptenMainLoop(void* application);
+
+#endif
+
+} // namespace impl
+
 struct ApplicationConfig {
-	std::string title{ "Default Title" };
+	const char* title{ "Default Title" };
 	V2_int window_size{ 800, 800 };
 };
 
@@ -27,17 +44,84 @@ public:
 	Application(Application&&)				   = delete;
 	Application& operator=(Application&&)	   = delete;
 
-private:
-	Window window_;
+	template <impl::SceneType TScene, impl::SceneTransitionType TransitionIn, class... TArgs>
+		requires std::constructible_from<TScene, TArgs...>
+	void StartWith(const SceneKey& key, TransitionIn&& transition_in, TArgs&&... args) {
+		// Construct and register the scene
+		scene_.Transition<TScene>(
+			std::nullopt, key, std::forward<TransitionIn>(transition_in), NoTransition{},
+			std::forward<TArgs>(args)...
+		);
+		EnterMainLoop();
+	}
+
+	// @return True if inside the main loop, false otherwise.
+	[[nodiscard]] bool IsRunning() const;
+
+	void Stop();
+
+	// TODO: Move these elsewhere.
+
+	float dt() const;
+
+	float time() const;
+
+	// TODO: Move this to private.
+	impl::DebugSystem debug_;
+	// TODO: Move this to private.
+	static Application& Get();
+	// TODO: Move this to private.
 	Renderer render_;
+	// TODO: Move this to private.
+	Window window_;
+	// TODO: Move this to private.
+	SceneManager scene_;
+	// TODO: Move this to private.
 	InputHandler input_;
-	AssetManager assets_;
-	SceneManager scenes_;
-	EventRegistry events_;
-	DebugSystem debug_;
+
+	// TODO: Merge these all into asset manager and make it private.
+	impl::JsonManager json;
+	impl::SoundManager sound;
+	impl::MusicManager music;
+	impl::TextureManager texture;
+	impl::FontManager font;
+	impl::ShaderManager shader;
+
+private:
+#ifdef __EMSCRIPTEN__
+
+	friend void impl::EmscriptenMainLoop(void* application);
+
+#endif
+	friend class impl::ProfileInstance;
+	// TODO: Get rid of this.
+	friend class InputHandler;
+	// TODO: Get rid of this.
+	friend class Scene;
+	// TODO: Get rid of this.
+	friend class impl::DebugSystem;
+
+	void EnterMainLoop();
+	void Update();
+
+	struct SDLInstance {
+		SDLInstance();
+		~SDLInstance();
+		SDLInstance(const SDLInstance&)			   = delete;
+		SDLInstance& operator=(const SDLInstance&) = delete;
+		SDLInstance(SDLInstance&&)				   = delete;
+		SDLInstance& operator=(SDLInstance&&)	   = delete;
+	};
+
+	SDLInstance sdl_;
+	// TODO: Add this.
+	// EventRegistry events_;
+	// AssetManager assets_;
 
 	float dt_{ 0.0f };
 	bool running_{ false };
+
+	inline static Application* instance_{ nullptr };
 };
 
 } // namespace ptgn
