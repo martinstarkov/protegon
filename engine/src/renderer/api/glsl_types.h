cfd20a4ecaee1bf348f1cc27c50a4e824f00c60e
Martin <emailregisterlogin@gmail.com>
2025-11-04 21:11:33 +0100

    Move glsl types into api

--- engine/src/renderer/api/glsl_types.h
+++ engine/src/renderer/api/glsl_types.h
@@ -3,24 +3,22 @@
 #include <array>
 #include <cstdint>
 
-#include "core/util/concepts.h"
-
 namespace ptgn::glsl {
-
-using float_ = std::array<float, 1>;
-using vec2	 = std::array<float, 2>;
-using vec3	 = std::array<float, 3>;
-using vec4	 = std::array<float, 4>;
-
-using double_ = std::array<double, 1>;
-using dvec2	  = std::array<double, 2>;
-using dvec3	  = std::array<double, 3>;
-using dvec4	  = std::array<double, 4>;
 
 using bool_ = std::array<bool, 1>;
 using bvec2 = std::array<bool, 2>;
 using bvec3 = std::array<bool, 3>;
 using bvec4 = std::array<bool, 4>;
+
+using float_ = std::array<std::float_t, 1>;
+using vec2	 = std::array<std::float_t, 2>;
+using vec3	 = std::array<std::float_t, 3>;
+using vec4	 = std::array<std::float_t, 4>;
+
+using double_ = std::array<std::double_t, 1>;
+using dvec2	  = std::array<std::double_t, 2>;
+using dvec3	  = std::array<std::double_t, 3>;
+using dvec4	  = std::array<std::double_t, 4>;
 
 using int_	= std::array<std::int32_t, 1>;
 using ivec2 = std::array<std::int32_t, 2>;
