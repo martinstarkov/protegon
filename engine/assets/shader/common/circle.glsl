eacea4cfdf1e83c4e12a11b2e8adf4798b4b3e64
Martin <emailregisterlogin@gmail.com>
2026-01-22 22:07:33 +0100

    Add assets (shaders + default font)

---
+++ engine/assets/shader/common/circle.glsl
@@ -0,0 +1,32 @@
+#option auto_layout
+
+#type fragment
+
+out vec4 o_Color;
+
+in vec4 v_Color;
+in vec2 v_TexCoord;
+in vec4 v_Data; // x = thickness, y = fade
+
+float CircleDistance(vec2 point, float radius) {
+    return radius - length(point) / radius;
+}
+
+void main() {
+    float thickness = v_Data.x; // 0.0f = hollow, 1.0f = filled
+    float fade = v_Data.y;
+    
+    float radius = 1.0f;
+    vec2 uv = v_TexCoord * 2.0f - 1.0f; // Normalize to: [-1, 1]
+    // Not technically an exact ellipse, for that see: https://iquilezles.org/articles/distfunctions2d/
+
+    float distance = CircleDistance(uv, radius);
+
+    float alpha = smoothstep(0.0f, fade, distance);
+    alpha *= smoothstep(thickness + fade, thickness, distance);
+
+    if (alpha <= 0.0f)
+        discard;
+
+    o_Color = vec4(v_Color.rgb, v_Color.a * alpha);
+}
