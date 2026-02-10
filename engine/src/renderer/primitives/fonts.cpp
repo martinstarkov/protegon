40bc6ce7eeeac1346ae00c66abc1d7da8ac4a9bd
Martin <emailregisterlogin@gmail.com>
2026-02-10 14:13:37 +0100

    Add old engine files

---
+++ engine/src/renderer/primitives/fonts.cpp
@@ -0,0 +1,11 @@
+#include "renderer/primitives/fonts.h"
+
+#include "fonts/LiberationSans-Regular.h"
+
+namespace ptgn::impl {
+
+FontBinary GetLiberationSansRegular() {
+	return FontBinary{ LiberationSans_Regular_ttf, LiberationSans_Regular_ttf_len };
+}
+
+} // namespace ptgn::impl
