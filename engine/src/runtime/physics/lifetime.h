cd03a52cd04a67fb16ae3b60019d862cb4236e96
Martin <emailregisterlogin@gmail.com>
2026-03-02 21:22:42 +0100

    Fix lifetime component

--- engine/src/runtime/physics/lifetime.h
+++ engine/src/runtime/physics/lifetime.h
@@ -1,37 +1,36 @@
-// #pragma once
-//
-// #include "core/util/time.h"
-// #include "core/util/timer.h"
-// #include "serialization/json/serializable.h"
-//
-// namespace ptgn {
-//
-// class Manager;
-// class Entity;
-// class Scene;
-//
-// struct Lifetime {
-//	Lifetime() = default;
-//
-//	Lifetime(milliseconds lifetime, bool start = false);
-//
-//	// Will restart if lifetime is already running.
-//	void Start();
-//
-//	void Update(Entity& entity) const;
-//
-//	milliseconds duration{ 0 };
-//
-//	PTGN_SERIALIZER_REGISTER_NAMED(
-//		Lifetime, KeyValue("duration", duration), KeyValue("timer", timer_)
-//	)
-//
-// private:
-//	friend class Scene;
-//
-//	static void Update(Scene& scene);
-//
-//	Timer timer_;
-// };
-//
-// } // namespace ptgn
+#pragma once
+
+#include "core/time/time.h"
+#include "core/time/timer.h"
+#include "runtime/ecs/entity.h"
+#include "serialization/json/serialize.h"
+
+namespace ptgn {
+
+class Scene;
+
+struct Lifetime {
+	Lifetime() = default;
+
+	explicit Lifetime(milliseconds lifetime, bool start = false);
+
+	/// @brief Will restart if lifetime is already running.
+	void Start();
+
+	void Update(Entity entity) const;
+
+	milliseconds duration{ 0 };
+
+	PTGN_SERIALIZER_REGISTER_NAMED(
+		Lifetime, KeyValue("duration", duration), KeyValue("timer", timer_)
+	)
+
+private:
+	friend class Scene;
+
+	static void Update(Scene& scene);
+
+	Timer timer_;
+};
+
+} // namespace ptgn
