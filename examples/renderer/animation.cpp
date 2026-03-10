12147e18a851668dcb7f6eb19046eb715a94e103
Martin <emailregisterlogin@gmail.com>
2026-03-10 03:53:48 +0100

    Fix animation demo

--- examples/renderer/animation.cpp
+++ examples/renderer/animation.cpp
@@ -1,16 +1,31 @@
-#include "runtime/graphics/animation.h"
+#include "runtime/animation/animation.h"
 
 #include "app/application.h"
+#include "core/event/dispatcher.h"
 #include "core/math/vector2.h"
 #include "core/time/time.h"
 #include "platform/input/input_handler.h"
+#include "runtime/animation/animation.h"
+#include "runtime/animation/tween.h"
 #include "runtime/scene/scene.h"
 #include "runtime/scene/scene_manager.h"
 #include "runtime/scripting/script.h"
+#include "runtime/scripting/scripts.h"
 
 using namespace ptgn;
 
-struct MyAnimationScript1 : public Script<MyAnimationScript1, AnimationScript> {
+struct MyAnimationScript1 : public Script {
+	void OnEvent(EventDispatcher d) override {
+		d.Dispatch<AnimationStart>([this](auto& e) { OnAnimationStart(); });
+		d.Dispatch<AnimationUpdate>([this](auto& e) { OnAnimationUpdate(); });
+		d.Dispatch<AnimationRepeat>([this](auto& e) { OnAnimationRepeat(); });
+		d.Dispatch<AnimationFrameChange>([this](auto& e) { OnAnimationFrameChange(); });
+		d.Dispatch<AnimationComplete>([this](auto& e) { OnAnimationComplete(); });
+		d.Dispatch<AnimationPause>([this](auto& e) { OnAnimationPause(); });
+		d.Dispatch<AnimationResume>([this](auto& e) { OnAnimationResume(); });
+		d.Dispatch<AnimationStop>([this](auto& e) { OnAnimationStop(); });
+	}
+
 	void OnAnimationStart() {
 		PTGN_LOG("OnAnimationStart");
 	}
@@ -52,29 +67,17 @@ public:
 
 	void OnEnter() override {
 		app().asset.Load("anim", "assets/animation.png");
-		app().asset.Load("anim2", "assets/animation4.png");
-		// app().asset.Load("anim3", "assets/animation3.png");
 
-		/*sprite = CreateSprite(*this, "anim", GetPosition(camera) + V2_int{ 64, 0 });
-		SetScale(sprite, 3.0f);
-		auto& crop	  = sprite.Add<TextureCrop>();
-		crop.size	  = V2_int{ 16, 32 };
-		crop.position = V2_int{ 0, 0 };
-		Hide(sprite);*/
-
-		// animation2 =
-		// CreateAnimation(*this, "anim3", GetPosition(camera), 16, milliseconds{ 1000 },
-		// V2_int{ 512, 512 }, -1, {});
 		animation = CreateAnimation(
-			*this, "anim2", GetPosition(camera), 16, milliseconds{ 2000 }, V2_int{ 512, 512 }, -1,
-			{}
+			*this, "anim", GetPosition(camera), 4, milliseconds{ 500 }, V2_int{ 16, 32 }, -1,
+			{ 0, 32 }
 		);
+
+		SetScale(animation, 4.0f);
+
 		AddScript<MyAnimationScript1>(animation);
-		// CreateAnimation(*this, "anim", GetPosition(camera), 4, milliseconds{ 500
-		// },V2_int{ 16, 32 }, -1, {}); SetScale(animation2, 0.5f);
 
 		animation.Start();
-		// animation2.Start();
 	}
 
 	void OnUpdate() override {
@@ -86,11 +89,6 @@ public:
 		if (input.KeyPressed(Key::T)) {
 			animation.Toggle();
 		}
-	}
-
-	void OnExit() override {
-		json j = *this;
-		SaveJson(j, "assets/animation_scene.json");
 	}
 };
 
