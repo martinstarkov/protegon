3d34dab4be6bfbfe089a1a121bab165bfcc50e6d
Martin <emailregisterlogin@gmail.com>
2026-03-11 02:00:12 +0100

    Add concept SpecializationOf

--- engine/src/core/util/concepts.h
+++ engine/src/core/util/concepts.h
@@ -9,6 +9,19 @@
 
 namespace ptgn {
 
+namespace impl {
+
+template <typename Test, template <typename...> class Ref>
+struct is_specialization : std::false_type {};
+
+template <template <typename...> class Ref, typename... Args>
+struct is_specialization<Ref<Args...>, Ref> : std::true_type {};
+
+} // namespace impl
+
+template <typename T, template <typename...> class Ref>
+concept SpecializationOf = impl::is_specialization<T, Ref>::value;
+
 template <typename... Ts>
 concept NonEmptyPack = (sizeof...(Ts) > 0);
 
