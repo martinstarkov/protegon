2089cd74229e85a701500e1721f49fe9191d77d4
Martin <emailregisterlogin@gmail.com>
2026-03-15 03:09:49 +0100

    Clean up ripple shader

--- examples/assets/ripple.glsl
+++ examples/assets/ripple.glsl
@@ -12,26 +12,26 @@ uniform float u_Time;
 void main() {
     float time = u_Time;
 
-    vec2 center = vec2(0.5, 0.5);
+    vec2 center = vec2(0.5f, 0.5f);
 
-	float speed = 0.035;
+	float speed = 0.035f;
 
     vec2 uv = v_TexCoord;
 		
-	vec3 col = vec4(uv,0.5+0.5*sin(time),1.0).xyz;
+	vec3 col = vec4(uv, 0.5f + 0.5f * sin(time), 1.0f).xyz;
    
     vec3 texcol;
 			
-	float x = center.x-uv.x;
-	float y = center.y-uv.y;
+	float x = center.x - uv.x;
+	float y = center.y - uv.y;
 		
-	//float r = -sqrt(x*x + y*y); //uncoment this line to symmetric ripples
-	float r = -(x*x + y*y);
-	float z = 1.0 + 0.5*sin((r+time*speed)/0.013);
+	//float r = -sqrt(x * x + y * y); // Uncoment this line for symmetric ripples
+	float r = -(x * x + y * y);
+	float z = 1.0f + 0.5f * sin((r + time * speed) / 0.013f);
 	
 	texcol.x = z;
 	texcol.y = z;
 	texcol.z = z;
 	
-	o_Color = vec4(col*texcol,1.0) * v_Color;
+	o_Color = vec4(col * texcol, 1.0f) * v_Color;
 }
