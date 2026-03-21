0068f3c66f1862f3a98cab09960399f8f1801367
Martin <emailregisterlogin@gmail.com>
2026-03-21 09:03:14 +0100

    Add animated bell demo

--- examples/ui/button_animated.cpp
+++ examples/ui/button_animated.cpp
@@ -22,33 +22,60 @@ using namespace ptgn;
 class AnimatedButtonScene : public Scene {
 public:
 	Button b1;
+	Button b2;
 
 	void OnEnter() override {
 		input.SetSettings({ .debug_draw_enabled = true });
 
-		app().asset.Load("idle", "assets/button_idle.png");
-		app().asset.Load("animation_hover", "assets/button_animation_hover.png");
-		app().asset.Load("animation_activate", "assets/button_animation_activate.png");
+		app().asset.Load("idle", "assets/bell.png");
+		app().asset.Load("animation_hover", "assets/bell_hover_animation.png");
+		app().asset.Load("animation_activate", "assets/bell_click_animation.png");
+		app().asset.LoadAudio("hover", "assets/hover.ogg");
+		app().asset.LoadAudio("click", "assets/bell.ogg");
+
+		app().asset.Load("idle2", "assets/button_idle.png");
+		app().asset.Load("animation_hover2", "assets/button_animation_hover.png");
+		app().asset.Load("animation_activate2", "assets/button_animation_activate.png");
+		app().asset.LoadAudio("click2", "assets/click.ogg");
 
 		auto hover_animation{ CreateAnimation(
-			*this, "animation_activate", V2_int{}, { 4, milliseconds{ 500 }, V2_int{ 32, 16 }, -1 }
+			*this, "animation_hover", V2_int{}, { 3, milliseconds{ 400 }, V2_int{ 253, 167 }, -1 }
 		) };
 
 		auto activate_animation{ CreateAnimation(
-			*this, "animation_activate", V2_int{}, { 4, milliseconds{ 200 }, V2_int{ 32, 16 }, 1 }
+			*this, "animation_activate", V2_int{}, { 3, milliseconds{ 200 }, V2_int{ 253, 167 }, 1 }
 		) };
 
 		b1 = CreateButton(*this, *GetDisplaySize(activate_animation));
 		b1.SetTexture("idle")
 			.SetAnimation(std::move(hover_animation), ButtonState::Hover)
 			.SetAnimation(std::move(activate_animation), ButtonState::Press)
-			.SetTint(color::Red, ButtonState::Idle)
-			.SetTint(color::Green, ButtonState::Hover)
-			.SetTint(color::Blue, ButtonState::Press);
+			.SetSound("hover", ButtonState::Hover)
+			.SetSound("click", ButtonState::Press);
 
-		SetScale(b1, 4.0f);
+		SetScale(b1, 1.0f);
 
-		b1.OnActivate([]() { PTGN_LOG("Clicked button!"); });
+		b1.OnActivate([]() { PTGN_LOG("Clicked bell!"); });
+
+		auto hover_animation2{ CreateAnimation(
+			*this, "animation_hover2", V2_int{}, { 4, milliseconds{ 400 }, V2_int{ 32, 16 }, -1 }
+		) };
+
+		auto activate_animation2{ CreateAnimation(
+			*this, "animation_activate2", V2_int{}, { 4, milliseconds{ 200 }, V2_int{ 32, 16 }, 1 }
+		) };
+
+		b2 = CreateButton(*this, *GetDisplaySize(activate_animation2));
+		b2.SetTexture("idle2")
+			.SetAnimation(std::move(hover_animation2), ButtonState::Hover)
+			.SetAnimation(std::move(activate_animation2), ButtonState::Press)
+			.SetSound("hover", ButtonState::Hover)
+			.SetSound("click2", ButtonState::Press);
+
+		SetPosition(b2, { 0, 200 });
+		SetScale(b2, 4.0f);
+
+		b2.OnActivate([]() { PTGN_LOG("Clicked button!"); });
 	}
 };
 
