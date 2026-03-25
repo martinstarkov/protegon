b5a6d4a9812dc4430172f161a8b455969224339f
Martin <emailregisterlogin@gmail.com>
2026-03-25 08:16:26 +0100

    Make scene priority explicit

--- engine/src/runtime/scene/scene_command.h
+++ engine/src/runtime/scene/scene_command.h
@@ -9,6 +9,14 @@ namespace ptgn {
 
 class Scene;
 
+struct ScenePriority {
+	explicit ScenePriority() = default;
+
+	explicit ScenePriority(std::size_t value) : value{ value } {}
+
+	std::size_t value{ 0 };
+};
+
 namespace impl {
 
 enum class SceneCommandType {
@@ -23,12 +31,12 @@ struct SceneCommand {
 	std::size_t from_scene_key{ 0 };
 	std::size_t to_scene_key{ 0 };
 
-	std::size_t priority{ 0 };
+	ScenePriority priority;
 
 	std::function<std::unique_ptr<Scene>()> scene_factory;
 
-	std::unique_ptr<SceneTransition> transition_in;
 	std::unique_ptr<SceneTransition> transition_out;
+	std::unique_ptr<SceneTransition> transition_in;
 };
 
 } // namespace impl
