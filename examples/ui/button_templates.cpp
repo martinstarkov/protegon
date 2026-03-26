d065d9a1bfeed0650c54717affdc93515c42033c
Martin <emailregisterlogin@gmail.com>
2026-03-26 17:01:04 +0100

    Add textures to config

--- examples/ui/button_templates.cpp
+++ examples/ui/button_templates.cpp
@@ -19,6 +19,7 @@
 #include "runtime/asset/asset_manager.h"
 #include "runtime/ecs/component.h"
 #include "runtime/ecs/entity.h"
+#include "runtime/graphics/draw.h"
 #include "runtime/graphics/font.h"
 #include "runtime/graphics/text.h"
 #include "runtime/scene/scene.h"
@@ -39,39 +40,43 @@ struct ScaleButtonConfig {
 	Ease ease{ Ease::Linear };
 };
 
-struct TemplateButtonConfig {
-	std::string_view content;
-	Color text_color{ color::White };
-	Color text_hover_color{ color::Green };
-	Color text_click_color{ text_hover_color };
-	int text_outline_width{ 0 };
-	Color text_outline_color{ color::Black };
+struct TextButtonConfig {
+	std::optional<std::string_view> content;
+	std::optional<Color> text_color{ color::White };
+	std::optional<Color> text_color_hover;
+	std::optional<Color> text_color_press;
+	std::optional<int> text_outline_width;
+	std::optional<Color> text_outline_color;
 
 	FontSize font_size;
 	FontOrKey font;
 
-	Color background_color{ color::Transparent };
-	Color background_hover_color{ color::Transparent };
-	Color background_click_color{ color::Transparent };
+	std::optional<TextureOrKey> texture;
+	std::optional<TextureOrKey> texture_hover;
+	std::optional<TextureOrKey> texture_press;
+
+	std::optional<Color> texture_tint;
+	std::optional<Color> texture_tint_hover;
+	std::optional<Color> texture_tint_press;
+
+	std::optional<Color> background_color;
+	std::optional<Color> background_color_hover;
+	std::optional<Color> background_color_press;
 
 	std::optional<V2_float> background_size;
 
-	AudioOrKey click{};
-	AudioOrKey hover{};
+	std::optional<AudioOrKey> sound_press;
+	std::optional<AudioOrKey> sound_hover;
 
 	std::optional<MoveButtonConfig> move;
 	std::optional<ScaleButtonConfig> scale;
 };
 
-static Button CreateTemplateButton(
-	Scene& scene, V2_float position, V2_float size, const TemplateButtonConfig& config = {}
+static Button CreateTextButton(
+	Scene& scene, V2_float position, V2_float size, const TextButtonConfig& config = {}
 ) {
 	auto button = CreateButton(scene, size);
 	SetPosition(button, position);
-
-	TextProperties text_properties;
-	text_properties.outline.width = config.text_outline_width;
-	text_properties.outline.color = config.text_outline_color;
 
 	std::optional<std::variant<Rect, Circle>> shape;
 
@@ -81,25 +86,72 @@ static Button CreateTemplateButton(
 
 	button.SetBackgroundShape(shape, ButtonState::Idle);
 
-	button.SetBackgroundColor(config.background_color, ButtonState::Idle);
-	button.SetBackgroundColor(config.background_hover_color, ButtonState::Hover);
-	button.SetBackgroundColor(config.background_click_color, ButtonState::Press);
+	if (config.background_color.has_value()) {
+		button.SetBackgroundColor(*config.background_color, ButtonState::Idle);
+	}
+	if (config.background_color_hover.has_value()) {
+		button.SetBackgroundColor(*config.background_color_hover, ButtonState::Hover);
+	}
 
-	button.SetText(
-		config.content, config.text_color, config.font_size, config.font, text_properties,
-		ButtonState::Idle
-	);
-	button.SetText(
-		config.content, config.text_hover_color, config.font_size, config.font, text_properties,
-		ButtonState::Hover
-	);
-	button.SetText(
-		config.content, config.text_click_color, config.font_size, config.font, text_properties,
-		ButtonState::Press
-	);
+	if (config.background_color_press.has_value() || config.background_color_hover.has_value()) {
+		Color bg_color{ config.background_color_press
+							.or_else([&config]() { return config.background_color_hover; })
+							.value() };
+		button.SetBackgroundColor(bg_color, ButtonState::Press);
+	}
 
-	button.SetSound(config.click, ButtonState::Press);
-	button.SetSound(config.hover, ButtonState::Hover);
+	if (config.texture.has_value()) {
+		button.SetTexture(*config.texture, ButtonState::Idle);
+	}
+	if (config.texture_hover.has_value()) {
+		button.SetTexture(*config.texture_hover, ButtonState::Hover);
+	}
+	if (config.texture_press.has_value()) {
+		button.SetTexture(*config.texture_press, ButtonState::Press);
+	}
+
+	if (config.texture_tint.has_value()) {
+		button.SetTextureTint(*config.texture_tint, ButtonState::Idle);
+	}
+
+	if (config.texture_tint_hover.has_value()) {
+		button.SetTextureTint(*config.texture_tint_hover, ButtonState::Hover);
+	}
+
+	if (config.texture_tint_press.has_value() || config.texture_tint_hover.has_value()) {
+		Color tint{
+			config.texture_tint_press.or_else([&config]() { return config.texture_tint_hover; }
+			).value()
+		};
+		button.SetTextureTint(tint, ButtonState::Press);
+	}
+
+	if (config.content.has_value()) {
+		TextProperties text_properties;
+		if (config.text_outline_width.has_value()) {
+			text_properties.outline.width = *config.text_outline_width;
+		}
+		if (config.text_outline_color.has_value()) {
+			text_properties.outline.color = *config.text_outline_color;
+		}
+		auto idle_color{ config.text_color.value_or(impl::kDefaultButtonTextColor) };
+		button.SetText(
+			*config.content, idle_color, config.font_size, config.font, text_properties,
+			ButtonState::Idle
+		);
+		auto hover_color{ config.text_color_hover.value_or(idle_color) };
+		button.SetText(
+			*config.content, hover_color, config.font_size, config.font, text_properties,
+			ButtonState::Hover
+		);
+		button.SetText(
+			*config.content, config.text_color_press.value_or(hover_color), config.font_size,
+			config.font, text_properties, ButtonState::Press
+		);
+	}
+
+	button.SetSound(config.sound_hover, ButtonState::Hover);
+	button.SetSound(config.sound_press, ButtonState::Press);
 
 	if (config.move.has_value()) {
 		button.OnHoverStart([button, config = *config.move]() {
@@ -160,72 +212,96 @@ public:
 	void OnEnter() override {
 		ctx().input.SetSettings({ .debug_draw_enabled = true });
 		SetBackgroundColor(color::LightGray);
-		ctx().asset.LoadAudio("hover", "assets/hover.ogg");
-		ctx().asset.LoadAudio("click", "assets/click.ogg");
+		ctx().asset.Load("hover", "assets/hover.ogg");
+		ctx().asset.Load("press", "assets/press.ogg");
+		ctx().asset.Load("idle", "assets/big_button_idle.png");
+		ctx().asset.Load("hover", "assets/big_button_hover.png");
+		ctx().asset.Load("press", "assets/big_button_press.png");
 
 		V2_float size{ 200, 50 };
 		V2_float offset{ 0, 70 };
 
-		CreateTemplateButton(
+		CreateTextButton(
 			*this, offset * 3, size,
 			{ .content			  = "Celeste",
-			  .text_hover_color	  = color::Red,
+			  .text_color_hover	  = color::Green,
 			  .text_outline_width = 1,
-			  .click			  = "click",
-			  .hover			  = "hover",
+			  .sound_press		  = "press",
+			  .sound_hover		  = "hover",
 			  .move				  = MoveButtonConfig{} }
 		);
 
-		CreateTemplateButton(
+		CreateTextButton(
 			*this, offset * 2, size,
 			{ .content			  = "Terraria",
 			  .text_color		  = color::Gray,
-			  .text_hover_color	  = color::Gold,
+			  .text_color_hover	  = color::Gold,
 			  .text_outline_width = 1,
-			  .click			  = "click",
-			  .hover			  = "hover",
+			  .sound_press		  = "press",
+			  .sound_hover		  = "hover",
 			  .scale			  = ScaleButtonConfig{} }
 		);
 
-		CreateTemplateButton(
+		CreateTextButton(
 			*this, offset * 1, size,
 			{ .content				  = "Payday 2",
 			  .text_color			  = color::LightBlue,
-			  .text_hover_color		  = color::Blue,
+			  .text_color_hover		  = color::Blue,
 			  .text_outline_width	  = 1,
-			  .background_hover_color = color::Blue.WithAlpha(0.1f),
-			  .click				  = "click",
-			  .hover				  = "hover" }
+			  .background_color_hover = color::Blue.WithAlpha(0.1f),
+			  .sound_press			  = "press",
+			  .sound_hover			  = "hover" }
 		);
 
-		CreateTemplateButton(
+		CreateTextButton(
 			*this, offset * 0, size,
 			{ .content			= "Enter the Gungeon",
 			  .text_color		= color::Gray,
-			  .text_hover_color = color::White,
-			  .click			= "click",
-			  .hover			= "hover" }
+			  .text_color_hover = color::White,
+			  .sound_press		= "press",
+			  .sound_hover		= "hover" }
+		);
+
+		CreateTextButton(
+			*this, offset * -1, size,
+			{ .content		 = "Rogue Legacy 2",
+			  .text_color	 = color::Gray,
+			  .texture_hover = "hover",
+			  .texture_press = "hover",
+			  .sound_press	 = "press",
+			  .sound_hover	 = "hover" }
+		);
+
+		CreateTextButton(
+			*this, offset * -2, size,
+			{ .content			  = "Dogs Organized Neatly",
+			  .text_color		  = color::Black,
+			  .text_color_hover	  = color::White,
+			  .texture			  = "idle",
+			  .texture_tint_hover = color::Orange,
+			  .sound_press		  = "press",
+			  .sound_hover		  = "hover" }
 		);
 
 		// ctx().asset.Load("idle", "assets/bell.png");
 		// ctx().asset.Load("animation_hover", "assets/bell_hover_animation.png");
-		// ctx().asset.Load("animation_activate", "assets/bell_click_animation.png");
+		// ctx().asset.Load("animation_press", "assets/bell_press_animation.png");
 		// ctx().asset.LoadAudio("hover", "assets/hover.ogg");
-		// ctx().asset.LoadAudio("click", "assets/bell.ogg");
+		// ctx().asset.LoadAudio("press", "assets/bell.ogg");
 		// auto hover_animation{ CreateAnimation(
 		//	*this, "animation_hover", V2_int{}, { 3, milliseconds{ 400 }, V2_int{ 253, 167 }, -1 }
 		//) };
-		// auto activate_animation{ CreateAnimation(
-		//	*this, "animation_activate", V2_int{}, { 3, milliseconds{ 200 }, V2_int{ 253, 167 }, 1 }
+		// auto press_animation{ CreateAnimation(
+		//	*this, "animation_press", V2_int{}, { 3, milliseconds{ 200 }, V2_int{ 253, 167 }, 1 }
 		//) };
-		// b1 = CreateButton(*this, *GetDisplaySize(activate_animation));
+		// b1 = CreateButton(*this, *GetDisplaySize(press_animation));
 		// b1.SetTexture("idle")
 		//	.SetAnimation(std::move(hover_animation), ButtonState::Hover)
-		//	.SetAnimation(std::move(activate_animation), ButtonState::Press)
+		//	.SetAnimation(std::move(press_animation), ButtonState::Press)
 		//	.SetSound("hover", ButtonState::Hover)
-		//	.SetSound("click", ButtonState::Press);
+		//	.SetSound("press", ButtonState::Press);
 		// SetScale(b1, 1.0f);
-		// b1.OnActivate([]() { PTGN_LOG("Clicked bell!"); });
+		// b1.OnPress([]() { PTGN_LOG("Pressed bell!"); });
 	}
 };
 
