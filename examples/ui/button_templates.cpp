8ad72e70ab6808dbb75414926aca44ca12a5186d
Martin <emailregisterlogin@gmail.com>
2026-03-26 11:21:49 +0100

    Combine template buttons

--- examples/ui/button_templates.cpp
+++ examples/ui/button_templates.cpp
@@ -25,6 +25,18 @@
 using namespace ptgn;
 
 struct MoveButtonConfig {
+	V2_float offset{ 20, 0 };
+	milliseconds duration{ 100 };
+	Ease ease{ Ease::Linear };
+};
+
+struct ScaleButtonConfig {
+	float scale{ 1.25f };
+	milliseconds duration{ 100 };
+	Ease ease{ Ease::Linear };
+};
+
+struct TemplateButtonConfig {
 	std::string_view content;
 	Color text_color{ color::White };
 	Color text_hover_color{ color::Green };
@@ -39,13 +51,12 @@ struct MoveButtonConfig {
 	AudioOrKey click{};
 	AudioOrKey hover{};
 
-	V2_float move_offset{ 20, 0 };
-	milliseconds duration{ 100 };
-	Ease ease{ Ease::Linear };
+	std::optional<MoveButtonConfig> move;
+	std::optional<ScaleButtonConfig> scale;
 };
 
-static Button CreateMoveButton(
-	Scene& scene, V2_float position, V2_float size, const MoveButtonConfig& config = {}
+static Button CreateTemplateButton(
+	Scene& scene, V2_float position, V2_float size, const TemplateButtonConfig& config = {}
 ) {
 	auto button = CreateButton(scene, size);
 	SetPosition(button, position);
@@ -70,102 +81,56 @@ static Button CreateMoveButton(
 	button.SetSound(config.click, ButtonState::Press);
 	button.SetSound(config.hover, ButtonState::Hover);
 
-	button.OnHoverStart([button, config]() {
-		using enum ButtonState;
-		TranslateTo(
-			std::vector<Text>{ *button.GetText(Idle), *button.GetText(Hover),
-							   *button.GetText(Press) },
-			config.move_offset, config.duration, config.ease
-		);
-	});
+	if (config.move.has_value()) {
+		button.OnHoverStart([button, config = *config.move]() {
+			using enum ButtonState;
+			TranslateTo(
+				std::vector<Text>{ *button.GetText(Idle), *button.GetText(Hover),
+								   *button.GetText(Press) },
+				config.offset, config.duration, config.ease
+			);
+		});
 
-	button.OnHoverStop([button, config]() {
-		using enum ButtonState;
-		TranslateTo(
-			std::vector<Text>{ *button.GetText(Idle), *button.GetText(Hover),
-							   *button.GetText(Press) },
-			V2_float{}, config.duration, config.ease
-		);
-	});
-	return button;
-}
-
-struct ScaleButtonConfig {
-	std::string_view content;
-	Color text_color{ color::Gray };
-	Color text_hover_color{ color::Gold };
-	Color text_click_color{ text_hover_color };
-
-	FontSize font_size;
-	FontOrKey font;
-
-	int text_outline_width{ 1 };
-	Color text_outline_color{ color::Black };
-
-	AudioOrKey click{};
-	AudioOrKey hover{};
-
-	float scale{ 1.25f };
-	milliseconds duration{ 100 };
-	Ease ease{ Ease::Linear };
-};
-
-static Button CreateScaleButton(
-	Scene& scene, V2_float position, V2_float size, const ScaleButtonConfig& config = {}
-) {
-	auto button = CreateButton(scene, size);
-	SetPosition(button, position);
-
-	TextProperties text_properties;
-	text_properties.outline.width = config.text_outline_width;
-	text_properties.outline.color = config.text_outline_color;
-
-	button.SetText(
-		config.content, config.text_color, config.font_size, config.font, text_properties,
-		ButtonState::Idle
-	);
-	button.SetText(
-		config.content, config.text_hover_color, config.font_size, config.font, text_properties,
-		ButtonState::Hover
-	);
-	button.SetText(
-		config.content, config.text_click_color, config.font_size, config.font, text_properties,
-		ButtonState::Press
-	);
-
-	button.SetSound(config.click, ButtonState::Press);
-	button.SetSound(config.hover, ButtonState::Hover);
-
-	struct ButtonTextFontSize {};
-
-	auto sf_idle  = button.GetFontSize(ButtonState::Idle);
-	auto sf_hover = button.GetFontSize(ButtonState::Hover);
-	auto sf_press = button.GetFontSize(ButtonState::Press);
-
-	PTGN_ASSERT(sf_idle.has_value());
-
-	const std::vector<float> start_fonts{ *sf_idle, sf_hover.value_or(*sf_idle),
-										  sf_press.value_or(*sf_idle) };
-
-	auto target_fonts{ start_fonts };
-
-	for (auto& target_font : target_fonts) {
-		target_font *= config.scale;
+		button.OnHoverStop([button, config = *config.move]() {
+			using enum ButtonState;
+			TranslateTo(
+				std::vector<Text>{ *button.GetText(Idle), *button.GetText(Hover),
+								   *button.GetText(Press) },
+				V2_float{}, config.duration, config.ease
+			);
+		});
 	}
 
-	const auto tween_fonts = [button, config](const auto& fonts) {
-		using enum ButtonState;
-		std::vector<Text> texts{
-			*button.GetText(Idle),
-			*button.GetText(Hover),
-			*button.GetText(Press),
+	if (config.scale.has_value()) {
+		auto sf_idle  = button.GetFontSize(ButtonState::Idle);
+		auto sf_hover = button.GetFontSize(ButtonState::Hover);
+		auto sf_press = button.GetFontSize(ButtonState::Press);
+
+		PTGN_ASSERT(sf_idle.has_value());
+
+		const std::vector<float> start_fonts{ *sf_idle, sf_hover.value_or(*sf_idle),
+											  sf_press.value_or(*sf_idle) };
+
+		auto target_fonts{ start_fonts };
+
+		for (auto& target_font : target_fonts) {
+			target_font *= config.scale->scale;
+		}
+
+		const auto tween_fonts = [button, config = *config.scale](const auto& fonts) {
+			using enum ButtonState;
+			std::vector<Text> texts{
+				*button.GetText(Idle),
+				*button.GetText(Hover),
+				*button.GetText(Press),
+			};
+
+			ScaleTextSize(texts, fonts, config.duration, config.ease);
 		};
 
-		ScaleTextSize(texts, fonts, config.duration, config.ease);
-	};
-
-	button.OnHoverStop([tween_fonts, start_fonts]() { tween_fonts(start_fonts); });
-	button.OnHoverStart([tween_fonts, target_fonts]() { tween_fonts(target_fonts); });
+		button.OnHoverStop([tween_fonts, start_fonts]() { tween_fonts(start_fonts); });
+		button.OnHoverStart([tween_fonts, target_fonts]() { tween_fonts(target_fonts); });
+	}
 
 	return button;
 }
@@ -178,21 +143,23 @@ public:
 		ctx().asset.LoadAudio("hover", "assets/hover.ogg");
 		ctx().asset.LoadAudio("click", "assets/click.ogg");
 
-		CreateMoveButton(
+		CreateTemplateButton(
 			*this, { 0, 300 }, { 150, 50 },
 			{ .content			= "Click Me!",
 			  .text_hover_color = color::Red,
 			  .click			= "click",
-			  .hover			= "hover" }
+			  .hover			= "hover",
+			  .move				= MoveButtonConfig{} }
 		);
 
-		CreateScaleButton(
+		CreateTemplateButton(
 			*this, { 0, 200 }, { 150, 50 },
 			{ .content			= "Click Me!",
+			  .text_color		= color::Gray,
 			  .text_hover_color = color::Gold,
 			  .click			= "click",
 			  .hover			= "hover",
-			  .scale			= 1.25f }
+			  .scale			= ScaleButtonConfig{} }
 		);
 
 		// ctx().asset.Load("idle", "assets/bell.png");
