de82a3e531f2b5408f879b493c08efb226e24497
Martin <emailregisterlogin@gmail.com>
2026-04-13 03:01:35 +0200

    Add editor demo

---
+++ examples/editor/editor.cpp
@@ -0,0 +1,30 @@
+
+#include "app/application.h"
+#include "protegon_editor/layer.h"
+#include "runtime/asset/asset_manager.h"
+#include "runtime/event/event_dispatcher.h"
+#include "runtime/graphics/sprite.h"
+#include "runtime/scene/scene.h"
+
+using namespace ptgn;
+
+class EditorScene : public Scene {
+public:
+	void OnEnter() override {
+		ctx().asset.Load("tree", "assets/jpg.jpg");
+
+		CreateSprite(*this, "tree", {});
+	}
+
+	void OnUpdate() override {}
+
+	void OnExit() override {}
+
+	void OnEvent(EventDispatcher d) override {}
+};
+
+int main(int, char**) {
+	Application app{ "EditorScene" };
+	app.PushLayer<editor::EditorLayer>();
+	app.StartWith<EditorScene>();
+}
