cf2cc5279af6b0d4fa8fa72dbbe16e387d21b9ad
Martin <emailregisterlogin@gmail.com>
2026-05-13 02:04:27 +0200

    Add effect registry

--- engine/src/runtime/graphics/fx/effect.cpp
+++ engine/src/runtime/graphics/fx/effect.cpp
@@ -1,3 +1,13 @@
 #include "runtime/graphics/fx/effect.h"
 
-namespace ptgn {} // namespace ptgn
+#include "renderer/pipeline/draw_context.h"
+#include "runtime/ecs/entity.h"
+#include "runtime/scene/scene_render_graph.h"
+
+namespace ptgn {
+
+void Effect::Draw(DrawContext& draw, Entity entity) {
+	draw.GetGraphBuilder().ApplyEffect(entity);
+}
+
+} // namespace ptgn
--- engine/src/runtime/graphics/fx/effect.h
+++ engine/src/runtime/graphics/fx/effect.h
@@ -1,3 +1,42 @@
 #pragma once
 
-namespace ptgn {} // namespace ptgn
+#include <string>
+#include <type_traits>
+
+#include "runtime/ecs/entity.h"
+#include "runtime/graphics/draw.h"
+#include "runtime/graphics/drawable.h"
+#include "runtime/graphics/fx/effect_registry.h"
+#include "runtime/graphics/visible.h"
+#include "runtime/scene/scene.h"
+
+namespace ptgn {
+
+class DrawContext;
+
+class Effect : public Entity {
+public:
+	Effect() = default;
+
+	explicit Effect(Entity entity) : Entity{ entity } {}
+
+	static void Draw(DrawContext& draw, Entity entity);
+};
+
+PTGN_REGISTER_DRAWABLE(Effect);
+
+template <typename TEffect, typename... TArgs>
+	requires std::is_constructible_v<TEffect, TArgs...>
+Effect CreateEffect(Scene& scene, TArgs&&... args) {
+	Effect effect{ scene.CreateEntity() };
+
+	effect.Add<TEffect>(std::forward<TArgs>(args)...);
+	effect.Add<impl::EffectName>(std::string{ impl::EffectNameOf<TEffect>::value });
+
+	SetDraw<Effect>(effect);
+	Show(effect, false);
+
+	return effect;
+}
+
+} // namespace ptgn
