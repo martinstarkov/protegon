e5fba422bcfac38888f7c8c7dc5fce282cfa8391
Martin <emailregisterlogin@gmail.com>
2026-05-24 17:17:01 +0200

    Add vertex file

---
+++ engine/src/renderer/pipeline/vertex.cpp
@@ -0,0 +1,148 @@
+#include "renderer/pipeline/vertex.h"
+
+#include <array>
+#include <utility>
+
+#include "core/assert.h"
+#include "core/graphics/flip.h"
+#include "core/log.h"
+#include "core/math/geometry/rect.h"
+#include "core/math/vector2.h"
+#include "core/math/vector4.h"
+
+namespace ptgn::impl {
+
+std::array<V2_float, 4> GetTextureCoordinates(
+	V2_float source_position, V2_float source_size, V2_float texture_size, bool flip_vertically,
+	bool offset_texels
+) {
+	PTGN_ASSERT(texture_size.x > 0.0f, "Texture must have width > 0");
+	PTGN_ASSERT(texture_size.y > 0.0f, "Texture must have height > 0");
+
+	PTGN_ASSERT(
+		source_position.x < texture_size.x, "Source position X must be within texture width"
+	);
+	PTGN_ASSERT(
+		source_position.y < texture_size.y, "Source position Y must be within texture height"
+	);
+
+	if (source_size.IsZero()) {
+		source_size = texture_size - source_position;
+	}
+
+	V2_float texel = offset_texels ? (0.5f / texture_size) : V2_float{ 0, 0 };
+
+	V2_float min = (source_position - texel) / texture_size;
+	V2_float max = (source_position + source_size - texel) / texture_size;
+
+	if (max.x > 1.0f || max.y > 1.0f) {
+		PTGN_WARN("Drawing source size from outside of texture size");
+	}
+
+	std::array<V2_float, 4> uv{ min, { max.x, min.y }, max, { min.x, max.y } };
+
+	if (flip_vertically) {
+		FlipTextureCoordinates(uv, Flip::Vertical);
+	}
+
+	return uv;
+}
+
+void FlipTextureCoordinates(std::array<V2_float, 4>& tex_coords, V2_float scale) {
+	bool flip_x{ scale.x < 0.0f };
+	bool flip_y{ scale.y < 0.0f };
+
+	using enum Flip;
+
+	if (flip_x && flip_y) {
+		impl::FlipTextureCoordinates(tex_coords, Both);
+	} else if (flip_x) {
+		impl::FlipTextureCoordinates(tex_coords, Horizontal);
+	} else if (flip_y) {
+		impl::FlipTextureCoordinates(tex_coords, Vertical);
+	}
+}
+
+void FlipTextureCoordinates(std::array<V2_float, 4>& tex_coords, Flip flip) {
+	const auto flip_x = [&]() {
+		std::swap(tex_coords[0].x, tex_coords[1].x);
+		std::swap(tex_coords[2].x, tex_coords[3].x);
+	};
+	const auto flip_y = [&]() {
+		std::swap(tex_coords[0].y, tex_coords[3].y);
+		std::swap(tex_coords[1].y, tex_coords[2].y);
+	};
+	switch (flip) {
+		using enum Flip;
+		case None:		 break;
+		case Vertical:	 flip_y(); break;
+		case Horizontal: flip_x(); break;
+		case Both:
+			flip_x();
+			flip_y();
+			break;
+		default: PTGN_ERROR("Unrecognized flip state");
+	}
+}
+
+std::array<V2_float, 4> GetCenteredQuadPoints(V2_float size) {
+	V2_float half{ size / 2.0f };
+	return { -half, V2_float{ half.x, -half.y }, half, V2_float{ -half.x, half.y } };
+}
+
+ColorVertex::ColorVertex(V2_float position, float depth, V4_float color, int entity_id) :
+	position{ position.x, position.y, depth },
+	color{ color[0], color[1], color[2], color[3] },
+	entity_id{ entity_id } {}
+
+ShapeVertex::ShapeVertex(
+	V2_float position, float depth, V4_float color, V2_float local_coord,
+	const std::array<float, 4>& shape_data, int entity_id
+) :
+	position{ position.x, position.y, depth },
+	color{ color[0], color[1], color[2], color[3] },
+	local_coord{ local_coord.x, local_coord.y },
+	shape_data{ shape_data },
+	entity_id{ entity_id } {}
+
+TextureVertex::TextureVertex(
+	V2_float position, float depth, V4_float color, V2_float tex_coord, float tex_index,
+	int entity_id
+) :
+	position{ position.x, position.y, depth },
+	color{ color[0], color[1], color[2], color[3] },
+	tex_coord{ tex_coord.x, tex_coord.y },
+	tex_index{ tex_index },
+	entity_id{ entity_id } {}
+
+RenderQuad<TextureVertex> CreateRenderQuad(
+	const std::array<V2_float, 4>& positions, float depth, V4_float color_n,
+	const std::array<V2_float, 4>& tex_coords, int entity_id
+) {
+	constexpr auto tex_index{ 0.0f };
+
+	return {
+		TextureVertex{ positions[0], depth, color_n, tex_coords[0], tex_index, entity_id },
+		TextureVertex{ positions[1], depth, color_n, tex_coords[1], tex_index, entity_id },
+		TextureVertex{ positions[2], depth, color_n, tex_coords[2], tex_index, entity_id },
+		TextureVertex{ positions[3], depth, color_n, tex_coords[3], tex_index, entity_id },
+	};
+}
+
+RenderQuad<TextureVertex> CreateLocalRenderQuad(
+	const Rect& rect, float depth, V4_float color_n, const std::array<V2_float, 4>& tex_coords,
+	int entity_id
+) {
+	constexpr auto tex_index{ 0.0f };
+
+	auto positions{ rect.GetLocalVertices() };
+
+	return {
+		TextureVertex{ positions[0], depth, color_n, tex_coords[0], tex_index, entity_id },
+		TextureVertex{ positions[1], depth, color_n, tex_coords[1], tex_index, entity_id },
+		TextureVertex{ positions[2], depth, color_n, tex_coords[2], tex_index, entity_id },
+		TextureVertex{ positions[3], depth, color_n, tex_coords[3], tex_index, entity_id },
+	};
+}
+
+} // namespace ptgn::impl
---
+++ engine/src/renderer/pipeline/vertex.h
@@ -0,0 +1,123 @@
+#pragma once
+
+#include <array>
+
+#include "core/graphics/flip.h"
+#include "core/math/vector2.h"
+#include "core/math/vector4.h"
+#include "renderer/pipeline/buffer_layout.h"
+#include "renderer/pipeline/glsl_types.h"
+
+namespace ptgn {
+
+class Rect;
+
+namespace impl {
+
+template <VertexType TVertex>
+using RenderQuad = std::array<TVertex, 4>;
+
+template <VertexType TVertex>
+using RenderTriangle = std::array<TVertex, 3>;
+
+template <bool kFlipY>
+constexpr std::array<V2_float, 4> GetDefaultTextureCoordinates() {
+	if constexpr (kFlipY) {
+		return { V2_float{ 0.0f, 1.0f }, V2_float{ 1.0f, 1.0f }, V2_float{ 1.0f, 0.0f },
+				 V2_float{ 0.0f, 0.0f } };
+
+	} else {
+		return {
+			V2_float{ 0.0f, 0.0f },
+			V2_float{ 1.0f, 0.0f },
+			V2_float{ 1.0f, 1.0f },
+			V2_float{ 0.0f, 1.0f },
+		};
+	}
+}
+
+/// @brief Values from [-1, 1]
+constexpr std::array<V2_float, 4> GetNDCTextureCoordinates() {
+	return { V2_float{ -1.0f, 1.0f }, V2_float{ 1.0f, 1.0f }, V2_float{ 1.0f, -1.0f },
+			 V2_float{ -1.0f, -1.0f } };
+}
+
+constexpr std::array<V2_float, 4> GetDefaultTextureCoordinates(bool flip_y) {
+	if (flip_y) {
+		return GetDefaultTextureCoordinates<true>();
+	} else {
+		return GetDefaultTextureCoordinates<false>();
+	}
+}
+
+std::array<V2_float, 4> GetTextureCoordinates(
+	V2_float source_position, V2_float source_size, V2_float texture_size, bool flip_vertically,
+	bool offset_texels
+);
+
+void FlipTextureCoordinates(std::array<V2_float, 4>& tex_coords, V2_float scale);
+void FlipTextureCoordinates(std::array<V2_float, 4>& tex_coords, Flip flip);
+
+struct ColorVertex : public VertexLayout<ColorVertex, glsl::vec3, glsl::vec4, glsl::int_> {
+	ColorVertex() = default;
+
+	ColorVertex(V2_float position, float depth, V4_float color, int entity_id);
+
+	glsl::vec3 position{};
+	glsl::vec4 color{};
+	glsl::int_ entity_id{ -1 };
+};
+
+struct ShapeVertex :
+	public VertexLayout<ShapeVertex, glsl::vec3, glsl::vec4, glsl::vec2, glsl::vec4, glsl::int_> {
+	ShapeVertex() = default;
+
+	ShapeVertex(
+		V2_float position, float depth, V4_float color, V2_float local_coord,
+		const std::array<float, 4>& shape_data, int entity_id
+	);
+
+	glsl::vec3 position{};
+	glsl::vec4 color{};
+	glsl::vec2 local_coord{};
+	/// @brief Shape-specific data
+	/// For circle: x = thickness, y = fade
+	/// For ellipse: x = thickness, y = fade
+	/// For capsule: x = thickness, y = fade, z = normalized_radius
+	/// For rounded rect: x = thickness, y = fade, z = normalized_radius, w = aspect_ratio
+	/// For arc: x = thickness, y = fade, z = aperture, w = direction (positive = CW, negative =
+	/// CCW)
+	glsl::vec4 shape_data{};
+	glsl::int_ entity_id{ -1 };
+};
+
+struct TextureVertex :
+	public VertexLayout<
+		TextureVertex, glsl::vec3, glsl::vec4, glsl::vec2, glsl::float_, glsl::int_> {
+	TextureVertex() = default;
+
+	TextureVertex(
+		V2_float position, float depth, V4_float color, V2_float tex_coord, float tex_index,
+		int entity_id
+	);
+
+	glsl::vec3 position{};
+	glsl::vec4 color{};
+	glsl::vec2 tex_coord{};
+	glsl::float_ tex_index{};
+	glsl::int_ entity_id{ -1 };
+};
+
+RenderQuad<TextureVertex> CreateRenderQuad(
+	const std::array<V2_float, 4>& positions, float depth, V4_float color_n,
+	const std::array<V2_float, 4>& tex_coords, int entity_id
+);
+
+RenderQuad<TextureVertex> CreateLocalRenderQuad(
+	const Rect& rect, float depth, V4_float color_n, const std::array<V2_float, 4>& tex_coords,
+	int entity_id
+);
+
+} // namespace impl
+
+} // namespace ptgn
