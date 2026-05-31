d16adf178a5af60226f7c5c19e1b5d71afa20d73
Martin <emailregisterlogin@gmail.com>
2026-05-31 13:48:17 +0200

    Add position and entity id accessor

--- engine/src/renderer/pipeline/vertex.h
+++ engine/src/renderer/pipeline/vertex.h
@@ -73,6 +73,81 @@ struct TextureVertex :
 	glsl::int_ entity_id{ -1 };
 };
 
+struct PositionTag {};
+
+template <typename T>
+concept GlslVec3Ref = requires(T& value) {
+	{ value[0] } -> std::same_as<float&>;
+	{ value[1] } -> std::same_as<float&>;
+	{ value[2] } -> std::same_as<float&>;
+};
+
+template <typename T>
+concept ConstGlslVec3Ref = requires(const T& value) {
+	{ value[0] } -> std::same_as<const float&>;
+	{ value[1] } -> std::same_as<const float&>;
+	{ value[2] } -> std::same_as<const float&>;
+};
+
+template <GlslVec3Ref T>
+constexpr T& PositionVec3Ref(T& value) noexcept {
+	return value;
+}
+
+template <ConstGlslVec3Ref T>
+constexpr const T& PositionVec3Ref(const T& value) noexcept {
+	return value;
+}
+
+template <typename TVertex>
+concept HasRegisteredPosition = requires(TVertex& vertex) {
+	{ Position(PositionTag{}, vertex) } -> GlslVec3Ref;
+};
+
+template <typename TVertex>
+concept HasRegisteredConstPosition = requires(const TVertex& vertex) {
+	{ Position(PositionTag{}, vertex) } -> ConstGlslVec3Ref;
+};
+
+template <typename TVertex>
+concept HasConventionalPosition = requires(TVertex& vertex) {
+	{ PositionVec3Ref(vertex.position) } -> GlslVec3Ref;
+};
+
+template <typename TVertex>
+concept HasConventionalConstPosition = requires(const TVertex& vertex) {
+	{ PositionVec3Ref(vertex.position) } -> ConstGlslVec3Ref;
+};
+
+template <typename TVertex>
+struct PositionAccessor {
+	static constexpr bool has_position{ HasRegisteredPosition<TVertex> ||
+										HasConventionalPosition<TVertex> };
+
+	static constexpr bool has_const_position{ HasRegisteredConstPosition<TVertex> ||
+											  HasConventionalConstPosition<TVertex> };
+
+	static constexpr auto& Get(TVertex& vertex) noexcept
+		requires has_position
+	{
+		if constexpr (HasRegisteredPosition<TVertex>) {
+			return Position(PositionTag{}, vertex);
+		} else {
+			return PositionVec3Ref(vertex.position);
+		}
+	}
+
+	static constexpr const auto& Get(const TVertex& vertex) noexcept
+		requires has_const_position
+	{
+		if constexpr (HasRegisteredConstPosition<TVertex>) {
+			return Position(PositionTag{}, vertex);
+		} else {
+			return PositionVec3Ref(vertex.position);
+		}
+	}
+};
+
 struct TextureIndexTag {};
 
 template <typename T>
@@ -80,8 +155,18 @@ concept GlslFloatRef = requires(T& value) {
 	{ value[0] } -> std::same_as<float&>;
 };
 
+template <typename T>
+concept ConstGlslFloatRef = requires(const T& value) {
+	{ value[0] } -> std::same_as<const float&>;
+};
+
 template <GlslFloatRef T>
 constexpr float& TextureIndexFloatRef(T& value) noexcept {
+	return value[0];
+}
+
+template <ConstGlslFloatRef T>
+constexpr const float& TextureIndexFloatRef(const T& value) noexcept {
 	return value[0];
 }
 
@@ -91,14 +176,27 @@ concept HasRegisteredTextureIndex = requires(TVertex& vertex) {
 };
 
 template <typename TVertex>
+concept HasRegisteredConstTextureIndex = requires(const TVertex& vertex) {
+	{ TextureIndex(TextureIndexTag{}, vertex) } -> std::same_as<const float&>;
+};
+
+template <typename TVertex>
 concept HasConventionalTextureIndex = requires(TVertex& vertex) {
 	{ TextureIndexFloatRef(vertex.tex_index) } -> std::same_as<float&>;
+};
+
+template <typename TVertex>
+concept HasConventionalConstTextureIndex = requires(const TVertex& vertex) {
+	{ TextureIndexFloatRef(vertex.tex_index) } -> std::same_as<const float&>;
 };
 
 template <typename TVertex>
 struct TextureIndexAccessor {
 	static constexpr bool has_texture_index{ HasRegisteredTextureIndex<TVertex> ||
 											 HasConventionalTextureIndex<TVertex> };
+
+	static constexpr bool has_const_texture_index{ HasRegisteredConstTextureIndex<TVertex> ||
+												   HasConventionalConstTextureIndex<TVertex> };
 
 	static constexpr float& Get(TVertex& vertex) noexcept
 		requires has_texture_index
@@ -109,13 +207,119 @@ struct TextureIndexAccessor {
 			return TextureIndexFloatRef(vertex.tex_index);
 		}
 	}
+
+	static constexpr const float& Get(const TVertex& vertex) noexcept
+		requires has_const_texture_index
+	{
+		if constexpr (HasRegisteredConstTextureIndex<TVertex>) {
+			return TextureIndex(TextureIndexTag{}, vertex);
+		} else {
+			return TextureIndexFloatRef(vertex.tex_index);
+		}
+	}
+};
+
+struct EntityIdTag {};
+
+template <typename T>
+concept GlslIntRef = requires(T& value) {
+	{ value[0] } -> std::same_as<int&>;
+};
+
+template <typename T>
+concept ConstGlslIntRef = requires(const T& value) {
+	{ value[0] } -> std::same_as<const int&>;
+};
+
+template <GlslIntRef T>
+constexpr int& EntityIdIntRef(T& value) noexcept {
+	return value[0];
+}
+
+template <ConstGlslIntRef T>
+constexpr const int& EntityIdIntRef(const T& value) noexcept {
+	return value[0];
+}
+
+template <typename TVertex>
+concept HasRegisteredEntityId = requires(TVertex& vertex) {
+	{ EntityId(EntityIdTag{}, vertex) } -> std::same_as<int&>;
+};
+
+template <typename TVertex>
+concept HasRegisteredConstEntityId = requires(const TVertex& vertex) {
+	{ EntityId(EntityIdTag{}, vertex) } -> std::same_as<const int&>;
+};
+
+template <typename TVertex>
+concept HasConventionalEntityId = requires(TVertex& vertex) {
+	{ EntityIdIntRef(vertex.entity_id) } -> std::same_as<int&>;
+};
+
+template <typename TVertex>
+concept HasConventionalConstEntityId = requires(const TVertex& vertex) {
+	{ EntityIdIntRef(vertex.entity_id) } -> std::same_as<const int&>;
+};
+
+template <typename TVertex>
+struct EntityIdAccessor {
+	static constexpr bool has_entity_id{ HasRegisteredEntityId<TVertex> ||
+										 HasConventionalEntityId<TVertex> };
+
+	static constexpr bool has_const_entity_id{ HasRegisteredConstEntityId<TVertex> ||
+											   HasConventionalConstEntityId<TVertex> };
+
+	static constexpr int& Get(TVertex& vertex) noexcept
+		requires has_entity_id
+	{
+		if constexpr (HasRegisteredEntityId<TVertex>) {
+			return EntityId(EntityIdTag{}, vertex);
+		} else {
+			return EntityIdIntRef(vertex.entity_id);
+		}
+	}
+
+	static constexpr const int& Get(const TVertex& vertex) noexcept
+		requires has_const_entity_id
+	{
+		if constexpr (HasRegisteredConstEntityId<TVertex>) {
+			return EntityId(EntityIdTag{}, vertex);
+		} else {
+			return EntityIdIntRef(vertex.entity_id);
+		}
+	}
 };
 
 } // namespace ptgn::impl
 
-#define PTGN_TEXTURE_INDEX_MEMBER(VertexType, Member)             \
+#define PTGN_GLSL_TEXTURE_INDEX_MEMBER(VertexType, Member)        \
 	friend constexpr float& TextureIndex(                         \
 		::ptgn::impl::TextureIndexTag, VertexType& vertex         \
 	) noexcept {                                                  \
 		return ::ptgn::impl::TextureIndexFloatRef(vertex.Member); \
+	}                                                             \
+	friend constexpr const float& TextureIndex(                   \
+		::ptgn::impl::TextureIndexTag, const VertexType& vertex   \
+	) noexcept {                                                  \
+		return ::ptgn::impl::TextureIndexFloatRef(vertex.Member); \
+	}
+
+#define PTGN_GLSL_POSITION_MEMBER(VertexType, Member)                                         \
+	friend constexpr auto& Position(::ptgn::impl::PositionTag, VertexType& vertex) noexcept { \
+		return ::ptgn::impl::PositionVec3Ref(vertex.Member);                                  \
+	}                                                                                         \
+	friend constexpr const auto& Position(                                                    \
+		::ptgn::impl::PositionTag, const VertexType& vertex                                   \
+	) noexcept {                                                                              \
+		return ::ptgn::impl::PositionVec3Ref(vertex.Member);                                  \
+	}
+
+#define PTGN_GLSL_ENTITY_ID_MEMBER(VertexType, Member)                                       \
+	friend constexpr int& EntityId(::ptgn::impl::EntityIdTag, VertexType& vertex) noexcept { \
+		return ::ptgn::impl::EntityIdIntRef(vertex.Member);                                  \
+	}                                                                                        \
+	friend constexpr const int& EntityId(                                                    \
+		::ptgn::impl::EntityIdTag, const VertexType& vertex                                  \
+	) noexcept {                                                                             \
+		return ::ptgn::impl::EntityIdIntRef(vertex.Member);                                  \
 	}
