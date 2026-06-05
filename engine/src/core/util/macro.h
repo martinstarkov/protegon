43e3424701bebea65fbf91e3e033ce0e40f7ce29
Martin <emailregisterlogin@gmail.com>
2026-06-05 22:08:25 +0200

    Add PTGN_IMPL_FIRST_OR_DEFAULT_IMPL

--- engine/src/core/util/macro.h
+++ engine/src/core/util/macro.h
@@ -3,3 +3,8 @@
 #define PTGN_EXPAND(x)	  x
 #define PTGN_STRINGIFY(x) #x
 #define PTGN_UNPAREN(...) __VA_ARGS__
+
+#define PTGN_IMPL_FIRST_OR_DEFAULT_IMPL(value, ...) value
+
+#define PTGN_IMPL_FIRST_OR_DEFAULT(default_value, ...) \
+	PTGN_IMPL_FIRST_OR_DEFAULT_IMPL(__VA_ARGS__ __VA_OPT__(, ) default_value)
