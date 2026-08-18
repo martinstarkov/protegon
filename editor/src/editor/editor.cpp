03db1c81ae1c431abff384f857c4476fc7ab8e7f
Nicole Burg <emailregisterlogin@gmail.com>
2026-08-18 04:21:47 -0400

    Comment out test panel

--- editor/src/editor/editor.cpp
+++ editor/src/editor/editor.cpp
@@ -3556,7 +3556,7 @@ void Editor::DrawPanels() {
 	scene_list_panel_.OnRender(*context_);
 	screen_effects_panel_.OnRender(*context_);
 	inspector_panel_.OnRender(*context_);
-	entity_target_demo::DrawScriptTargetingDemo(*context_);
+	//entity_target_demo::DrawScriptTargetingDemo(*context_);
 	if (ConsumeAcceptedAssetKeyDrop()) {
 		scene_asset_dependencies_dirty_ = true;
 	}
