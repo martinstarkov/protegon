c6fb13f5ec4b1be935d7ae1357492728aa79076a
Martin Starkov <emailregisterlogin@gmail.com>
2026-08-23 07:36:13 +0200

    Switch to std::string for effect registration name

--- engine/src/runtime/graphics/fx/effect_registration.h
+++ engine/src/runtime/graphics/fx/effect_registration.h
@@ -9,20 +9,20 @@
 namespace ptgn::impl {
 
 struct EffectRegistrationOptions {
-	std::optional<std::string_view> name;
+	std::optional<std::string> name{};
 	bool hdr{ false };
 };
 
 struct EffectRegistrationData {
-	std::string_view type_name;
-	EffectRegistrationOptions options;
+	std::string type_name{};
+	EffectRegistrationOptions options{};
 };
 
 constexpr EffectRegistrationData MakeEffectRegistration(
 	std::string_view type_name, EffectRegistrationOptions options = {}
 ) {
 	return {
-		.type_name = type_name,
+		.type_name = std::string{ type_name },
 		.options   = options,
 	};
 }
@@ -41,19 +41,19 @@ struct EffectRegistration {
 
 #define PTGN_REGISTER_EFFECT(Type, ...)                                                   \
 	template <>                                                                           \
-	struct ::ptgn::impl::EffectRegistration<Type> {                                       \
-		static constexpr ::ptgn::impl::EffectRegistrationData Get() {                     \
-			return ::ptgn::impl::MakeEffectRegistration(                                  \
-				#Type __VA_OPT__(, ::ptgn::impl::EffectRegistrationOptions __VA_ARGS__)   \
+	struct ptgn::impl::EffectRegistration<Type> {                                       \
+		static constexpr ptgn::impl::EffectRegistrationData Get() {                     \
+			return ptgn::impl::MakeEffectRegistration(                                  \
+				#Type __VA_OPT__(, ptgn::impl::EffectRegistrationOptions __VA_ARGS__)   \
 			);                                                                            \
 		}                                                                                 \
 	};                                                                                    \
 	template <>                                                                           \
-	struct ::ptgn::impl::DrawableRegistration<Type> {                                     \
-		static constexpr ::ptgn::impl::DrawableRegistrationData Get() {                   \
-			constexpr auto registration{ ::ptgn::impl::EffectRegistration<Type>::Get() }; \
+	struct ptgn::impl::DrawableRegistration<Type> {                                     \
+		static constexpr ptgn::impl::DrawableRegistrationData Get() {                   \
+			auto registration{ ptgn::impl::EffectRegistration<Type>::Get() }; \
                                                                                           \
-			return ::ptgn::impl::MakeDrawableRegistration(                                \
+			return ptgn::impl::MakeDrawableRegistration(                                \
 				registration.type_name,                                                   \
 				{                                                                         \
 					.name  = registration.options.name,                                   \
@@ -62,12 +62,12 @@ struct EffectRegistration {
 			);                                                                            \
 		}                                                                                 \
 	};                                                                                    \
-	template class ::ptgn::impl::DrawableRegistrar<Type>;                                 \
+	template class ptgn::impl::DrawableRegistrar<Type>;                                 \
 	namespace {                                                                           \
 	[[maybe_unused]] const bool PTGN_DETAIL_EFFECT_CONCAT(                                 \
 		ptgn_registered_effect_, __COUNTER__                                               \
-	){ ::ptgn::impl::EffectRegistry::Register<                                             \
+	){ ptgn::impl::EffectRegistry::Register<                                             \
 		Type,                                                                               \
-		::ptgn::impl::EffectRegistration<Type>::Get().options.hdr                          \
-	>(::ptgn::impl::EffectRegistration<Type>::Get()) };                                   \
+		ptgn::impl::EffectRegistration<Type>::Get().options.hdr                          \
+	>(ptgn::impl::EffectRegistration<Type>::Get()) };                                   \
 	}
