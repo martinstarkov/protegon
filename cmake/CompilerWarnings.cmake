3487ea96dab51748acaefa6a81f87406770a9c69
Martin Starkov <emailregisterlogin@gmail.com>
2026-08-23 07:37:10 +0200

    Remove double promotion warnings

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -52,7 +52,6 @@ function(set_project_warnings project_name warnings_as_errors)
       -Wconversion # warn on type conversions that may lose data
       -Wsign-conversion  # warn on sign conversions
       -Wnull-dereference # warn if a null dereference is detected
-      -Wdouble-promotion # warn if float is implicit promoted to double
       -Wformat=2 # warn on security issues around functions that format output
                  # (ie printf)
       -Wimplicit-fallthrough # warn on implicit fallthrough in switch statements
